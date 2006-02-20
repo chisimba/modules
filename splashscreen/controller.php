332631ba9420f288cd8a6c86e3436ce2d004fabb
paulscott <unknown@unknown.com>
2006-02-20 23:29:31 +0000

    fixed up contextCode indices for PHP5

--- splashscreen/controller.php
+++ splashscreen/controller.php
@@ -13,10 +13,10 @@ if (!$GLOBALS['kewl_entry_point_run'])
 class splashscreen extends controller
 {
     /**
-    * 
+    *
     */
     var $objSkin;
-    
+
     /**
     * Constructor for the class
     */
@@ -26,14 +26,14 @@ class splashscreen extends controller
     }
 
     /**
-    * Method to set login requirement to False 
+    * Method to set login requirement to False
     * Required to be false. prelogin screen
     */
     function requiresLogin()
     {
         return FALSE;
     }
-    
+
     /**
 	* Method to process actions to be taken
     */
@@ -43,37 +43,37 @@ class splashscreen extends controller
         return $this->showSplashScreen();
     }
 
-    
+
     /**
 	* Method to process the splashscreen and display it
 	*/
     function showSplashScreen()
     {
         $skinDetailsFile = $this->objSkin->getSkinUrl().'skinsplashscreeninfo.php';
-        
+
         // Load JavaScript to validate login and place in header
         $js='lib/javascript/find_validate.js';
         $this->setVar('jsLoad', array($js));
-        
+
         // Suppress Toolbar - user isn't logged in yet
         $this->setVar('pageSuppressToolbar', TRUE);
-        
+
         //Suppress IM
         $this->setVar('pageSuppressToolbar', TRUE);
-        
+
         // Set Layout Template To Null
         $this->setLayoutTemplate(NULL);
-        
+
         // Check if splash screen information file exists
         if (file_exists($skinDetailsFile)) {
             @include($skinDetailsFile); // @ is unnecessary - though all it does is suppress errors
         }
-        
+
         return 'login_tpl.php';
     }
 
 
 
 }
-        
+
 ?>
