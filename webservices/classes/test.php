9b699692ce0998d5c6dfbf06a19cbd33c5e2f85c
paulscott <unknown@unknown.com>
2006-03-06 01:52:32 +0000

    committed wsdl parser class and test script - Please test on Windows...

--- webservices/classes/test.php
+++ webservices/classes/test.php
@@ -2,5 +2,13 @@
 
 require_once('wsdlparser_class_inc.php');
 
-$wp = new wsdlparser('http://kngforge.uwc.ac.za/kng_unstable/modules/webservices/server.php?wsdl');
-print_r($wp->getDocs());
+$wp = new wsdlparser("http://kngforge.uwc.ac.za/kng_unstable/modules/mirrordb/mirrordb.php?wsdl");
+$docs = $wp->getDocs();
+//var_dump($docs);
+$ns = $wp->getTargetNameSpace();
+//var_dump($ns);
+$service = $wp->declareService();
+//var_dump($service);
+$wp->getOperations();
+//var_dump($wp->service);
+echo $wp->writeCode();
