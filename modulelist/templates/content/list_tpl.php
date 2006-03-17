d9bd9bd272049bca6c6d512cc110fc3c9be6eca5
paulscott <unknown@unknown.com>
2006-03-17 06:44:43 +0000

    cleaned up code

--- modulelist/templates/content/list_tpl.php
+++ modulelist/templates/content/list_tpl.php
@@ -1,14 +1,4 @@
 <?php 
-/* modules/modulelist/templates/content/list_tpl.php
-   A PHP template for the list modules output.  
-   
-   NOTES: Derek, I removed the stripslashes calls as I couldn't really see the necessity for it.
-          Also module_path is now not really a path, maybe should be renamed 
-		  DEREK SAYS:  What if the title or description has a \' in it?
-   
-   NOTES: Derek says - corrected line 24 to read $moduleRow['description']. 
-   *  Agree module_path should be renamed. Shouldn't it be module code?
-   */
 
     $objButtons=&$this->getObject('navbuttons','navigation');
     if ($moduleList)
