eaffcd7f9ba1c9954fc5793cfa587dae84cdca14
wnitsckie <unknown@unknown.com>
2006-07-06 10:32:53 +0000

    fixed my package tags

--- mediamanager/classes/mmutils_class_inc.php
+++ mediamanager/classes/mmutils_class_inc.php
@@ -8,8 +8,8 @@ if (!$GLOBALS['kewl_entry_point_run']) {
 // end security check
 /**
 * This object manages the media folders and files
-* @package mmutils
-* @category mmutils
+* @package media manager
+* @category document manager
 * @copyright 2004, University of the Western Cape & AVOIR Project
 * @license GNU GPL
 * @version
