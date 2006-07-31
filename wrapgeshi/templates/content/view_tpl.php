db9f6e628d36f650f5be33b4bc1849e8000e2082
tohir <unknown@unknown.com>
2006-07-31 11:04:20 +0000

    added wrapgeshi module

---
+++ wrapgeshi/templates/content/view_tpl.php
@@ -0,0 +1,17 @@
+<?php
+
+$objGeshi = & $this->getObject('geshiwrapper');
+//Language and source must be specified before startGeshi
+$objGeshi->language = $this->getParam('language', 'php');
+$objGeshi->source = $this->getParam('code', NULL);
+//Start the geshi object
+$objGeshi->startGeshi();
+//Specific geshi settings must come after startGeshi
+$objGeshi->enableLineNumbers();
+$line=$this->getParam('line', 1);
+$objGeshi->startLineNumbersAt($line);
+//Render the output
+echo $objGeshi->show();
+
+
+?>
