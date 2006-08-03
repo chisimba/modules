8fcfc6cee20bb1a1b5cdb151684f4f46d281473d
paulscott <unknown@unknown.com>
2006-08-03 07:17:12 +0000

    pdf module

---
+++ pdf/controller.php
@@ -0,0 +1,63 @@
+<?php
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run'])
+{
+    die("You cannot view this page directly");
+}
+// end security check
+
+/**
+* http client Controller
+*
+* @author Paul Scott
+* @copyright (c) 2004 University of the Western Cape
+* @package httpclient
+* @version 1
+*/
+class pdf extends controller
+{
+
+    public $objPdf;
+
+	/**
+	* Constructor method to instantiate objects and get variables
+	*/
+    public function init()
+    {
+        try {
+        	$this->objPdf = $this->getObject('zpdf');
+        	//Get the activity logger class
+        	$this->objLog=$this->newObject('logactivity', 'logger');
+        	//Log this module call
+        	$this->objLog->log();
+        }
+        catch (customException $e)
+        {
+        	echo customException::cleanUp();
+        	die();
+        }
+    }
+
+    /**
+	* Method to process actions to be taken
+    *
+    * @param string $action String indicating action to be taken
+	*/
+    public function dispatch($action=Null)
+    {
+        switch ($action)
+        {
+            default:
+            case 'genpdf':
+            	try {
+            		$this->objPdf->dotest();
+
+				} catch (customException $e) {
+        			echo $e->getMessage();
+    			}
+                break;
+
+    	}
+    }
+}
+?>
