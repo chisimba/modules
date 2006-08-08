1188612f831f7843968ad46c4da620e24c290056
paulscott <unknown@unknown.com>
2006-08-08 13:12:34 +0000

    took out the sidemenu show() method as it exists not

--- calendar/templates/layout/calendar_layout_tpl.php
+++ calendar/templates/layout/calendar_layout_tpl.php
@@ -1,14 +1,18 @@
 <?php
-
+try {
 // Create an Instance of the CSS Layout
 $cssLayout =& $this->newObject('csslayout', 'htmlelements');
 
 // Create an Instance of the User Side Menu
 $sideMenuBar=& $this->getObject('sidemenu','toolbar');
-
+}
+catch (customException $e)
+{
+	customException::cleanUp();
+}
 
 // Set the Content of left side column
-    $cssLayout->setLeftColumnContent($sideMenuBar->show('user'));
+//    $cssLayout->setLeftColumnContent($sideMenuBar->show('user'));
 
 // Set the Content of middle column
 $cssLayout->setMiddleColumnContent($this->getContent());
