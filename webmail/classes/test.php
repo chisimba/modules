5deba358f044f6e5dfadb25897b17049b652527f
paulscott <unknown@unknown.com>
2006-08-16 13:14:42 +0000

    ""

--- webmail/classes/test.php
+++ webmail/classes/test.php
@@ -2,15 +2,39 @@
 
 require_once("imap_class_inc.php");
 try {
+	//$dsn = 'imap://kcyster:81141@itsnw.uwc.ac.za:143/INBOX';
+	//$dsn = 'imap://pmbekwa:prince@itsnw.uwc.ac.za:143/INBOX';
+	//$dsn = 'imap://fsiu:fsiu@itsnw.uwc.ac.za:143/INBOX';
+	$dsn = 'imap://pscott:scott@itsnw.uwc.ac.za:143/INBOX';
+
+	//$dsn = 'pop://fsiu:fsiu@itsnw.uwc.ac.za:110/INBOX';
+	//$dsn = array(
+	//	'imapserver' => 'itsnw.uwc.ac.za',
+	//	'imapuser' => 'pmbekwa',
+	//	'imappass' => 'prince',
+	//	'imapport' => 143,
+	//	'imapprotocol' => 'imap',
+	//	'imapmailbox' => 'INBOX',
+	//	);
 	$m = new imap;
-	$m->setconn("itsnw.uwc.ac.za","fsiu","fsiu");
-	$heads = $m->getHeaders();
-	$nummails = $m->numMails();
-	$thebox = $m->checkMbox();
+	//$m->factory($dsn);
+	echo $m->setAddress('pscott', 'uwc.ac.za', 'Paul Scott');
+	//$m->pingServer();
+	//$m->listMailBoxes();
+	//$acl = $m->getACL();
+	//print_r($acl);
+
+	//$stats = $m->checkMailboxStatus();
+	//print_r($stats);
+	//$m->getQuotas();
+	//$m->setconn("itsnw.uwc.ac.za","fsiu","fsiu");
+	//$heads = $m->getHeaders();
+	//$nummails = $m->numMails();
+	//$thebox = $m->checkMbox();
 	//var_dump($thebox);
-	$theheads = $m->getHeaderInfo(21);
-	var_dump($theheads);
-	$themess = $m->getMessage(21);
+	//$theheads = $m->getHeaderInfo(21);
+	//var_dump($theheads);
+	//$themess = $m->getMessage(21);
 	//header('Content-type: image/jpg');
 	//echo base64_decode($themess[1][0]['filedata']);
 	//echo "<h1>$nummails</h1>";
