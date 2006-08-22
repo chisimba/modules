b62f1d2870af94651e680a2c17ab482c04bbbd92
paulscott <unknown@unknown.com>
2006-08-22 12:42:29 +0000

    added dropdown

--- beautifier/templates/content/success_tpl.php
+++ beautifier/templates/content/success_tpl.php
@@ -11,8 +11,11 @@ echo $header->show();
 $themod = $this->getParam('mod');
 
 $intro = $this->objLanguage->languageText('mod_beautifier_beautifying', 'beautifier');
+$patience = $this->objLanguage->languageText('mod_beautifier_patience', 'beautifier');
 $tail = $this->objLanguage->languageText('mod_beautifier_finished', 'beautifier');
 $complete = $this->objLanguage->languageText('mod_beautifier_completemess', 'beautifier');
 
-echo $intro . "  " . $themod . "  " . $complete;
+echo $intro . "  " . $themod . " " . $patience . "  ... " . $tail;
+echo "<br />";
+echo $complete;
 ?>
