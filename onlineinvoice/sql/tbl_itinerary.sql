9b2bd3e81ccc5a52c28c6de9408eb688b8c08d8a
colleen <unknown@unknown.com>
2006-08-22 11:07:42 +0000

    table that details of the travellers itenirary

---
+++ onlineinvoice/sql/tbl_itinerary.sql
@@ -0,0 +1,63 @@
+<?php
+// Table Name
+$tablename = 'tbl_itinerary';
+
+//Options line for comments, encoding and character set
+$options = array('comment' => 'The table itinerary is managed by the onlineinvoice module', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
+
+// Fields
+$fields = array(
+	'id' => array(
+    'type' => 'text',
+    'length' => 32,
+    'notnull'     => 1
+    ),
+  'createdby'  => array(
+     'type'  =>  'text',
+     'length'=>  32
+    ),
+  'datecreated' =>  array(
+      'type'  =>  'date'
+    ),
+  'modifiedby'  =>  array(
+      'type'    =>  'text',
+      'length'  =>  32
+    ),
+   'datemodified' =>  array(
+    'type'        =>  'date'
+    ),
+    'updated'     =>  array(
+    'type'        =>  'date'
+    ),
+   'departuredate'  =>  array(
+      'type'        =>  'date',
+      'notnull'     => 1
+   ),
+   'departuretime'  =>  array(
+      'type'        =>  'time',
+      'notnull'     => 1
+   ),
+   'departurecity'  =>  array(
+      'type'        =>  'text',
+      'length'      =>  32,
+      'notnull'     => 1
+   ),
+   'arrivaledate'  =>  array(
+      'type'        =>  'date',
+      'notnull'     => 1
+   ),
+   'arrivaltime'  =>  array(
+      'type'        =>  'time',
+      'notnull'     => 1
+   ),
+   'arrivalcity'  =>  array(
+      'type'        =>  'text',
+      'length'      =>  32,
+      'notnull'     => 1
+   )
+   );
+//create other indexes here...
+
+
+
+
