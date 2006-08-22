98fa190abe821ee7544f538f983c83143ebbaed1
colleen <unknown@unknown.com>
2006-08-22 11:06:43 +0000

    table that contains all details pertaining to incidents

---
+++ onlineinvoice/sql/tbl_incident.sql
@@ -0,0 +1,76 @@
+<?php
+// Table Name
+$tablename = 'tbl_incident';
+
+//Options line for comments, encoding and character set
+$options = array('comment' => 'The table incident is managed by the onlineinvoice module', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
+
+// Fields
+$fields = array(
+	'id' => array(
+    'type' => 'text',
+    'length' => 32,
+    'notnull'     => 1
+    ),
+  'createdby'  => array(
+     'type'  =>  'text',
+     'length'=>  32
+    ),
+  'datecreated' =>  array(
+      'type'  =>  'date'
+    ),
+  'modifiedby'  =>  array(
+      'type'    =>  'text',
+      'length'  =>  32
+    ),
+   'datemodified' =>  array(
+    'type'        =>  'date'
+    ),
+    'updated'     =>  array(
+    'type'        =>  'date'
+    ),
+   'date'         =>  array(
+    'type'        =>  'date',
+    'notnull'     => 1
+   ),
+   'vendor'       =>  array(
+    'type'        =>  'text',
+    'length'      =>  32,
+    'notnull'     => 1
+   ),
+   'description'   => array(
+    'type'         => 'text',
+    'length'       => 255,
+    'notnull'     => 1
+   ),
+   'currency'      => array(
+    'type'         => 'text',
+    'length'       => 3,
+    'notnull'     => 1
+   ),
+   'cost'         =>  array(
+    'type'        =>  'float',
+    'notnull'     => 1
+   ),
+   'exchangerate' =>  array(
+      'type'      =>  'float',
+      'notnull'     => 1
+  )
+  );
+
+//create other indexes here...
+
+
+
+?>
+//create other indexes here...
+
+/*$name = 'ind_groups_FK';
+
+$indexes = array(
+                'fields' => array(
+                	'parent_id' => array()
+                )
+        );
+
+*/
