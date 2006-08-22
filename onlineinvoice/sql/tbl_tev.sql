bfcdb369fb8d2bbc85f86ce5d32d5efd2468ce2b
colleen <unknown@unknown.com>
2006-08-22 11:08:35 +0000

    table that contains the travel voucher issued to traveller

--- onlineinvoice/sql/tbl_tev.sql
+++ onlineinvoice/sql/tbl_tev.sql
@@ -3,18 +3,83 @@
 $tablename = 'tbl_tev';
 
 //Options line for comments, encoding and character set
-$options = array('comment' => 'travel expenses', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
+$options = array('comment' => 'The table tev - travel expense voucher is managed by the onlineinvoice module', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
 
 // Fields
 $fields = array(
 	'id' => array(
-		'type' => 'text',
-		'length' => 32
-		)
+    'type' => 'text',
+    'length' => 32,
+    'notnull'=> 1
+    ),
+  'createdby'  => array(
+     'type'  =>  'text',
+     'length'=>  32,
+    ),
+  'datecreated' =>  array(
+      'type'  =>  'date',
+    ),
+  'modifiedby'  =>  array(
+      'type'    =>  'text',
+      'length'  =>  32
+    ),
+   'datemodified' =>  array(
+    'type'        =>  'date'
+    ),
+    'updated'     =>  array(
+    'type'        =>  'date'
+    ),
+   'claimanantname' =>  array(
+      'type'        =>  'text',
+      'length'      =>  32,
+      'notnull'     => 1
+    ),
+    'title'         =>  array(
+      'type'        =>  'text',
+      'length'      =>  32,
+      'notnull'     => 1
+    ),
+    'mailaddress'   =>  array(
+      'type'        =>  'text',
+      'length'      =>  255,
+      'notnull'     => 1
+    ),
+    'city'          =>  array(
+      'type'        =>  'text',
+      'length'      =>  32,
+      'notnull'     => 1
+    ),
+    'province'      =>  array(
+     'type'         =>  'text',
+     'length'       =>  32,
+     'notnull'     => 1 
+    ),
+    'postalcode'    =>  array(
+      'type'        =>  'text',
+      'length'      =>  32,
+      'notnull'     => 1
+      
+    ),
+    'country'       =>  array(
+      'type'        =>  'text',
+      'length'      =>  32,
+      'notnull'     => 1
+    )      
     );
 
-//create other indexes here...
+
 
 
 
 ?>
+//create other indexes here...
+
+/*$name = 'ind_groups_FK';
+
+$indexes = array(
+                'fields' => array(
+                	'parent_id' => array()
+                )
+        );
+
+*/
