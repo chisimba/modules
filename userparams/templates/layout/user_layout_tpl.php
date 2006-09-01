b09171a2b0b57d3d219d4e135b72403c8b874e06
Prince Mbekwa <pmbekwa@gmail.com>
2006-09-01 08:32:03 +0000

    *** empty log message ***

--- userparams/templates/layout/user_layout_tpl.php
+++
@@ -1,9 +0,0 @@
-<?
-$cssLayout =& $this->newObject('csslayout', 'htmlelements');
-$leftMenu =& $this->newObject('sidemenu','toolbar');
-
-$cssLayout->setLeftColumnContent($leftMenu->show('user'));
-$cssLayout->setMiddleColumnContent($this->getContent());
-
-echo $cssLayout->show();
-?>
