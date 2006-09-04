707d8c58c1ce80bc451101af005548ddfbb8befd
colleen <unknown@unknown.com>
2006-09-04 08:04:24 +0000

    contains functions for db manipulation such as inserting into the db

--- onlineinvoice/classes/dbLodging_class_inc.php
+++ onlineinvoice/classes/dbLodging_class_inc.php
@@ -1,25 +1,33 @@
 <?php
+
 class dbLodging extends dbTable{
+  
+
+
+
 	/**
 	* Constructor
 	*/
+
 	function init()
 	{
 		parent::init('tbl_lodging');
 	}
 
+
 	function add($data)
 	{
-    
    $results = $this->insert($data); 
    return $results;
-   
-   
-                
   }
-	
+
 	function getTev()
-	{}
+	{
+  
+  }
+
 }
+
+
 
 ?>
