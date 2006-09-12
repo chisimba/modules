438566715042d22ad6d4b5cd571de90eec3c311d
colleen <unknown@unknown.com>
2006-09-12 14:31:45 +0000

    basic form layout

--- onlineinvoice/templates/layout/invoice_layout_tpl.php
+++ onlineinvoice/templates/layout/invoice_layout_tpl.php
@@ -2,24 +2,67 @@
 
 
 
-// Create an Instance of the CSS Layout
-
+/**
+ * create objects of classes to be user
+ */
+   
 $cssLayout =& $this->newObject('csslayout', 'htmlelements');
-// Create an Instance of the User Side Menu
+//$help = & $this->newObject('')
 $this->sideMenuBar=& $this->getObject('sidemenu','toolbar');
-$sideMenuBar=& $this->getObject('sidemenu','toolbar');
-//Set the Content of left side column
-$cssLayout->setLeftColumnContent($this->sideMenuBar->userDetails());
-//$cssLayout->setRightColunmContent($this->sideMenuBar->userDetails()); 
-// Set the Content of left side column
-//    $cssLayout->setLeftColumnContent($sideMenuBar->show('user'));
+//$this->loadClass('helplink','help');
+//$objhelplink  = new helplink($helpinformer,$val);
+//$displayhelp  = $objhelplink->show(helpinformer);
 
-// Set the Content of middle column
+$this->objHelp=& $this->getObject('helplink','help');
+//help_onlineinvoice_invoicedate
+$displayhelp  = $this->objHelp->show('help_onlineinvoice_about','onlineinvoice'); // check wot to place as parameter
+
+$cssLayout->setNumColumns(3);
+$sideMenuBar=& $this->getObject('sidemenu','toolbar');
+
+/**
+ *create a log out link and add to rightcolumn
+ */
+$logout    = ucfirst($this->objLanguage->languageText('word_logout'));
+$logmessage = ucfirst($this->objLanguage->languageText('mod_onlineinvoice_logoutsystem','onlineinvoice'));
+$help = ucfirst($this->objLanguage->languageText('mod_onlineinvoice_helptext','onlineinvoice'));
+ 
+$urltext = $logout;
+$content = $logmessage;
+$caption = '';
+$url = $this->uri(array('action'=>NULL));
+$this->objlogoutlink  = & $this->newObject('mouseoverpopup','htmlelements');
+$this->objlogoutlink->mouseoverpopup($urltext,$content,$caption,$url);
+$rightcolumn = $this->objlogoutlink->show();  
+
+
+
+$this->loadClass('featurebox','navigation');
+$objfeature = new featurebox($val,$val2);
+$displaylink = $objfeature->show($rightcolumn);
+
+
+
+$objfeaturehelp = new featurebox($val,$val2);
+$display = $objfeaturehelp->show($help  . " " .'<br />' . $displayhelp);
+
+$objuserdetails = new featurebox($val,$val2);
+$displayleft = $objuserdetails->show($this->sideMenuBar->userDetails());
+        
+
+/**
+ * Set the Content of left side column and right side column and middle column
+ */  
+$cssLayout->setLeftColumnContent($displayleft);
+$cssLayout->setRightColumnContent($displaylink . '<br />' . '<br />' . '<br />' .$display);
 $cssLayout->setMiddleColumnContent($this->getContent()); 
-// Display the Layout
+
+/**
+ * Display the Layout
+ */  
 echo $cssLayout->show();
 
-
+/****************************************************************************************************************/
 
 ?>
 
