15d3c41617ade33b38b0f61172908f3f1251777b
tohir <unknown@unknown.com>
2006-09-20 08:38:16 +0000

    fixed problem with apostraphes

--- contextcontent/classes/db_contextcontent_pages_class_inc.php
+++ contextcontent/classes/db_contextcontent_pages_class_inc.php
@@ -1,15 +1,31 @@
 <?php
-
+/**
+* class that contains the content of pages in the contextcontent module
+* @author Tohir Solomons
+*/
 class db_contextcontent_pages extends dbtable
 {
 
-    function init()
+    /**
+	* Constructor
+	*/
+	public function init()
     {
         parent::init('tbl_contextcontent_pages');
         $this->objUser =& $this->getObject('user', 'security');
     }
     
-    function addPage($titleId, $menutitle, $content, $language, $headerScript)
+    /**
+	* Method to add a Page
+	*
+	* @param string $titleId Record Id of the Title
+	* @param string $menutitle Title of the Page
+	* @param string $content Content of the Page
+	* @param string $language Language of the Page
+	* @param string $headerScript Header JS of the Page
+	* @return boolean Result of Insert
+	*/
+    public function addPage($titleId, $menutitle, $content, $language, $headerScript)
     {
         if (!$this->checkPageExists($titleId, $language)) {
             return $this->insert(array(
@@ -27,7 +43,14 @@ class db_contextcontent_pages extends dbtable
         }
     }
     
-    function checkPageExists($titleId, $language)
+    /**
+	* Method to Check whether a Page exists for a title
+	*
+	* @param string $titleId Record Id of the Title
+	* @param string $language Requested language
+	* @return boolean
+	*/
+    public function checkPageExists($titleId, $language)
     {
         $recordCount = $this->getRecordCount('WHERE titleid=\''.$titleId.'\' AND language=\''.$language.'\'');
         
@@ -38,9 +61,24 @@ class db_contextcontent_pages extends dbtable
         }
     }
     
-    function updatePage($id, $title, $content, $headerScripts)
+    /**
+    * Method to Update the Content of a Page
+    *
+    * @param string id Record Id of the Page
+    * @param string $menutitle Title of the Page
+    * @param string $content Content of the Page
+    * @param string $headerScript Header JS of the Page
+    * @return boolean
+     */
+    public function updatePage($id, $title, $content, $headerScripts)
     {
-        return $this->update('id', $id, array('menutitle'=>$title, 'pagecontent'=>$content, 'headerscripts'=>$headerScripts));
+        return $this->update('id', $id, array(
+        		'menutitle'=>addslashes(stripslashes($title)), 
+        		'pagecontent'=>addslashes(stripslashes($content)), 
+        		'headerscripts'=>addslashes(stripslashes($headerScripts)),
+                'creatorid' => $this->objUser->userId(),
+                'datecreated' => strftime('%Y-%m-%d %H:%M:%S', mktime())
+    		));
     }
 
 }
