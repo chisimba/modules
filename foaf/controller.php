768068b18ac1e3b092ac01fa00278c4357972e88
paulscott <unknown@unknown.com>
2006-09-26 11:53:14 +0000

    foaf model

--- foaf/controller.php
+++ foaf/controller.php
@@ -77,6 +77,13 @@ class foaf extends controller
 	public $dbFUsers;
 
 	/**
+	 * Foaf factory class
+	 *
+	 * @var object
+	 */
+	public $objFoafOps;
+
+	/**
 	 * Path to save RDF file to
 	 *
 	 * @var string
@@ -101,10 +108,12 @@ class foaf extends controller
 			$this->objFoaf = $this->getObject('foafcreator');
 			//Object to parse and display FOAF RDF
 			$this->objFoafParser = $this->getObject('foafparser');
+			//load up the foaf factory class
+			$this->objFoafOps = $this->getObject('foafops');
 			//LOAD UP THE USER OBJECT
 			$this->objUser = $this->getObject('user', 'security');
 			//hook up the database models
-			$this->dbFUsers = $this->getObject('dbfoafusers');
+			$this->dbFoaf = $this->getObject('dbfoaf');
 
 			//Get the activity logger class
 			$this->objLog = $this->newObject('logactivity', 'logger');
@@ -131,55 +140,20 @@ class foaf extends controller
 		switch ($action) {
 			default:
 			case 'createfoaf':
-				//set the path where we will save the users foaf rdf file for publishing
-				$this->savepath = $this->objConfig->getContentBasePath() . "users/" . $this->objUser->userId() . "/";
-				//get the users userId
-				$userid = $this->objUser->userId();
-				//get the users full name
-				$fullname = $this->objUser->fullname();
-				//retrieve what ever other info about the user we can get from tbl_users
-				$uarr = $this->dbFUsers->getRecordSet($userid);
-				//set the user details to an array that we can use
-				$userdetails = $uarr[0];
-				//get some of the foaf info
-				//title
-				$title = $userdetails['title'];
-				//users first name
-				$firstname = $userdetails['firstname'];
-				//users surname
-				$surname = $userdetails['surname'];
-				//users email address
-				$email = $userdetails['emailaddress'];
-				//we need a usrimage as well for the foaf depiction
-				$depiction = $this->objUser->getUserImageNoTags();
 
-				//var_dump($this->objFoaf);
-				$this->objFoaf->newAgent('person');
-				$this->objFoaf->setName($fullname);
-				$this->objFoaf->setTitle($title);
-				$this->objFoaf->setFirstName($firstname);
-				$this->objFoaf->setSurname($surname);
-				$this->objFoaf->setGeekcode('GCS/S d-- s++:+ a- C+++$ UBL+$ P+ L+++$ E- !W+++$ N+ !o !K-- !w-- O M V PS+++ PE++ Y+ PGP+ t+ 5 X R tv-- b+++ DI-- D++ G e++ h---- r+++ y++++');
-				$this->objFoaf->addMbox('mailto:'.$email,TRUE); // see also: XML_FOAF::setMboxSha1Sum();
-				$this->objFoaf->addHomepage('http://fsiu.uwc.ac.za');
-				$this->objFoaf->addWeblog('http://fsiu.uwc.ac.za/index.php?module=blog');
-				$this->objFoaf->addImg('http://fsiu.uwc.ac.za/usr_images/pscott.jpg');
-				$this->objFoaf->addPage('http://fsiu.uwc.ac.za/kinky/','Stuff about me','Paul Scotts project homepage');
-				$this->objFoaf->addPage('http://www.php-mag.net/itr/online_artikel/psecom,id,484,nodeid,114.html','Sticking The Fork In','Creating Daemons in PHP');
-				$this->objFoaf->addPage('http://pawscon.com/', 'PHP and Web Standards Conference UK 2004', 'A Conference dedicated to PHP, Web Standards and the Semantic Web');
-				$this->objFoaf->addPhone('0834360955');
-				$this->objFoaf->addJabberID('paul@fsiu.uwc.ac.za');
-				$this->objFoaf->addTheme('http://php.net');
-				$this->objFoaf->addOnlineAccount('Paul','http://freenode.info','http://xmlns.com/foaf/0.1/OnlineChatAccount');
-				$this->objFoaf->addOnlineGamingAccount('Paul_S','http://www.there.com');
-				$this->objFoaf->addWorkplaceHomepage('http://fsiu.uwc.ac.za');
-				$this->objFoaf->addSchoolHomepage('http://www.uwc.ac.za/');
-				$this->objFoaf->addInterest('http://xmlns.com/foaf/0.1/');
-				$this->objFoaf->addFundedBy('http://www.idrc.ca');
-				$this->objFoaf->addLogo('http://www.php.net/image.jpg');
-				$this->objFoaf->setBasedNear(32.565475,-25.162895);
-				$this->objFoaf->addDepiction($depiction);
-				$this->objFoaf->addDepiction('http://example.org/depiction/2');
+				//create the basic foaf profile from tbl_users
+				$this->objFoafOps->newPerson($this->objUser->userId());
+				//add in other details if they exist
+				$this->objFoafOps->myFoaf($this->objUser->userId());
+
+
+
+				$this->objFoafOps->writeFoaf();
+				echo $this->objFoafOps->foaf2html($this->objUser->userId());
+
+
+				/*
+
 
 				//var_dump($this->objFoaf->foaftree);
 				//who do we know?
@@ -213,6 +187,7 @@ class foaf extends controller
     	$this->objFoaf->toFile($this->savepath, $this->objUser->userId() . '.rdf', $this->objFoaf->get());
     	//$foaf->dump();
     	$this->objFoaf->dump();
+    	*/
 				break;
 
 			case 'parsefoaf':
@@ -220,7 +195,7 @@ class foaf extends controller
 				$fp = $this->objFoafParser->parseFromUri('/var/www/5ive/app/usrfiles/paulfoaf.rdf');
 				//var_dump($this->objFoafParser->rdf_parser);
 				//print_r($this->objFoafParser->foaf_data);
-				var_dump($this->objFoafParser->toObject($this->objFoafParser->foaf_data));
+				echo $this->objFoafParser->toHtml($this->objFoafParser->foaf_data);
 		}
 	}
 }
