4ee4eb3406273869fdede5fd652bd46d9bd4a7f9
paulscott <unknown@unknown.com>
2006-10-13 07:37:19 +0000

    blog module start

---
+++ blog/controller.php
@@ -0,0 +1,56 @@
+<?php
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+class blog extends controller
+{
+	public $objUser;
+	public $objLanguage;
+	public $objLog;
+	public $objFeed;
+	public $objFeedCreator;
+	public $objClient;
+
+    /**
+     * Constructor method to instantiate objects and get variables
+     */
+    public function init()
+    {
+        try {
+        	$this->objFeed = $this->getObject('feeds', 'feed');
+        	$this->objFeedCreator = $this->getObject('feeder', 'feed');
+        	$this->objClient = $this->getObject('client','httpclient');
+            $this->objLanguage = $this->getObject('language', 'language');
+            //Get the activity logger class
+            $this->objLog = $this->newObject('logactivity', 'logger');
+            //Log this module call
+            $this->objLog->log();
+        }
+        catch(customException $e) {
+            echo customException::cleanUp();
+            die();
+        }
+    }
+
+    /**
+     * Method to process actions to be taken
+     *
+     * @param string $action String indicating action to be taken
+     */
+    public function dispatch($action = Null)
+    {
+        switch ($action) {
+            default:
+                try {
+                    }
+                    catch(customException $e) {
+                        customException::cleanUp();
+                    }
+        }
+
+    }
+}
+?>
