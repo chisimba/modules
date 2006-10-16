a3e4793e5bfc667ce4c74899078d0633782ff16b
colleen <unknown@unknown.com>
2006-10-16 06:59:38 +0000

    *** empty log message ***

---
+++ rubric/templates/layout/layout_tpl.php
@@ -0,0 +1,21 @@
+<?php
+$cssLayout =& $this->newObject('csslayout', 'htmlelements');
+//if ($this->objUser->isContextLecturer()
+//|| $this->objUser->isContextStudent() ) {
+//	$userMenuBar=& $this->getObject('sidemenu','toolbar');
+//}
+//else if ($this->objUser->isLecturer()) {
+//	$userMenuBar=& $this->getObject('sidemenu','toolbar');
+//}
+//else {
+//	die('Access denied');
+//}
+//$cssLayout->setLeftColumnContent($userMenuBar->show('context'));
+$this->sideMenuBar=& $this->getObject('sidemenu','toolbar');
+$sideMenuBar=& $this->getObject('sidemenu','toolbar');
+
+$cssLayout->setLeftColumnContent($this->sideMenuBar->menuContext()); //change to what needs to be done
+$cssLayout->setMiddleColumnContent($this->getContent());
+
+echo $cssLayout->show();
+?>
