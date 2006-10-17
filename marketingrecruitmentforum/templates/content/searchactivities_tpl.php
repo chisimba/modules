3f28134b1403c422fb727514c0b00e75ae9738ee
colleen <unknown@unknown.com>
2006-10-17 10:56:52 +0000

    display the various search options in a dropdown list

---
+++ marketingrecruitmentforum/templates/content/searchactivities_tpl.php
@@ -0,0 +1,50 @@
+<?php
+
+//template used to display all search facilities for student cards relevant to faculty
+      
+      /**
+       *load all classes
+       */
+       $this->objsearchinfo = & $this->getObject('searchinfo','marketingrecruitmentforum');
+       
+/*---------------------------------------------------------------------------------------------------*/       
+                    
+      /**
+        *create form heading
+        */
+        $this->objMainheading =& $this->getObject('htmlheading','htmlelements');
+        $this->objMainheading->type=1;
+        $this->objMainheading->str=$objLanguage->languageText('mod_marketingrecruitmentforum_activities','marketingrecruitmentforum');
+/*---------------------------------------------------------------------------------------------------*/  
+      /**
+        *define all language items
+        */
+        $searchmsg = $this->objLanguage->languageText('mod_marketingrecruitmentforum_searchinstruction','marketingrecruitmentforum');
+        $go = $this->objLanguage->languageText('word_go');
+/*---------------------------------------------------------------------------------------------------*/   
+      /**
+       *create form button -- go
+       */
+                    
+      $this->objButtonGo  = new button('go', $go);
+      $this->objButtonGo->setToSubmit();
+/*---------------------------------------------------------------------------------------------------*/   
+  /**
+   *create a form to place all elements in
+   */
+   $val  = $this->objsearchinfo->activitysearch();
+   $objForm = new form('searchactivity',$this->uri(array('action'=>'NULL')));
+   $objForm->displayType = 3;
+   $objForm->addToForm($this->objMainheading->show() . '<br />' . '<br />'. $searchmsg . ' ' . $val . ' ' .$this->objButtonGo->show());
+    
+   echo $objForm->show();
+/*------------------------------------------------------------------------------*/   
+       /**
+        *display all information to the screen
+        */                       
+        /*$val  = $this->objsearchinfo->activitysearch();
+        echo $this->objMainheading->show();
+        echo '<br />'  . '<br />';
+        echo '<b>' . $searchmsg . '</b>';
+        echo ' ' .   $val;*/
+?>
---
+++ marketingrecruitmentforum/templates/content/searchslu_tpl.php
@@ -0,0 +1,54 @@
+<?php
+//template used to display all search facilities for SLU Activities
+      
+      /**
+       *load all classes
+       */
+       $this->loadClass('dropdown', 'htmlelements');
+       $this->loadclass('button','htmlelements');
+       
+       $this->objsearchinfo = & $this->getObject('searchinfo','marketingrecruitmentforum');
+       $searchlist  = new dropdown('searchlist');
+/*---------------------------------------------------------------------------------------------------*/       
+                    
+      /**
+        *create form heading
+        */
+        $this->objMainheading =& $this->getObject('htmlheading','htmlelements');
+        $this->objMainheading->type=1;
+        $this->objMainheading->str=$objLanguage->languageText('mod_marketingrecruitmentforum_infocardslu','marketingrecruitmentforum');
+/*---------------------------------------------------------------------------------------------------*/  
+      /**
+        *define all language items
+        */
+        $searchmsg = $this->objLanguage->languageText('mod_marketingrecruitmentforum_searchinstruction','marketingrecruitmentforum');
+        $go = $this->objLanguage->languageText('word_go');
+
+/*---------------------------------------------------------------------------------------------------*/        
+      /**
+       *create form button -- go
+       */
+                    
+      $this->objButtonGo  = new button('go', $go);
+      $this->objButtonGo->setToSubmit();
+/*---------------------------------------------------------------------------------------------------*/
+ /**
+   *create a form to place all elements in
+   */
+   $val  = $this->objsearchinfo->slusearchlist();
+   $objForm = new form('searchslu',$this->uri(array('action'=>'NULL')));
+   $objForm->displayType = 3;
+   $objForm->addToForm($this->objMainheading->show() . '<br />' . '<br />'. $searchmsg . ' ' . $val . ' ' .$this->objButtonGo->show());
+
+/*------------------------------------------------------------------------------*
+       /**
+        *display all information to the screen
+        */                       
+        /*$val  = $this->objsearchinfo->slusearchlist();
+        echo $this->objMainheading->show();
+        echo '<br />'  . '<br />';
+        echo '<b>' . $searchmsg . '</b>';
+        echo ' ' .   $val;*/
+        echo $objForm->show();
+
+?>
