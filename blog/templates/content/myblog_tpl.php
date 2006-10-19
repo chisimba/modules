652fbe6b962f04f1f9571da6411b8f636bca3065
paulscott <unknown@unknown.com>
2006-10-19 12:24:27 +0000

    added in sidebar

--- blog/templates/content/myblog_tpl.php
+++ blog/templates/content/myblog_tpl.php
@@ -1,5 +1,6 @@
 <?php
 $cssLayout = &$this->newObject('csslayout', 'htmlelements');
+$objSideBar = $this->newObject('sidebar', 'navigation');
 // Set columns to 3
 $cssLayout->setNumColumns(3);
 $leftMenu = "RSS FEEDS"; //&$this->newObject('usermenu', 'toolbar');
@@ -8,20 +9,30 @@ $rightSideColumn = NULL; //$this->objLanguage->languageText('mod_blog_instructio
 //print_r($cats); die();
 $rightSideColumn .= "<em>" . $this->objLanguage->languageText("mod_blog_categories", "blog") . "</em>";
 $rightSideColumn .= "<br />";
-$rightSideColumn .= "<ul>";
-//print_r($cats); die();
+
+//print_r($cats);
+$nodes = array();
+$childnodes = array();
+
 if(!empty($cats))
 {
 	foreach($cats as $categories)
 	{
-		if(isset($categories['cat_name']))
-		{
-			$rightSideColumn .= "<li>" . $categories['cat_name'] . "</li>";
-		}
-
+			//build the sub list as well
+			if(isset($categories['child']))
+			{
+				foreach($categories['child'] as $kid)
+				{
+					$childnodes[] = array('text' => $kid['cat_nicename'], 'uri' => $this->uri(array('action' => 'showsection', 'id' => $kid['id']), 'blog'));
+				}
+			}
+			$nodestoadd[] = array('text'=> $categories['cat_nicename'], 'uri' => $this->uri(array('action' => 'showsection', 'id' => $categories['id']), 'blog'), 'haschildren' => $childnodes);
+			$childnodes = NULL;
+			$nodes = NULL;
 	}
 }
-$rightSideColumn .= "</ul>";
+
+$rightSideColumn .= $objSideBar->show($nodestoadd, NULL, NULL, 'blog');
 
 
 $cssLayout->setMiddleColumnContent($this->getContent());
