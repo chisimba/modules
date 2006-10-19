ac17aae09fccd1dc478af5b0a7f7c94003bbfe16
colleen <unknown@unknown.com>
2006-10-19 12:54:57 +0000

    class used to manipulate db interactions for studcard information added the insert function

---
+++ marketingrecruitmentforum/classes/dbstudentcard_class_inc.php
@@ -0,0 +1,41 @@
+<?php
+//class use to perform all database manipulation of all student card info captured
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+/**
+* This object generates data used perform database manipulation
+* @package 
+* @category sems
+* @copyright 2004, University of the Western Cape & AVOIR Project
+* @license GNU GPL
+* @version
+* @author Colleen Tinker
+*/
+class dbstudentcard extends dbTable{ 
+
+  //protected $_objUser;
+ function init()
+	{
+	 try {
+     
+     
+      parent::init('tbl_studcard');
+		
+			
+		}catch (Exception $e){
+       		echo 'Caught exception: ',  $e->getMessage();
+        	exit();
+    }
+	}
+/*------------------------------------------------------------------------------*/
+  //insert all studcard information to database
+  public function addstudcard($studcarddata)
+	{
+        $studinfo = $this->insert($studcarddata);
+        return $studinfo;
+  }	 
+} 
+?>
