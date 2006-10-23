f65c5a4a138b5256147b6e9bc586b053f59a547f
paulscott <unknown@unknown.com>
2006-10-23 06:41:37 +0000

    blog operations class

---
+++ blog/classes/blogops_class_inc.php
@@ -0,0 +1,60 @@
+<?php
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+/**
+ * Class to handle blog elements
+ *
+ * @author Paul Scott
+ * @copyright GNU/GPL, AVOIR
+ * @package blog
+ * @access public
+ */
+
+class blogops extends object
+{
+	public function init()
+	{
+
+	}
+
+	public function catAddForm()
+	{
+
+	}
+
+	public function catDelForm()
+	{
+
+	}
+
+	public function postDelForm()
+	{
+
+	}
+
+	public function commentAddForm()
+	{
+
+	}
+
+	public function commentDelForm()
+	{
+
+	}
+
+	public function passProtectPost()
+	{
+
+	}
+
+	public function liveSearchForm()
+	{
+
+	}
+
+}
+?>
