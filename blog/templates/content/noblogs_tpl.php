f24825d28070efd7427a48efb364ded30ff533f6
paulscott <unknown@unknown.com>
2006-10-23 08:14:11 +0000

    added home link

--- blog/templates/content/noblogs_tpl.php
+++ blog/templates/content/noblogs_tpl.php
@@ -14,7 +14,7 @@ $leftCol = NULL;
 $middleColumn .= "<h1><em><center>" . $this->objLanguage->languageText("mod_blog_noblogs", "blog") . "</center></em></h1>";
 $middleColumn .= "<br />";
 
-$homelink = new href($this->uri(array(NULL, '_default')), $this->objLanguage->languageText("mod_blog_loginhere", "blog"));
+$homelink = new href($this->uri(NULL, '_default'), $this->objLanguage->languageText("mod_blog_loginhere", "blog"));
 $middleColumn .= "<center>" .$homelink->show() . "</center>";
 
 $cssLayout->setMiddleColumnContent($middleColumn);
