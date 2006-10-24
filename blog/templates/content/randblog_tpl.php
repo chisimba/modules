f415d36fc31d71ab1ec6850430b2d53058d278e6
paulscott <unknown@unknown.com>
2006-10-24 09:17:05 +0000

    added featurebox switch to blocklets

--- blog/templates/content/randblog_tpl.php
+++ blog/templates/content/randblog_tpl.php
@@ -8,9 +8,9 @@ $cssLayout = &$this->newObject('csslayout', 'htmlelements');
 $objSideBar = $this->newObject('sidebar', 'navigation');
 // Set columns to 3
 $cssLayout->setNumColumns(3);
-$leftMenu = NULL; //&$this->newObject('usermenu', 'toolbar');
+$leftMenu = NULL;
 
-$rightSideColumn = NULL; //$this->objLanguage->languageText('mod_blog_instructions', 'blog');
+$rightSideColumn = NULL;
 
 //set up a link to the other users blogs...
 //show the link to all blogs
@@ -19,14 +19,13 @@ $rightSideColumn .= $this->objblogOps->showBlogsLink(TRUE);
 //show the categories menu (if there are cats)
 $rightSideColumn .= $this->objblogOps->showCatsMenu($cats, TRUE);
 
-
 $leftCol = NULL;
 $middleColumn = NULL;
 
 $middleColumn .= $this->objblogOps->showPosts($posts);
 
 //left menu section
-$leftCol = NULL; //$leftMenu->show();
+$leftCol = NULL;
 $leftCol = $this->objblogOps->loginBox(TRUE);
 
 //show the feeds section
