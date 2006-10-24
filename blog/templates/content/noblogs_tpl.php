ef6c7993ea6b461c619626c607e01426cadcdd32
paulscott <unknown@unknown.com>
2006-10-24 15:52:43 +0000

    added loginbox

--- blog/templates/content/noblogs_tpl.php
+++ blog/templates/content/noblogs_tpl.php
@@ -5,22 +5,21 @@ $cssLayout = &$this->newObject('csslayout', 'htmlelements');
 $objSideBar = $this->newObject('sidebar', 'navigation');
 // Set columns to 3
 $cssLayout->setNumColumns(3);
-$leftMenu = NULL; //&$this->newObject('usermenu', 'toolbar');
+$leftMenu = NULL;
 
-$rightSideColumn = NULL; //$this->objLanguage->languageText('mod_blog_instructions', 'blog');
+$rightSideColumn = NULL;
 $middleColumn = NULL;
 $leftCol = NULL;
+$leftCol .= $this->objblogOps->loginBox(TRUE);
 
 $middleColumn .= "<h1><em><center>" . $this->objLanguage->languageText("mod_blog_noblogs", "blog") . "</center></em></h1>";
 $middleColumn .= "<br />";
 
-$homelink = new href($this->uri(NULL, '_default'), $this->objLanguage->languageText("mod_blog_loginhere", "blog"));
-$middleColumn .= "<center>" .$homelink->show() . "</center>";
+//$homelink = new href($this->uri(NULL, '_default'), $this->objLanguage->languageText("mod_blog_loginhere", "blog"));
+//$middleColumn .= "<center>" .$homelink->show() . "</center>";
 
 $cssLayout->setMiddleColumnContent($middleColumn);
 $cssLayout->setLeftColumnContent($leftCol); //$leftMenu->show());
 $cssLayout->setRightColumnContent($rightSideColumn);
 echo $cssLayout->show();
-//sleep(3);
-//$this->nextAction(array('module' => '_default'));
 ?>
