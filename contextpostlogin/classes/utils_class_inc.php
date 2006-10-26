60c242fe614f9c53e770df009979c5590147a9a3
wnitsckie <unknown@unknown.com>
2006-10-26 11:27:25 +0000

    fixed it

--- contextpostlogin/classes/utils_class_inc.php
+++ contextpostlogin/classes/utils_class_inc.php
@@ -118,7 +118,25 @@ class utils extends object
 	   */
 	  public function getRightContent()
 	  {
-	      return 'site dashboard goes here';
+	     $rightSideColumn = "";
+	     $objBlocks = & $this->newObject('blocks', 'blocks');
+		//Add the getting help block
+		$rightSideColumn .= $objBlocks->showBlock('gettinghelp', 'help');
+		//Add the latest in blog as a a block
+		$rightSideColumn .= $objBlocks->showBlock('latest', 'blog');
+		//Add the latest in blog as a a block
+		$rightSideColumn .= $objBlocks->showBlock('latestpodcast', 'podcast');
+		//Add a block for chat
+		$rightSideColumn .= $objBlocks->showBlock('chat', 'chat');
+		//Add a block for the google api search
+		$rightSideColumn .= $objBlocks->showBlock('google', 'websearch');
+		//Put the google scholar google search
+		$rightSideColumn .= $objBlocks->showBlock('scholarg', 'websearch');
+		//Put a wikipedia search
+		$rightSideColumn .= $objBlocks->showBlock('wikipedia', 'websearch');
+		//Put a dictionary lookup
+		
+		return $rightSideColumn;
 	  } 
 	  
 	  
