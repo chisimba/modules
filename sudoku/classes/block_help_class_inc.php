a695fc0d8d851531f2a49d97791d001b99a27e57
kevinc <unknown@unknown.com>
2006-11-02 12:23:32 +0000

    Sudoku module ported to chisimba New feature : Timer

---
+++ sudoku/classes/block_help_class_inc.php
@@ -0,0 +1,40 @@
+<?
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run'])
+{
+	die("You cannot view this page directly");
+}
+// end security check
+
+/**
+* The class to display a help block for sudoku
+*
+* @author Kevin Cyster
+*/
+class block_help extends object
+{
+    var $title;
+    
+    /**
+    * Constructor for the class
+    */
+    function init()
+    {
+        $this -> objLanguage =& $this -> getObject('language', 'language');
+        $this -> objInput =& $this -> getObject('textinput', 'htmlelements');
+        //Set the title
+        $this -> title = $this -> objLanguage -> languageText('word_instructions', 'sudoku');
+    }
+    
+    /**
+    * Method to output a block with information on how help works
+    */
+    function show()
+	{
+        //Add the text to the output
+        $ret = $this -> objLanguage -> languageText('mod_sudoku_help', 'sudoku');
+        
+        return $ret;
+    }
+}
+?>
