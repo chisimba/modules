fececf001afb002d883b66fe72d3936c35e3e8a3
ghinde <unknown@unknown.com>
2006-11-02 10:15:40 +0000

    initial commit

---
+++ cmsadmin/classes/articlebox_class_inc.php
@@ -0,0 +1,47 @@
+<?php
+
+
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']){
+    die("You cannot view this page directly");
+}
+
+/**
+* Class for building a article box for KEWL.nextgen.
+*
+* The class builds a css style feature box 
+*
+* @author Wesley Nitsckie
+* @copyright (c)2004 UWC
+* @package featurebox
+* @version 0.1
+*/
+
+class articlebox extends object
+{
+    /**
+    * Method to construct the class.
+    **/
+    public function init()
+    {
+    
+    }
+    
+    /**
+     * Method to show the article
+     * 
+     * @param null
+     * @access publc
+     * @return string
+     */
+    public function show($content = null)
+    {
+    	
+  		$article = '<div class="featurebox">';
+		$article .= '<small>'.$content.'</small>';		
+		$article .= '</div>';
+  		return $article;
+  		  	
+    }
+}
+?>
