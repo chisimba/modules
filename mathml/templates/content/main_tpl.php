e70ee738754a491aea84a0794abd4fbc8b5210a2
abdurahim <unknown@unknown.com>
2006-12-06 12:52:34 +0000

    Update link

--- mathml/templates/content/main_tpl.php
+++ mathml/templates/content/main_tpl.php
@@ -1,5 +1,12 @@
 <?php
-
-    print $ar."<br />";
-    print '<a href="http://www.mozilla.org/projects/mathml/fonts">Click to download fonts</a>';
+	$this->loadClass('windowpop','htmlelements');
+  $this->objPop=&new windowpop;
+  $this->objPop->set('location','http://www.mozilla.org/projects/mathml/fonts');
+  $this->objPop->set('linktext','Click to download fonts');
+  $this->objPop->set('width','800');
+  $this->objPop->set('height','600');
+  $this->objPop->set('left','300');
+  $this->objPop->set('top','400');
+  $this->objPop->putJs(); 
+  print $ar."<br />".$this->objPop->show();
 ?>
