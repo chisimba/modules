846304eded8f3fb7145062034283f0ab1c1ad006
paulscott <unknown@unknown.com>
2006-12-08 06:14:54 +0000

    added text to the header and reduced table size

--- gmaps/classes/gmapapi_class_inc.php
+++ gmaps/classes/gmapapi_class_inc.php
@@ -1,8 +1,8 @@
 <?php
 // security check - must be included in all scripts
-if (!$GLOBALS['kewl_entry_point_run']) {
-	die("You cannot view this page directly");
-}
+//if (!$GLOBALS['kewl_entry_point_run']) {
+//	die("You cannot view this page directly");
+//}
 // end security check
 
 /**
