ba513f2a0883f5700a4cd07eb134b2f85a4e8ec2
paulscott <unknown@unknown.com>
2006-12-13 08:48:57 +0000

    fixing up userid needs

--- blog/classes/blogops_class_inc.php
+++ blog/classes/blogops_class_inc.php
@@ -1619,7 +1619,7 @@ class blogops extends object
 
 
 		$this->bbcode = $this->getObject('bbcodeparser', 'utilities');
-		$ret = $objFeatureBox->show($this->objLanguage->languageText("mod_blog_trackback4post", "blog"), $this->bbcode->parse4bbcode($tbtext));
+		$ret = $objFeatureBox->show($this->objLanguage->languageText("mod_blog_trackback4post", "blog"), $this->bbcode->parse4bbcode(htmlentities($tbtext)));
 		return $ret;
 
 
