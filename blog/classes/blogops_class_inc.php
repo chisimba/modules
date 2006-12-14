5342914efb37c74daa73b891591473678ae05032
paulscott <unknown@unknown.com>
2006-12-14 11:53:43 +0000

    added bbcode parser

--- blog/classes/blogops_class_inc.php
+++ blog/classes/blogops_class_inc.php
@@ -832,7 +832,7 @@ class blogops extends object
 		}
 		else {
 			$search = array('@<script[^>]*?>.*?</script>@si',  // Strip out javascript
-            	   '@<[\/\!]*?[^<>]*?>@si',            // Strip out HTML tags
+            	  /* '@<[\/\!]*?[^<>]*?>@si', */            // Strip out HTML tags
                	   /*'@<style[^>]*?>.*?</style>@siU',*/    // Strip style tags properly
                	   '@<![\s\S]*?--[ \t\n\r]*>@'        // Strip multi-line comments including CDATA
 				   );
