f84dfed51f956a3d02acc563bcba9637cdfcdb92
tohir <unknown@unknown.com>
2006-12-18 07:57:51 +0000

    added class that loads prototype and scriptaculous libraries

---
+++ ajaxwrapper/classes/scriptaculous_class_inc.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+* Class to load the Prototype and Scriptaculous JavaScript
+*
+* This class merely loads the JavaScript for Prototype and Scriptaculous effects
+* It is not a wrapper. Developers still need to code their own JS functions
+*
+* @author Tohir Solomons
+* @package ajaxwrapper
+*/
+class scriptaculous extends object
+{
+	/**
+	* Constructor
+	*/
+	public function init() {}
+	
+	/**
+	* Method to load the Prototype JavaScript
+	*
+	* This function locates the Prototype JS and adds it to the headerParams of a page template
+	*/
+	public function loadPrototype()
+	{
+		// Suppress application/xhtml+xml mimetype
+		$this->setVar('pageSuppressXML', TRUE);
+		
+		// Load JS
+		return $this->appendArrayVar('headerParams', $this->getJavascriptFile('prototype/1.5.0_rc1/prototype.js', 'ajaxwrapper'));
+	}
+	
+	/**
+	* Method to load the Scriptaculous JavaScript
+	*
+	* This function locates the Scriptaculous JS and adds it to the headerParams of a page template
+	* It also loads the Prototype JS, since it depends on it.
+	*/
+	function show()
+	{
+		// Load Prototype
+		$this->loadPrototype();
+		// Add Scriptaculous
+		return $this->appendArrayVar('headerParams', $this->getJavascriptFile('scriptaculous/1.6.5/scriptaculous.js', 'ajaxwrapper'));
+	}
+}
+
+?>
