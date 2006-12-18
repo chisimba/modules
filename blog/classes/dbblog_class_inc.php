90a38abfd248da90033a3abd5298a1b5419c32ed
paulscott <unknown@unknown.com>
2006-12-18 11:46:46 +0000

    added quotes css, this may require serious cleanup by someone with CSS skills

--- blog/classes/dbblog_class_inc.php
+++ blog/classes/dbblog_class_inc.php
@@ -443,7 +443,7 @@ class dbblog extends dbTable
 			$this->pcleaner = $this->newObject('htmlcleaner', 'utilities');
 			$this->ecleaner = $this->newObject('htmlcleaner', 'utilities');
 			$postarr['postcontent'] = preg_replace("/(\r\n|\n|\r)/", "", $postarr['postcontent']);
-			$pc = preg_replace('=<br */?>=i', "\n", $postarr['postcontent']);
+			$pc = $postarr['postcontent'];
 			$insarr = array('userid' => $userid,
 							'post_date' => date('r'),
 							'post_content' => addslashes($pc),
