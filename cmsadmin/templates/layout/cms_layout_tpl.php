2455f0f39cdc3c62b3245f0c55f63b0bb38c428f
tohir <unknown@unknown.com>
2006-12-18 10:08:09 +0000

    removed unnecessary js code

--- cmsadmin/templates/layout/cms_layout_tpl.php
+++ cmsadmin/templates/layout/cms_layout_tpl.php
@@ -1,16 +1,7 @@
 <?php
 
-//Insert script for generating tree menu
-$js = $this->getJavascriptFile('tree.js', 'cmsadmin');
-$this->appendArrayVar('headerParams', $js);
-//Include tree menu css script
-$css = '<link rel="stylesheet" type="text/css" media="all" href="modules/cmsadmin/resources/tree.css" />';
-$this->appendArrayVar('headerParams', $css);
-//Set to automatically render htmllist into tree menu
-$this->appendArrayVar('bodyOnLoad', 'autoInit_trees()');
-
 // Create an instance of the CSS Layout
-$cssLayout =& $this->newObject('csslayout', 'htmlelements');
+$cssLayout = $this->getObject('csslayout', 'htmlelements');
 $cssLayout->setNumColumns(2);
 // Set the Content of middle column
 $cssLayout->setLeftColumnContent($this->getCMSMenu());
