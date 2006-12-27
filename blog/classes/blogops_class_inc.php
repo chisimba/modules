d511b327542ad15287d21b92577ee4979d0c4469
paulscott <unknown@unknown.com>
2006-12-27 19:32:14 +0000

    added setSelected method call, but it doesn;t seem to work

--- blog/classes/blogops_class_inc.php
+++ blog/classes/blogops_class_inc.php
@@ -1097,12 +1097,14 @@ class blogops extends object
         $pDrop = new dropdown('cat');
         if (isset($editparams['post_category'])) {
             $pDrop->addOption($editparams['post_category'], $editparams['post_category']);
+            $pDrop->setSelected($editparams['post_category']);
         }
-        $pDrop->addOption(0, $this->objLanguage->languageText("mod_blog_defcat", "blog"));
+
         $pcats = $this->objDbBlog->getAllCats($userid);
         foreach($pcats as $adds) {
             $pDrop->addOption($adds['id'], stripslashes($adds['cat_name']));
         }
+        $pDrop->addOption(0, $this->objLanguage->languageText("mod_blog_defcat", "blog"));
         $ptable->addCell($pdlabel->show());
         $ptable->addCell($pDrop->show());
         $ptable->endRow();
