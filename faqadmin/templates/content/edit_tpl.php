078405447babaaf472f589c39a118ba9d1b0550a
acej <unknown@unknown.com>
2007-01-17 08:35:50 +0000

    Updated Templates

---
+++ faqadmin/templates/content/edit_tpl.php
@@ -0,0 +1,35 @@
+<?php
+// Show the heading.
+$objHeading =& $this->getObject('htmlheading','htmlelements');
+$objHeading->type=1;
+$objHeading->str =$objLanguage->languageText("mod_faqadmin_editcategory","faqadmin").' : <em>'.$list[0]['categoryId'].'</em>';
+echo $objHeading->show();
+// Load the classes.
+$this->loadClass("form","htmlelements");
+$this->loadClass("textinput","htmlelements");
+$this->loadClass("button","htmlelements");
+// Create the form.
+$form = new form("createcategory", 
+$this->uri(array(
+ 	'module'=>'faqadmin',
+    'action'=>'editConfirm',
+    'id'=>$list[0]['id']
+))	
+);
+$form->setDisplayType(1);
+
+$textInput = new textinput("category",$list[0]['categoryId']);
+$textInput->size = 40;
+
+$form->addToForm($textInput->show());
+$form->addToForm("&nbsp;");
+$button = new button("submit", $objLanguage->languageText("word_save"));
+$button->setToSubmit();
+
+$cancelButton = new button("submit", $objLanguage->languageText('word_cancel'));
+$cancelButton->setOnClick("window.location='".$this->uri(NULL)."';");
+
+$form->addToForm($button->show().' / '.$cancelButton->show());
+// Show the form.
+echo $form->show();
+?>
---
+++ faqadmin/templates/content/main_tpl.php
@@ -0,0 +1,105 @@
+<?php
+// Show the heading
+$objHeading =& $this->getObject('htmlheading','htmlelements');
+$objHeading->type=1;
+
+// Show the add link
+$objLink =& $this->getObject('link','htmlelements');
+$objLink->link($this->uri(array(
+ 		'module'=>'faqadmin',
+		'action'=>'add',
+)));
+$iconAdd = $this->getObject('geticon','htmlelements');
+$iconAdd->setIcon('add');
+$iconAdd->alt = $objLanguage->languageText("mod_faqadmin_addcategory", "faqadmin");
+$iconAdd->align=false;
+$objLink->link = $iconAdd->show();
+ 		
+// Add the Icon to the heading
+$objHeading->str =$objLanguage->languageText("mod_faqadmin_heading","faqadmin").' '.$objLink->show();
+//$objHeading->str =$objLanguage->languageText("mod_faqadmin_heading")." : ".$contextTitle;
+echo $objHeading->show();
+
+// Create table for categories.
+$objTable =& $this->newObject('htmltable','htmlelements');
+$objTable->width='';
+$objTable->cellpadding='5';
+$objTable->cellspacing='2';
+// Add the table header.
+$objTable->startHeaderRow();
+$objTable->addHeaderCell($objLanguage->languageText("mod_faqadmin_category","faqadmin"), 200);
+$objTable->addHeaderCell($objLanguage->languageText("mod_faqadmin_noitems","faqadmin"), 200);
+$objTable->addHeaderCell($objLanguage->languageText("mod_faqadmin_action","faqadmin"), 100);
+$objTable->endHeaderRow();
+$index = 0;
+foreach ($categories as $item) {
+    // Create the edit link.
+	$objEditLink=&$this->getObject('link','htmlelements');
+	$objEditLink->link($this->uri(array(
+		'module'=>'faqadmin',
+		'action'=>'edit',
+		'id'=>$item['id']
+	)));
+    $iconEdit=&$this->getObject('geticon','htmlelements');
+    $iconEdit->setIcon('edit');
+    $iconEdit->alt=$objLanguage->languageText("word_edit");
+    $iconEdit->align=false;
+  	$objEditLink->link = $iconEdit->show();
+    // Create the delete link.
+	$objConfirm=&$this->newObject('confirm','utilities');
+    $iconDelete=$this->getObject('geticon','htmlelements');
+    $iconDelete->setIcon('delete');
+    $iconDelete->alt=$objLanguage->languageText("word_delete");
+    $iconDelete->align=false;
+	$objConfirm->setConfirm(
+    	$iconDelete->show(),
+		$this->uri(array(
+	    	'module'=>'faqadmin',
+		  	'action'=>'deleteConfirm',
+		  	'id'=>$item["id"]
+		)),
+        $objLanguage->languageText('phrase_suredelete')
+    );
+    // Add a row to the table.
+   	/*
+    // Create add icon                               
+    $objAddLink = $this->getObject('link', 'htmlelements');
+    $objAddLink->link($this->uri(array('action'=>'add','category'=>$item['id']), 'faq'));
+    $objAddIcon =& $this->getObject('geticon', 'htmlelements');
+    $objAddIcon->setIcon('add');
+    $objAddIcon->alt = $this->objLanguage->languageText('faq_sayitadd');
+    $objAddLink->link = $objAddIcon->show();
+	*/
+    // Count no items in category
+    $this->objFaqEntries =& $this->getObject('dbfaqentries', 'faq');
+    $list=$this->objFaqEntries->listAll($item['contextId'], $item['id']);
+    $count=count($list);
+    // Create link to category
+    $categoryLink =& $this->getObject('link', 'htmlelements');
+    $categoryLink->link($this->uri(array('action'=>'view','category'=>$item['id']), 'faq'));
+    $categoryLink->link = $item['categoryId'];
+    $categoryLink->title = $this->objLanguage->languageText('mod_faqadmin_viewcategory','faqadmin');
+	// Create the actions
+    $ncaction = /*$objAddLink->show();*/"";
+    $action = /*$objAddLink->show()."&nbsp;".*/$objEditLink->show()."&nbsp;".$objConfirm->show();
+    // This does a check so that the not categorised item can never be deleted.
+    if ($item['categoryId'] == 'Not Categorised' && $item['userId'] == 'admin') {
+     	$row = array($categoryLink->show().'&nbsp;'.'<b>'.'*'.'</b>', $count, $ncaction);
+    } 
+	else { 
+    	$row = array($categoryLink->show(), $count, $action);
+    }  
+    $objTable->addRow($row, $index%2?'even':'odd');
+    $index++;
+}
+// Show the table.
+echo($objTable->show());
+
+$addLink = new link($this->uri(array('action'=>'add')));
+$addLink->link = $objLanguage->languageText('mod_faqadmin_addcategory','faqadmin');
+
+//$returnToFaqLink = new link($this->uri(NULL, 'faq'));
+//$returnToFaqLink->link = $objLanguage->languageText('mod_faqadmin_returntofaq');
+
+echo '<p>'.$addLink->show()./*' / '.$returnToFaqLink->show().*/'</p>';
+?>
