b95fcf10abe6cd77faa21a06a9b5b53390569ec5
Derek Keats <derek@dkeats.com>
2007-01-20 19:14:45 +0000

    Replaced hard coded anchors with href method calls

--- blockalicious/classes/block_commlinks_class_inc.php
+++ blockalicious/classes/block_commlinks_class_inc.php
@@ -17,8 +17,7 @@ if (!$GLOBALS['kewl_entry_point_run'])
 class block_commlinks extends object
 {
     public $title;
-    public $objLanguage;
-    public $messageForBlock;
+    public $objLanugage;
     
     /**
     * Constructor for the class
@@ -27,26 +26,39 @@ class block_commlinks extends object
     {
     	//Instantiate the language object
     	$this->objLanguage = &$this->getObject('language', 'language');
-    	$this->_objContent = $this->getObject('dbcontent', 'cmsadmin');
-		$objModule = $this->getObject('modules','modulecatalogue');
-		if ($objModule->checkIfRegistered('blog', 'blof')) {
-			$this->messageForBlock = "<a href=\"index.php?module=blog\">Blog</a><br />";		    
-		}
-		if ($objModule->checkIfRegistered('podcast', 'podcast')) {
-			$this->messageForBlock .= "<a href=\"index.php?module=Podcast\">Podcast</a><br />";
-		}
-		if ($objModule->checkIfRegistered('wiki', 'wiki')) {
-			$this->messageForBlock .= "<a href=\"index.php?module=wiki\">Wiki</a><br />";
-		}
-		//Set the title - 
+        //Set the title - 
         $this->title=$this->objLanguage->languageText("mod_blockalicious_commlinks_title", "blockalicious");
     }
     
     /**
-    * Method to output a block with all news stories
+    * Method to output a block with information on how help works
     */
     function show()
 	{
-       return $this->messageForBlock;
+		//Initialize return string
+		$ret = "";
+		//Instant the module object to check if modules are registered
+		$objModule=& $this->getObject('modules','modulecatalogue');
+		$this->loadClass('href', 'htmlelements');
+		//Add blog link
+		if ($objModule->checkIfRegistered('blog','blog')) {
+			$ln = new href;
+			$ln->link="index.php?module=blog";
+			$ln->text="Blog";
+			$ret .= $ln->show() . "<br />";
+		}
+		if ($objModule->checkIfRegistered('podcast','podcast')) {
+			$ln = new href;
+			$ln->link="index.php?module=podcast";
+			$ln->text="Podcast";
+			$ret .= $ln->show() . "<br />";
+		}
+		if ($objModule->checkIfRegistered('wiki','wiki')) {
+			$ln = new href;
+			$ln->link="index.php?module=wiki";
+			$ln->text="Wiki";
+			$ret .= $ln->show() . "<br />";
+		}
+       return $ret;
     }
 }
