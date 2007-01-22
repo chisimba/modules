35eff4665ed86018721b2ad52f18ccbf8fb86de5
wnitsckie <unknown@unknown.com>
2007-01-22 12:39:33 +0000

    *** empty log message ***

--- readinglist/templates/layout/layout_tpl.php
+++ readinglist/templates/layout/layout_tpl.php
@@ -5,7 +5,7 @@ if($objDBContext->isInContext())
     $objContextUtils = & $this->getObject('utilities','context');
     $cm = $objContextUtils->getHiddenContextMenu('readinglist','none');
 } else {
-    $cm = $this->getMenu();
+    $cm = '';
 }
 
 $cssLayout =& $this->newObject('csslayout', 'htmlelements');
