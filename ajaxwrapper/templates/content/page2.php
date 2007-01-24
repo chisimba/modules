2b0315b4c3586477c32d334ca52c5a79d8290956
kevinc <unknown@unknown.com>
2007-01-24 11:16:40 +0000

    Latest version of the xajax code

--- ajaxwrapper/templates/content/page2.php
+++ ajaxwrapper/templates/content/page2.php
@@ -2,5 +2,5 @@
 <p>Enter a username. You will receive feedback whether the username exists or not.<br />In addition, the submit button will either be enabled or disabled.<br />Also try an 'illegal character' like slashes, apostraphes, etc.</p>
 <input name="usernametest" id="username" type="text" size="50" onkeyup="xajax_userNameExists(this.value);" />
 <div id ="someDiv"></div>
-<p><input type="button" id="submitbutton" value="Submit Button" onclick="alert('You submitted '+username.value);" /></p>
+<p><input type="button" id="submitbutton" value="Submit Button" onclick="alert('You submitted '+document.getElementById('username').value);" /></p>
 <a href="<?php echo $this->uri(NULL); ?>">Simple AJAX test</a>
