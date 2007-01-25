a3001b8789a0415238c242b70769faf5590e015b
abdurahim <unknown@unknown.com>
2007-01-25 11:42:51 +0000

    Added links for context

--- glossary/classes/modulelinks_glossary_class_inc.php
+++ glossary/classes/modulelinks_glossary_class_inc.php
@@ -15,8 +15,7 @@ class modulelinks_glossary extends object
     public function init()
     {
         //$this->loadClass('treenode','tree');
-       $this->_objDBEventsCalendar = & $this->newObject('dbeventscalendar','eventscalendar');
-       $this->_objDBCategories = & $this->newObject('dbeventscalendarcategories','eventscalendar');
+       $this->objGlossary = & $this->newObject('dbglossary','glossary');
     }
     
     public function show()
@@ -32,26 +31,23 @@ class modulelinks_glossary extends object
      */
     public function getContextLinks($contextCode)
     { 
-       /*
-          $catId = $this->_objDBCategories->getCatId('context', $contextCode);
-         
-          $events =  $this->_objDBEventsCalendar->getAll('WHERE catid="'.$catId.'" ORDER BY event_date' );
-          
+       
+       	$glossarys = $this->objGlossary->fetchAllRecords($contextCode);	   
           $bigArr = array();
-         
-          foreach ($events as $event)
+
+          foreach ($glossarys as $glossary)
           {
                 $newArr = array();    
-              $newArr['menutext'] = $event['title'];
-              $newArr['description'] = $event['description'];
-              $newArr['itemid'] = $event['id'];
-              $newArr['moduleid'] = 'eventscalendar';
-              $newArr['params'] = array('month' => date('m',$event['event_date']),'action' => 'events');
+              $newArr['menutext'] = $glossary['term'];
+              $newArr['description'] = '';
+              $newArr['itemid'] = $glossary['item_id'];
+              $newArr['moduleid'] = 'glossary';
+              $newArr['params'] = array('id'=>$glossary['item_id'],'action' => 'viewbyletter');
               $bigArr[] = $newArr;
           }
           
           return $bigArr;
-          */
+         
     }
     
 }
