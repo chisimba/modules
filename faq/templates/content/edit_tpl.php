89fdbcbdaaabb1f81d41daf78ae0b2197b8baba4
acej <unknown@unknown.com>
2007-01-25 10:06:07 +0000

    Updated Language Items

--- faq/templates/content/edit_tpl.php
+++ faq/templates/content/edit_tpl.php
@@ -53,7 +53,7 @@
 	$button = new button("submit", $objLanguage->languageText("word_save"));
 	$button->setToSubmit();
 
-    $cancelButton =new button("submit", $objLanguage->languageText("word_cancel","faq"));
+    $cancelButton =new button("submit", $objLanguage->languageText("word_cancel","faqadmin"));
     $cancelButton->setOnClick("window.location='".$this->uri(array('action'=>'view', 'category'=>$categoryId))."';");
 
 	$form->addToForm($button->show().' / '.$cancelButton->show());
