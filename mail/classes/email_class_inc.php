ec6f4bdbf8f0dd1a5a37b5b9c9f1aba24df1e03a
rwhitney <unknown@unknown.com>
2007-01-26 14:05:06 +0000

    Mail was hardcoded to a lib directory.  Changed to the resources directory and added the getResourceUri call

--- mail/classes/email_class_inc.php
+++ mail/classes/email_class_inc.php
@@ -7,7 +7,7 @@ if (!$GLOBALS['kewl_entry_point_run'])
 // end security check
 
 //Load the library class
-require_once("modules/mail/lib/class.phpmailer.php");
+require_once($this->getResourceUri('class.phpmailer.php', 'mail'));
 //Get the abstract and interface classes
 require_once("absendmail_class_inc.php");
 require_once("ifsendmail_class_inc.php");
