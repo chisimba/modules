eb52a73b611519db98558f81249ca173921d4af0
rwhitney <unknown@unknown.com>
2007-01-30 16:03:32 +0000

    Removing klorserver

--- klorserver/templates/content/filedownload_tpl.php
+++
@@ -1,3 +0,0 @@
-<?
-// This is a place-holder file, as the real logic has to happen on the page file to avoid crashes
-?>
--- klorserver/templates/content/main_tpl.php
+++
@@ -1,4 +0,0 @@
-<?
-header("Content-Type: text/xml; charset=utf-8"); 
-print_r($myvar); 
-?>
