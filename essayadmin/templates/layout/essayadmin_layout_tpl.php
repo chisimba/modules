23240c404e5b9d5e58bffafadbae2eba9f94f9de
abdurahim <unknown@unknown.com>
2007-02-01 09:16:14 +0000

    Added layout template

---
+++ essayadmin/templates/layout/essayadmin_layout_tpl.php
@@ -0,0 +1,36 @@
+<?php
+/*
+* Layout template for essay management.
+* @package essayadmin
+*/
+
+/**
+* @param string $leftNav The left panel on the page containing user information
+*/
+ 
+$leftMenu=& $this->newObject('sidemenu','toolbar');
+$objLayer=$this->objLayer;
+
+$objHead=$this->newObject('htmlheading','htmlelements');
+$cssLayout =& $this->newObject('csslayout', 'htmlelements');
+
+$objHead->str=$heading;
+$objHead->type=1;
+$main = $objHead->show();
+/*
+if(isset($body)){
+    $main .= $body;
+}*/
+$objLayer->str = $this->getContent();
+
+$main.=$objLayer->show();
+/*
+if(isset($foot)){
+    $main.=$foot;
+}*/
+
+$cssLayout->setLeftColumnContent($leftMenu->menuContext());
+$cssLayout->setMiddleColumnContent($main);
+
+echo $cssLayout->show();
+?>
