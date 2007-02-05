35b3991b2ed648450e6d402ba13bf3426f254e90
Derek Keats <derek@dkeats.com>
2007-02-05 15:53:28 +0000

    Made it a more generic demo

--- simplemaptime/templates/content/demomashup_tpl.php
+++ simplemaptime/templates/content/demomashup_tpl.php
@@ -6,15 +6,22 @@
 // Create the configuration object
 $objRsConfig = $this->getObject('altconfig', 'config');
 $moduleBase =  "http://" . $_SERVER['SERVER_NAME'] . $objRsConfig->getItem('MODULE_URI');
-$tlUrl = urlencode("http://localhost/chisimba/index.php?module=timeline&mode=plain");
+$tlBase = $this->Uri(array("mode" => "plain"), "timeline");
+$mapBase = $this->Uri(array("mode" => "plain"), "simplemap");
+$tlUrl = htmlentities("http://localhost/chisimba/index.php?module=timeline&mode=plain");
 ?>
 <div id="threecolumn">
 	<div id="wrapper"> 
 		<div id="content"> 
 			<div id="contentcontent">
-			<iframe id="mytimelines" name="mytimelines" src="http://localhost/chisimba/index.php?module=timeline&amp;mode=plain" width="100%" height="230"></iframe>
+			<iframe id="mytimelines" name="mytimelines" 
+			  src="http://localhost/chisimba/index.php?module=timeline&amp;mode=plain" width="100%" 
+			  height="350">
+			</iframe>
 			<br /><br />
-			<iframe  id="mymap" name="mymap" src="http://localhost/chisimba/index.php?module=simplemap&amp;mode=plain&amp;width=1000" width="100%" height="620"></iframe>
+			<iframe  id="mymap" name="mymap" 
+			  src="http://localhost/chisimba/index.php?module=simplemap&amp;mode=plain&amp;width=1000" 
+			  width="100%" height="620"></iframe>
 			</div>
 		</div>
 	</div>
@@ -22,17 +29,17 @@ $tlUrl = urlencode("http://localhost/chisimba/index.php?module=timeline&mode=pla
 		<div id="leftcontent">
 			<h3>Key times</h3>
 			<ul>
-				<li><a href="http://localhost/chisimba/index.php?module=timeline&amp;mode=plain" target="mytimelines">Restart</a></li>
-				<li><a href="http://localhost/chisimba/index.php?module=timeline&amp;mode=plain&amp;focusDate=1920" target="mytimelines">1920</a></li>
-				<li><a href="http://localhost/chisimba/index.php?module=timeline&amp;mode=plain&amp;focusDate=1930" target="mytimelines">1930</a></li>
-				<li><a href="http://localhost/chisimba/index.php?module=timeline&amp;mode=plain&amp;focusDate=1940" target="mytimelines">1940</a></li>
-				<li><a href="http://localhost/chisimba/index.php?module=timeline&amp;mode=plain&amp;focusDate=1950" target="mytimelines">1950</a></li>
-				<li><a href="http://localhost/chisimba/index.php?module=timeline&amp;mode=plain&amp;focusDate=1960" target="mytimelines">1960</a></li>
-				<li><a href="http://localhost/chisimba/index.php?module=timeline&amp;mode=plain&amp;focusDate=1970" target="mytimelines">1970</a></li>
-				<li><a href="http://localhost/chisimba/index.php?module=timeline&amp;mode=plain&amp;focusDate=1980" target="mytimelines">1980</a></li>
-				<li><a href="http://localhost/chisimba/index.php?module=timeline&amp;mode=plain&amp;focusDate=1990" target="mytimelines">1990</a></li>
-				<li><a href="http://localhost/chisimba/index.php?module=timeline&amp;mode=plain&amp;focusDate=2000" target="mytimelines">2000</a></li>
-				<li><a href="http://localhost/chisimba/index.php?module=timeline&amp;mode=plain&amp;focusDate=2010" target="mytimelines">World Cup</a></li>
+				<li><a href="<?php echo $tlBase; ?>" target="mytimelines">Restart</a></li>
+				<li><a href="<?php echo $tlBase; ?>&amp;focusDate=1920" target="mytimelines">1920</a></li>
+				<li><a href="<?php echo $tlBase; ?>&amp;focusDate=1930" target="mytimelines">1930</a></li>
+				<li><a href="<?php echo $tlBase; ?>&amp;focusDate=1940" target="mytimelines">1940</a></li>
+				<li><a href="<?php echo $tlBase; ?>&amp;focusDate=1950" target="mytimelines">1950</a></li>
+				<li><a href="<?php echo $tlBase; ?>&amp;focusDate=1960" target="mytimelines">1960</a></li>
+				<li><a href="<?php echo $tlBase; ?>&amp;focusDate=1970" target="mytimelines">1970</a></li>
+				<li><a href="<?php echo $tlBase; ?>&amp;focusDate=1980" target="mytimelines">1980</a></li>
+				<li><a href="<?php echo $tlBase; ?>&amp;focusDate=1990" target="mytimelines">1990</a></li>
+				<li><a href="<?php echo $tlBase; ?>&amp;focusDate=2000" target="mytimelines">2000</a></li>
+				<li><a href="<?php echo $tlBase; ?>&amp;focusDate=2010" target="mytimelines">World Cup</a></li>
 			</ul>				
 		</div>
 	</div>
@@ -40,10 +47,10 @@ $tlUrl = urlencode("http://localhost/chisimba/index.php?module=timeline&mode=pla
 		<div id="rightcontent">
 			<h3>Key places</h3>
 			<ul>
-				<li><a href="http://localhost/chisimba/index.php?module=simplemap&amp;gLat=-31.333576&amp;gLong=28.979391&amp;mode=plain&amp;width=1000" target="mymap">Qunu, South Africa</a></li>
-				<li><a href="http://localhost/chisimba/index.php?module=simplemap&amp;gLat=-25.732912&amp;gLong=28.187903&amp;mode=plain&amp;width=1000" target="mymap">Pretoria, South Africa</a></li>
-				<li><a href="http://localhost/chisimba/index.php?module=simplemap&amp;gLat=59.902214&amp;gLong=10.742189&amp;mode=plain&amp;width=1000" target="mymap">Oslo, Norway</a></li>
-				<li><a href="http://localhost/chisimba/index.php?module=simplemap&amp;gLat=43.641709&amp;gLong=-79.391686&amp;mode=plain&amp;width=1000" target="mymap">Toronto, Canada</a></li>
+				<li><a href="<?php echo $mapBase; ?>&amp;gLat=-31.333576&amp;gLong=28.979391&amp;width=1000" target="mymap">Qunu, South Africa</a></li>
+				<li><a href="<?php echo $mapBase; ?>&amp;gLat=-25.732912&amp;gLong=28.187903&amp;width=1000" target="mymap">Pretoria, South Africa</a></li>
+				<li><a href="<?php echo $mapBase; ?>&amp;gLat=59.902214&amp;gLong=10.742189&amp;width=1000" target="mymap">Oslo, Norway</a></li>
+				<li><a href="<?php echo $mapBase; ?>&amp;gLat=43.641709&amp;gLong=-79.391686&amp;width=1000" target="mymap">Toronto, Canada</a></li>
 			</ul>
 		</div>
 	</div>
