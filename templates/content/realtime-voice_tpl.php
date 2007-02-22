6e934bfd96b7f38b86354e4ba824c05424cee775
adrian <unknown@unknown.com>
2007-02-22 15:40:58 +0000

    fixed parameters to use new code layout

--- templates/content/realtime-voice_tpl.php
+++ templates/content/realtime-voice_tpl.php
@@ -1,9 +1,9 @@
 <?php
    echo '      <applet code="avoir.realtime.voice.audio.applet.VoiceApplet.class" width="400" height="200">';
-   echo '         <param name="archive" value="voice-client.jar,jmf.jar,swing-worker.jar"> ';
+   echo '         <param name="archive" value="'.$voiceUrl.'/voice-client.jar,'.$voiceUrl.'/swing-worker.jar"> ';
    echo '         <param name="userName" value="' . $userName . '">';
    echo '         <param name="localdirectory" value="/tmp/">';
-   echo '         <param name="realtimeModuleBase" value="'.$appUrl.'">';
+   echo '         <param name="realtimeModuleBase" value="'.$realtimeUrl.'">';
    echo "\n         </param></param></param></param>\n";
    echo "      </applet> ";
 ?>
