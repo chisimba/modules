fb2f866e0c2c72e78e5032f24ef0eaccd5cad778
irshaad <unknown@unknown.com>
2007-02-23 07:37:17 +0000

    Added for rss feeds

---
+++ blog/templates/content/showfeed_tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+$this->setVar('pageSuppressToolbar', TRUE);
+$this->setVar('pageSuppressContainer', TRUE);
+$this->setVar('pageSuppressSearch', TRUE);
+$this->setVar('footerStr', '');
+
+echo $feed;
+
+?>
