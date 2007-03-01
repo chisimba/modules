ec02d095e58e006b54e0e3a8e5d7a93aa3ea412c
adrian <unknown@unknown.com>
2007-03-01 13:38:48 +0000

    fixed end tags for param elements

--- realtime/templates/content/realtime-voice_tpl.php
+++ realtime/templates/content/realtime-voice_tpl.php
@@ -1,9 +1,8 @@
 <?php
-   echo '      <applet code="avoir.realtime.voice.VoiceApplet.class" width="400" height="200">';
-   echo '         <param name="archive" value="'.$voiceUrl.'/voice-client.jar,'.$voiceUrl.'/swing-worker.jar"> ';
-   echo '         <param name="userName" value="' . $userName . '">';
-   echo '         <param name="localdirectory" value="/tmp/">';
-   echo '         <param name="realtimeModuleBase" value="'.$realtimeUrl.'">';
-   echo "\n         </param></param></param></param>\n";
+   echo '      <applet code="avoir.realtime.voice.VoiceApplet.class" width="500" height="200">';
+   echo '         <param name="archive" value="'.$voiceUrl.'/voice-client.jar,'.$voiceUrl.'/swing-worker.jar"/> ';
+   echo '         <param name="userName" value="' . $userName . '"/>';
+   echo '         <param name="localdirectory" value="/tmp/"/>';
+   echo '         <param name="realtimeModuleBase" value="'.$realtimeUrl.'"/>';
    echo "      </applet> ";
 ?>
