297b29250597472c8398b387af9c297293726454
adrian <unknown@unknown.com>
2007-03-02 15:24:09 +0000

    added whiteboard template

---
+++ templates/content/realtime-whiteboard_tpl.php
@@ -0,0 +1,8 @@
+<?php
+   echo '<applet width="800" height="600" code="avoir.realtime.whiteboard.client.ClientApplet.class">';
+   echo '    <param name="archive" value="'.$whiteboardUrl.'/whiteboard-client.jar"/> ';
+   echo '    <param name="userName" value="' . $userName . '"/>';
+   echo '    <param name="userLevel" value="' . $userLevel . '"/>';
+   echo '    <param name="port" value="1981"/>';
+   echo "</applet> ";
+?>
