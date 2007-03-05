c246ce99f323afcbf5b37223990622c214c2abbf
adrian <unknown@unknown.com>
2007-03-05 15:32:21 +0000

    removed dependency on swing worker

--- templates/content/realtime-voice_tpl.php
+++ templates/content/realtime-voice_tpl.php
@@ -1,6 +1,6 @@
 <?php
    echo '      <applet code="avoir.realtime.voice.VoiceApplet.class" width="500" height="200">';
-   echo '         <param name="archive" value="'.$voiceUrl.'/voice-client.jar,'.$voiceUrl.'/swing-worker.jar"/> ';
+   echo '         <param name="archive" value="'.$voiceUrl.'/voice-client.jar"/> ';
    echo '         <param name="userName" value="' . $userName . '"/>';
    echo '         <param name="localdirectory" value="/tmp/"/>';
    echo '         <param name="realtimeModuleBase" value="'.$realtimeUrl.'"/>';
