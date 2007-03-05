1c3d55adbaa0a81615c5df9bd34a1cacfd68156b
adrian <unknown@unknown.com>
2007-03-05 15:30:26 +0000

    added GPL reformatted

--- realtime/controller.php
+++ realtime/controller.php
@@ -5,12 +5,29 @@ if (!$GLOBALS['kewl_entry_point_run'])
 	die("You cannot view this page directly");
 }
 // end security check
-
+/**
+ *  $Id$
+ *
+ *  Copyright (C) GNU/GPL AVOIR 2007
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 /**
 * Realtime Controller
 * This class controls all functionality to run the realtime module.
 * @author Jessie
-* @copyright (c) 2006 University of the Western Cape
 * @package realtime
 * @version 1
 */
@@ -45,7 +62,7 @@ class realtime extends controller
     }
     
     /**
-     * Method to assign the current user a level based on their highest group
+     * Method to determine the user level
      *
      * @param string $userName 
      * @return string $userLevel
@@ -62,15 +79,14 @@ class realtime extends controller
 
             // Check if lecturer first, then student
             if ($this->objGroups->isGroupMember($uKey, $this->getGroupID("Lecturers")))
-	    {
-		return "lecturer";
+	        {
+        		return "lecturer";
             } else if ($this->objGroups->isGroupMember($uKey, $this->getGroupID("Students") ))
-	    {
-		return "student";
-	    }
+	        {
+        		return "student";
+	        }
         }
-
-	// if user is in no group, guest -- the least access
+		// if user is in no group, guest -- the least access
         return "guest";
     }
 
