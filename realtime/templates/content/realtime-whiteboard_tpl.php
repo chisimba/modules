5a2519d1420d85f70d07e1c894c326c3506810e7
adrian <unknown@unknown.com>
2007-03-05 15:32:07 +0000

    moved ClientApplet to WhiteboardApplet

--- realtime/templates/content/realtime-whiteboard_tpl.php
+++ realtime/templates/content/realtime-whiteboard_tpl.php
@@ -1,5 +1,5 @@
 <?php
-   echo '<applet width="800" height="600" code="avoir.realtime.whiteboard.client.ClientApplet.class">';
+   echo '<applet width="800" height="600" code="avoir.realtime.whiteboard.client.WhiteboardApplet.class">';
    echo '    <param name="archive" value="'.$whiteboardUrl.'/whiteboard-client.jar"/> ';
    echo '    <param name="userName" value="' . $userName . '"/>';
    echo '    <param name="userLevel" value="' . $userLevel . '"/>';
