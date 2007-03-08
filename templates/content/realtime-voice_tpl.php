4a8a825f800c75dcd80771a1d65b0d9ec5a81b87
adrian <unknown@unknown.com>
2007-03-08 14:01:51 +0000

    cleaning up layout on server

--- templates/content/realtime-voice_tpl.php
+++ templates/content/realtime-voice_tpl.php
@@ -1,9 +1,9 @@
 <?php
-   echo '      <applet code="avoir.realtime.voice.VoiceApplet.class" width="500" height="100">';
-   echo '         <param name="archive" value="'.$voiceUrl.'/voice-client.jar"/> ';
-   echo '         <param name="userName" value="' . $userName . '"/>';
-   echo '         <param name="userLevel" value="' . $userLevel . '"/>';
-   echo '         <param name="localdirectory" value="/tmp/"/>';
-   echo '         <param name="realtimeModuleBase" value="'.$realtimeUrl.'"/>';
-   echo "      </applet> ";
+echo '<applet code="avoir.realtime.voice.VoiceApplet.class" width="500" height="100">';
+echo '	<param name="archive" value="'.$this->voiceURL.'/voice-client.jar"/> ';
+echo '	<param name="userName" value="' . $this->userName . '"/>';
+echo '  <param name="userLevel" value="' . $this->userLevel . '"/>';
+echo '  <param name="localdirectory" value="/tmp/"/>';
+echo '  <param name="voiceURL" value="'.$this->voiceURL.'"/>';
+echo "</applet> ";
 ?>
--- templates/content/realtime-whiteboard_tpl.php
+++ templates/content/realtime-whiteboard_tpl.php
@@ -1,8 +1,8 @@
 <?php
-   echo '<applet width="800" height="600" code="avoir.realtime.whiteboard.client.WhiteboardApplet.class">';
-   echo '    <param name="archive" value="'.$whiteboardUrl.'/whiteboard-client.jar"/> ';
-   echo '    <param name="userName" value="' . $userName . '"/>';
-   echo '    <param name="userLevel" value="' . $userLevel . '"/>';
-   echo '    <param name="port" value="1981"/>';
-   echo "</applet> ";
+echo '<applet width="800" height="600" code="avoir.realtime.whiteboard.client.WhiteboardApplet.class">';
+echo '    <param name="archive" value="'.$this->whiteboardURL.'/whiteboard-client.jar"/> ';
+echo '    <param name="userName" value="' . $this->userName . '"/>';
+echo '    <param name="userLevel" value="' . $this->userLevel . '"/>';
+echo '    <param name="port" value="1981"/>';
+echo "</applet> ";
 ?>
