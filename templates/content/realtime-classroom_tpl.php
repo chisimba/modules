547a63442ec323718b4bb8e3d46c86b7f34f3005
kevinc <unknown@unknown.com>
2007-03-08 12:30:35 +0000

    New realtime tools module format

--- templates/content/realtime-classroom_tpl.php
+++ templates/content/realtime-classroom_tpl.php
@@ -1,10 +1,34 @@
 <?php
+$this->objLanguage = $this->getObject('language', 'language');
+$this->objFeaturebox = $this->newObject('featurebox', 'navigation');
 
-$this->setLayoutTemplate('layout_tpl.php');
-   echo '<applet width="100" height="600" code="avoir.realtime.whiteboard.client.WhiteboardApplet.class">';
-   echo '    <param name="archive" value="'.$whiteboardUrl.'/whiteboard-client.jar"/> ';
-   echo '    <param name="userName" value="' . $userName . '"/>';
-   echo '    <param name="userLevel" value="' . $userLevel . '"/>';
-   echo '    <param name="port" value="1981"/>';
-   echo "</applet> ";
+$whiteboard = '<applet width="100" height="600" code="avoir.realtime.whiteboard.client.WhiteboardApplet.class">';
+$whiteboard .= '    <param name="archive" value="'.$this->whiteboardURL.'/whiteboard-client.jar"/> ';
+$whiteboard .= '    <param name="userName" value="' . $this->userName . '"/>';
+$whiteboard .= '    <param name="userLevel" value="' . $this->userLevel . '"/>';
+$whiteboard .= '    <param name="port" value="1981"/>';
+$whiteboard .= "</applet> ";
+
+$voiceTitle = $this->objLanguage->languageText('mod_realtime_voice', 'realtime');
+$voice = '      <applet code="avoir.realtime.voice.VoiceApplet.class" width="500" height="100">';
+$voice .= '         <param name="archive" value="'.$this->voiceURL.'/voice-client.jar"/> ';
+$voice .= '         <param name="userName" value="' . $this->userName . '"/>';
+$voice .= '         <param name="userLevel" value="' . $this->userLevel . '"/>';
+$voice .= '         <param name="localdirectory" value="/tmp/"/>';
+$voice .= '         <param name="realtimeModuleBase" value="'.'TODO'.'"/>';
+$voice .= "      </applet> ";
+
+$voiceBox = $this->objFeaturebox->show($voiceTitle, $voice);
+
+$cssLayout=&$this->newObject('csslayout','htmlelements');
+$cssLayout->setNumColumns(3);
+
+$objBlocks = $this->newObject('blocks', 'blocks');
+$chatBlock = $objBlocks->showBlock('contextchat', 'messaging');
+
+$cssLayout->setLeftColumnContent($chatBlock);
+$cssLayout->setMiddleColumnContent($whiteboard);
+$cssLayout->setRightColumnContent($voiceBox);
+
+echo $cssLayout->show();
 ?>
--- templates/layout/layout_tpl.php
+++ templates/layout/layout_tpl.php
@@ -7,17 +7,4 @@
 * Default layout for the messaging module
 */
 
-$cssLayout=&$this->newObject('csslayout','htmlelements');
-$cssLayout->setNumColumns(3);
-
-$objBlocks = $this->newObject('blocks', 'blocks');
-$chatBlock = $objBlocks->showBlock('contextchat', 'messaging');
-$voiceBlock = $objBlocks->showBlock('voice', 'realtime');
-
-
-$cssLayout->setLeftColumnContent($chatBlock);
-$cssLayout->setMiddleColumnContent($this->getContent());
-$cssLayout->setRightColumnContent($voice);
-
-echo $cssLayout->show();
 ?>
