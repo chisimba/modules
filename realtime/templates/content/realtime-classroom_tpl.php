90c2121f8edfd6f893071aca07977d68a2c18b50
adrian <unknown@unknown.com>
2007-03-08 14:01:51 +0000

    cleaning up layout on server

--- realtime/templates/content/realtime-classroom_tpl.php
+++ realtime/templates/content/realtime-classroom_tpl.php
@@ -2,7 +2,7 @@
 $this->objLanguage = $this->getObject('language', 'language');
 $this->objFeaturebox = $this->newObject('featurebox', 'navigation');
 
-$whiteboard = '<applet width="100" height="600" code="avoir.realtime.whiteboard.client.WhiteboardApplet.class">';
+$whiteboard = '<applet width="400" height="600" code="avoir.realtime.whiteboard.client.WhiteboardApplet.class">';
 $whiteboard .= '    <param name="archive" value="'.$this->whiteboardURL.'/whiteboard-client.jar"/> ';
 $whiteboard .= '    <param name="userName" value="' . $this->userName . '"/>';
 $whiteboard .= '    <param name="userLevel" value="' . $this->userLevel . '"/>';
@@ -10,13 +10,13 @@ $whiteboard .= '    <param name="port" value="1981"/>';
 $whiteboard .= "</applet> ";
 
 $voiceTitle = $this->objLanguage->languageText('mod_realtime_voice', 'realtime');
-$voice = '      <applet code="avoir.realtime.voice.VoiceApplet.class" width="500" height="100">';
-$voice .= '         <param name="archive" value="'.$this->voiceURL.'/voice-client.jar"/> ';
-$voice .= '         <param name="userName" value="' . $this->userName . '"/>';
-$voice .= '         <param name="userLevel" value="' . $this->userLevel . '"/>';
-$voice .= '         <param name="localdirectory" value="/tmp/"/>';
-$voice .= '         <param name="realtimeModuleBase" value="'.'TODO'.'"/>';
-$voice .= "      </applet> ";
+$voice = '<applet code="avoir.realtime.voice.VoiceApplet.class" width="500" height="100">';
+$voice .= '	<param name="archive" value="'.$this->voiceURL.'/voice-client.jar"/> ';
+$voice .= ' <param name="userName" value="' . $this->userName . '"/>';
+$voice .= ' <param name="userLevel" value="' . $this->userLevel . '"/>';
+$voice .= ' <param name="localdirectory" value="/tmp/"/>';
+$voice .= ' <param name="voiceURL" value="'.$this->voiceURL.'"/>';
+$voice .= "</applet> ";
 
 $voiceBox = $this->objFeaturebox->show($voiceTitle, $voice);
 
