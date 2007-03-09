e50936e12546bd4756a25f8f9f8a4b0b4e1cc928
adrian <unknown@unknown.com>
2007-03-09 12:59:10 +0000

    added explanatory text

--- realtime/templates/content/realtime_tpl.php
+++ realtime/templates/content/realtime_tpl.php
@@ -1,6 +1,16 @@
 <?php
-   echo '<p><a href="index.php?module=realtime&amp;action=voice">Voice</a></p>';
-   echo '<p><a href="index.php?module=realtime&amp;action=whiteboard">Whiteboard</a></p>';
-   echo '<p><a href="index.php?module=realtime&amp;action=classroom">Classroom</a></p>';
-   echo "\n";
 ?>
+<br/>
+<p>
+Welcome to the realtime tools. Select a link below to load and run a specific tool:
+</p>
+<ul>
+	<li><a href="index.php?module=realtime&amp;action=jmfinstall">JMF Installer</a> - install the JMF on your machine 
+	(currently supports Linux only).</li>
+	<li><a href="index.php?module=realtime&amp;action=voice">Voice</a> - the realtime voice applet (requires JMF).</li>
+	<li><a href="index.php?module=realtime&amp;action=whiteboard">Whiteboard</a> - the realtime whiteboard applet (requires a 
+	running whiteboard server).</li>
+	<li><a href="index.php?module=realtime&amp;action=classroom">Classroom</a> - the realtime classroom consisting of a mashup of 
+	the voice and classroom applets as well as the messaging module. You must be in a context for the 
+	messaging to work. The whiteboard server must also be running and JMF must be installed.</li>
+</ul>
