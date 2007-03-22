142557a81ddf05a4a47f0bc56d37be4225daf9b3
mohamed <unknown@unknown.com>
2007-03-22 14:04:38 +0000

    *** empty log message ***

--- classes/dbrealtime_class_inc.php
+++ classes/dbrealtime_class_inc.php
@@ -1,179 +1,227 @@
 <?php
+
 /*
  * Created on 15 Mar 2007
  * @author Mohamed Yusuf
  */
- 
- class dbrealtime extends dbTable
- {
- 	
- 	
- 	/**
- 	 * initializing
- 	 */
- 	public function init()
- 	{
-		parent::init('tbl_realtime_conversation');
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run'])
+{
+	die("You cannot view this page directly");
+}
+
+class dbrealtime extends dbTable
+{
+
+	public function init()
+	{
+		parent :: init('tbl_realtime_conversation');
 		define("SUCCESS", "SUCCESS");
 		define("FAILURE", "FAILURE");
- 	}
- 	
- 	/**
- 	 * request toke
- 	 * return true/false
- 	 */
- 	public function requestToken($userId, $userLevel, $contextCode)
- 	{
-		$query = "select * from tbl_realtime_conversation";
-		$res = $this->getArray($query);
-		/**
-		 *  check if th
-		 */
-		if(empty($res)){
-			/**
-			 * no open conversation and student can't start one
-			 */
-			if(eregi("student",$userLevel)){
-				return FAILURE;
-			}elseif(eregi("lecturer", $userLevel)){
-				/**
-				 * no open session yet
-				 */
-				
-				 return SUCCESS;
-			}else{
-				return FAILURE;
-			}
-		}else{
-			 
-			 $ssql = "select * from tbl_realtime_voicequeue";
-			 $ress = $this->getArray($ssql);
-			 
-			if(empty($ress)){
-				$sql = "select id from tbl_realtime_conversation";
-				$res = $this->getArray($sql);
-				foreach($res as $result){
-					$lastId = $result['id'];
-				}
-				$assignToToken = array();
-				$assignToToken['conversationid'] = $lastId;
-				$assignToToken['userid'] = $userId;
-				$assignToToken['hastoken'] = 1;
-				$this->insert($assignToToken, 'tbl_realtime_voicequeue');				
-				return SUCCESS;
-			}else{
-				/**
-				 * check who is requesting token.
-				 * if the requester is lecturer,
-				 * give the token. otherwise return token not available
-				 */
-				 if(eregi("lecturer", $userLevel)){
-				 	//delete details of who ever has the token.
-				 	
-				 	$sql = "delete from tbl_realtime_voicequeue";
-				 	$this->query($sql);
-				 	
-				 	//insert lecturer details//insert lecturer details
-					$sql = "select id from tbl_realtime_conversation";
-					$res = $this->getArray($sql);
-					foreach($res as $result){
-						$lastId = $result['id'];
-					}				 	
-				 	
-				 	$lastId = $this->getLastInsertId();
-					$assignToken = array();
-					$assignToken['conversationid'] = $lastId;
-					$assignToken['userid'] = $userId;
-					$assignToken['hastoken'] = 1;
-					$this->insert($assignToken, 'tbl_realtime_voicequeue');				 	
-				 	return SUCCESS;
-				 }else{
-				 	return FAILURE;
-				 }
-			}
-		}
- 	}
- 	
- 	/**
- 	 * release token
- 	 * return true, token available.
- 	 */
- 	public function releaseToken()
- 	{
- 		$sql = "delete from tbl_realtime_voicequeue";
-		$this->query($sql);
-		return SUCCESS;
- 	}
- 	
- 	/**
- 	 *  end conversation.
- 	 *	return false
- 	 */
- 	public function stopConversation($contextcode){
-		$this->removeConversation($contextcode);
- 		return SUCCESS;
- 	}
- 	
- 	public function startConversation($userid, $userlevel, $contextcode)
- 	{
- 		$sql = "select contextcode from tbl_realtime_conversation where contextcode=\"$contextcode\"";
- 		$result = $this->getArray($sql);
- 		
- 		if(empty($result)){
-			$this->addConversation($userid, $userlevel, $contextcode);
-			return SUCCESS;	
- 		}else{
- 			foreach ($result as $context){
- 				$contextC = $context['contextcode'];
- 				$this->removeConversation($contextC);
- 			}
- 			$this->addConversation($userid, $userlevel, $contextcode);
- 			return SUCCESS;
- 		}		
- 	}
- 	
- 	private function removeConversation($contextCode)
- 	{
- 		$sql = "select conversationid from tbl_realtime_conversation where contextcode=\"$contextCode\"";
- 		$result = $this->getArray($sql);
- 		if(empty($result)){
- 			//return FAILURE;
- 		}else{
- 			foreach ($result as $id){
- 				$convId = $id['conversationid'];
- 			}
- 			//delete entries from voicequeue table
- 			$sql = "delete from tbl_realtime_voicequeue where conversationid=\"$convId\"";
- 			$this->getArray($sql);
- 			
- 			//delete entries from conversation table
- 			$sql = "delete from tbl_realtime_conversation where id=\"$convId\"";
- 			$this->getArray($sql); 	
- 			//return SUCCESS;		
- 		}
- 	}
- 	
- 	private function addConversation($userid, $userlevel, $contextcode)
- 	{
- 		$addRow = array();
-		$starttime = strftime('%Y-%m-%d %H:%M:%S', mktime());
-		$addRow['userid'] = $userid;
-		$addRow['contextcode'] = $contextcode;
-		$addRow['starttime'] = $starttime;
-		$this->insert($addRow, 'tbl_realtime_conversation');			
-		 
-	 	//lecturer starts one.
-		$sql = "select id from tbl_realtime_conversation";
-		$res = $this->getArray($sql);
-		foreach($res as $result){
-			$lastId = $result['id'];
-		}
+	}
+
+	/**
+	 * @author Mohamed Yusuf
+	 * @param string userId user identification number
+	 * @param string userLevel group that user belongs to
+	 * @param string contextCode Course code
+	 * @return String SUCCESS/FAILURE
+	 */
+	public function requestToken($userId, $userLevel, $contextCode)
+	{
 		
-		$queue = array();
+		$userLevel = strtolower($userLevel);
+		$convId = $this->getConversationId($contextCode);
+		$sql = "select hastoken from tbl_realtime_voicequeue where conversationid=\"$convId\"";
+		$result = $this->getArray($sql);
+		if (eregi("student",$userLevel))
+		{
+			if(empty($result)){
+				$this->addQueue($userId, $userLevel, $contextCode);
+				return SUCCESS;				
+			} else { 
+				foreach ($result as $res)
+				{
+					$hasToken = $res['hastoken'];
+				}
+				if ("1" == $hasToken)
+				{
+					return FAILURE;
+				} else {
+					//assumed that token had been released
+					$this->addQueue($userId, $userLevel, $contextCode);
+					return SUCCESS;
+				}
+			}
+		}
+		elseif (eregi("lecturer", $userLevel))
+		{
+			//explicitly realease token
+			$this->releaseToken($userId, $userLevel, $contextCode);
+			$this->addQueue($userId, $userLevel, $contextCode);
+			return SUCCESS;
+		} else
+		{
+			//what to do with administrators and guests.
+			return FAILURE;
+		}
+	}
+
+	/**
+	* @author Mohamed Yusuf
+	* @param string contextCode Course code
+	* @return String SUCCESS/FAILURE
+	*/
+	public function releaseToken($userId, $userLevel, $contextCode)
+	{
+		$convId = $this->getConversationId($contextCode);
+		$sql = "delete from tbl_realtime_voicequeue where conversationid=\"$convId\"";
+		$this->getArray($sql);
+		return SUCCESS;
+	}
+
+	/**
+	 * @author Mohamed Yusuf
+	 * @param string contextCode Course code
+	 * @return String SUCCESS/FAILURE
+	 */
+	public function stopConversation($userId, $userLevel, $contextCode)
+	{
+		$sql = "select userid from tbl_realtime_conversation where userid=\"$userId\"";
+		$result = $this->getArray($sql);
+		if (!empty ($result))
+		{
+			$this->removeConversation($contextCode);
+			return SUCCESS;
+		} else
+		{
+			return FAILURE;
+		}
+	}
+
+	/**
+	* @author Mohamed Yusuf
+	* @param string userId user identification number
+	* @param string userLevel group that user belongs to
+	* @param string contextCode Course code
+	* @return String SUCCESS/FAILURE
+	*/
+	public function startConversation($userId, $userLevel, $contextCode)
+	{
+		$sql = "select contextcode from tbl_realtime_conversation where contextcode=\"$contextCode\"";
+		$result = $this->getArray($sql);
+
+		if (empty($result))
+		{
+			$this->createConversation($userId, $userLevel, $contextCode);
+			$this->addQueue($userId, $userLevel, $contextCode);
+			return SUCCESS;
+		} else
+		{
+			$this->removeConversation($contextCode);
+			$this->createConversation($userId, $userLevel, $contextCode);
+			$this->addQueue($userId, $userLevel, $contextCode);
+			return SUCCESS;
+		}
+	}
+
+	/**
+	* @author Mohamed Yusuf
+	* @param string contextCode Course code
+	* @return String SUCCESS/FAILURE
+	*/
+	private function removeConversation($contextCode)
+	{
+		$convId = $this->getConversationId($contextCode);
+		//delete entries from voicequeue table
+		$sql = "delete from tbl_realtime_voicequeue where conversationid=\"$convId\"";
+		$this->getArray($sql);
+
+		//delete entries from conversation table
+		$sql = "delete from tbl_realtime_conversation where id=\"$convId\"";
+		$this->getArray($sql);
+		//return SUCCESS;		
+	}
+
+	/**
+	* @author Mohamed Yusuf
+	* @param string userId user identification number
+	* @param string userLevel group that user belongs to
+	* @param string contextCode Course code
+	* @return String SUCCESS/FAILURE
+	*/
+	private function addQueue($userId, $userLevel, $contextCode)
+	{
+		$lastId = $this->getConversationId($contextCode);
+		$queue = array ();
 		$queue['conversationid'] = $lastId;
-		$queue['userid'] = $userid;
+		$queue['userid'] = $userId;
 		$queue['hastoken'] = 1;
-		$this->insert($queue, 'tbl_realtime_voicequeue'); 		
- 	}
- }
+		$this->insert($queue, 'tbl_realtime_voicequeue');
+	}
+
+	/**
+	* @author Mohamed Yusuf
+	* @param string contextCode Course code
+	* @return String id
+	*/
+	private function getConversationId($contextCode)
+	{
+		$sql = "select id  from tbl_realtime_conversation where contextcode=\"$contextCode\"";
+		$result = $this->getArray($sql);
+		foreach ($result as $res)
+		{
+			$conversationId = $res['id'];
+		}
+		return $conversationId;
+	}
+
+	/**
+	* @author Mohamed Yusuf
+	* @param string userId user identification number
+	* @param string userLevel group that user belongs to
+	* @param string contextCode Course code
+	* @return String SUCCESS/FAILURE
+	*/
+	private function createConversation($userId, $userLevel, $contextCode)
+	{
+		$addRow = array ();
+		$starttime = strftime('%Y-%m-%d %H:%M:%S', mktime());
+		$addRow['userid'] = $userId;
+		$addRow['contextcode'] = $contextCode;
+		$addRow['starttime'] = $starttime;
+		$this->insert($addRow, 'tbl_realtime_conversation');
+	}
+
+	/**
+	* @author Mohamed Yusuf
+	* @param string userId user identification number
+	* @param string userLevel group that user belongs to
+	* @param string contextCode Course code
+	* @return String SUCCESS/FAILURE
+	*/
+	public function joinConversation($userId, $userLevel, $contextCode)
+	{
+		$convId = $this->getConversationId($contextCode);
+		if (empty ($convId))
+		{
+			return FAILURE;
+		} else
+		{
+			return SUCCESS;
+		}
+	}
+
+	/**
+	* @author Mohamed Yusuf
+	* @param string userId user identification number
+	* @param string userLevel group that user belongs to
+	* @param string contextCode Course code
+	* @return String SUCCESS/FAILURE
+	*/
+	public function leaveConversation($userId, $userLevel, $contextCode)
+	{
+		return SUCCESS;
+	}
+}
 ?>
