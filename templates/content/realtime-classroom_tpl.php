05b4c014b8fadd5c756bea33c5c7213c134d8eaf
adrian <unknown@unknown.com>
2007-03-26 08:43:18 +0000

    added file manager fixed formatting

--- templates/content/realtime-classroom_tpl.php
+++ templates/content/realtime-classroom_tpl.php
@@ -1,8 +1,44 @@
+<!-- START FILE MANAGER FILE CHOOSER CODE-->
+<script type="text/javascript">
+
+	function openWindow(theURL,winName,features) { 
+	  newwindow=window.open(theURL,winName,features);
+       if (window.focus) {
+       	newwindow.focus()
+	   }
+    } 
+
+    /*
+     * Function that is called by Java applet when user wants to select an image.
+     */
+    function openImageFileChooser() {
+		openWindow('<?php echo str_replace('amp;', '', $this->uri(array('action'=>'selectrealtimeimagewindow', 'name'=>'selectimage'), 'filemanager')); ?>','new','toolbar=no, menubar=no, width=600, height=400, resizable=yes, scrollbars=auto, toolbar=no top=200 screenY=200 left=300 screenY=300');
+    }
+
+	/*
+	 * Function that is called by file manager image chooser popup when a file has been selected.
+	 */    
+    function callFunctionFromParent()
+	{
+	    var imageURL = document.getElementById('hidden_selectimage').value;
+	    document.getElementById("whiteboardapplet").imageSelected(imageURL);
+	}
+</script>
+<div style="display:none;">
+<?php
+$objSelectFile = $this->getObject('selectrealtimeimage', 'filemanager');
+$objSelectFile->name = 'selectimage';
+$this->setVar('pageSuppressXML', TRUE);
+echo $objSelectFile->show();
+?>
+</div>
+<!-- END FILE MANAGER FILE CHOOSER CODE-->
+
 <?php
 $this->objLanguage = $this->getObject('language', 'language');
 $this->objFeaturebox = $this->newObject('featurebox', 'navigation');
 
-$whiteboard = '<applet width="600" height="600" code="avoir.realtime.whiteboard.client.WhiteboardApplet.class">';
+$whiteboard = '<applet id="whiteboardapplet" width="660" height="600" code="avoir.realtime.whiteboard.client.WhiteboardApplet.class">';
 $whiteboard .= '    <param name="archive" value="'.$this->whiteboardURL.'/whiteboard-client.jar"/> ';
 $whiteboard .= '    <param name="userName" value="' . $this->userName . '"/>';
 $whiteboard .= '    <param name="userLevel" value="' . $this->userLevel . '"/>';
@@ -10,12 +46,12 @@ $whiteboard .= '    <param name="port" value="1981"/>';
 $whiteboard .= "</applet> ";
 
 $voiceTitle = $this->objLanguage->languageText('mod_realtime_voice', 'realtime');
-$voice = '<applet code="avoir.realtime.voice.VoiceApplet.class" width="320" height="100">';
-$voice .= '	<param name="archive" value="'.$this->voiceURL.'/voice-client.jar"/> ';
+$voice = '<applet code="avoir.realtime.voice.VoiceApplet.class" width="300" height="120">';
+$voice .= '	<param name="archive" value="'.$this->voiceURL.'/voice-client.jar"/>';
 $voice .= ' <param name="userName" value="' . $this->userName . '"/>';
 $voice .= ' <param name="userLevel" value="' . $this->userLevel . '"/>';
-$voice .= ' <param name="localdirectory" value="/tmp/"/>';
 $voice .= ' <param name="voiceURL" value="'.$this->voiceURL.'"/>';
+$voice .= ' <param name="baseModuleURL" value="'.$this->baseModuleURL.'"/>';
 $voice .= "</applet> ";
 
 $voiceBox = $this->objFeaturebox->show($voiceTitle, $voice);
@@ -25,17 +61,13 @@ $chatBlock = $objBlocks->showBlock('contextchat', 'messaging');
 
 $objLayer = $this->newObject('layer', 'htmlelements');
 $objLayer->str = $voiceBox.$chatBlock;
-$objLayer->border = '; width: 27%; float: left';
+$objLayer->border = '; width: 320px; float: left';
 $layer1 = $objLayer->show();
 
 $objLayer->str = $whiteboard;
-$objLayer->border = '; width: 78%;';
+$objLayer->border = '; width: 660px; float: right';
 $objLayer->align = 'center';
 $layer2 = $objLayer->show();
 
-$objLayer->str = '';
-$objLayer->border = '; clear: both;';
-$layer3 = $objLayer->show();
-
-echo $layer1.$layer2.$layer3;
+echo $layer1.$layer2;
 ?>
