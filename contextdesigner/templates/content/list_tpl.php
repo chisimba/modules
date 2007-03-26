15244175905b48dcb2364890ee713b62425950b8
adrian <unknown@unknown.com>
2007-03-26 09:51:00 +0000

    fixed incorrect spelling of administer

--- contextdesigner/templates/content/list_tpl.php
+++ contextdesigner/templates/content/list_tpl.php
@@ -1,6 +1,6 @@
 <?php
 
-//shows the list of links that the context designer has and can be administored.
+//shows the list of links that the context designer has and can be administered.
 
-echo 'shows a list of links that can admined'
+echo 'shows a list of links that can administered'
 ?>
