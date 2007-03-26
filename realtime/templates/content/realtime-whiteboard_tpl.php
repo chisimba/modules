1ea8e6575d23b60cd3b27599ce1bd4961a06280f
adrian <unknown@unknown.com>
2007-03-26 08:41:50 +0000

    sorted out file manager

--- realtime/templates/content/realtime-whiteboard_tpl.php
+++ realtime/templates/content/realtime-whiteboard_tpl.php
@@ -1,3 +1,4 @@
+<!-- START FILE MANAGER FILE CHOOSER CODE-->
 <script type="text/javascript">
 
 	function openWindow(theURL,winName,features) { 
@@ -6,20 +7,32 @@
        	newwindow.focus()
 	   }
     } 
-    
+
+    /*
+     * Function that is called by Java applet when user wants to select an image.
+     */
     function openImageFileChooser() {
-		openWindow('http://localhost/chisimba_framework/app/index.php?module=filemanager&action=selectrealtimeimagewindow&name=selectimage&context=no&workgroup=no&value='+document.getElementById('hidden_selectimage').value+'&','new','toolbar=no, menubar=no, width=600, height=400, resizable=yes, scrollbars=auto, toolbar=no top=200 screenY=200 left=300 screenY=300');
+		openWindow('<?php echo str_replace('amp;', '', $this->uri(array('action'=>'selectrealtimeimagewindow', 'name'=>'selectimage'), 'filemanager')); ?>','new','toolbar=no, menubar=no, width=600, height=400, resizable=yes, scrollbars=auto, toolbar=no top=200 screenY=200 left=300 screenY=300');
     }
 
 	/*
-	* Function that is called by image chooser popup when a file has been selected.
-	*/    
+	 * Function that is called by file manager image chooser popup when a file has been selected.
+	 */    
     function callFunctionFromParent()
 	{
 	    var imageURL = document.getElementById('hidden_selectimage').value;
 	    document.getElementById("whiteboardapplet").imageSelected(imageURL);
 	}
 </script>
+<div style="display:none;">
+<?php
+$objSelectFile = $this->getObject('selectrealtimeimage', 'filemanager');
+$objSelectFile->name = 'selectimage';
+$this->setVar('pageSuppressXML', TRUE);
+echo $objSelectFile->show();
+?>
+</div>
+<!-- END FILE MANAGER FILE CHOOSER CODE-->
 
 <?php
 echo '<applet id="whiteboardapplet" width="800" height="600" code="avoir.realtime.whiteboard.client.WhiteboardApplet.class">';
@@ -28,11 +41,4 @@ echo '    <param name="userName" value="' . $this->userName . '"/>';
 echo '    <param name="userLevel" value="' . $this->userLevel . '"/>';
 echo '    <param name="port" value="1981"/>';
 echo "</applet> ";
-?>
-
-<?php
-$objSelectFile = $this->getObject('selectrealtimeimage', 'filemanager');
-$objSelectFile->name = 'selectimage';
-$this->setVar('pageSuppressXML', TRUE);
-echo $objSelectFile->show();
 ?>
