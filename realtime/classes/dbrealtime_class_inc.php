c5f8627d9bffaf2cfaa37d93c2a41231fe06bd0f
mohamed <unknown@unknown.com>
2007-03-28 13:41:26 +0000

    *** empty log message ***

--- realtime/classes/dbrealtime_class_inc.php
+++ realtime/classes/dbrealtime_class_inc.php
@@ -1,10 +1,13 @@
 <?php
 
-/*
+
+/**
  * Created on 15 Mar 2007
  * @author Mohamed Yusuf
  */
-// security check - must be included in all scripts
+/**
+ * security check - must be included in all scripts
+ */
 if (!$GLOBALS['kewl_entry_point_run'])
 {
 	die("You cannot view this page directly");
@@ -21,7 +24,6 @@ class dbrealtime extends dbTable
 	}
 
 	/**
-	 * @author Mohamed Yusuf
 	 * @param string userId user identification number
 	 * @param string userLevel group that user belongs to
 	 * @param string contextCode Course code
@@ -29,26 +31,36 @@ class dbrealtime extends dbTable
 	 */
 	public function requestToken($userId, $userLevel, $contextCode)
 	{
-		
+		//log_debug("userid: " . $userId . " userlevel: " . $userLevel . " contextcode: " . $contextCode);
 		$userLevel = strtolower($userLevel);
 		$convId = $this->getConversationId($contextCode);
-		$sql = "select hastoken from tbl_realtime_voicequeue where conversationid=\"$convId\"";
+		$sql = "select hastoken, puid from tbl_realtime_voicequeue where conversationid=\"$convId\"";
 		$result = $this->getArray($sql);
-		if (eregi("student",$userLevel))
+
+		foreach ($result as $res)
 		{
-			if(empty($result)){
+			$hasToken = $res['hastoken'];
+			$puid = $res['puid'];
+		}
+		if (eregi("student", $userLevel) || eregi("guest", $userLevel) || eregi("administrator", $userLevel))
+		{
+			if (empty ($result))
+			{
 				$this->addQueue($userId, $userLevel, $contextCode);
-				return SUCCESS;				
-			} else { 
-				foreach ($result as $res)
-				{
-					$hasToken = $res['hastoken'];
-				}
+				return SUCCESS;
+			} else
+			{
 				if ("1" == $hasToken)
 				{
+					$inQueue = array ();
+					$inQueue['conversationid'] = $this->getConversationId($contextCode);
+					$inQueue['userid'] = $userId;
+					$inQueue['hastoken'] = 0;
+					$this->insert($inQueue, 'tbl_realtime_voicequeue');
 					return FAILURE;
-				} else {
-					//assumed that token had been released
+				} else
+				{
+					//assumed that token have been released
 					$this->addQueue($userId, $userLevel, $contextCode);
 					return SUCCESS;
 				}
@@ -57,7 +69,8 @@ class dbrealtime extends dbTable
 		elseif (eregi("lecturer", $userLevel))
 		{
 			//explicitly realease token
-			$this->releaseToken($userId, $userLevel, $contextCode);
+			$sql = "delete from tbl_realtime_voicequeue where hastoken=1";
+			$this->getArray($sql);
 			$this->addQueue($userId, $userLevel, $contextCode);
 			return SUCCESS;
 		} else
@@ -68,20 +81,34 @@ class dbrealtime extends dbTable
 	}
 
 	/**
-	* @author Mohamed Yusuf
-	* @param string contextCode Course code
-	* @return String SUCCESS/FAILURE
-	*/
+	 * @param string contextCode Course code
+	 * @return String SUCCESS/FAILURE
+	 */
 	public function releaseToken($userId, $userLevel, $contextCode)
 	{
 		$convId = $this->getConversationId($contextCode);
-		$sql = "delete from tbl_realtime_voicequeue where conversationid=\"$convId\"";
+		$sql = "delete from tbl_realtime_voicequeue where conversationid=\"$convId\" && userid=\"$userId\"";
 		$this->getArray($sql);
+
+		$sql = "select puid from tbl_realtime_voicequeue where conversationid=\"$convId\" order by puid limit 1";
+		$result = $this->getArray($sql);
+		if (empty ($result))
+		{
+			//do nothing
+		} else
+		{
+			foreach ($result as $res)
+			{
+				$puid = $res['puid'];
+			}
+			$update = array ();
+			$update['hastoken'] = 1;
+			$this->update("puid", $puid, $update, "tbl_realtime_voicequeue");
+		}
 		return SUCCESS;
 	}
 
 	/**
-	 * @author Mohamed Yusuf
 	 * @param string contextCode Course code
 	 * @return String SUCCESS/FAILURE
 	 */
@@ -100,18 +127,17 @@ class dbrealtime extends dbTable
 	}
 
 	/**
-	* @author Mohamed Yusuf
-	* @param string userId user identification number
-	* @param string userLevel group that user belongs to
-	* @param string contextCode Course code
-	* @return String SUCCESS/FAILURE
-	*/
+	 * @param string userId user identification number
+	 * @param string userLevel group that user belongs to
+	 * @param string contextCode Course code
+	 * @return String SUCCESS/FAILURE
+	 */
 	public function startConversation($userId, $userLevel, $contextCode)
 	{
 		$sql = "select contextcode from tbl_realtime_conversation where contextcode=\"$contextCode\"";
 		$result = $this->getArray($sql);
 
-		if (empty($result))
+		if (empty ($result))
 		{
 			$this->createConversation($userId, $userLevel, $contextCode);
 			$this->addQueue($userId, $userLevel, $contextCode);
@@ -126,10 +152,68 @@ class dbrealtime extends dbTable
 	}
 
 	/**
-	* @author Mohamed Yusuf
-	* @param string contextCode Course code
-	* @return String SUCCESS/FAILURE
-	*/
+	 * @param string userId user identification number
+	 * @param string userLevel group that user belongs to
+	 * @param string contextCode Course code
+	 * @return String SUCCESS/FAILURE
+	 */
+	public function joinConversation($userId, $userLevel, $contextCode)
+	{
+		$convId = $this->getConversationId($contextCode);
+		if (empty ($convId))
+		{
+			return FAILURE;
+		} else
+		{
+			return SUCCESS;
+		}
+	}
+
+	/**
+	 * @param string userId user identification number
+	 * @param string userLevel group that user belongs to
+	 * @param string contextCode Course code
+	 * @return String SUCCESS/FAILURE
+	 */
+	public function leaveConversation($userId, $userLevel, $contextCode)
+	{
+		return SUCCESS;
+	}
+
+	/**
+	 * @param string userId user identification number
+	 * @param string userLevel group that user belongs to
+	 * @param string contextCode Course code
+	 * @return String SUCCESS/FAILURE
+	 */
+	public function checkToken($userId, $userLevel, $contextCode)
+	{
+		$sql = "select hastoken from tbl_realtime_voicequeue where userid=\"$userId\"";
+		$result = $this->getArray($sql);
+		if (empty ($result))
+		{
+			//log_debug("empty array, statement did not fetch rows");
+			return "false";
+		} else
+		{
+			foreach ($result as $res)
+			{
+				$hasToken = $res['hastoken'];
+			}
+			if (1 == $hasToken)
+			{
+				return "true";
+			} else
+			{
+				return "false";
+			}
+		}
+	}
+
+	/**
+	 * @param string contextCode Course code
+	 * @return String SUCCESS/FAILURE
+	 */
 	private function removeConversation($contextCode)
 	{
 		$convId = $this->getConversationId($contextCode);
@@ -144,12 +228,11 @@ class dbrealtime extends dbTable
 	}
 
 	/**
-	* @author Mohamed Yusuf
-	* @param string userId user identification number
-	* @param string userLevel group that user belongs to
-	* @param string contextCode Course code
-	* @return String SUCCESS/FAILURE
-	*/
+	 * @param string userId user identification number
+	 * @param string userLevel group that user belongs to
+	 * @param string contextCode Course code
+	 * @return String SUCCESS/FAILURE
+	 */
 	private function addQueue($userId, $userLevel, $contextCode)
 	{
 		$lastId = $this->getConversationId($contextCode);
@@ -161,10 +244,9 @@ class dbrealtime extends dbTable
 	}
 
 	/**
-	* @author Mohamed Yusuf
-	* @param string contextCode Course code
-	* @return String id
-	*/
+	 * @param string contextCode Course code
+	 * @return String id
+	 */
 	private function getConversationId($contextCode)
 	{
 		$sql = "select id  from tbl_realtime_conversation where contextcode=\"$contextCode\"";
@@ -177,12 +259,11 @@ class dbrealtime extends dbTable
 	}
 
 	/**
-	* @author Mohamed Yusuf
-	* @param string userId user identification number
-	* @param string userLevel group that user belongs to
-	* @param string contextCode Course code
-	* @return String SUCCESS/FAILURE
-	*/
+	 * @param string userId user identification number
+	 * @param string userLevel group that user belongs to
+	 * @param string contextCode Course code
+	 * @return String SUCCESS/FAILURE
+	 */
 	private function createConversation($userId, $userLevel, $contextCode)
 	{
 		$addRow = array ();
@@ -191,58 +272,6 @@ class dbrealtime extends dbTable
 		$addRow['contextcode'] = $contextCode;
 		$addRow['starttime'] = $starttime;
 		$this->insert($addRow, 'tbl_realtime_conversation');
-	}
-
-	/**
-	* @author Mohamed Yusuf
-	* @param string userId user identification number
-	* @param string userLevel group that user belongs to
-	* @param string contextCode Course code
-	* @return String SUCCESS/FAILURE
-	*/
-	public function joinConversation($userId, $userLevel, $contextCode)
-	{
-		$convId = $this->getConversationId($contextCode);
-		if (empty ($convId))
-		{
-			return FAILURE;
-		} else
-		{
-			return SUCCESS;
-		}
-	}
-
-	/**
-	* @author Mohamed Yusuf
-	* @param string userId user identification number
-	* @param string userLevel group that user belongs to
-	* @param string contextCode Course code
-	* @return String SUCCESS/FAILURE
-	*/
-	public function leaveConversation($userId, $userLevel, $contextCode)
-	{
-		return SUCCESS;
-	}
-
-	/**
-	* @author Mohamed Yusuf
-	* @param string userId user identification number
-	* @param string userLevel group that user belongs to
-	* @param string contextCode Course code
-	* @return String SUCCESS/FAILURE
-	*/	
-	public function checkToken($userId, $userLevel, $contextCode)
-	{
-		$sql = "select hastoken from tbl_realtime_voicequeue where userid=\"$userId\"";
-		$result = $this->getArray($sql);
-		if(empty($result)){
-			//log_debug("empty array, statement did not fetch rows");
-			return FAILURE;
-		}else{
-			//log_debug("statement did fetch rows");
-			return SUCCESS;
-		}
-		
 	}
 }
 ?>
--- realtime/controller.php
+++ realtime/controller.php
@@ -1,4 +1,5 @@
 <?php
+
 // security check - must be included in all scripts
 if (!$GLOBALS['kewl_entry_point_run'])
 {
@@ -13,192 +14,204 @@ if (!$GLOBALS['kewl_entry_point_run'])
 */
 class realtime extends controller
 {
-    /**
-    * @var object $objUser: The user class in the security module
-    * @access public
-    */
-    public $objUser;
+	/**
+	* @var object $objUser: The user class in the security module
+	* @access public
+	*/
+	public $objUser;
 
-    /**
-    * @var string $userId: The user id of the currently logged in user
-    * @access public
-    */
-    public $userId;
+	/**
+	* @var string $userId: The user id of the currently logged in user
+	* @access public
+	*/
+	public $userId;
 
-    /**
-    * @var string $userName: The username of the currently logged in user
-    * @access public
-    */
-    public $userName;
+	/**
+	* @var string $userName: The username of the currently logged in user
+	* @access public
+	*/
+	public $userName;
 
-    /**
-    * @var string $userLevel: The user's access level
-    * @access public
-    */
-    public $userLevel;
+	/**
+	* @var string $userLevel: The user's access level
+	* @access public
+	*/
+	public $userLevel;
 
-    /**
-    * @var object $objConfig: The altconfig class in the config module
-    * @access public
-    */
-    public $objConfig;
+	/**
+	* @var object $objConfig: The altconfig class in the config module
+	* @access public
+	*/
+	public $objConfig;
 
-    /**
-    * @var object $objLog: The logactivity class in the logger module
-    * @access public
-    */
-    public $objLog;
+	/**
+	* @var object $objLog: The logactivity class in the logger module
+	* @access public
+	*/
+	public $objLog;
 
-    /**
-    * @var object $whiteboardURL: The URL for the whiteboard applet files
-    * @access public
-    */
-    public $whiteboardURL;
+	/**
+	* @var object $whiteboardURL: The URL for the whiteboard applet files
+	* @access public
+	*/
+	public $whiteboardURL;
 
-    /**
-    * @var object $voiceURL: The URL for the voice applet files
-    * @access public
-    */
-    public $voiceURL;
-	
+	/**
+	* @var object $voiceURL: The URL for the voice applet files
+	* @access public
+	*/
+	public $voiceURL;
+
 	/**
 	 * @access public
 	 * @var contexctcode
 	 */
 	public $contextCode;
-	
+
 	public $realtimeControllerURL;
 
-    /**
-     * Constructor method to instantiate objects and get variables
-     */
-    function init()
-    {
-        //Get the activity logger class
-        $this->objLog = $this->newObject('logactivity', 'logger');
-        //Log this module call
-        $this->objLog->log();
-		$this->objrealtime =& $this->getObject('dbrealtime');
-        // classes we need
-        $this->objUser = $this->newObject('user', 'security');
-        $this->userId = $this->objUser->userId();
-        $this->userName = $this->objUser->username($this->userId);
-        if($this->objUser->isAdmin()){
-            $this->userLevel = 'admin';
-        }elseif($this->objUser->isLecturer()){
-            $this->userLevel = 'lecturer';
-        }elseif($this->objUser->isStudent()){
-            $this->userLevel = 'student';
-        }else{
-            $this->userLevel = 'guest';
-        }   
-        $this->objContext = $this->getObject('dbcontext', 'context');
-        $this->objConfig = $this->getObject('altconfig', 'config');
-        $location = "http://". $_SERVER['HTTP_HOST'];
-        $this->whiteboardURL = $location.$this->getResourceUri('whiteboard', 'realtime');
-        $this->voiceURL = $location.$this->getResourceUri('voice', 'realtime');
-        $this->realtimeControllerURL = $location."/chisimba_framework/app/index.php?module=realtime";
-    }
-    
-    /**
-    * Method to process actions to be taken
-    *
-    * @param string $action String indicating action to be taken
-	*/
-    function dispatch($action=Null)
-    {
-		$this->contextCode = $this->objContext->getContextCode();
-		switch($action)
+	/**
+	 * Constructor method to instantiate objects and get variables
+	 */
+	function init()
+	{
+		//Get the activity logger class
+		$this->objLog = $this->newObject('logactivity', 'logger');
+		//Log this module call
+		$this->objLog->log();
+		$this->objrealtime = & $this->getObject('dbrealtime');
+		// classes we need
+		$this->objUser = $this->newObject('user', 'security');
+		$this->userId = $this->objUser->userId();
+		$this->userName = $this->objUser->username($this->userId);
+		if ($this->objUser->isAdmin())
 		{
-		  case 'classroom':
-		     return "realtime-classroom_tpl.php";
-
-		  case 'jmfinstall':
-		     return "realtime-jmfinstall_tpl.php";
-
-		  case 'voice':
-		     return "realtime-voice_tpl.php";
-		
-		  case 'whiteboard':
-		     return "realtime-whiteboard_tpl.php";
-		  case 'requesttoken':
-		  	 return $this->requestToken($this->userId, $this->userLevel, $this->contextCode);
-		  case 'releasetoken':
-		  	return $this->releaseToken($this->userId, $this->userLevel, $this->contextCode); 
-		  case 'startconversation':
-		  	return $this->startConversation($this->userId, $this->userLevel, $this->contextCode);								
-		  case 'stopconversation':
-		  	return $this->stopConversation($this->userId, $this->userLevel, $this->contextCode);
-		  case 'joinconversation':
-		  	return $this->joinConversation($this->userId, $this->userLevel, $this->contextCode);
-		  case 'leaveconversation':
-		  	return $this->leaveConversation($this->userId, $this->userLevel, $this->contextCode);
-		  case 'checktoken':
-		  	return $this->checkToken($this->userId, $this->userLevel, $this->contextCode);
-		  default:
-		     return "realtime_tpl.php";
+			$this->userLevel = 'admin';
 		}
-	
-    }
-    
-    function requestToken($userid, $userlevel, $contextcode)
-    {
-    	if(empty($userid) || empty($userlevel) || empty($contextcode)){
-    		return "realtime_tpl.php";
-    	}else{
-    		$hasToken = $this->objrealtime->requestToken($userid, $userlevel, $contextcode);
-    		$this->setVar('hastoken', $hasToken);
-    		return "redirect_tpl.php";
-    	}
-    }
-    
-    function releaseToken($userid, $userlevel, $contextcode)
-    {
-		$hasToken = $this->objrealtime->releaseToken($userid, $userlevel, $contextcode);
-    	$this->setVar('hastoken', $hasToken);
-    	return "redirect_tpl.php";
+		elseif ($this->objUser->isLecturer())
+		{
+			$this->userLevel = 'lecturer';
+		}
+		elseif ($this->objUser->isStudent())
+		{
+			$this->userLevel = 'student';
+		} else
+		{
+			$this->userLevel = 'guest';
+		}
+		$this->objContext = $this->getObject('dbcontext', 'context');
+		$this->objConfig = $this->getObject('altconfig', 'config');
+		$location = "http://" . $_SERVER['HTTP_HOST'];
+		$this->whiteboardURL = $location . $this->getResourceUri('whiteboard', 'realtime');
+		$this->voiceURL = $location . $this->getResourceUri('voice', 'realtime');
+		$this->realtimeControllerURL = $location . "/chisimba_framework/app/index.php?module=realtime";
+	}
 
-    }
-    
-    function startConversation($userid, $userlevel, $contextcode)
-    {
-    	if(empty($userid) || empty($userlevel) || empty($contextcode)){
-    		$hasToken = "FAILURE";
-    		$this->setVar('hastoken', $hasToken);
-    		return "redirect_tpl.php";
-    	}else{
-    		$hasToken = $this->objrealtime->startConversation($userid, $userlevel, $contextcode);
-    		$this->setVar('hastoken', $hasToken);
-    		return "redirect_tpl.php";
-    	}
-    }
-    
-    function stopConversation($userid, $userlevel, $contextcode)
-    {
-    	$hasToken = $this->objrealtime->stopConversation($userid, $userlevel, $contextcode);
-    	$this->setVar('hastoken', $hasToken);
-    	return "redirect_tpl.php";
-    }
-    
-    function leaveConversation($userid, $userlevel, $contextcode)
-    {
-    	$hasToken = $this->objrealtime->leaveConversation($userid, $userlevel, $contextcode);
-    	$this->setVar('hastoken', $hasToken);
-    	return "redirect_tpl.php";
-    }
-    
-    function joinConversation($userid, $userlevel, $contextcode)
-    {
-    	$hasToken = $this->objrealtime->joinConversation($userid, $userlevel, $contextcode);
-    	$this->setVar('hastoken', $hasToken);
-    	return "redirect_tpl.php";    	
-    }
-    
-    function checkToken($userid, $userlevel, $contextcode)
-    {
-    	$hasToken = $this->objrealtime->checkToken($userid, $userlevel, $contextcode);
-    	$this->setVar('hastoken', $hasToken);
-    	return "redirect_tpl.php";
-    }
+	/**
+	* Method to process actions to be taken
+	*
+	* @param string $action String indicating action to be taken
+	*/
+	function dispatch($action = Null)
+	{
+		$this->contextCode = $this->objContext->getContextCode();
+		switch ($action)
+		{
+			case 'classroom' :
+				return "realtime-classroom_tpl.php";
+
+			case 'jmfinstall' :
+				return "realtime-jmfinstall_tpl.php";
+
+			case 'voice' :
+				return "realtime-voice_tpl.php";
+
+			case 'whiteboard' :
+				return "realtime-whiteboard_tpl.php";
+			case 'requesttoken' :
+				return $this->requestToken($this->userId, $this->userLevel, $this->contextCode);
+			case 'releasetoken' :
+				return $this->releaseToken($this->userId, $this->userLevel, $this->contextCode);
+			case 'startconversation' :
+				return $this->startConversation($this->userId, $this->userLevel, $this->contextCode);
+			case 'stopconversation' :
+				return $this->stopConversation($this->userId, $this->userLevel, $this->contextCode);
+			case 'joinconversation' :
+				return $this->joinConversation($this->userId, $this->userLevel, $this->contextCode);
+			case 'leaveconversation' :
+				return $this->leaveConversation($this->userId, $this->userLevel, $this->contextCode);
+			case 'checktoken' :
+				return $this->checkToken($this->userId, $this->userLevel, $this->contextCode);
+			case 'checkturn' :
+				return $this->checkToken($this->userId, $this->userLevel, $this->contextCode);
+			default :
+				return "realtime_tpl.php";
+		}
+
+	}
+
+	function requestToken($userid, $userlevel, $contextcode)
+	{
+		if (empty ($userid) || empty ($userlevel) || empty ($contextcode))
+		{
+			return "realtime_tpl.php";
+		} else
+		{
+			$hasToken = $this->objrealtime->requestToken($userid, $userlevel, $contextcode);
+			$this->setVar('hastoken', $hasToken);
+			return "redirect_tpl.php";
+		}
+	}
+
+	function releaseToken($userid, $userlevel, $contextcode)
+	{
+		$hasToken = $this->objrealtime->releaseToken($userid, $userlevel, $contextcode);
+		$this->setVar('hastoken', $hasToken);
+		return "redirect_tpl.php";
+
+	}
+
+	function startConversation($userid, $userlevel, $contextcode)
+	{
+		if (empty ($userid) || empty ($userlevel) || empty ($contextcode))
+		{
+			$hasToken = "FAILURE";
+			$this->setVar('hastoken', $hasToken);
+			return "redirect_tpl.php";
+		} else
+		{
+			$hasToken = $this->objrealtime->startConversation($userid, $userlevel, $contextcode);
+			$this->setVar('hastoken', $hasToken);
+			return "redirect_tpl.php";
+		}
+	}
+
+	function stopConversation($userid, $userlevel, $contextcode)
+	{
+		$hasToken = $this->objrealtime->stopConversation($userid, $userlevel, $contextcode);
+		$this->setVar('hastoken', $hasToken);
+		return "redirect_tpl.php";
+	}
+
+	function leaveConversation($userid, $userlevel, $contextcode)
+	{
+		$hasToken = $this->objrealtime->leaveConversation($userid, $userlevel, $contextcode);
+		$this->setVar('hastoken', $hasToken);
+		return "redirect_tpl.php";
+	}
+
+	function joinConversation($userid, $userlevel, $contextcode)
+	{
+		$hasToken = $this->objrealtime->joinConversation($userid, $userlevel, $contextcode);
+		$this->setVar('hastoken', $hasToken);
+		return "redirect_tpl.php";
+	}
+
+	function checkToken($userid, $userlevel, $contextcode)
+	{
+		$hasToken = $this->objrealtime->checkToken($userid, $userlevel, $contextcode);
+		$this->setVar('hastoken', $hasToken);
+		return "redirect_tpl.php";
+	}
 }
 ?>
