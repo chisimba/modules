665daad8e4f7e212c62f8f622ab405b1a0dbaccb
megan <unknown@unknown.com>
2007-04-05 10:39:15 +0000

    Added new module for extending the forum

---
+++ hivaidsforum/classes/dbhivforum_class_inc.php
@@ -0,0 +1,496 @@
+<?php
+/**
+* dbhivforum class extends dbtable
+* @package hivaidsforum
+* @filesource
+*/
+
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']){
+    die("You cannot view this page directly");
+}
+
+/**
+* dbhivforum class
+* @author Megan Watson
+* @copyright (c) 2007 UWC
+* @version 0.1
+*/
+
+class dbhivforum extends dbtable
+{
+    /**
+    * @var string $context The current context - set it to root/lobby
+    * @access private
+    */
+    private $context = 'root';
+    
+    /**
+    * Constructor method
+    */
+    public function init()
+    {
+        $this->objUser = $this->getObject('user', 'security');
+        $this->objLanguage = $this->getObject('language', 'language');
+        $this->objDate = $this->getObject('dateandtime', 'utilities');
+        
+        $this->dbForum = $this->getObject('dbforum', 'forum');
+        $this->dbTopic = $this->getObject('dbtopic', 'forum');
+        $this->dbForumPost = $this->getObject('dbpost', 'forum');
+        $this->dbForumPostText = $this->getObject('dbposttext', 'forum');
+        
+        $this->objFeatureBox = $this->getObject('featurebox', 'navigation');
+        $this->objRound = $this->newObject('roundcorners', 'htmlelements');
+        $this->loadClass('link', 'htmlelements');
+        $this->loadClass('htmlheading', 'htmlelements');
+        $this->loadClass('htmltable', 'htmlelements');
+    }
+    
+    /**
+	* Method to dynamically switch tables
+	*
+	* @access private
+	* @param string $table: The name of the table
+	* @return boolean: TRUE on success FALSE on failure
+	*/
+	private function _changeTable($table)
+	{
+		try {
+			parent::init($table);
+			return TRUE;
+		}catch (Exception $e){
+			throw customException($e->getMessage());
+			exit();
+		}
+	}
+	
+	/**
+	* Method to display the topic post and the posted responses in round divs
+	*
+	* @access public
+	* @return string html
+	*/
+	public function showPosts()
+	{
+	    $topicId = $this->getSession('topicId');
+	    $data = $this->getPosts();
+	    $lbPosted = $this->objLanguage->languageText('word_posted');
+	    $lbReply = $this->objLanguage->languageText('phrase_replytotopic');
+	    
+	    $topStr = '';
+	    $replyStr = '';
+	    
+	    // Create reply link
+	    $objLink = new link($this->uri(array('action' => 'showreply')));
+	    $objLink->link = $lbReply;
+	    $lnReply = '<br />'.$objLink->show();
+	    
+	    if(!empty($data)){
+	        foreach($data as $item){
+	            $name = ''; $posted = '';
+	            
+	            // check if post is the topic parent
+	            if($item['post_parent'] == '0'){
+    	            $objHead = new htmlheading();
+    	            $objHead->str = $item['post_title'];
+    	            $objHead->type = 4;
+    	            
+    	            $inStr = $objHead->show();
+    	            $inStr .= $item['post_text'];
+	           
+	                $topStr = $this->objRound->show($inStr.$lnReply);
+	            }else{
+	                if(!empty($item['userid'])){
+	                    $name = ' ... '.$this->objUser->fullname($item['userid']);
+	                }
+	                if(!empty($item['datelastupdated'])){
+	                    $date = $this->objDate->formatDateOnly($item['datelastupdated']);
+	                    $posted = $lbPosted.': '.$date;
+	                }
+	                
+	                $objTable = new htmltable();
+	                $objTable->startRow();
+	                $objTable->addCell('<b>'.$item['post_title'].'</b>');
+	                $objTable->addCell($posted, '', '', 'right');
+    	            $inStr = $objTable->show();
+    	            $inStr .= $item['post_text'];
+    	            
+	                $replyStr .= $this->objRound->show($inStr.$lnReply);
+	            }
+	        }
+	    }
+	    return $topStr.$replyStr;
+	}
+	
+	/**
+	* Method to get a topic post and responses
+	*
+	* @access private
+	* @return array $data The posts
+	*/
+	private function getPosts()
+	{
+	    $topicId = $this->getSession('topicId');
+	    
+        // Get the posts from the DB
+        $this->_changeTable('tbl_forum_post');
+        $sql = "SELECT * FROM tbl_forum_post AS post, tbl_forum_post_text AS posttext 
+            WHERE post.topic_id = '{$topicId}' AND posttext.post_id = post.id 
+            ORDER BY post.datelastupdated ASC ";
+            
+        $data = $this->getArray($sql);
+        return $data;
+	}
+	
+	/**
+	* Method to display the top post
+	*
+	* @access public
+	* @param string $data
+	* @return string html
+	*/
+	public function displayTopPost($data)
+	{	    
+	    $objHead = new htmlheading();
+    	$objHead->str = $data['post_title'];
+    	$objHead->type = 4;
+        $inStr = $objHead->show();
+        $inStr .= $data['post_text'];
+	           
+        return $this->objRound->show($inStr);
+	}
+	
+	/**
+	* Method to display the forum details
+	*
+	* @access public
+	* @param string $data
+	* @return string html
+	*/
+	public function displayForum($data)
+	{	    
+	    $objHead = new htmlheading();
+    	$objHead->str = $data['forum_name'];
+    	$objHead->type = 4;
+        $inStr = $objHead->show();
+        $inStr .= $data['forum_description'];
+	           
+        return $this->objRound->show($inStr);
+	}
+	
+	/**
+	* Method to show a post
+	*
+	* @access public
+	* @param string $postId
+	* @return string html
+	*/
+	public function showSinglePost($postId)
+	{
+	    $data = $this->getPost($postId);
+	    $lbPosted = $this->objLanguage->languageText('word_posted');
+	    
+	    if(!empty($data)){
+	        if(!empty($data['userid'])){
+	            $name = ' ... '.$this->objUser->fullname($data['userid']);
+	        }
+	        if(!empty($data['datelastupdated'])){
+	            $date = $this->objDate->formatDateOnly($data['datelastupdated']);
+	            $posted = $lbPosted.': '.$date;
+	        }
+	        
+	        $objTable = new htmltable();
+	        $objTable->startRow();
+	        $objTable->addCell('<b>'.$data['post_title'].'</b>');
+	        $objTable->addCell($posted, '', '', 'right');
+    	    $inStr = $objTable->show();
+            $inStr .= $data['post_text'];
+    	            
+	        return $this->objRound->show($inStr);
+	    }
+	}
+	
+	/**
+	* Method to get a topic post and responses
+	*
+	* @access private
+	* @param string $postId
+	* @return array $data The posts
+	*/
+	private function getPost($postId)
+	{   
+        // Get the posts from the DB
+        $this->_changeTable('tbl_forum_post');
+        $sql = "SELECT * FROM tbl_forum_post AS post, tbl_forum_post_text AS posttext 
+            WHERE post.id = '{$postId}' AND posttext.post_id = post.id ";
+            
+        $data = $this->getArray($sql);
+        if(!empty($data)){
+            return $data[0];
+        }
+        return array();
+	}
+	
+	/**
+	* Method to list the topics for display in a box / block
+	*
+	* @access public
+	* @param string $dispType The format of the display - featurebox or div
+	* @return string html
+    */
+    public function showTopicList($dispType = 'box')
+    {
+        $topicId = $this->getSession('topicId');
+        $lbTopics = $this->objLanguage->languageText('word_topics');
+        $list = $this->getTopicList();
+        
+        $str = '';
+        if(!empty($list)){
+            foreach($list as $item){
+                if($item['topicid'] == $topicId){
+                    $lnTopic = $item['post_title'];
+                }else{
+                    $objLink = new link($this->uri(array('action' => 'showtopic', 'topicId' => $item['topicid'])));
+                    $objLink->link = $item['post_title'];
+                    $lnTopic = $objLink->show();
+                }
+                $str .= '<p style="margin: 0px;">'.$lnTopic.'</p>';
+            }
+        }
+        if($dispType == 'round'){
+            $objHead = new htmlheading();
+            $objHead->str = $lbTopics;
+            $objHead->type = '4';
+            return $this->objRound->show($objHead->show().$str);
+        }
+        return $this->objFeatureBox->show($lbTopics, $str);
+    }
+    
+    /**
+    * Method to get the list of topics
+    *
+    * @access private
+    * @return array $data the topic list
+    */
+    private function getTopicList()
+    {
+        // Get the current forum
+        $forumId = $this->getSession('forumId');
+        
+        // Get the list from the DB
+        $this->_changeTable('tbl_forum_topic');
+        $sql = "SELECT *, topic.id as topicid FROM tbl_forum_topic AS topic, tbl_forum_post AS post, tbl_forum_post_text AS posttext ";
+        
+        // Use the current forum, if not set then find the default
+        if(isset($forumId) && !empty($forumId)){
+            $sql .= "WHERE topic.forum_id = '{$forumId}' ";
+        }else{
+            $sql .= " WHERE topic.forum_id = 
+            (SELECT id FROM tbl_forum AS forum WHERE forum.defaultforum = 'Y' AND forum.forum_context = '$this->context') ";
+        }
+        
+        // Get the topic text
+        $sql .= "AND topic.first_post = post.id AND post.post_parent = '0' 
+            AND posttext.post_id = post.id ";
+        
+        $data = $this->getArray($sql);
+        
+        return $data;
+    }
+    
+    /**
+    * Method to get the topic post
+    *
+    * @access public
+    * @return array $data the topic
+    */
+    public function getTopicPost()
+    {
+        // Get the current forum
+        $topicId = $this->getSession('topicId');
+        
+        // Get the list from the DB
+        $this->_changeTable('tbl_forum_post');
+        $sql = "SELECT *, post.id as postid FROM tbl_forum_post AS post, tbl_forum_post_text AS posttext 
+            WHERE post.topic_id = '{$topicId}' AND post.post_parent = '0' AND posttext.post_id = post.id ";
+        
+        $data = $this->getArray($sql);
+        if(!empty($data)){
+            return $data[0];
+        }
+        return array();
+    }
+    
+	/**
+	* Method to list the categories for display in a box / block
+	*
+	* @access public
+	* @return string html
+    */
+    public function showCategoryList()
+    {
+        // Get the current forum
+        $forumId = $this->getSession('forumId');
+        
+        $lbCats = $this->objLanguage->languageText('word_categories');
+        $list = $this->getCategoryList();
+        
+        $str = '';
+        if(!empty($list)){
+            foreach($list as $item){
+                if($item['id'] == $forumId){
+                    $lnForum = $item['forum_name'];
+                }else{
+                    $objLink = new link($this->uri(array('action' => 'showcat', 'catId' => $item['id'])));
+                    $objLink->link = $item['forum_name'];
+                    $lnForum = $objLink->show();
+                }
+                $str .= '<p style="margin: 0px;">'.$lnForum.'</p>';
+            }
+        }
+        return $this->objFeatureBox->show($lbCats, $str);
+    }
+
+    /**
+    * Method to get the list of categories / forums
+    *
+    * @access private
+    * @return array $data The categories
+    */
+    private function getCategoryList()
+    {   
+        // Get the list from the DB
+        $this->_changeTable('tbl_forum');
+        $sql = "SELECT * FROM tbl_forum AS forum
+            WHERE forum.forum_context = '{$this->context}' AND forum.forum_visible = 'Y' ";
+            
+        $data = $this->getArray($sql);
+        return $data;
+    }
+    
+	/**
+	* Method to display the recent posts in a box / block
+	*
+	* @access public
+	* @return string html
+    */
+    public function showRecentPosts()
+    {
+        $lbPosts = $this->objLanguage->languageText('phrase_recentposts');
+        $list = $this->getRecentPosts();
+        
+        $str = '';
+        if(!empty($list)){
+            foreach($list as $item){
+                $objLink = new link($this->uri(array('action' => 'showpost', 'postId' => $item['postid'])));
+                $objLink->link = $item['post_title'];
+                $lnPost = $objLink->show();
+                $str .= '<p style="margin: 0px;">'.$lnPost.'</p>';
+            }
+        }
+        return $this->objFeatureBox->show($lbPosts, $str);
+    }
+
+    /**
+    * Method to get the recent posts
+    *
+    * @access private
+    * @return array $data The categories
+    */
+    private function getRecentPosts()
+    {   
+        // Get the list from the DB
+        $this->_changeTable('tbl_forum_post');
+        $sql = "SELECT *, post.id as postid FROM tbl_forum_post AS post, tbl_forum AS forum, tbl_forum_topic AS topic, tbl_forum_post_text AS posttext 
+            WHERE forum.forum_context = '{$this->context}' AND topic.forum_id = forum.id 
+            AND post.topic_id = topic.id AND posttext.post_id = post.id
+            ORDER BY post.datelastupdated DESC LIMIT 5";
+            
+        $data = $this->getArray($sql);
+        return $data;
+    }
+    
+    /**
+    * Method to get the forum details - if the forum is not set, then use the default
+    *
+    * @access public
+    * @return array $data
+    */
+    public function getForumDetails()
+    {
+        $forumId = $this->getSession('forumId');
+        
+        $this->_changeTable('tbl_forum');
+        $sql = "SELECT id, forum_name, forum_description FROM tbl_forum  ";
+        
+        // Use the current forum, if not set then find the default
+        if(isset($forumId) && !empty($forumId)){
+            $sql .= "WHERE id = '{$forumId}' ";
+        }else{
+            $sql .= " WHERE defaultforum = 'Y' AND forum_context = '$this->context') ";
+        }
+        
+        $data = $this->getArray($sql);
+        if(!empty($data)){
+            $this->setSession('forumId', $data[0]['id']);
+            return $data[0];
+        }
+        return array();
+    }
+    
+    /**
+    * Method to save a new topic
+    *
+    * @access public
+    * @return void
+    */
+    public function saveTopic()
+    {
+        $forumId = $this->getSession('forumId');
+        $userId = $this->objUser->userId();
+        $typeId = 'init_1';
+        
+        $topicId = $this->dbTopic->insertSingle($forumId, $typeId, '0', $userId);
+        $this->setSession('topicId', $topicId);
+        $postId = $this->saveTopicPost();
+        $this->dbTopic->updateFirstPost($topicId, $postId);
+        return $topicId;
+    }
+    
+    /**
+    * Method to save a reply to a topic
+    *
+    * @access public
+    * @return void
+    */
+    public function saveTopicPost()
+    {
+        $userId = $this->objUser->userId();
+        $topicId = $this->getSession('topicId');
+        $forumId = $this->getSession('forumId');
+        $postParent = $this->getParam('postid', '0');
+        $subject = $this->getParam('subject');
+        $message = $this->getParam('message');
+        
+        $postId = $this->dbForumPost->insertSingle($postParent, '0', $forumId, $topicId,  $userId);
+        $this->dbForumPostText->insertSingle($postId, $subject, $message,  'en', '1', $userId);
+        $this->dbTopic->updateLastPost($topicId, $postId);
+        return $postId;
+    }
+    
+    /**
+    * Method to save a category or forum
+    *
+    * @access public
+    * @return void
+    */
+    public function saveCategory()
+    {
+        $forum = $this->getParam('forum');
+        $description = $this->getParam('description');
+        $visible = $this->getParam('visible');
+        
+        return $this->dbForum->insertSingle($this->context, '', $forum, $description, 'N',  $visible);
+    }
+}
+?>
---
+++ hivaidsforum/classes/hivtools_class_inc.php
@@ -0,0 +1,301 @@
+<?php
+/**
+* hivtools class extends object
+* @package hivaidsforum
+* @filesource
+*/
+
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']){
+    die("You cannot view this page directly");
+}
+
+/**
+* hivtools class
+* @author Megan Watson
+* @copyright (c) 2007 UWC
+* @version 0.1
+*/
+
+class hivtools extends object
+{
+    /**
+    * @var string $userId The user id for tracking the user
+    * @access private
+    */
+    private $userId = '';
+    
+    /**
+    * @var string $userPkId The unique id for tracking the user
+    * @access private
+    */
+    private $userPkId = '';
+
+    /**
+    * Constructor method
+    */
+    public function init()
+    {
+        $this->objTopic = $this->getObject('dbhivforum', 'hivaidsforum');
+        
+        $this->objUser = $this->getObject('user', 'security');
+        $this->objLanguage = $this->getObject('language', 'language');
+        $this->objBlocks = $this->getObject('blocks', 'blocks');
+        
+        $this->objFeatureBox = $this->newObject('featurebox', 'navigation');
+        $this->objEditor = $this->newObject('htmlarea', 'htmlelements');
+        $this->loadClass('form', 'htmlelements');
+        $this->loadClass('textinput', 'htmlelements');
+        $this->loadClass('button', 'htmlelements');
+        $this->loadClass('label', 'htmlelements');
+        $this->loadClass('radio', 'htmlelements');
+        
+        $this->userId = $this->objUser->userId();
+        $this->userPkId = $this->objUser->PKId();
+    }
+    
+    /**
+    * Method to get the blocks for the left side column
+    *
+    * @access public
+    * @return string html
+    */
+    public function getLeftBlocks()
+    {
+        $topicBlock = $this->objTopic->showTopicList();
+        $categoryBlock = $this->objTopic->showCategoryList();
+        $recentBlock = $this->objTopic->showRecentPosts();
+        $loginBlock = $this->objBlocks->showBlock('login', 'security');
+        $manageBlock = $this->showManageList();
+        
+        return $topicBlock.$loginBlock.$recentBlock.$categoryBlock.$manageBlock.'<br />';
+    }
+    
+    /**
+    * Method to display a list of functions for managing the forum
+    * The functions include: adding categories, topics, moderating posts
+    *
+    * @access private
+    * @return string html
+    */
+    private function showManageList()
+    {
+        // Check users permissions - must be site lecturer or admin
+        if(!$this->checkPermissions()){
+            return '';
+        }
+        
+        $lbManage = $this->objLanguage->languageText('phrase_manageforum');
+        $lbAddCat = $this->objLanguage->languageText('phrase_addcategory');
+        $lbAddTopic = $this->objLanguage->languageText('phrase_addtopic');
+        //$lbModerate = $this->objLanguage->languageText('phrase_moderateposts');
+        
+        $objLink = new link($this->uri(array('action' => 'addcategory')));
+        $objLink->link = $lbAddCat;
+        $str = $objLink->show();
+        
+        $objLink = new link($this->uri(array('action' => 'addtopic')));
+        $objLink->link = $lbAddTopic;
+        $str .= '<br />'.$objLink->show();
+        
+        /*$objLink = new link($this->uri(array('action' => 'moderate')));
+        $objLink->link = $lbModerate;
+        $str .= '<br />'.$objLink->show();*/
+        
+        return $this->objFeatureBox->show($lbManage, $str);
+    }
+    
+    /**
+    * Method to display a single post
+    *
+    * @access public
+    * @param string $postId The id of the post in the db
+    * @return string html
+    */
+    public function showSinglePost($postId)
+    {
+        return $this->objTopic->showSinglePost($postId);
+    }
+    
+    /**
+    * Method to display the topic and responses
+    *
+    * @access public
+    * @return string html
+    */
+    public function showTopicPosts()
+    {
+        $topicId = $this->getSession('topicId');
+        if(!empty($topicId)){
+            return $this->objTopic->showPosts();
+        }else{
+            return $this->objTopic->showTopicList('round');
+        }
+    }
+    
+    /**
+    * Method to display a form for replying to a post
+    *
+    * @access public
+    * @return string html
+    */
+    public function showReplyPage()
+    {
+        $lbReply = ucwords($this->objLanguage->languageText('phrase_replytotopic'));
+        $lbSubject = $this->objLanguage->languageText('word_subject');
+        $lbMessage = $this->objLanguage->languageText('word_message');
+        $lbSave = $this->objLanguage->languageText('word_save');
+        
+        $topicId = $this->getSession('topicId');
+        
+        $topic = $this->objTopic->getTopicPost();
+        $str = $this->objTopic->displayTopPost($topic);
+        $postId = $topic['postid'];
+        
+        // Create reply form
+        
+        // subject
+        $objLabel = new label($lbSubject, 'input_subject');
+        $objInput = new textinput('subject');
+        $formStr = '<p>'.$objLabel->show().':<br />'.$objInput->show().'</p>';
+        
+        // message
+        $objLabel = new label($lbMessage, 'input_message');
+        $this->objEditor->init('message');
+        $formStr .= '<p>'.$objLabel->show().':<br />'.$this->objEditor->show().'</p>';
+        
+        // post button
+        $objButton = new button('save', $lbSave);
+        $objButton->setToSubmit();
+        $formStr .= '<p>'.$objButton->show().'</p>';
+        
+        $objInput = new textinput('postid', $postId, 'hidden');
+        $formStr .= $objInput->show();
+        
+        $objForm = new form('posttopic', $this->uri(array('action' => 'savepost')));
+        $objForm->addToForm($formStr);
+        $str .= $this->objFeatureBox->showContent($lbReply, $objForm->show());
+        
+        return $str;
+    }
+    
+    /**
+    * Method to display a form for adding a category
+    *
+    * @access public
+    * @return string html
+    */
+    public function showAddCategory()
+    {
+        $lbAddCat = ucwords($this->objLanguage->languageText('phrase_addcategory'));
+        $lbCategory = $this->objLanguage->languageText('phrase_categoryname');
+        $lbDescription = $this->objLanguage->languageText('word_description');
+        $lbVisible = $this->objLanguage->languageText('word_visible');
+        $lbYes = $this->objLanguage->languageText('word_yes');
+        $lbNo = $this->objLanguage->languageText('word_no');
+        $lbSave = $this->objLanguage->languageText('word_save');
+        
+        // category name
+        $objLabel = new label($lbCategory, 'input_forum');
+        $objInput = new textinput('forum', '', '', '60');
+        $formStr = '<p>'.$objLabel->show().':<br />'.$objInput->show().'</p>';
+        
+        // description
+        $objLabel = new label($lbDescription, 'input_description');
+        $objInput = new textinput('description', '', '', '80');
+        $formStr .= '<p>'.$objLabel->show().':<br />'.$objInput->show().'</p>';
+        
+        // is visible
+        $objLabel = new label($lbVisible, 'input_visible');
+        $objRadio = new radio('visible');
+        $objRadio->addOption('Y', $lbYes);
+        $objRadio->addOption('N', $lbNo);
+        $objRadio->setSelected('yes');
+        $objRadio->setBreakSpace('&nbsp;&nbsp;&nbsp;');
+        $formStr .= '<p>'.$objLabel->show().':&nbsp;&nbsp;&nbsp;'.$objRadio->show().'</p>';
+        
+        $objButton = new button('save', $lbSave);
+        $objButton->setToSubmit();
+        $formStr .= '<p>'.$objButton->show().'</p>';
+        
+        $objForm = new form('newforum', $this->uri(array('action' => 'savecat')));
+        $objForm->addToForm($formStr);
+        
+        return $this->objFeatureBox->showContent($lbAddCat, $objForm->show());
+    }
+    
+    /**
+    * Method to display a form for adding a topic
+    *
+    * @access public
+    * @return string html
+    */
+    public function showAddTopic()
+    {
+        $lbAddTopic = ucwords($this->objLanguage->languageText('phrase_addtopic'));
+        $lbSubject = $this->objLanguage->languageText('word_subject');
+        $lbMessage = $this->objLanguage->languageText('word_message');
+        $lbSave = $this->objLanguage->languageText('word_save');
+        
+        // Get forum details
+        $forum = $this->objTopic->getForumDetails();
+        $str = $this->objTopic->displayForum($forum);
+        
+        // Create form
+        
+        // subject
+        $objLabel = new label($lbSubject, 'input_subject');
+        $objInput = new textinput('subject');
+        $formStr = '<p>'.$objLabel->show().':<br />'.$objInput->show().'</p>';
+        
+        // message
+        $objLabel = new label($lbMessage, 'input_message');
+        $this->objEditor->init('message');
+        $formStr .= '<p>'.$objLabel->show().':<br />'.$this->objEditor->show().'</p>';
+        
+        // post button
+        $objButton = new button('save', $lbSave);
+        $objButton->setToSubmit();
+        $formStr .= '<p>'.$objButton->show().'</p>';
+        
+        $objForm = new form('newtopic', $this->uri(array('action' => 'savetopic')));
+        $objForm->addToForm($formStr);
+        $str .= $this->objFeatureBox->showContent($lbAddTopic, $objForm->show());
+        
+        return $str;
+    }
+    
+    /**
+    * Method to check if user is a lecturer or site admin
+    *
+    * @access private
+    * @return bool TRUE if yes
+    */
+    private function checkPermissions()
+    {
+        $perms = $this->getSession('isManager');
+        if(!empty($perms)){
+            if($perms == 'yes'){
+                return TRUE;
+            }
+            return FALSE;
+        }
+        // Check if user is a lecturer
+        $objGroups = $this->getObject('groupadminmodel', 'groupadmin');
+        
+        $groupId = $objGroups->getLeafId(array('Lecturers'));
+        if($objGroups->isGroupMember($this->userPkId, $groupId)){
+            $this->setSession('isManager', 'yes');
+            return TRUE;
+        }
+        
+        $groupId = $objGroups->getLeafId(array('Site Admin'));
+        if($objGroups->isGroupMember($this->userPkId, $groupId)){
+            $this->setSession('isManager', 'yes');
+            return TRUE;
+        }
+        $this->setSession('isManager', 'no');
+        return FALSE;
+    }
+}
+?>
