f481a534c16dc059a2e6cb880b6543001ed192e8
colleen <unknown@unknown.com>
2007-04-05 10:43:10 +0000

    *** empty log message ***

--- marketingrecruitmentforum/templates/content/schoollist_tpl.php
+++ marketingrecruitmentforum/templates/content/schoollist_tpl.php
@@ -1,5 +1,5 @@
 <?php
-/**create a template for all school list information**/ 
+/**create a template for all school list information**/
 
 /**
  *load all classes
@@ -7,12 +7,14 @@
  $this->loadClass('textinput','htmlelements');
  $this->loadClass('textarea','htmlelements');
  $this->loadclass('button','htmlelements');
-/*------------------------------------------------------------------------------*/ 
  
+ $this->schoolnames =& $this->getObject('schoolnames','marketingrecruitmentforum');
+/*------------------------------------------------------------------------------*/
+
  /**
   *define all language items
   */
-     
+
  $schoolname = $this->objLanguage->languageText('phrase_schoolname');
  $schooladdy  = $this->objLanguage->languageText('phrase_schooladdress');
  $telnumber  = $this->objLanguage->languageText('phrase_telnumber');
@@ -26,7 +28,7 @@
  $provinceselect = $this->objLanguage->languageText('mod_marketingrecruitmentforum_provinceselect','marketingrecruitmentforum');
  $areaselect = $this->objLanguage->languageText('mod_marketingrecruitmentforum_areaselect','marketingrecruitmentforum');
  $schoolselect = $this->objLanguage->languageText('mod_marketingrecruitmentforum_schoolselect','marketingrecruitmentforum');
- 
+
  $wcprov = $this->objLanguage->languageText('mod_marketingrecruitmentforum_wcprov','marketingrecruitmentforum');
  $ecprov = $this->objLanguage->languageText('mod_marketingrecruitmentforum_ecprov','marketingrecruitmentforum');
  $ncprov = $this->objLanguage->languageText('mod_marketingrecruitmentforum_ncprov','marketingrecruitmentforum');
@@ -36,7 +38,7 @@
  $nwprov = $this->objLanguage->languageText('mod_marketingrecruitmentforum_nwprov','marketingrecruitmentforum');
  $fprov = $this->objLanguage->languageText('mod_marketingrecruitmentforum_fprov','marketingrecruitmentforum');
  $knprov = $this->objLanguage->languageText('mod_marketingrecruitmentforum_knprov','marketingrecruitmentforum');
- 
+
  $schvalid  = $this->objLanguage->languageText('mod_marketingrecruitmentforum_schvalid','marketingrecruitmentforum');
  $telvalid  = $this->objLanguage->languageText('mod_marketingrecruitmentforum_telvalid','marketingrecruitmentforum');
  $faxval = $this->objLanguage->languageText('mod_marketingrecruitmentforum_faxval','marketingrecruitmentforum');
@@ -53,31 +55,32 @@
  $enterPrincipalname = $this->objLanguage->languageText('mod_marketingrecruitmentforum_enterPrincipalname','marketingrecruitmentforum');
  $maxprincename = $this->objLanguage->languageText('mod_marketingrecruitmentforum_maxprincname1','marketingrecruitmentforum');
  $enterGuidanceName = $this->objLanguage->languageText('mod_marketingrecruitmentforum_enterguidancename','marketingrecruitmentforum');
- 
-/*------------------------------------------------------------------------------*/  
- 
+
+/*------------------------------------------------------------------------------*/
+
   /**
   *create form heading
   */
   $this->objMainheading =& $this->getObject('htmlheading','htmlelements');
   $this->objMainheading->type=1;
   $this->objMainheading->str=$objLanguage->languageText('mod_marketingrecruitmentforum_schoollist','marketingrecruitmentforum');
-  
+
   $this->objheading =& $this->newObject('htmlheading','htmlelements');
   $this->objheading->type=5;
   $this->objheading->str=$objLanguage->languageText('mod_marketingrecruitmentforum_warning','marketingrecruitmentforum');
-  
-/*------------------------------------------------------------------------------*/  
- 
+
+/*------------------------------------------------------------------------------*/
+
  /**
    *create all textinputs
    */
+
     $this->dbschoollist =& $this->getObject('dbschoollist','marketingrecruitmentforum');
     $schooldata []= $this->getSession('schoolvalues');
     $namevalue = $this->getSession('nameschool');
     $schoolbyname11 = $this->dbschoollist->getschoolbyname($namevalue, $field = 'schoolname', $start = 0, $limit = 0);
-    
-    $txtschoolname  = ' '; 
+
+    $txtschoolname  = ' ';
     $schooladdress  = ' ';
     $txttelcode = ' ';
     $txttelnumber = ' ';
@@ -90,12 +93,12 @@
     $principalcellno  = ' ';
     $guidanceteacheamil = ' ';
     $guidanceteachcellno  = ' ';
-    
-  
+
+
   if(!empty($schooldata)){
         foreach($schooldata as $sessschool){
-        
-            $txtschoolname  = $sessschool['schoolname']; 
+
+            $txtschoolname  = $sessschool['schoolname'];
             $schooladdress  = $sessschool['schooladdress'];
             $txttelcode = $sessschool['telcode'];
             $txttelnumber = $sessschool['telnumber'];
@@ -110,10 +113,10 @@
             $guidanceteachcellno  = $sessschool['guidanceteachcellno'];
         }
    }
-  
+
   if(!empty($schoolbyname11)){
           for($i=0; $i< count($schoolbyname11); $i++){
-            $txtschoolname  = $schoolbyname11[$i]->SCHOOLNAME; 
+            $txtschoolname  = $schoolbyname11[$i]->SCHOOLNAME;
             $schooladdress  = $schoolbyname11[$i]->SCHOOLADDRESS;
             $txttelnumber = $schoolbyname11[$i]->TELNUMBER;
             $txttelcode = $schoolbyname11[$i]->TELCODE;
@@ -128,29 +131,34 @@
             $guidanceteachcellno  = $schoolbyname11[$i]->GUIDANCETEACHCELLNO;
          }
   }
-/*--------------------------------------------------------------------------------------------*/               
+/*--------------------------------------------------------------------------------------------*/
 
-       $this->objfaculties =& $this->getObject('dbstudentcard','marketingrecruitmentforum');
+       /*$this->objfaculties =& $this->getObject('dbstudentcard','marketingrecruitmentforum');
        $schoolvalues = array();
-       $schoolnames = $this->objfaculties->getSchools(); 
+       $schoolnames = $this->objfaculties->getSchools();
        for($i=0; $i < count($schoolnames); $i++){
             $schoolvalues[$i]=$schoolnames[$i]->SCHOOLNAME;
            // $schoolcodeval[$i]  = $schoolnames[$i]->SCHOOLCODE;
             //$this->setSession('schoolcodeval',$schoolcodeval);
-       }
+       }*/
+/*       $schoolnames = $this->schoolnames->readfiledata();
+
+        for($i=0; $i < count($schoolnames); $i++){
+            $schoolvalues[$i]=$schoolnames[$i];
+        }
         
        //create dropdown list
        $searchlist  = new dropdown('schoollistactivity');
-       
+
        sort($schoolvalues);
        foreach($schoolvalues as $sessschool){
           $searchlist->addOption(NULL, ''.$schooselect);
           $searchlist->addOption($sessschool,$sessschool);
-       }
-/*--------------------------------------------------------------------------------------------*/ 
+       }*/
+/*--------------------------------------------------------------------------------------------*/
    /**
      *create dropdown list for all province values
-     */       
+     */
    $this->objprovincedropdown  = new dropdown('provinceschool');
    $this->objprovincedropdown->addOption(NULL,$provinceselect."&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp") ;
    $this->objprovincedropdown->addOption($wcprov,$wcprov) ;
@@ -163,172 +171,173 @@
    $this->objprovincedropdown->addOption($fprov,$fprov) ;
    $this->objprovincedropdown->addOption($knprov,$knprov) ;
 
-/*--------------------------------------------------------------------------------------------*/       
+/*--------------------------------------------------------------------------------------------*/
 /**
  *create a dropdown list with all area values
  */
-     $postAreaInfo = $this->objfaculties->getPostInfo(); 
-       for($i=0; $i < count($postAreaInfo); $i++){
-            $areavals[$i]=$postAreaInfo[$i]->CITY;
-       }
+
+     $postAreaInfo = $this->schoolnames->readareadata();
+      for($i=0; $i < count($postAreaInfo); $i++){
+            $areavals[$i]=$postAreaInfo[$i];
+        }
        //create dropdown list
        $arealist  = new dropdown('areaschool');
        sort($areavals);
        foreach($areavals as $sessarea){
           $arealist->addOption(NULL, " ".$areaselect."&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp");
           $arealist->addOption($sessarea,$sessarea);
-       }  
-/*--------------------------------------------------------------------------------------------*/ 
+       }
+/*--------------------------------------------------------------------------------------------*/
   /**
    *create all textboxes
-   */     
+   */
    $textArea = 'schooladdress';
    $this->objSchooladdress = new textArea('schooladdress');
    $this->objSchooladdress->setRows(3);
    $this->objSchooladdress->setColumns(48);
    $this->objSchooladdress->setContent($schooladdress);
-   
+
    $this->objtxttelcode = new textinput("txttelcode");
    $this->objtxttelcode->value  = $txttelcode;
    $this->objtxttelcode->size = 3;
-   
+
    $this->objtxtfaxcode = new textinput("txtfaxcode");
    $this->objtxtfaxcode->value  = $txtfaxcode;
    $this->objtxtfaxcode->size = 3;
-   
+
    $this->objtxttelnumber = new textinput("txttelnumber");
    $this->objtxttelnumber->value  = $txttelnumber;
    $this->objtxttelnumber->size = 26;
-   
+
    $this->objtxtfaxnumber = new textinput("txtfaxnumber");
    $this->objtxtfaxnumber->value  = $txtfaxnumber;
    $this->objtxtfaxnumber->size = 26;
-   
+
    $this->objtxtemail = new textinput("txtemail");
    $this->objtxtemail->value  = $txtemail;
    $this->objtxtemail->size = 35;
-   
+
    $this->objtxtprincipal = new textinput("txtprincipal");
    $this->objtxtprincipal->value  = $txtprincipal;
    $this->objtxtprincipal->size = 35;
-   
+
    $this->objtxtPrincEmailAddy = new textinput("txtprincemailaddy");
    $this->objtxtPrincEmailAddy->value  = $principalemailaddy;
    $this->objtxtPrincEmailAddy->size = 35;
-   
+
    $this->objtxtPrincCellNo = new textinput("txtprinccellno");
    $this->objtxtPrincCellNo->value  = $principalcellno;
    $this->objtxtPrincCellNo->size = 35;
-   
+
    $this->objtxtteacher = new textinput("txtteacher");
    $this->objtxtteacher->value  = $txtteacher;
    $this->objtxtteacher->size = 35;
-   
+
    $this->objtxtTeacherEmail = new textinput("txtteacheremail");
    $this->objtxtTeacherEmail->value  = $guidanceteacheamil;
    $this->objtxtTeacherEmail->size = 35;
-   
+
    $this->objtxtTeacherCellNo = new textinput("txtteachercellno");
    $this->objtxtTeacherCellNo->value  = $guidanceteachcellno;
    $this->objtxtTeacherCellNo->size = 35;
 
-/*------------------------------------------------------------------------------*/   
+/*------------------------------------------------------------------------------*/
    /**
      *create a next button
      */
     $this->objButtonNext  = new button('schoolnext', $str1);
     $this->objButtonNext->setToSubmit();
-   
+
    /**
      * get the schoolname selected from session variable
-     */        
+     */
      $nameselected  = $this->getSession('nameschool');
-/*------------------------------------------------------------------------------*/   
+/*------------------------------------------------------------------------------*/
   /**
    *create a table to place all form elements in
    */
-    
+
     $myTable=$this->newObject('htmltable','htmlelements');
     $myTable->width='80%';
     $myTable->border='0';
     $myTable->cellspacing='4';
     $myTable->cellpadding='10';
-           
+
     $myTable->startRow();
     $myTable->addCell(ucfirst($schoolname));
     $myTable->addCell('<h3>'.$nameselected.'<h3/>');
-    $myTable->endRow();   
-    
+    $myTable->endRow();
+
     $myTable->startRow();
     $myTable->addCell(ucfirst($schooladdy));
     $myTable->addCell("<span class=error>" .'<b>'.'*'."</span>".'</b>'.' '.$this->objSchooladdress->show());
-    $myTable->endRow(); 
-    
+    $myTable->endRow();
+
     $myTable->startRow();
     $myTable->addCell(ucfirst('Area / Town'));
     $myTable->addCell("<span class=error>" .'<b>'.'*'."</span>".'</b>'.' '.$arealist->show());
-    $myTable->endRow(); 
-    
+    $myTable->endRow();
+
     $myTable->startRow();
     $myTable->addCell(ucfirst($province));
     $myTable->addCell("<span class=error>" .'<b>'.'*'."</span>".'</b>'.' '.$this->objprovincedropdown->show());
     $myTable->endRow();
- 
+
     $myTable->startRow();
     $myTable->addCell(ucfirst($telnumber));
     $myTable->addCell("<span class=error>" .'<b>'.'&nbsp'."</span>".'</b>'.' '.$this->objtxttelcode->show().' '.$this->objtxttelnumber->show());
-    $myTable->endRow();  
-    
+    $myTable->endRow();
+
     $myTable->startRow();
     $myTable->addCell(ucfirst($faxnumber));
     $myTable->addCell("<span class=error>" .'<b>'.'&nbsp'."</span>".'</b>'.' '.$this->objtxtfaxcode->show().' '.$this->objtxtfaxnumber->show());
-    $myTable->endRow();  
-    
+    $myTable->endRow();
+
     $myTable->startRow();
     $myTable->addCell(ucfirst($email));
     $myTable->addCell("<span class=error>" .'<b>'.'*'."</span>".'</b>'.' '.$this->objtxtemail->show());
-    $myTable->endRow();  
-    
+    $myTable->endRow();
+
     $myTable->startRow();
     $myTable->addCell(ucfirst($principal));
     $myTable->addCell("<span class=error>" .'<b>'.'*'."</span>".'</b>'.' '.$this->objtxtprincipal->show());
-    $myTable->endRow();  
-    
+    $myTable->endRow();
+
     $myTable->startRow();
     $myTable->addCell($principalEmailAddy);
     $myTable->addCell("<span class=error>" .'<b>'.'*'."</span>".'</b>'.' '.$this->objtxtPrincEmailAddy->show());
-    $myTable->endRow(); 
-    
+    $myTable->endRow();
+
     $myTable->startRow();
     $myTable->addCell($prinicipalCellNo);
     $myTable->addCell("&nbsp"."&nbsp".$this->objtxtPrincCellNo->show());
-    $myTable->endRow(); 
-    
+    $myTable->endRow();
+
     $myTable->startRow();
     $myTable->addCell(ucfirst($guidanceteacher));
     $myTable->addCell("<span class=error>" .'<b>'.'*'."</span>".'</b>'.' '.$this->objtxtteacher->show());
     $myTable->endRow();
-    
+
     $myTable->startRow();
     $myTable->addCell($guidanceteachemail);
     $myTable->addCell("&nbsp"."&nbsp".$this->objtxtTeacherEmail->show());
     $myTable->endRow();
-    
+
     $myTable->startRow();
     $myTable->addCell($guidanceteachcellno1);
     $myTable->addCell("&nbsp"."&nbsp".$this->objtxtTeacherCellNo->show());
     $myTable->endRow();
-    
+
     $myTable->startRow();
     $myTable->addCell($this->objButtonNext->show());
-    $myTable->endRow();    
+    $myTable->endRow();
 
-/*------------------------------------------------------------------------------*/    
-    
+/*------------------------------------------------------------------------------*/
+
   /**
    *create a form to place all elements in
    */
-   
+
    $objForm = new form('schoollist',$this->uri(array('action'=>'showschooloutput')));
    $objForm->displayType = 3;
    $objForm->addToForm($this->objMainheading->show().'<br />'."<span class=error>".'<i>'.$this->objheading->show().'</i>'."</span>".'<br />'.$myTable->show());
@@ -346,13 +355,12 @@
    $objForm->addRule('txtprincemailaddy',$emailformat,'email');
    $objForm->addRule('txtteacher',$enterGuidanceName,'required');
    $objForm->addRule(array('name'=>'txtteacher','length'=>45),$maxprincename, 'maxlength');
-   
-/*------------------------------------------------------------------------------*/   
-          
+
+/*------------------------------------------------------------------------------*/
+
    /**
      *display the schoolist interface
      */
-                                
-   echo  $objForm->show();	          
-  
+   echo  $objForm->show();
+
 ?>
