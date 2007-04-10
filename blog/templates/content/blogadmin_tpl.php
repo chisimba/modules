a03830a35d6cbf6e17db23b62d790e7a1a962e91
paulscott <unknown@unknown.com>
2007-04-10 11:42:21 +0000

    fixed up blogadmin page

--- blog/templates/content/blogadmin_tpl.php
+++ blog/templates/content/blogadmin_tpl.php
@@ -8,6 +8,17 @@ $this->loadClass('form', 'htmlelements');
 $this->loadClass('href', 'htmlelements');
 $this->loadClass('htmlarea', 'htmlelements');
 
+$userid = $this->objUser->userId();
+$profile = $this->objDbBlog->checkProfile($userid);
+/*$dsnparse = $this->objblogOps->getMailDSN();
+if($dsnparse == FALSE)
+{
+	//$mailsetup = $this->objblogOps->showMailSetup(TRUE);
+}
+else {
+	//$mailsetup = $this->objblogOps->showMailSetup(TRUE, $dsnparse);
+}*/
+
 $tt = $this->newObject('domtt', 'htmlelements');
 $pane = &$this->newObject('tabpane', 'htmlelements');
 
@@ -25,8 +36,53 @@ $middleColumn = NULL;
 $leftCol = $leftMenu->show();
 //$leftCol .= $this->objblogOps->rssBox('http://slashdot.org/slashdot.rdf', 'Slashdot');
 
+$tabpane = $this->getObject('tabcontent', 'htmlelements');
+$tabpane->width = '80%';
 
-$middleColumn .= $this->objblogOps->showAdminSection(TRUE, TRUE);
+//write new post
+$nplabel = $this->objLanguage->languageText("mod_blog_writepost", "blog");
+$npcontent = $this->objblogOps->postEditor($userid, NULL);
+$nplink = NULL;
+$tabpane->addTab($nplabel, $npcontent, $nplink, TRUE);
+
+//Profile editor
+$prflabel = $this->objLanguage->languageText("mod_blog_setprofile", "blog");
+$prfcontent = $this->objblogOps->profileEditor($userid, $profile);
+$prflink = NULL;
+$tabpane->addTab($prflabel, $prfcontent, $prflink, FALSE);
+
+//blog importer
+//$implabel = $this->objLanguage->languageText("mod_blog_blogimport", "blog");
+//$impcontent = $this->objblogOps->showImportForm(FALSE);
+//$implink = NULL;
+//$tabpane->addTab($implabel, $impcontent, $implink, FALSE);
+
+//edit posts
+$eplabel = $this->objLanguage->languageText("mod_blog_word_editposts", "blog");
+$epcontent = $this->objblogOps->managePosts($userid);
+$eplink = NULL;
+$tabpane->addTab($eplabel, $epcontent, $eplink, FALSE);
+
+//edit/create cats
+//$eclabel = $this->objLanguage->languageText("mod_blog_word_categories", "blog");
+//$eccontent = $this->objblogOps->categoryEditor($userid);
+//$eclink = NULL;
+//$tabpane->addTab($eclabel, $eccontent, $eclink, FALSE);
+
+//add delete rss feeds
+$rslabel = $this->objLanguage->languageText("mod_blog_rssaddedit", "blog");
+$rscontent = $this->objblogOps->rssEditor(FALSE);
+$rslink = NULL;
+$tabpane->addTab($rslabel, $rscontent, $rslink, FALSE);
+
+//mail setup
+//$maillabel = $this->objLanguage->languageText("mod_blog_setupmail", "blog");
+//$mailcontent = $mailsetup;
+//$maillink = NULL;
+//$tabpane->addTab($maillabel, $mailcontent, $maillink, FALSE);
+
+$middleColumn .= $tabpane->show();
+//$middleColumn .= $this->objblogOps->showAdminSection(TRUE, TRUE);
 
 
 $rightSideColumn .= $this->objblogOps->quickCats(TRUE);
