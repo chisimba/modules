329d87615a402779b51180eb7ce9c98d291d18e0
paulscott <unknown@unknown.com>
2007-04-18 05:16:04 +0000

    fixed up captcha

--- feedback/controller.php
+++ feedback/controller.php
@@ -54,7 +54,7 @@ class feedback extends controller
 					$fbee = $this->getParam('fbee');
 					$fbw = $this->getParam('fbw');
 					
-					if (md5(strtoupper($captcha)) != $this->getParam('request_captcha') || !isset($captcha))
+					if (md5(strtoupper($captcha)) != $this->getParam('captcha') || empty($captcha))
 					{
 						$insarr = array('userid' => $userid, 'fbname' => $fbname, 'fbemail' => $fbemail, 'fbww' => $fbww, 'fbnw' => $fbnw, 'fblo' => $fblo, 'fbsp' => $fbsp, 'fbee' => $fbee, 'fbw' => $fbw);
 						$msg = 'badcaptcha';
