b9dd198f4e99f964dbe6ccd9008527fd39ba0443
colleen <unknown@unknown.com>
2007-05-14 07:46:51 +0000

    *** empty log message ***

--- marketingrecruitmentforum/templates/content/idnumberform_tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-//template that shows details or no details , determined by id no
-  $this->objstudcard = & $this->getObject('searchstudcard','marketingrecruitmentforum');
-  $results  = $this->objstudcard->searchID($idsearch);
-  
-  echo $results;
-?>
--- marketingrecruitmentforum/templates/content/schoolform_tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-//template that shows details or no details , determined by id no
-
-  $this->objschool  = & $this->getObject('searchschools','marketingrecruitmentforum');
-  $result = $this->objschool->schoolexisting($schoolbyname);
-  
-  echo $result;
-?>
