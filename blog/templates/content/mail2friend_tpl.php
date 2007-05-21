a595f7272ad4df5e91882e9b7f469649302c1167
paulscott <unknown@unknown.com>
2007-05-21 11:03:42 +0000

    blog UI elements

--- blog/templates/content/mail2friend_tpl.php
+++ blog/templates/content/mail2friend_tpl.php
@@ -1,38 +1,19 @@
 <?php
 //mail2friend template
 $userid = $m2fdata['bloggerid'];
-
-//$this->loadClass('href', 'htmlelements');
-//$tt = $this->newObject('domtt', 'htmlelements');
 $cssLayout = &$this->newObject('csslayout', 'htmlelements');
-//$objSideBar = $this->newObject('usermenu', 'toolbar');
 // Set columns to 3
 $cssLayout->setNumColumns(3);
-//$leftMenu = NULL;
 $objUi = $this->getObject('blogui');
 // left hand blocks
 $leftCol = $objUi->leftBlocks($userid);
 // right side blocks
 $rightSideColumn = $objUi->rightBlocks($userid, NULL);
-//$rightSideColumn = NULL;
-
-//$leftCol = NULL;
 $middleColumn = NULL;
-
 //load up a featurebox and display it nicely
 $objFeatureBox = $this->getObject('featurebox', 'navigation');
-
 //gooi the form with a message and the name thing with email address(es) to send to
 $middleColumn .= $this->objblogOps->sendMail2FriendForm($m2fdata);
-
-//if($this->objUser->isLoggedIn())
-//{
-//	$leftCol .= $objSideBar->show();
-//}
-//else {
-//	$leftCol = $this->objblogOps->loginBox(TRUE);
-//}
-
 $cssLayout->setMiddleColumnContent($middleColumn);
 $cssLayout->setLeftColumnContent($leftCol); //$leftMenu->show());
 $cssLayout->setRightColumnContent($rightSideColumn);
--- blog/templates/content/mailsetup_tpl.php
+++ blog/templates/content/mailsetup_tpl.php
@@ -1,29 +1,14 @@
 <?php
-
-//initiate objects
-$this->loadClass('label', 'htmlelements');
-$this->loadClass('textinput', 'htmlelements');
-$this->loadClass('form', 'htmlelements');
-//$this->loadClass('heading', 'htmlelements');
-$this->loadClass('href', 'htmlelements');
-$this->loadClass('htmlarea', 'htmlelements');
-
-$tt = $this->newObject('domtt', 'htmlelements');
-$pane = &$this->newObject('tabpane', 'htmlelements');
-
+$userid = $this->objUser->userId();
+$objUi = $this->getObject('blogui');
+// left hand blocks
+$leftCol = $objUi->leftBlocks($userid);
+// right side blocks
+$rightSideColumn = $objUi->rightBlocks($userid, NULL);
 $cssLayout = &$this->newObject('csslayout', 'htmlelements');
-$objSideBar = $this->newObject('sidebar', 'navigation');
-
 // Set columns to 3
 $cssLayout->setNumColumns(3);
-$leftMenu = &$this->newObject('usermenu', 'toolbar');
-$rightSideColumn = NULL;
-$leftCol = NULL;
 $middleColumn = NULL;
-
-//left menu section
-$leftCol = $leftMenu->show();
-
 //check if there are previous values
 $dsnparse = $this->objblogOps-> getMailDSN();
 if($dsnparse == FALSE)
@@ -33,11 +18,6 @@ if($dsnparse == FALSE)
 else {
 	$middleColumn .= $this->objblogOps->showMailSetup(TRUE, $dsnparse);
 }
-
-
-//$rightSideColumn .= $this->objblogOps->quickCats(TRUE);
-$rightSideColumn .= $this->objblogOps->showAdminSection(TRUE);
-//$rightSideColumn .= $this->objblogOps->quickPost($userid, TRUE);
 $cssLayout->setMiddleColumnContent($middleColumn);
 $cssLayout->setLeftColumnContent($leftCol);
 $cssLayout->setRightColumnContent($rightSideColumn);
