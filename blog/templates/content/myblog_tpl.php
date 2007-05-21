df576d9409d7aced0d2b1d1fea31fa6bca7d5f52
paulscott <unknown@unknown.com>
2007-05-21 11:16:27 +0000

    blog UI elements

--- blog/templates/content/myblog_tpl.php
+++ blog/templates/content/myblog_tpl.php
@@ -3,43 +3,13 @@
 $cssLayout = &$this->newObject('csslayout', 'htmlelements');
 // Set columns to 3
 $cssLayout->setNumColumns(3);
-$leftMenu = &$this->newObject('usermenu', 'toolbar');
-$rightSideColumn = NULL; //$this->objLanguage->languageText('mod_blog_instructions', 'blog');
 $middleColumn = NULL;
 
-//show the link to all blogs
-$rightSideColumn .= $this->objblogOps->showBlogsLink(TRUE);
-
-
-$rightSideColumn .= $this->objblogOps->blogTagCloud($userid);
-//$rightSideColumn .= "<br />";
-
-//show the categories menu (if there are cats)
-$rightSideColumn .= $this->objblogOps->showCatsMenu($cats, TRUE, $userid);
-$rightSideColumn .= $this->objblogOps->archiveBox($userid, TRUE);
-$leftCol = NULL;
-//show the admin section (if user is logged in)
-if(!$this->objUser->isLoggedIn())
-{
-	$leftCol = $this->objblogOps->loginBox(TRUE);
-	$leftCol .= $this->objblogOps->showProfile($userid);
-}
-else {
-	$guestid = $this->objUser->userId();
-	if($guestid == $userid)
-	{
-		$leftCol .= $leftMenu->show();
-		$leftCol .= $this->objblogOps->showProfile($userid);
-	}
-	else {
-		//echo "guest is diff";
-		$leftCol .= $this->objblogOps->showFullProfile($userid);
-	}
-	//$leftCol .= $objSideBar->show();
-	$leftCol .= "<br />";
-	$leftCol .= $this->objblogOps->showProfile($userid);
-	$rightSideColumn .= $this->objblogOps->showAdminSection(TRUE);
-}
+$objUi = $this->getObject('blogui');
+// left hand blocks
+$leftCol = $objUi->leftBlocks($userid);
+// right side blocks
+$rightSideColumn = $objUi->rightBlocks($userid, NULL);
 
 //show all the posts
 if(isset($catid) && empty($posts) && empty($latestpost))
@@ -109,33 +79,7 @@ else {
 			$middleColumn .= "<center>" . $linker->show() . "</center>";
 		}
 	}
-
-	/*
-	$header = new htmlheading();
-	$header->type = 3;
-	$header->str = $this->objLanguage->languageText("mod_blog_latestpost", "blog") . ": " . $this->objDbBlog->getCatById($latestpost[0]['post_category']);
-	$middleColumn .= $header->show();
-	if($posts[0]['id'] == $latestpost[0]['id'])
-	{
-		unset($posts[0]);
-	}
-	$middleColumn .= $this->objblogOps->showPosts($latestpost);
-	$middleColumn .= "<hr />";
-
-	$headerprev = new htmlheading();
-	$headerprev->type = 3;
-	$headerprev->str = $this->objLanguage->languageText("mod_blog_previousposts", "blog");
-	$middleColumn .= $headerprev->show();
-	$middleColumn .= ($this->objblogOps->showPosts($posts));
-*/
 }
-
-
-
-//show the feeds section
-$leftCol .= $this->objblogOps->showFeeds($userid, TRUE);
-$leftCol .= $this->objblogOps->showBlinks($userid, TRUE);
-$leftCol .= $this->objblogOps->showBroll($userid, TRUE);
 if(!empty($rss))
 {
 	foreach($rss as $feeds)
@@ -155,11 +99,6 @@ if(!empty($rss))
 
 	}
 }
-
-
-//$leftCol .= $this->objblogOps->rssBox('http://5ive.uwc.ac.za/index.php?module=blog&action=feed&userid=5729061010', 'Paul on 5ive');
-
-//$rightSideColumn .= $this->objblogOps->archiveBox($userid, TRUE);
 //dump the cssLayout to screen
 $cssLayout->setMiddleColumnContent($middleColumn);
 $cssLayout->setLeftColumnContent($leftCol);
