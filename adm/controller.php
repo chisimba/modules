e3c334ca3e4603317e3e8946825dd580bf81bdc3
paulscott <unknown@unknown.com>
2007-06-07 09:46:48 +0000

    adm module

---
+++ adm/controller.php
@@ -0,0 +1,40 @@
+<?php
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+ini_set("max_execution_time", -1);
+// end security check
+class adm extends controller
+{
+	/**
+     * Constructor method to instantiate objects and get variables
+     */
+    public function init()
+    {
+        try {
+            $this->objLanguage = $this->getObject('language', 'language');
+            
+        }
+        catch(customException $e) {
+            echo customException::cleanUp();
+            die();
+        }
+    }
+    /**
+     * Method to process actions to be taken
+     *
+     * @param string $action String indicating action to be taken
+     */
+    public function dispatch($action = Null)
+    {
+        //$this->setLayoutTemplate('beautifier_layout_tpl.php');
+
+        switch ($action) {
+            default:
+                echo "ADM is a work in progress!";
+                die();
+        }
+    }
+}
+?>
