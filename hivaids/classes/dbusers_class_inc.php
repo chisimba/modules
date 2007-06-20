4320d2e0667a36cefe7586b38b46f71b4439200b
megan <unknown@unknown.com>
2007-06-20 10:50:27 +0000

    Added a db class for the users table

---
+++ hivaids/classes/dbusers_class_inc.php
@@ -0,0 +1,58 @@
+<?php
+/**
+* dbusers class extends object
+* @package hivaids
+* @filesource
+*/
+
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']){
+    die("You cannot view this page directly");
+}
+
+/**
+* dbusers class
+* @author Megan Watson
+* @copyright (c) 2007 UWC
+* @version 0.1
+*/
+
+class dbusers extends dbtable
+{
+    /**
+    * Constructor method
+    */
+    public function init()
+    {
+        parent::init('tbl_hivaids_users');
+        $this->table = 'tbl_hivaids_users';
+        
+        $this->objUser = $this->getObject('user', 'security');
+        $this->userId = $this->objUser->userId();
+    }
+    
+    /**
+    * Method to add a new user
+    *
+    * @access public
+    * @param string $id The row id of the user
+    * @return void
+    */
+    public function addUser($userId = NULL, $id = NULL)
+    {
+        $fields = array();
+        $fields['course'] = $this->getParam('course');
+        $fields['study_year'] = $this->getParam('yearstudy');
+        $fields['updated'] = $this->now();
+        
+        if(!empty($id)){
+            $fields['modifierid'] = $this->userId;
+            $this->update('id', $id, $fields);
+        }else{
+            $fields['user_id'] = $userId;
+            $fields['datecreated'] = $this->now();
+            $id = $this->insert($fields);
+        }
+    }
+}
+?>
