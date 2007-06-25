293553c8c5ef783ccc72775580460fcdb52fac4d
David Wafula <davidwaf@gmail.com>
2007-06-25 16:32:46 +0000

    added the open office presentation functionality

--- templates/content/realtime_tpl.php
+++ templates/content/realtime_tpl.php
@@ -2,6 +2,7 @@
 ?>
 <br/>
 <p>
+$link='<a href="popo">popo</a>';
 Welcome to the realtime tools. Select a link below to load and run a specific tool:
 </p>
 <ul>
@@ -10,6 +11,7 @@ Welcome to the realtime tools. Select a link below to load and run a specific to
 	<li><a href="index.php?module=realtime&amp;action=voice">Voice</a> - the realtime voice applet (requires JMF).</li>
 	<li><a href="index.php?module=realtime&amp;action=whiteboard">Whiteboard</a> - the realtime whiteboard applet (requires a 
 	running whiteboard server).</li>
+	<li><a href="index.php?module=realtime&amp;action=presentations">Presentations</a> - the realtime presentations.</li>
 	<li><a href="index.php?module=realtime&amp;action=classroom">Classroom</a> - the realtime classroom consisting of a mashup of 
 	the voice and classroom applets as well as the messaging module. You must be in a context for the 
 	messaging to work. The whiteboard server must also be running and JMF must be installed.</li>
