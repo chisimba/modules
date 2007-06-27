cb7d4e4ec241602f5bd598217cfc8ac56579cb32
nsabagwa <unknown@unknown.com>
2007-06-27 09:35:57 +0000

    Second version of tbl_fixtures

--- sportsadmin/sql/tbl_fixtures.sql
+++ sportsadmin/sql/tbl_fixtures.sql
@@ -1,5 +1,5 @@
 <?php
-/*
+
   $sqldata[]="CREATE TABLE tbl_fixtures (
   id varchar(32) NOT NULL default '',
   team_A varchar(32) NOT NULL default '',
@@ -29,61 +29,6 @@
   
 )TYPE=InnoDB
 ";
-*/
 
-//5ive definition
-$tablename = 'tbl_fixtures';
 
-//Options line for comments, encoding and character set
-$options = array('comment' => 'Table to hold the fixtures', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
-
-//
-$fields = array(
-	'id' => array(
-		'type' => 'text',
-		'length' => 32,
-		'notnull' => 1
-		),
-	'team_A' => array(
-		'type' => 'text',
-		'length' => 32,
-		'notnull'=> 1
-		),
-	'team_B' => array(
-		'type' => 'text',
-		'length' => 32,
-		'notnull' => 1
-		),
-	'tournamentId' => array(
-		'type' => 'text',
-		'length' => 32,
-		'notnull' => 1
-		),
-	'place' => array(
-		'type' => 'text',
-		'length' => 32
-		),
-	'matchDate' => array(
-		'type' => 'timestamp'
-		),
-	'sportId' => array(
-		'type' => 'text',
-		'length' => 32
-		),
-	'updated' => array(
-		'type' => 'timestamp'
-		)
-	);
-	
-// create other indexes here...
-$name = 'fixtures_index';
-
-$indexes = array(
-                'fields' => array(
-					'tournamentId' => array(),
-                    'team_A' => array(),
-					'team_B' => array(),
-                    'sportId' => array(),
-                ),
-        );
 ?>
