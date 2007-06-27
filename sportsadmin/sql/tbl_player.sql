1d8fb7d12ea32f987ee6d30ad4fea9b812d32422
nsabagwa <unknown@unknown.com>
2007-06-27 09:43:29 +0000

    second version of the class that manages players

--- sportsadmin/sql/tbl_player.sql
+++ sportsadmin/sql/tbl_player.sql
@@ -1,5 +1,5 @@
 <?php
-/*
+
   $sqldata[]="CREATE TABLE tbl_player (
   id varchar(32) NOT NULL default '',
   name varchar(32) NOT NULL default '',
@@ -18,60 +18,6 @@
   
 )TYPE=InnoDB
 ";
-*/
 
 
-//5ive definition
-$tablename = 'tbl_player';
-
-//Options line for comments, encoding and character set
-$options = array('comment' => 'Table to hold the players information', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
-
-//
-$fields = array(
-	'id' => array(
-		'type' => 'text',
-		'length' => 32,
-		'notnull' => 1
-		),
-	'name' => array(
-		'type' => 'text',
-		'length' => 32
-		),
-	'dateOfBirth' => array(
-		'type' => 'timestamp'
-		),
-	'team' => array(
-		'type' => 'text',
-		'length' => 32
-		),
-	'country' => array(
-		'type' => 'text',
-		'length' => 32
-		),
-	'sportId' => array(
-		'type' => 'text',
-		'length' => 32
-		),
-	'playerimage' => array(
-		'type' => 'text',
-		'length' => 32
-		),
-	'position' => array(
-		'type' => 'text',
-		'length' => 32
-		),
-	'updated' => array(
-		'type' => 'timestamp'
-		)
-	);
-	
-// create other indexes here...
-$name = 'player_index';
-
-$indexes = array(
-                'fields' => array(
-                    'sportId' => array()
-                )
-        );
 ?>
