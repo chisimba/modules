c8f90a0a596b4d89ac5c42c762d7e57f900e7152
tohir <unknown@unknown.com>
2007-06-28 16:22:22 +0000

    template no longer needed

--- contextcontent/templates/layout/layout_contextcontent_tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$objTOC = $this->getObject('tableofcontents');
-
-$cssLayout = $this->newObject('csslayout', 'htmlelements');
-$cssLayout->setLeftColumnContent('Nav goes here');//$objTOC->show());
-$cssLayout->setMiddleColumnContent($this->getContent());
-echo $cssLayout->show();
-?>
