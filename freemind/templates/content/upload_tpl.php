96a8317c568b628645420c74a71b1513b4eed566
nitsckie <unknown@unknown.com>
2007-07-02 10:23:00 +0000

    *** empty log message ***

--- freemind/templates/content/upload_tpl.php
+++ freemind/templates/content/upload_tpl.php
@@ -1,17 +1,22 @@
 <?php
-$form=&$this->newObject('form','htmlelements');
+
+$this->loadClass('textinput', 'htmlelements');
+$this->loadClass('htmlheading', 'htmlelements');
+$this->loadClass('form', 'htmlelements');
+
+$form=new form();
 $form->setAction($this->uri(array('action'=>'save')));
 $form->setDisplayType(3);
 $form->extra=' enctype="multipart/form-data" ';
 
-$h3=$this->newObject('htmlheading','htmlelements');
+$h3=new htmlheading();
 $h3->str=$this->objLanguage->languageText("mod_freemind_upload", "freemind");
 
-$file=&$this->newObject('textinput','htmlelements');
+$file=new textinput();
 $file->fldType='file';
 $file->name='mapupload';
 
-$submit=&$this->newObject('navbuttons','navigation');
+$submit= $this->newObject('navbuttons','navigation');
 
 $objSelectFile = $this->newObject('selectfile', 'filemanager');
 
