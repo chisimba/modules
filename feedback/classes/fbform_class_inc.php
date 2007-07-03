fa2364c403b8675fd1b8a79a52cfcaf57b62059b
acquim <unknown@unknown.com>
2007-07-03 09:09:14 +0000

    *** empty log message ***

--- feedback/classes/fbform_class_inc.php
+++ feedback/classes/fbform_class_inc.php
@@ -7,7 +7,7 @@ if (!$GLOBALS['kewl_entry_point_run']) {
 
 class fbform extends object
 {
-
+    
 	public function init()
 	{
 		try {
@@ -27,7 +27,8 @@ class fbform extends object
 	}
 
 	public function dfbform($insarr)
-	{
+	{   
+       
 		$objCaptcha = $this->getObject('captcha', 'utilities');
 		$required = '<span class="warning"> * '.$this->objLanguage->languageText('word_required', 'system', 'Required').'</span>';
 		$dfbform = new form('save', $this->uri(array(
@@ -37,110 +38,70 @@ class fbform extends object
 		//start a fieldset
 		$fbfieldset = $this->getObject('fieldset', 'htmlelements');
 		
-		$fbadd = $this->newObject('htmltable', 'htmlelements');
-		$fbadd->cellpadding = 3;
-
+		$fbtable = $this->newObject('htmltable', 'htmlelements');
+		$fbtable->cellpadding = 3;
+        
+        
+       
+        
 		//name textfield
-		$fbadd->startRow();
+		$fbtable->startRow();
 		$fbnamelabel = new label($this->objLanguage->languageText('mod_feedback_thename', 'feedback') .':', 'input_fbname');
 		$fbname = new textinput('fbname');
 		if(isset($insarr['fbname']))
 		{
 			$fbname->setValue($insarr['fbname']);
 		}
-		$fbadd->addCell($fbnamelabel->show().$required);
-		$fbadd->addCell($fbname->show());
-		$fbadd->endRow();
-
+		$fbtable->addCell($fbnamelabel->show().$required);
+		$fbtable->addCell($fbname->show());
+		$fbtable->endRow();
+        
 		//email textfield
-		$fbadd->startRow();
+		$fbtable->startRow();
 		$fbemaillabel = new label($this->objLanguage->languageText('mod_feedback_email', 'feedback') .':', 'input_fbemail');
 		$fbemail = new textinput('fbemail');
 		if(isset($insarr['fbemail']))
 		{
 			$fbemail->setValue($insarr['fbemail']);
 		}
-		$fbadd->addCell($fbemaillabel->show().$required);
-		$fbadd->addCell($fbemail->show());
-		$fbadd->endRow();
+		$fbtable->addCell($fbemaillabel->show().$required);
+		$fbtable->addCell($fbemail->show());
+		$fbtable->endRow();
 
-		//worked well text field
-		$fbadd->startRow();
-		$fbwwlabel = new label($this->objLanguage->languageText('mod_feedback_ww', 'feedback') .':', 'input_fbww');
-		$fbww = new textarea('fbww');
-		if(isset($insarr['fbww']))
-		{
-			$fbww->setValue($insarr['fbww']);
-		}
-		$fbadd->addCell($fbwwlabel->show());
-		$fbadd->addCell($fbww->show());
-		$fbadd->endRow();
-
-		//didn't work well text field
-		$fbadd->startRow();
-		$fbnwlabel = new label($this->objLanguage->languageText('mod_feedback_nw', 'feedback') .':', 'input_fbnw');
-		$fbnw = new textarea('fbnw');
-		if(isset($insarr['fbnw']))
-		{
-			$fbnw->setValue($insarr['fbnw']);
-		}
-		$fbadd->addCell($fbnwlabel->show());
-		$fbadd->addCell($fbnw->show());
-		$fbadd->endRow();
-
-		//what did we leave out text field
-		$fbadd->startRow();
-		$fblolabel = new label($this->objLanguage->languageText('mod_feedback_lo', 'feedback') .':', 'input_fblo');
-		$fblo = new textarea('fblo');
-		if(isset($insarr['fblo']))
-		{
-			$fblo->setValue($insarr['fblo']);
-		}
-		$fbadd->addCell($fblolabel->show());
-		$fbadd->addCell($fblo->show());
-		$fbadd->endRow();
-
-		//speakers & programme
-		$fbadd->startRow();
-		$fbsplabel = new label($this->objLanguage->languageText('mod_feedback_sp', 'feedback') .':', 'input_fbsp');
-		$fbsp = new textarea('fbsp');
-		if(isset($insarr['fbsp']))
-		{
-			$fbsp->setValue($insarr['fbsp']);
-		}
-		$fbadd->addCell($fbsplabel->show());
-		$fbadd->addCell($fbsp->show());
-		$fbadd->endRow();
-
-		//exhibition and exhibitors
-		$fbadd->startRow();
-		$fbeelabel = new label($this->objLanguage->languageText('mod_feedback_ee', 'feedback') .':', 'input_fbee');
-		$fbee = new textarea('fbee');
-		if(isset($insarr['fbee']))
-		{
-			$fbee->setValue($insarr['fbee']);
-		}
-		$fbadd->addCell($fbeelabel->show());
-		$fbadd->addCell($fbee->show());
-		$fbadd->endRow();
-
-		//website
-		$fbadd->startRow();
-		$fbwlabel = new label($this->objLanguage->languageText('mod_feedback_w', 'feedback') .':', 'input_fbw');
-		$fbw = new textarea('fbw');
-		if(isset($insarr['fbw']))
-		{
-			$fbw->setValue($insarr['fbw']);
-		}
-		$fbadd->addCell($fbwlabel->show());
-		$fbadd->addCell($fbw->show());
-		$fbadd->endRow();
-		
-		$fbadd->startRow();
+       
+		// addition started
+            $objDb = $this->getObject('dbfb_questions');
+            $questions = $objDb->get_questions();
+            //$questions =  $objDb->get_questions();
+            $responses_array_size = count($questions);
+            for($i = 0; $i < $responses_array_size; $i++){
+                //echo "question ".$questions[$i]['fb_question']."<br/>";
+                $fbtable->startRow();
+		        $fbwlabel = new label($questions[$i]['fb_question'], '');
+                $objInput = new textinput('inputQuestion_id_'.($i + 1), $questions[$i]['puid']);
+                //echo 'inputQuestion_id_'.($i + 1).": ".$questions[$i]['puid'].": ".$questions[$i]['fb_question']."<br/>";
+                $objInput->fldType = 'hidden';
+                //$hidden.= $objInput->show();
+		        $fbw = new textarea('inputQuestion_'.($i + 1));
+		        if(isset($insarr['']))
+		        {
+			        $fbw->setValue($insarr['fbw']);
+		        }
+               //$fbtable->addCell($objInput->show());
+                $dfbform->addToForm($objInput->show());
+		        $fbtable->addCell($fbwlabel->show());
+		        $fbtable->addCell($fbw->show());
+		        $fbtable->endRow();
+            }
+           $objInput = new textinput('responses_array_size', $responses_array_size);
+           $objInput->fldType = 'hidden';
+           $dfbform->addToForm($objInput->show());
+        //addition ended
+		$fbtable->startRow();
 		$captcha = new textinput('request_captcha');
 		$captchaLabel = new label($this->objLanguage->languageText('phrase_verifyrequest', 'security', 'Verify Request'), 'input_request_captcha');
-		$fbadd->addCell(stripslashes($this->objLanguage->languageText('mod_security_explaincaptcha', 'security', 'To prevent abuse, please enter the code as shown below. If you are unable to view the code, click on "Redraw" for a new one.')).'<br /><div id="captchaDiv">'.$objCaptcha->show().'</div>'.$captcha->show().$required.'  <a href="javascript:redraw();">'.$this->objLanguage->languageText('word_redraw', 'security', 'Redraw').'</a>');
-		$fbadd->endRow();
+		$fbtable->addCell(stripslashes($this->objLanguage->languageText('mod_security_explaincaptcha', 'security', 'To prevent abuse, please enter the code as shown below. If you are unable to view the code, click on "Redraw" for a new one.')).'<br /><div id="captchaDiv">'.$objCaptcha->show().'</div>'.$captcha->show().$required.'  <a href="javascript:redraw();">'.$this->objLanguage->languageText('word_redraw', 'security', 'Redraw').'</a>');
+		$fbtable->endRow();
 		
 		//add rules
 		$dfbform->addRule('fbname', $this->objLanguage->languageText("mod_feedback_phrase_needname", "feedback") , 'required');
@@ -151,11 +112,12 @@ class fbform extends object
 		$this->objSaveButton = &new button($this->objLanguage->languageText('word_save', 'system'));
 		$this->objSaveButton->setValue($this->objLanguage->languageText('word_save', 'system'));
 		$this->objSaveButton->setToSubmit();
-		$fbfieldset->addContent($fbadd->show());
+		$fbfieldset->addContent($fbtable->show());
 		$dfbform->addToForm($fbfieldset->show());
 		$dfbform->addToForm($this->objSaveButton->show());
 		$dfbform = $dfbform->show();
-
+       
+        
 		//featurebox it...
 		$objFbFeaturebox = $this->getObject('featurebox', 'navigation');
 		return $objFbFeaturebox->show($this->objLanguage->languageText("mod_feedback_feedback", "feedback"), $dfbform);
@@ -177,7 +139,7 @@ class fbform extends object
      */
     public function loginBox($featurebox = FALSE)
     {
-        $objLogin = &$this->getObject('logininterface', 'security');
+        $objLogin = $this->getObject('logininterface', 'security');
         if ($featurebox == FALSE) {
             return $objLogin->renderLoginBox('feedback');
         } else {
