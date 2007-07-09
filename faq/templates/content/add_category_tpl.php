0ce5ea4aac0e524c8807c7aa55f4acd6ce6858a3
mpscheidt <unknown@unknown.com>
2007-07-09 09:59:49 +0000

    Templates imported from faqadmin

---
+++ faq/templates/content/add_category_tpl.php
@@ -0,0 +1,30 @@
+<?php
+// Show the heading.
+$objHeading =& $this->getObject('htmlheading','htmlelements');
+$objHeading->type=1;
+$objHeading->str =$objLanguage->languageText("mod_faq_addcategory","faq");
+echo $objHeading->show();
+// Load the classes.
+$this->loadClass("form","htmlelements");
+$this->loadClass("textinput","htmlelements");
+$this->loadClass("button","htmlelements");
+// Create the form.
+$form = new form("createcategory", $this->uri(array('action'=>'addCategoryConfirm')));
+
+$textInput = new textinput("category", NULL);
+$textInput->size = 40;
+
+$form->setDisplayType(1);
+$form->addToForm($textInput->show());
+$form->addToForm("&nbsp;");
+$button = new button("submit", $objLanguage->languageText("word_save"));
+$button->setToSubmit();
+
+$cancelButton = new button("submit", $objLanguage->languageText('word_cancel'));
+$cancelButton->setOnClick("window.location='".$this->uri(NULL)."';");
+
+$form->addToForm($button->show().' / '.$cancelButton->show());
+// Show the form.
+echo $form->show();
+?>
+  
