30813f0aa2918a0525a4433e5ed3969eb6f51e7d
Prince Mbekwa <pmbekwa@gmail.com>
2007-07-18 09:23:34 +0000

    fixed tree layout bugs

--- cmsadmin/templates/layout/cms_layout_tpl.php
+++ cmsadmin/templates/layout/cms_layout_tpl.php
@@ -108,18 +108,18 @@ $history ='
 ';
 // Create an instance of the CSS Layout
 $cssLayout = $this->getObject('csslayout', 'htmlelements');
-$this->appendArrayVar('headerParams', $this->getJavascriptFile('yahoo/yahoo.js', 'cmsadmin'));
-$this->appendArrayVar('headerParams', $this->getJavascriptFile('event/event.js', 'cmsadmin'));
-$this->appendArrayVar('headerParams', $this->getJavascriptFile('dom/dom.js', 'cmsadmin'));
-$this->appendArrayVar('headerParams', $this->getJavascriptFile('animation/animation.js', 'cmsadmin'));
-$this->appendArrayVar('headerParams', $this->getJavascriptFile('connection/connection.js', 'cmsadmin'));
-$this->appendArrayVar('headerParams', $this->getJavascriptFile('history/history-experimental.js', 'cmsadmin'));
-$this->appendArrayVar('headerParams', $this->getJavascriptFile('container/container.js', 'cmsadmin'));
+$this->appendArrayVar('headerParams', $this->getJavascriptFile('yahoo/yahoo.js', 'yahoolib'));
+$this->appendArrayVar('headerParams', $this->getJavascriptFile('event/event.js', 'yahoolib'));
+$this->appendArrayVar('headerParams', $this->getJavascriptFile('dom/dom.js', 'yahoolib'));
+$this->appendArrayVar('headerParams', $this->getJavascriptFile('animation/animation.js', 'yahoolib'));
+$this->appendArrayVar('headerParams', $this->getJavascriptFile('connection/connection.js', 'yahoolib'));
+$this->appendArrayVar('headerParams', $this->getJavascriptFile('history/history-experimental.js', 'yahoolib'));
+$this->appendArrayVar('headerParams', $this->getJavascriptFile('container/container.js', 'yahoolib'));
 $this->appendArrayVar('headerParams', $this->getJavascriptFile('menu/menu.js', 'cmsadmin'));
 $this->appendArrayVar('headerParams',$script);
-$css = '<link rel="stylesheet" type="text/css" media="all" href="'.$this->getResourceURI("fonts/fonts.css", 'cmsadmin').'" />';
-$css .= '<link rel="stylesheet" type="text/css" media="all" href="'.$this->getResourceURI("reset/reset.css", 'cmsadmin').'" />';
-$css .= '<link rel="stylesheet" type="text/css" media="all" href="'.$this->getResourceURI("menu/assets/menu.css", 'cmsadmin').'" />';
+$css = '<link rel="stylesheet" type="text/css" media="all" href="'.$this->getResourceURI("fonts/fonts.css", 'yahoolib').'" />';
+$css .= '<link rel="stylesheet" type="text/css" media="all" href="'.$this->getResourceURI("reset/reset.css", 'yahoolib').'" />';
+$css .= '<link rel="stylesheet" type="text/css" media="all" href="'.$this->getResourceURI("menu/assets/menu.css", 'yahoolib').'" />';
 $this->appendArrayVar('headerParams', $css);
 //Set to automatically render htmllist into tree menu
 $cssLayout->setNumColumns(2);
