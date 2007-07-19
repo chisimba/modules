23a0c46d18d3da817d21bda48cba46265217f219
David Wafula <davidwaf@gmail.com>
2007-07-19 13:38:45 +0000

    added links for presenter and audience

--- realtime/templates/content/realtime-presentations_tpl.php
+++ realtime/templates/content/realtime-presentations_tpl.php
@@ -9,30 +9,55 @@ echo "</applet> ";
 // set up html elements
 $objHead=$this->newObject('htmlheading','htmlelements');
 
-$objHead2 = $this->newObject('htmlheading','htmlelements');
+$table = $this->newObject('htmltable', 'htmlelements');
+$table->cellpadding = 5;
 
-$objHead3 = $this->newObject('htmlheading','htmlelements');
+// Create an instance of the css layout class
+$cssLayout = & $this->newObject('csslayout', 'htmlelements');// Set columns to 2
+$cssLayout->setNumColumns(2);
+
 /**************** set up display page ********************/
-$str1= 'Wellcome to the realtime presentations module';
-$str2= 'Using this module, you can view Open Office 2.0 or later Presentations';
-$str3='Click on <a href="'.$this->presentationsURL.'/Presentations.jnlp">Open Office Viewer 0.1</a> to launch'; 
+$str1= 'Realtime Presentations';
+$str2= 'Using this module, you can present <a href="www.openoffice.org/"> Open Office 2.0</a> or later Presentations synchronously in realtime';
+$str3='<a href="'.$this->presentationsURL.'/Presenter.jnlp">Presenter Studio</a> is used to start a presentation.'; 
+$str4='<a href="'.$this->presentationsURL.'/Audience.jnlp">Join Active Presentation</a>  sessions started by others.';
 
-$objHead->type=3;
+$objHead->type=2;
 $objHead->str=$str1;
 
-$aobjHead2->type=3;
-$objHead2->str=$str2;
+//Set the content of the left side column
+$leftSideColumn = $str2;
+// Add Left column
+$cssLayout->setLeftColumnContent($leftSideColumn);
+$rightSideColumn = "<div align=\"left\">" . $objHead->show() . "</div>";
 
 
-$aobjHead3->type=3;
-$objHead3->str=$str3;
+
+$table->startRow();
+$table->addCell($str3);
+$table->endRow();
+
+$table->startRow();
+$table->addCell($str4);
+$table->endRow();
 
 
+//Add the table to the centered layer
+$rightSideColumn .= $table->show();
 
-echo $objHead->show();
-echo $objHead2->show();
-echo $objHead3->show();
+// Add Left column
+$cssLayout->setLeftColumnContent($leftSideColumn);
 
+// Add Right Column
+$cssLayout->setMiddleColumnContent($rightSideColumn);
+
+//Output the content to the page
+echo $cssLayout->show();
+
+
+//echo $objHead2->show();
+//echo $objHead3->show();
+//echo $objHead4->show();
 
 
 
