27f20ac02df403c39a683d407db3d946dc032ff6
David Wafula <davidwaf@gmail.com>
2007-07-19 13:35:05 +0000

    enhanced support for realtime presentations

---
+++ resources/presentations/run-server.sh
@@ -0,0 +1,1 @@
+/home/dwafula/software/jdk1.6.0/bin/java -cp forms-1.1.0.jar:resolver.jar:xercesImpl.jar:presentations-server.jar:serializer.jar:xml-apis.jar  avoir.realtime.presentations.server.Server
