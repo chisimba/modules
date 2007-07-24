1e497b6c9be63bd0ffb5bde4aa65f55003272078
Prince Mbekwa <pmbekwa@gmail.com>
2007-07-24 08:58:56 +0000

    added diaolog feature

--- cmsadmin/templates/layout/cms_layout_tpl.php
+++ cmsadmin/templates/layout/cms_layout_tpl.php
@@ -33,18 +33,13 @@ YAHOO.example.onMenuReady = function() {
 
 // Create an instance of the CSS Layout
 $cssLayout = $this->getObject('csslayout', 'htmlelements');
-$this->appendArrayVar('headerParams', $this->getJavascriptFile('yahoo/yahoo.js', 'yahoolib'));
-$this->appendArrayVar('headerParams', $this->getJavascriptFile('event/event.js', 'yahoolib'));
-$this->appendArrayVar('headerParams', $this->getJavascriptFile('dom/dom.js', 'yahoolib'));
-$this->appendArrayVar('headerParams', $this->getJavascriptFile('connection/connection.js', 'yahoolib'));
-$this->appendArrayVar('headerParams', $this->getJavascriptFile('history/history-experimental.js', 'yahoolib'));
-$this->appendArrayVar('headerParams', $this->getJavascriptFile('container/container.js', 'yahoolib'));
-$this->appendArrayVar('headerParams', $this->getJavascriptFile('menu/menu.js', 'yahoolib'));
-$this->appendArrayVar('headerParams',$script);
 $css = '<link rel="stylesheet" type="text/css" media="all" href="'.$this->getResourceURI("fonts/fonts.css", 'yahoolib').'" />';
 $css .= '<link rel="stylesheet" type="text/css" media="all" href="'.$this->getResourceURI("reset/reset.css", 'yahoolib').'" />';
+$css .= '<link rel="stylesheet" type="text/css" media="all" href="'.$this->getResourceURI("container/assets/container.css", 'yahoolib').'" />';
 $css .= '<link rel="stylesheet" type="text/css" media="all" href="'.$this->getResourceURI("menu/assets/menu.css", 'yahoolib').'" />';
+$this->appendArrayVar('headerParams', $this->getJavascriptFile('menu/menu.js', 'yahoolib'));
 $this->appendArrayVar('headerParams', $css);
+$this->appendArrayVar('headerParams',$script);
 //Set to automatically render htmllist into tree menu
 $cssLayout->setNumColumns(2);
 // Set the Content of middle column
