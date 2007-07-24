b62dfd62f4448d257f47361e8c1922a5bb3d608d
Prince Mbekwa <pmbekwa@gmail.com>
2007-07-24 09:25:37 +0000

    added dialog Panel for blocks

--- cmsadmin/templates/content/cms_frontpage_manager_tpl.php
+++ cmsadmin/templates/content/cms_frontpage_manager_tpl.php
@@ -4,10 +4,80 @@
  * for the cms module
  * 
  * @package cmsadmin
- * @author Warren Windvogel
- * @author Wesley Nitskie
+ * @author Prince Mbekwa
  */
+/**
+ * The Dialog component is an extension of Panel that is meant to emulate the behavior of an dialog window using a floating, draggable HTML element. 
+ * Dialog provides an interface for easily gathering information from the user without leaving the underlying page context. 
+ * The information gathered is collected via a standard HTML form; 
+ * Dialog supports the submission of form data through XMLHttpRequest, through a normal form submission, or through a manual script-based response 
+ * (where the script reads and responds to the form values and the form is never actually submitted to the server).
+*/
+$script ='
+<script type="text/javascript">
+//<![CDATA[
 
+YAHOO.namespace("example.container");
+
+				function init() {
+					
+					// Define various event handlers for Dialog
+					var handleSubmit = function() {
+						this.submit();
+					};
+					var handleCancel = function() {
+						this.cancel();
+					};
+					var handleSuccess = function(o) {
+						var response = o.responseText;
+						response = response.split("<!")[0];
+						document.getElementById("resp").innerHTML = response;
+						eval(response);
+					};
+					var handleFailure = function(o) {
+						alert("Submission failed: " + o.status);
+					};
+
+					// Instantiate the Dialog
+					YAHOO.example.container.dialog1 = new YAHOO.widget.Dialog("dialog1", 
+																				{ width : "100%",
+																				  fixedcenter : true,
+																				  visible : false, 
+																				  constraintoviewport : true,
+																				 
+																				 } );
+					
+					// Validate the entries in the form to require that both first and last name are entered
+					YAHOO.example.container.dialog1.validate = function() {
+						var data = this.getData();
+						
+							return true;
+						
+					};
+
+					// Wire up the success and failure handlers
+					YAHOO.example.container.dialog1.callback = { success: handleSuccess,
+																 failure: handleFailure };
+					
+					// Render the Dialog
+					YAHOO.example.container.dialog1.render();
+
+					YAHOO.util.Event.addListener("show", "click", YAHOO.example.container.dialog1.show, YAHOO.example.container.dialog1, true);
+					YAHOO.util.Event.addListener("hide", "click", YAHOO.example.container.dialog1.hide, YAHOO.example.container.dialog1, true);
+				}
+
+				YAHOO.util.Event.addListener(window, "load", init);
+
+//]]>
+</script>
+';
+$this->appendArrayVar('headerParams', $this->getJavascriptFile('yahoo/yahoo.js', 'yahoolib'));
+$this->appendArrayVar('headerParams', $this->getJavascriptFile('event/event.js', 'yahoolib'));
+$this->appendArrayVar('headerParams', $this->getJavascriptFile('dom/dom.js', 'yahoolib'));
+$this->appendArrayVar('headerParams', $this->getJavascriptFile('dragdrop/dragdrop.js', 'yahoolib'));
+$this->appendArrayVar('headerParams', $this->getJavascriptFile('container/container.js', 'yahoolib'));
+$this->appendArrayVar('headerParams', $this->getJavascriptFile('connection/connection.js', 'yahoolib'));
+$this->appendArrayVar('headerParams', $script);
 //initiate objects
 //$table =  $this->newObject('htmltable', 'htmlelements');
 $this->loadClass('htmltable', 'htmlelements');
@@ -18,7 +88,7 @@ $this->loadClass('form', 'htmlelements');
 $objIcon =  $this->newObject('geticon', 'htmlelements');
 $objRound =$this->newObject('roundcorners','htmlelements');
 $objLayer =$this->newObject('layer','htmlelements');
-
+$h3 = $this->getObject('htmlheading', 'htmlelements');
 //create link to add blocks to the front page
 //Get blocks icon
 $objIcon->setIcon('modules/blocks');
@@ -158,7 +228,18 @@ if (empty($files)) {
 echo $middleColumnContent;
 
 echo '<br />';
-echo '<h2>'.$this->objLanguage->languageText('mod_cmsadmin_frontpageblocks', 'cmsadmin', 'Front Page Blocks').'</h2>';
+$button = new button();
+$button->setId("show");
+$button->setValue($this->objLanguage->languageText('mod_cmsadmin_frontpageblocks', 'cmsadmin', 'Front Page Blocks'));
+ $showDialog = '<div>
+ 			'.$button->show().'
+			</div>';
+ echo $showDialog;
+  	 $h3->str =	$this->objLanguage->languageText('mod_cmsadmin_frontpageblocks', 'cmsadmin', 'Front Page Blocks');
+	 $h3->type = 3;
+	 $dialog1 = '<div id="dialog1">';
+     $dialog1 .= '<div class="hd">'.$h3->show().'</div>';
+     $dialog1 .= '<div class="bd">'.$this->_objUtils->showFrontBlocksForm().'</div></div>';
+     echo  $dialog1;
 
-echo $this->_objUtils->showFrontBlocksForm();
 ?>
