0b868e1cd122783e2b1558b5f3d3000fb0ffc93e
kudakwashe <unknown@unknown.com>
2007-07-26 13:54:55 +0000

    Only administrator may add links

--- foaf/templates/content/fdetails_tpl.php
+++ foaf/templates/content/fdetails_tpl.php
@@ -114,12 +114,13 @@ $pane->addTab(array(
     'content' => 'Visulalise the Network'
 ));
 
-
+if($this->objUser->isAdmin())
+{
 $pane->addTab(array(
     'name' => $foafLinks,
     'content' => $this->objUi->foafLinks()
 ));
-
+}
 //$pane->addTab(array('name'=>$surprise,'content' => $game));
 echo $pane->show();
 ?>
