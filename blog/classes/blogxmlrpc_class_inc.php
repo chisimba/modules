a51692d72f7aa29aacfafcf5a5a5e62cbb565d7e
paulscott <unknown@unknown.com>
2007-07-27 14:10:52 +0000

    added metaweblog api newPost method

--- blog/classes/blogxmlrpc_class_inc.php
+++ blog/classes/blogxmlrpc_class_inc.php
@@ -24,37 +24,75 @@ class blogxmlrpc extends object
 	{
 		// map web services to methods
 		$server = new XML_RPC_Server(
-   					array('blogger.newPost' => array('function' => array($this, 'metaWeblogNewPost'),
+   					array('blogger.newPost' => array('function' => array($this, 'bloggerNewPost'),
    											      'signature' => array(
                          											array('string', 'string', 'string', 'string','string', 'string', 'boolean'),
                      											 ),
                 								  'docstring' => 'new post'),
                 		  
-                		  'blogger.editPost' => array('function' => array($this, 'metaWeblogEditPost'),
+                		  'blogger.editPost' => array('function' => array($this, 'bloggerEditPost'),
    											      'signature' => array(
                          											array('boolean', 'string', 'string', 'string', 'string', 'string', 'boolean'),
                      											 ),
                 								  'docstring' => 'edit post'),
                 		  
-                		  'blogger.getPost' => array('function' => array($this, 'metaWeblogGetPost'),
+                		  'blogger.getPost' => array('function' => array($this, 'bloggerGetPost'),
    											      'signature' => array(
                          											array('struct', 'string', 'string', 'string'),
                      											 ),
                 								  'docstring' => 'get post'),
                 		  
-                		  'blogger.getRecentPosts' => array('function' => array($this, 'metaWeblogGetRecentPosts'),
+                		  'blogger.getRecentPosts' => array('function' => array($this, 'bloggerGetRecentPosts'),
                 		   						   'signature' => array(
                 		   						   					array('array', 'string', 'string', 'string', 'string', 'int'),
                 		   						   					),
                 		   						   'docstring' => 'get recent posts'),
                 		  
-                		  'blogger.getCategories' => array('function' => array($this, 'metaWeblogGetCategories'),
+                		  'blogger.getCategories' => array('function' => array($this, 'bloggerGetCategories'),
                 		   						   'signature' => array(
                 		   						   					array('array', 'string', 'string', 'string', 'string'),
                 		   						   					),
                 		   						   'docstring' => 'get categories'),
                 		  
-                		  'blogger.getUsersBlogs' => array('function' => array($this, 'metaWeblogGetUsersBlogs'),
+                		  'blogger.getUsersBlogs' => array('function' => array($this, 'bloggerGetUsersBlogs'),
+                		   						   'signature' => array(
+                		   						   					array('array', 'string', 'string', 'string'),
+                		   						   					),
+                		   						   'docstring' => 'get user blogs'),
+                		   						   
+                		   						   
+                		   // metaweblog section
+                		   'metaWeblog.newPost' => array('function' => array($this, 'metaWeblogNewPost'),
+   											      'signature' => array(
+                         											array('boolean', 'string', 'string', 'string', 'struct', 'boolean'),
+                     											 ),
+                								  'docstring' => 'new post'),
+                		  
+                		  'metaWeblog.editPost' => array('function' => array($this, 'metaWeblogEditPost'),
+   											      'signature' => array(
+                         											array('boolean', 'string', 'string', 'string', 'string', 'string', 'boolean'),
+                     											 ),
+                								  'docstring' => 'edit post'),
+                		  
+                		  'metaWeblog.getPost' => array('function' => array($this, 'metaWeblogGetPost'),
+   											      'signature' => array(
+                         											array('struct', 'string', 'string', 'string'),
+                     											 ),
+                								  'docstring' => 'get post'),
+                		  
+                		  'metaWeblog.getRecentPosts' => array('function' => array($this, 'metaWeblogGetRecentPosts'),
+                		   						   'signature' => array(
+                		   						   					array('array', 'string', 'string', 'string', 'string', 'int'),
+                		   						   					),
+                		   						   'docstring' => 'get recent posts'),
+                		  
+                		  'metaWeblog.getCategories' => array('function' => array($this, 'metaWeblogGetCategories'),
+                		   						   'signature' => array(
+                		   						   					array('array', 'string', 'string', 'string', 'string'),
+                		   						   					),
+                		   						   'docstring' => 'get categories'),
+                		  
+                		  'metaWeblog.getUsersBlogs' => array('function' => array($this, 'metaWeblogGetUsersBlogs'),
                 		   						   'signature' => array(
                 		   						   					array('array', 'string', 'string', 'string'),
                 		   						   					),
@@ -66,7 +104,7 @@ class blogxmlrpc extends object
 		return $server;
 	}
 	
-	public function metaWeblogNewPost($params)
+	public function bloggerNewPost($params)
 	{
 		$param = $params->getParam(0);
 		if (!XML_RPC_Value::isValue($param)) {
@@ -130,12 +168,13 @@ class blogxmlrpc extends object
                 'stickypost' => '0',
                 'showpdf' => '1'
             );
+        
     	$ret = $this->objDbBlog->insertPostAPI($userid, $postarray);
 		$val = new XML_RPC_Value($ret, 'string');
 		return new XML_RPC_Response($val);
 	}
 	
-	public function metaWeblogEditPost($params)
+	public function bloggerEditPost($params)
 	{
 		$param = $params->getParam(0);
 		if (!XML_RPC_Value::isValue($param)) {
@@ -203,7 +242,7 @@ class blogxmlrpc extends object
    		return new XML_RPC_Response($val);
 	}
 	
-	public function metaWeblogGetPost()
+	public function bloggerGetPost()
 	{
 		$myStruct = new XML_RPC_Value(array(
     		"title" => new XML_RPC_Value("Tom"),
@@ -212,7 +251,7 @@ class blogxmlrpc extends object
     	return new XML_RPC_Response($myStruct);
 	}
 	
-	public function metaWeblogGetRecentPosts($params)
+	public function bloggerGetRecentPosts($params)
 	{
 		$param = $params->getParam(0);
 		if (!XML_RPC_Value::isValue($param)) {
@@ -260,13 +299,13 @@ class blogxmlrpc extends object
     	return new XML_RPC_Response($ret);
 	}
 
-	public function metaWeblogGetCategories()
+	public function bloggerGetCategories()
 	{
 		$val = new XML_RPC_Value('a returned string', 'string');
 		return new XML_RPC_Response($val);
 	}
 	
-	public function metaWeblogGetUsersBlogs($params)
+	public function bloggerGetUsersBlogs($params)
 	{
 		$param = $params->getParam(0);
 		if (!XML_RPC_Value::isValue($param)) {
@@ -304,6 +343,98 @@ class blogxmlrpc extends object
     	
     	$arrofStructs = new XML_RPC_Value(array($myStruct), "array");
     	return new XML_RPC_Response($arrofStructs);
+	}
+	
+	
+	
+	/**
+	 * Metaweblog section - functions
+	 */
+	
+	
+	public function metaWeblogNewPost($params)
+	{
+		$param = $params->getParam(0);
+		if (!XML_RPC_Value::isValue($param)) {
+            log_debug($param);
+    	}
+    	$blogid = $param->scalarval();
+    	
+    	$param = $params->getParam(1);
+		if (!XML_RPC_Value::isValue($param)) {
+            log_debug($param);
+    	}
+    	$username = $param->scalarval();
+    	
+    	$param = $params->getParam(2);
+		if (!XML_RPC_Value::isValue($param)) {
+            log_debug($param);
+    	}
+    	$pass = $param->scalarval();
+    	
+    	$param = $params->getParam(3);
+		if (!XML_RPC_Value::isValue($param)) {
+            log_debug($param);
+    	}
+    	$content = $param->serialize($param);
+    	//$cont = simplexml_load_string($content);
+    	$cont = new SimpleXMLElement($content);
+    	$cont = $cont->struct;
+    	foreach($cont->member as $members)
+    	{
+    		if($members->name == 'title')
+    		{
+    			$title = $members->value;
+    		}
+    		elseif($members->name == 'description')
+    		{
+    			$postcontent = $members->value;
+    		}
+    		elseif($members->name == 'mt_excerpt')
+    		{
+    			$excerpt = $members->value;
+    		}
+    	}
+    	//log_debug($member);
+    	
+
+    	$param = $params->getParam(4);
+		if (!XML_RPC_Value::isValue($param)) {
+            log_debug($param);
+    	}
+    	$publish = $param->scalarval();
+    	
+    	if($publish)
+    	{
+    		$published = 0;
+    	}
+    	else {
+    		$published = 1;
+    	}
+    	
+    	$userid = $this->objUser->getUserId($username);
+    	
+    	//insert to the db now and return the generated id as a string
+    	$postarray = array(
+                'userid' => $userid,
+                'post_date' => date('r') ,
+                'post_content' => addslashes($postcontent->string) , 
+                'post_title' => addslashes($title->string),
+                'post_category' => '0',
+                'post_excerpt' => $excerpt->string,
+                'post_status' => $published,
+                'comment_status' => 'on',
+                'post_modified' => date('r'),
+                'comment_count' => '0',
+                'post_ts' => time() ,
+                'post_lic' => '',
+                'stickypost' => '0',
+                'showpdf' => '1'
+            );
+        //log_debug($postarray);
+    	$ret = $this->objDbBlog->insertPostAPI($userid, $postarray);
+		$val = new XML_RPC_Value($ret, 'string');
+		return new XML_RPC_Response($val);
 	}
 	
 }
