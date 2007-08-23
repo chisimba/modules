33e4a037a6a3df9f4c749fab2c11d39716ccb9a7
David Wafula <davidwaf@gmail.com>
2007-08-23 13:41:19 +0000

    Added Files:  	realtime-presentations-audience-applet_tpl.php  	realtime-presentations-presenter-applet_tpl.php

---
+++ templates/content/realtime-presentations-audience-applet_tpl.php
@@ -0,0 +1,9 @@
+<?php
+
+	echo '<applet code="avoir.realtime.presentations.client.ClientViewer" width="100%" height="600">';
+	echo '	<param name="archive" value="'.$this->presentationsURL.'/presentations-client.jar,'.$this->presentationsURL.'/forms-1.1.0.jar"/>';
+	echo '	<param name="host" value="localhost"/>';
+        echo '  <param name="port" value="1962"/>';
+	echo "</applet> ";
+
+?>
