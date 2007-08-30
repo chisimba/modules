cfdc690f431ad4f7c6db8777088a04fd7428800d
Jeremy O'Connor <joconnor@uwc.ac.za>
2007-08-30 16:11:32 +0000

    Initial revision.

---
+++ ircchat/templates/content/default_tpl.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * default_tpl.php
+ *
+ * @version $Id$
+ * @copyright (c) 2007 Avoir
+ */
+
+$uriEnabled = $this->uri(array('action'=>'enabled'));
+$uriNotEnabled = $this->uri(array('action'=>'notenabled'));
+
+?>
+<script type="text/javascript">
+// <![CDATA[
+if ( false && navigator.javaEnabled() ) {
+    window.location="<?= $uriEnabled ?>";
+}
+else {
+    window.location="<?= $uriNotEnabled ?>";
+}
+// ]]>
+</script>
---
+++ ircchat/templates/content/nochat_tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ *
+ *
+ * @version $Id$
+ * @copyright 2007
+ */
+
+echo "JAVA is not enabled! Please install the JRE by downloading it from ...";
+
+?>
