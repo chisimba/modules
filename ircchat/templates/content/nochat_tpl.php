342cbcfa9f6ba57626dacd406e5389dcafe199ef
Jeremy O'Connor <joconnor@uwc.ac.za>
2007-08-30 16:25:07 +0000

    Various changes.

--- ircchat/templates/content/nochat_tpl.php
+++ ircchat/templates/content/nochat_tpl.php
@@ -7,6 +7,5 @@
  * @copyright 2007
  */
 
-echo "JAVA is not enabled! Please install the JRE by downloading it from ...";
-
 ?>
+The JRE is not installed! Please install the JRE by downloading it from <a href="http://www.java.com/en/download/manual.jsp">here</a>.
