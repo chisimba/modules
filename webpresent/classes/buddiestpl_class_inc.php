c25a21d555c8e7e3e7b28ea3dc50240ec99565ef
tohir <unknown@unknown.com>
2007-09-24 14:03:00 +0000

    fixed buddies link

--- webpresent/classes/buddiestpl_class_inc.php
+++ webpresent/classes/buddiestpl_class_inc.php
@@ -3,27 +3,27 @@
 * Class to Render online buddies from buddies module
 *
 * Render online buddies from buddies module for use in the webpresent module
-* 
+*
 * PHP version 5
-* 
-* This program is free software; you can redistribute it and/or modify 
-* it under the terms of the GNU General Public License as published by 
-* the Free Software Foundation; either version 2 of the License, or 
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
 * (at your option) any later version.
-* This program is distributed in the hope that it will be useful, 
-* but WITHOUT ANY WARRANTY; without even the implied warranty of 
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
-* You should have received a copy of the GNU General Public License 
-* along with this program; if not, write to the 
-* Free Software Foundation, Inc., 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the
+* Free Software Foundation, Inc.,
 * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-* 
+*
 * @category  Chisimba
 * @package   filters
 * @author    Derek Keats <dkeats[AT]uwc[DOT]ac[DOT]za>
 * @copyright 2007 UWC and AVOIR
-* @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License 
+* @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
 * @version   CVS: $Id$
 * @link      http://avoir.uwc.ac.za
 */
@@ -53,48 +53,48 @@ $GLOBALS['kewl_entry_point_run']) {
 * @licence GNU/GPL
 *
 */
-class buddiestpl extends object 
+class buddiestpl extends object
 {
     /**
-    * 
-    * @var $objLanguage String object property for holding the 
+    *
+    * @var $objLanguage String object property for holding the
     * language object
     * @access private
-    * 
+    *
     */
     private $objLanguage;
-    
+
     /**
     *
-    * Standard init method  
+    * Standard init method
     *
     */
     public function init()
     {
-        // Get the language object. 
+        // Get the language object.
         $this->objLanguage = $this->getObject('language', 'language');
         // Get the user object.
         $this->objUser = $this->getObject("user", "security");
         $this->loadClass('link', 'htmlelements');
     }
-    
+
     /**
-     * 
+     *
      * Method to get all online buddies into an array property
      * @return TRUE
      * @access Public
-     * 
+     *
      */
     public function getMessages()
     {
 
         return TRUE;
     }
-    
+
     /**
-     * 
+     *
      * Method to render the unread email messages
-     * @return The rendered messages as String 
+     * @return The rendered messages as String
      * @access Public
      */
     public function show()
@@ -132,11 +132,13 @@ class buddiestpl extends object
                 $ret = $rowTable->show();
             } else {
                 $this->budCount = "";
-                $ret = "<span class='noRecordsMessage'>" . 
+                $ret = "<span class='noRecordsMessage'>" .
                   $this->objLanguage->languageText("mod_webpresent_nobuds", "webpresent")
                   . "</span>";
             }
-            return $ret . "<br /><br />" . $buddyLink->show();
+            $buddyLink->link = $this->objLanguage->languageText("mod_webpresent_managebuddies", "webpresent");
+            $buddyLink->href = $this->uri(array(), "buddies");
+            return $ret . "<br />" . $buddyLink->show();
         } else {
             return NULL;
         }
