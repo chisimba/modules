58a27be1ad188cfe6bdf608ab43b6ed8412bd84f
paulscott <unknown@unknown.com>
2007-09-26 10:28:57 +0000

    controller

---
+++ phonebook/controller.php
@@ -0,0 +1,57 @@
+<?php
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+class phonebook extends controller
+{
+	public $objLanguage;
+	public $objConfig;
+        public $objDbContacts;
+        public $objUser;
+
+    /**
+     * Constructor method to instantiate objects and get variables
+     */
+    public function init()
+    {
+        try {
+            $this->objUser = $this->getObject('user', 'security');
+            $this->objDbContacts = $this->getObject('dbcontacts', 'phonebook');
+            $this->objLanguage = $this->getObject('language', 'language');
+            $this->objConfig = $this->getObject('altconfig', 'config');
+        }
+        catch(customException $e) {
+            echo customException::cleanUp();
+            die();
+        }
+    }
+    /**
+     * Method to process actions to be taken
+     *
+     * @param string $action String indicating action to be taken
+     */
+    public function dispatch($action = Null)
+    {
+        switch ($action) {
+            default:
+            	
+           case 'addentry':
+                $firstname = $this->getParam('firstname');
+                
+
+
+                return 'addentry_tpl.php';
+            	break;
+            	
+            case 'editentry':
+                break;
+            	
+             case 'deleteentry':
+                break;
+        }
+    }
+}
+?>
