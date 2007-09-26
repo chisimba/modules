c71b27e0250c51689c885bc33f09a4aa4047d9f9
paulscott <unknown@unknown.com>
2007-09-26 10:29:17 +0000

    template

---
+++ phonebook/templates/content/addentry_tpl.php
@@ -0,0 +1,24 @@
+<?php
+// Create an instance of the css layout class
+$cssLayout =& $this->newObject('csslayout', 'htmlelements');
+// Set columns to 3
+$cssLayout->setNumColumns(3);
+
+// Initialize left column
+$leftSideColumn = NULL;
+$rightSideColumn = NULL;
+$middleColumn = NULL;
+
+
+
+$middleColumn = $this->objDbContacts->getContact($this->objUser->userId());
+
+
+
+//add left column
+$cssLayout->setLeftColumnContent($leftSideColumn);
+$cssLayout->setRightColumnContent($rightSideColumn);
+//add middle column
+$cssLayout->setMiddleColumnContent($middleColumn);
+echo $cssLayout->show();
+?>
