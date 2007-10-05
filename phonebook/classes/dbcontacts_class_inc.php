5cc51c68dc838281df78060fc5937d02039ab643
darkprince <unknown@unknown.com>
2007-10-05 08:44:01 +0000

    update

--- phonebook/classes/dbcontacts_class_inc.php
+++ phonebook/classes/dbcontacts_class_inc.php
@@ -23,113 +23,45 @@ class dbPhonebook extends dbTable
     }
 
 
-    /**
+      /**
         * Return all records
-	* @param string $userId The user ID
-	* @return array The contacts for a user
-    */
+      */ 
+
 	public function listAll($userId)
 	{
-		$sql = "SELECT 
-            tbl_phonebook.contactid AS contactid, 
-            tbl_phonebook.isContact, 
-            tbl_phonebook.firstName, 
-            tbl_phonebook.surname, 
-            tbl_phonebook.emailAddress 
-            tbl_phonebook.cellnumber
-            tbl_phonebook.landlinenumber
-        FROM tbl_phonebook, tbl_users
-		WHERE (tbl_phonebook.userid = '" . $userId . "')
-		AND (tbl_phonebook.contactid = tbl_users.userid)
-        ORDER BY tbl_users.firstName, tbl_users.surname";
-		return $this->getArray($sql);
+        $this->_changeTable('tbl_users');
+        $userrec = $this->getAll("WHERE userid = '$userid'");
+        return $userrec;
 	}
 
+        private function _changeTable($tablename)
+        {
+        parent::init($tablename);
+        }
 
 	/**
 	* Insert a record
-	* @param string $userId The user ID
-	* @param string $contactId The contact ID
 	*/
-	public function insertSingle($userId, $contactId)
+	public function insertRec($fields)
 	{
-        $list = $this->listSingle($userId, $contactId);
-        if (empty($list)) {
-    		$this->insert(array(
-    			'userid' => $userId,
-    			'contactid' => $contactId,
-                'isContact' => '1',
-    		));
-        }
-        else {
-            $this->update(
-                'id',
-                $list[0]['id'],
-                array(
-                    'isContact' => '1'
-                )
-            );
-        };
-        $list = $this->listSingle($contactId, $userId);
-        if (empty($list)) {
-    		$this->insert(array(
-    			'userid' => $buddyId,
-    			'contactid' => $userId,
-                'isContact' => '0',
-    		));
-        }
-        else {
-            $this->update(
-                'id',
-                $list[0]['id'],
-                array(
-                    'isContact' => '1'
-                )
-            );
-        };
-		return;	
+	return $this->insert($fields, 'tbl_phonebook');
 	}
 
 	/**
 	* Deletes records
-	* @param string $userId The user ID
-	* @param string $contactId The contact ID
 	*/
-	public function deleteSingle($userId, $contactId)
+	public function deleteRec($id)
 	{
-//<<<<<<< dbcontacts_class_inc.php
-//<<<<<<< dbcontacts_class_inc.php
-		$sql = "SELECT 
-            tbl_phonebook.contactId, 
-            CONCAT(tbl_users.firstName, ' ', tbl_users.surname) AS Fullname
-        FROM tbl_phoneboo, tbl_users
-		WHERE 
-		(tbl_phonebook.contactId = tbl_users.userId)
-        AND (tbl_phonebook.userId = '" . $userId . "')k
-        AND (tbl_phonebook.isContact = '1')
-        ORDER BY Fullname";
-		return $this->getArray($sql);
-	}=======
-		$sql = "WHERE userid = '$userId'";
-		return $this->getAll($sql);
-=======
-        $list = $this->listSingle($buddyId, $userId);
-        $this->update(
-            'id',
-            $list[0]['id'],
-            array(
-                'isContact' => '0'
-            )
-        );
-        if ($list[0]['isContact']=='0') {
-            $this->delete(
-                'id',
-                $list[0]['id']
-            );
-        }
-        return;
->>>>>>> 1.4
+        return $this->delete('id', $id, 'tbl_phonebook');
 	}
+
+        /**
+        * Update record
+        */
+        public function updateRec($id)
+        {
+        return $this->update('id', $id, $fields, 'tbl_phonebook');
+        }
 }
 ?>
->>>>>>> 1.3
+
--- phonebook/controller.php
+++ phonebook/controller.php
@@ -37,19 +37,27 @@ class phonebook extends controller
     {
         switch ($action) {
             default:
-            	
+            	$method=$this->_getMethod();
+ 		break;
+
            case 'addentry':
                 $firstname = $this->getParam('firstname');
-                
-
-
+                $lastname = $this->getParam('lastname');
+		$emailaddress = $this->getParam('emailaddress');
+		$cellnumber = $this->getParam('cellnumber');
+		$landlinenumber = $this->getParam('landlinenumber'); 
+		$this->objDbContacts->insertRec($fields);          
                 return 'addentry_tpl.php';
             	break;
             	
             case 'editentry':
+     	     $id = $this->getParam('id');
+             $this->objDbContacts->updateRec($id);
                 break;
             	
              case 'deleteentry':
+	     $id = $this->getParam('id');
+             $this->objDbContacts->deleteRec($id);
                 break;
         }
     }
