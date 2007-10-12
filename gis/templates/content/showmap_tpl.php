6300006c78e8c1ef1026670b249d43181c1978be
paulscott <unknown@unknown.com>
2007-10-12 07:45:18 +0000

    separated map template and upload template

--- gis/templates/content/showmap_tpl.php
+++ gis/templates/content/showmap_tpl.php
@@ -1,24 +1,18 @@
 <?php
-$cssLayout = &$this->newObject('csslayout', 'htmlelements');
+$cssLayout = $this->newObject('csslayout', 'htmlelements');
 $objSideBar = $this->newObject('usermenu', 'toolbar');
 $objFeatureBox = $this->newObject('featurebox', 'navigation');
 
-// Set columns to 3
-$cssLayout->setNumColumns(3);
+// Set columns to 2
+$cssLayout->setNumColumns(2);
 $leftMenu = NULL;
-
-$rightSideColumn = NULL;
-
 $leftCol = NULL;
 $middleColumn = NULL;
 
 $leftCol .= $objSideBar->show();
-$rightSideColumn .= "Please upload a zip file containing the .shp, .dbf and .shx files for the geometry";
-
-$middleColumn .= $this->objGisOps->uploadDataFile();
+$middleColumn .= "<img src='$themap'>";
 
 $cssLayout->setMiddleColumnContent($middleColumn);
 $cssLayout->setLeftColumnContent($leftCol); //$leftMenu->show());
-$cssLayout->setRightColumnContent($rightSideColumn);
 echo $cssLayout->show();
 ?>
