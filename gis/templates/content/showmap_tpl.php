99f28d5be19f176b8e48e236feb6ea26c9ec26c1
paulscott <unknown@unknown.com>
2007-10-12 14:02:31 +0000

    *** empty log message ***

--- gis/templates/content/showmap_tpl.php
+++ gis/templates/content/showmap_tpl.php
@@ -17,18 +17,17 @@ $script = '<div id="map_tag" style="position:absolute;
     top:10px; left:10px; width: 800px; height: 600px; border-width:1px;
     border-color:#000088;"></div>';
 
-$script .= '<script type="text/javascript">'; 
-$script .= "//<![CDATA[ 
-alert('waa');
-     myMap1 = new msMap( document.getElementById('map_tag') ); 
-	 myMap1.setCgi( 'http://localhost/cgi-bin/mapserv.exe' ); 
-	 myMap1.setFullExtent( 34.930152 -18.876982 39.353933 ); alert('loaded');
-     myMap1.setMapFile( 'zambezia2.map' ); 
-     myMap1.setLayers( 'MOZ_ZA_District_2007_polygon' ); 
-     myMap1.debug();
-	 alert(myMap1);
+$script .= '<script type="text/javascript">; 
+	//<![CDATA[ 
+     myMap1 = new msMap( document.getElementById(\'map_tag\') ); 
+	 myMap1.setCgi( \'http://localhost/cgi-bin/mapserv.exe\' ); 
+	 // alert("you gotta do MINX, MAXX, MinY as the args here...);
+	 myMap1.setFullExtent( 34.930152, 39.353933, -18.876982 );
+     myMap1.setMapFile( \'zambezia2.map\' ); 
+     myMap1.setLayers( \'MOZ_ZA_District_2007_polygon\' ); 
+	 myMap1.redraw();
      //]]> 
-</script>";
+</script>';
 $middleColumn .= $script;
 $leftCol .= $objSideBar->show();
 
