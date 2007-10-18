fd5a4dc28266f013a3a0f99e8f2e11fb58fefb91
darkprince <unknown@unknown.com>
2007-10-18 08:37:28 +0000

    cleaning up

--- phonebook/controller.php
+++ phonebook/controller.php
@@ -7,7 +7,7 @@ if (!$GLOBALS['kewl_entry_point_run']) {
 
 /**
 * Model controller for the table tbl_phonebook
-* @author Jacques Cilliers <2618315@uwc.ac.za>  
+* @authors:Godwin Du Plessis, Ewan Burns, Helio Rangeiro, Jacques Cilliers, Luyanda Mgwexa, George Amabeoku, Charl Daniels, and Qoane Seitlheko.  
 * @copyright 2007 University of the Western Cape
 */
 
@@ -33,7 +33,8 @@ class phonebook extends controller
             echo customException::cleanUp();
             die();
         }
-    }
+    }//end of init function
+		
     /**
      * Method to process actions to be taken
      *
@@ -42,85 +43,67 @@ class phonebook extends controller
     public function dispatch($action = Null)
     {
         switch ($action) {
-            default:
+           default:
            case 'default':
-
-//var_dump($this->objDbContacts->listAll($this->objUser->userId())); die();
-
-//var_dump($this->objDbContacts->listAll($this->objUser->userId()));
 
                 $userId = $this->objUser->userId();
                 $records=$this->objDbContacts->listAll($userId);
 		        $this->setVarByRef('records', $records);                
 				return 'view_tpl.php';
-                break;
+           break;
 
            case 'addentry';	
-
-						// Get Details from Form
-            $firstname = $this->getParam('firstname');
-            $lastname = $this->getParam('lastname');
-		        $emailaddress = $this->getParam('emailaddress');
-		        $cellnumber = $this->getParam('cellnumber');
-		        $landlinenumber = $this->getParam('landlinenumber');
-		        $address = $this->getParam('address');
-		        $this->objDbContacts->insertRecord($userId, $firstname, $lastname, $emailaddress, $cellnumber, 				 $landlinenumber, $address); 
-            $this->nextAction('');
-            	  break;
+// Get Details from Form
+		$userId = $this->objUser->userId();
+		$firstname = $this->getParam('firstname');
+		$lastname = $this->getParam('lastname');
+	        $emailaddress = $this->getParam('emailaddress');
+		$cellnumber = $this->getParam('cellnumber');
+	        $landlinenumber = $this->getParam('landlinenumber');
+	        $address = $this->getParam('address');
+		$this->objDbContacts->insertRecord($userId, $firstname, $lastname, $emailaddress, $cellnumber,	 			$landlinenumber, $address); 
+		$this->nextAction('');
+	   break;
             
-				case 'link':
-					return 'addentry_tpl.php';
-	        break;	
+	   case 'link':
+	   return 'addentry_tpl.php';
+	   break;	
            
-        case 'editentry':
-            $id = $this->getParam('id');
-		//print_r($id); die();
-						$oldrec = $this->objDbContacts->listSingle($id);
-            $this->setVarByRef('oldrec', $oldrec);
-            return 'editentry_tpl.php';
-						
-         case 'updateentry':
-						// Get Details from Form
-            $id = $this->getParam('id');
-					//	print_r($id); die();
-						$firstname = $this->getParam('firstname');
-            $lastname = $this->getParam('lastname');
-		        $emailaddress = $this->getParam('emailaddress');
-		        $cellnumber = $this->getParam('cellnumber');
-		        $landlinenumber = $this->getParam('landlinenumber');
-		        $address = $this->getParam('address');
+           case 'editentry':
+		$id = $this->getParam('id');
+		$oldrec = $this->objDbContacts->listSingle($id);
+		$this->setVarByRef('oldrec', $oldrec);
+          return 'editentry_tpl.php';
+// Get Details from Form						
+          case 'updateentry':
+		$id = $this->getParam('id');
+		$firstname = $this->getParam('firstname');
+		$lastname = $this->getParam('lastname');
+	        $emailaddress = $this->getParam('emailaddress');
+	        $cellnumber = $this->getParam('cellnumber');
+	        $landlinenumber = $this->getParam('landlinenumber');
+	        $address = $this->getParam('address');
             
-            $this->objUser = $this -> getObject('user', 'security');
-						$arrayOfRecords = array('userid' =>$this ->objUser ->userId(), 
-                                    'firstname' => $firstname, 
-                                    'lastname' => $lastname, 
-                                    'emailaddress' => $emailaddress, 
-                                    'cellnumber' => $cellnumber, 
-                                    'landlinenumber' => $landlinenumber,
-                                    'address' => $address, 
-                                    'created_by' =>$this ->objDbContacts->now());
+		$this->objUser = $this -> getObject('user', 'security');
+		$arrayOfRecords = array('userid' =>$this ->objUser ->userId(), 
+                                        'firstname' => $firstname, 
+                                        'lastname' => $lastname, 
+                                        'emailaddress' => $emailaddress, 
+                                        'cellnumber' => $cellnumber, 
+                                        'landlinenumber' => $landlinenumber,
+                                        'address' => $address, 
+                                        'created_by' =>$this ->objDbContacts->now());
 
-            $this->objDbContacts->updateRec($id, $arrayOfRecords);
-						               
-						//print_r($arrayOfRecords); die();
-            return $this->nextAction('view_tpl.php'); 
-					  break;
+		$this->objDbContacts->updateRec($id, $arrayOfRecords);
+		return $this->nextAction('view_tpl.php'); 
+	  break;
 						
-					
-						
-           
-
-     //return 'editentry_tpl.php';
-                //$this->objDbContacts->updateRec($this->getParam('id'));
-						//return $this->nextAction('view_tpl.php');
-                //break;
-            	
-           case 'deleteentry':
-          // var_dump($this->objDbContacts->listAll($this->getParam('id')));
-	   		   $this->objDbContacts->deleteRec($this->getParam('id'));
-	   		   return $this->nextAction('view_tpl.php'); 
-	           break;
-        }
-    }
-}
+// delete entry	
+          case 'deleteentry':
+		$this->objDbContacts->deleteRec($this->getParam('id'));
+		return $this->nextAction('view_tpl.php'); 
+	  break;
+        }//end of switch
+    }//end of dispatch function
+}//end of class
 ?>
