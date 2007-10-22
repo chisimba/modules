685f878d9b93b21ce8b37e730890e057c6103cc0
David Wafula <davidwaf@gmail.com>
2007-10-22 14:30:21 +0000

    added args to applet for file listing

--- realtime/templates/content/realtime-presentations-presenter-applet_tpl.php
+++ realtime/templates/content/realtime-presentations-presenter-applet_tpl.php
@@ -2,13 +2,17 @@
 
         $homeLink = new link ($this->uri(array('action'=>'presenter_applet')));
         $homeLink->link = 'Presentations Home';
+        $webPresentLink = new link ($this->uri(array('action'=>'presenter_applet')));
+        $webPresetLink->link = 'Web Present';
+      
         echo '<center>';
         echo '<p>'.$homeLink->show().'</p>';
 	echo '<applet code="avoir.realtime.presentations.client.presenter.ui.MainFrame" width="720" height="418" align="middle">';
 	echo '	<param name="archive" value="'.$this->presentationsURL.'/presentations-client.jar"/> ';
 	echo '	<param name="host" value="localhost"/>';
         echo '  <param name="port" value="1962"/>';
-        echo '  <param name="contentBasePath" value="'.$this->objAltConfig->getcontentBasePath().'realtime_presentations/'.$id.'"/>';
+        echo '  <param name="contentBasePath" value="'.$this->objAltConfig->getcontentBasePath().'webpresent/'.$id.'"/>';
+        echo '  <param name="files" value="'.$files.'"/>';
 	echo "</applet> ";
         echo '</center>';
 ?>
