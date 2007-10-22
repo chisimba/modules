38e394dece721153c612f68fba9e0e1adbb3fe6c
tohir <unknown@unknown.com>
2007-10-22 12:37:00 +0000

    fixed error in classes

--- photogallery/classes/dbimages_class_inc.php
+++ photogallery/classes/dbimages_class_inc.php
@@ -26,9 +26,9 @@ class dbimages extends dbTable
     {
         parent::init('tbl_photogallery_images');
         $this->_objUser = $this->getObject('user', 'security');
-        
+
     }
-    
+
    /**
    * Method to insert images into the database
    * @return boolean|string
@@ -36,113 +36,122 @@ class dbimages extends dbTable
    */
    public function insertImageData($fields)
    {
-		return $this->insert($fields);
-	
-	
-	}
-	
-	/**
-	* Method to the thumbnail
-	*/
-	public function getThumbNailFromFileId($fileId)
-	{
-		$objFile = $this->getObject('dbfile', 'filemanager');
-		$file = $objFile->getFile($fileId);
-		$objThumbnails = $this->getObject('thumbnails', 'filemanager');
-		return $objThumbnails->getThumbnail($fileId,$file['filename']);
-	}
-	
-	/**
-	* Method to get the list of images for a user
-	* @param string userId
-	* @access public
-	*/
-	public function getAlbumImages($albumId)
-	{		
-		return $this->getAll("WHERE album_id='".$albumId."' ORDER BY position ");
-		
-	}
-	
-	/**
-	* Method to update the images
-	* @param string $id
-	* @param array $fields
-	* @access public
-	*/
-	public function updateImage($id, $fields)
-	{
-		return $this->update('id', $id, $fields);
-	}
-     
-     
+        return $this->insert($fields);
+
+
+    }
+
+    /**
+    * Method to the thumbnail
+    */
+    public function getThumbNailFromFileId($fileId)
+    {
+        $objFile = $this->getObject('dbfile', 'filemanager');
+        $file = $objFile->getFile($fileId);
+        $objThumbnails = $this->getObject('thumbnails', 'filemanager');
+        return $objThumbnails->getThumbnail($fileId,$file['filename']);
+    }
+
+    /**
+    * Method to get the list of images for a user
+    * @param string userId
+    * @access public
+    */
+    public function getAlbumImages($albumId)
+    {
+        return $this->getAll("WHERE album_id='".$albumId."' ORDER BY position ");
+
+    }
+
+    /**
+    * Method to update the images
+    * @param string $id
+    * @param array $fields
+    * @access public
+    */
+    public function updateImage($id, $fields)
+    {
+        return $this->update('id', $id, $fields);
+    }
+
+
      /**
-	* Increase the hit count for an album
-	* @param string $albumId
-	* @access public
-	*/
-	public function incrementHitCount($imageId)
-	{
-	 	$image = $this->getRow('id', $imageId);
-	 	$views = array('no_views' => intval($image['no_views']) + 1 );
-		$this->update('id', $imageId,$views);
-	}
-	
-	/**
-	* Method to reorder the ablums
-	* 
-	*/
-	public function reOrderImages($albumId)
-	{
-		$order = str_replace('images[]=','',$this->getParam('imageOrder'));
-		$newOrder = split('&',$order);
-	
-		$images = $this->getAlbumImages($albumId);
-	   
-		$cnt = 0;
-		foreach($newOrder  as $arr)
-		{
-		 	$cnt++;		
-			$this->update('id', $images[$arr-1]['id'], array('position' => $cnt));
-		}
-	}
-	
-	/**
-	* Method to get the count of images
-	* for an album
-	*@param string $albumId
-	*/
-	public function getImageCount($albumId)
-	{
-		$images = $this->getAll("WHERE albumId = '.$albumId.'");
-		return count($images);
-	}
-	
-	
-	 /**
-	  * Method to get a random photo for the 
-	  * block
-	  *
-	  * @return string
-	  * @access public
-	  */
-	  public function getRandomPhoto()
-	  {
-	   	 $objAlbum = & $this->getObject('dbalbum', 'photogallery');
-	   	 $albums = $objAlbum->getAll("WHERE no_pics > 1 AND is_shared=1 ORDER BY rand() LIMIT 1 ");
-		 $rec = $this->getAll("WHERE album_id='".$albums[0]['id']."' ORDER BY rand() LIMIT 1 ");
-		 return $rec[0];
-		 
-	  }
-	
-	/**
-	* Method to update an image field
-	* @param string $id
-	* @param string $field
-	* @param string $value
-	*/
-	public function saveField($id, $field, $value)
-	{
-		return $this->update('id', $id, array($field => $value));
-	}
+    * Increase the hit count for an album
+    * @param string $albumId
+    * @access public
+    */
+    public function incrementHitCount($imageId)
+    {
+         $image = $this->getRow('id', $imageId);
+         $views = array('no_views' => intval($image['no_views']) + 1 );
+        $this->update('id', $imageId,$views);
+    }
+
+    /**
+    * Method to reorder the ablums
+    *
+    */
+    public function reOrderImages($albumId)
+    {
+        $order = str_replace('images[]=','',$this->getParam('imageOrder'));
+        $newOrder = split('&',$order);
+
+        $images = $this->getAlbumImages($albumId);
+
+        $cnt = 0;
+        foreach($newOrder  as $arr)
+        {
+             $cnt++;
+            $this->update('id', $images[$arr-1]['id'], array('position' => $cnt));
+        }
+    }
+
+    /**
+    * Method to get the count of images
+    * for an album
+    *@param string $albumId
+    */
+    public function getImageCount($albumId)
+    {
+        $images = $this->getAll("WHERE albumId = '.$albumId.'");
+        return count($images);
+    }
+
+
+     /**
+      * Method to get a random photo for the
+      * block
+      *
+      * @return string
+      * @access public
+      */
+      public function getRandomPhoto()
+      {
+        $objAlbum = & $this->getObject('dbalbum', 'photogallery');
+        $albums = $objAlbum->getAll("WHERE no_pics > 1 AND is_shared=1 ORDER BY rand() LIMIT 1 ");
+
+        if (count($albums) == 0) {
+            return FALSE;
+        }
+
+        $rec = $this->getAll("WHERE album_id='".$albums[0]['id']."' ORDER BY rand() LIMIT 1 ");
+
+        if (count($rec) == 0) {
+            return FALSE;
+        } else {
+            return $rec[0];
+        }
+      }
+
+    /**
+    * Method to update an image field
+    * @param string $id
+    * @param string $field
+    * @param string $value
+    */
+    public function saveField($id, $field, $value)
+    {
+        return $this->update('id', $id, array($field => $value));
+    }
 }
 ?>
