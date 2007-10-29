f7a3af8e02d2e012bbd232236b2ff6d5f661c996
Prince Mbekwa <pmbekwa@gmail.com>
2007-10-29 10:00:52 +0000

    bug fixes

--- radio/templates/content/stations.php
+++ radio/templates/content/stations.php
@@ -7,15 +7,15 @@ $this->setVar('pageSuppressBanner',true);
 $this->setVar('suppressFooter',true);
 
 if($_SESSION['id'] == "2"){
-if($_GET['action'] == "add"){ $this->settings->add($_POST['new_station_n'],"N/A","N/A","N/A","N/A","test");}
-if($_GET['action'] == "del"){ $this->stations->del($_GET['station_n']);}
-if($_GET['action'] == "edit"){
+if($_GET['command'] == "add"){ $this->settings->add($_POST['new_station_n'],"N/A","N/A","N/A","N/A","test");}
+if($_GET['command'] == "del"){ $this->stations->del($_GET['station_n']);}
+if($_GET['command'] == "edit"){
 	$station_n = $_GET['station_n'];
 	if($_GET['opt'] == "update"){
 	$this->settings->add($station_n,$_POST['headertitle'],$_POST['headergenre'],$_POST['headerbitrate'],$_POST['headersite'],$_POST['debugkey']);
 	}
 	$data = explode("&", $this->settings->get($station_n));
-	echo "<center><form action=?page=admin&subpage=stations&station_n=$station_n&action=edit&opt=update  method=\"POST\"><table>";
+	echo "<center><form action=?module=radio&action=stations&station_n=$station_n&command=edit&opt=update  method=\"POST\"><table>";
 	echo "<tr><td>Header title</td><td><input type=text name=headertitle value=".$data[0]."></td></tr>";
 	echo "<tr><td>Header genre</td><td><input type=text name=headergenre value=".$data[1]."></td></tr>";
 	echo "<tr><td>Header bitrate</td><td><input type=text name=headerbitrate value=".$data[2]."></td></tr>";
@@ -35,24 +35,24 @@ while($stop == "0")
 $station_n = $data[$teller];
 if($station_n != "")
 {
-echo "<tr><td>$station_n</td><td><a href=?page=admin&subpage=stations&action=edit&station_n=$station_n>Edit</a> | <a href=?page=admin&subpage=stations&action=del&station_n=$station_n>Delete</a></td></tr>";
+echo "<tr><td>$station_n</td><td><a href=?module=radio&action=stations&command=edit&station_n=$station_n>Edit</a> | <a href=?module=radio&action=stations&command=del&station_n=$station_n>Delete</a></td></tr>";
 }else
 {
 	$stop ="yes";
 }
 $teller++;
 }
-echo "<tr><td><form action=?page=admin&subpage=stations&action=add  method=\"POST\"><input type=text name=new_station_n></td><td><input type=submit value=Add></td></form></tr>";
+echo "<tr><td><form action=?module=radio&action=stations&command=add  method=\"POST\"><input type=text name=new_station_n></td><td><input type=submit value=Add></td></form></tr>";
 echo "</table></center>";
 }
 }else
 {
 $station_n = $_SESSION['station'];
 	if($_GET['opt'] == "update"){
-	$settings->add($station_n,$_POST['headertitle'],$_POST['headergenre'],$_POST['headerbitrate'],$_POST['headersite'],$_POST['debugkey']);
+	$this->settings->add($station_n,$_POST['headertitle'],$_POST['headergenre'],$_POST['headerbitrate'],$_POST['headersite'],$_POST['debugkey']);
 	}
 	$data = explode("&", $this->settings->get($station_n));
-	echo "<center><form action=?page=admin&subpage=stations&station_n=$station_n&action=edit&opt=update  method=\"POST\"><table>";
+	echo "<center><form action=?module=radio&action=stations&station_n=$station_n&command=edit&opt=update  method=\"POST\"><table>";
 	echo "<tr><td>Header title</td><td><input type=text name=headertitle value=".$data[0]."></td></tr>";
 	echo "<tr><td>Header ganre</td><td><input type=text name=headergenre value=".$data[1]."></td></tr>";
 	echo "<tr><td>Header bitrate</td><td><input type=text name=headerbitrate value=".$data[2]."></td></tr>";
