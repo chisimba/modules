86780ecdfc1de37eecf72584451b2de05616474e
David Wafula <davidwaf@gmail.com>
2007-10-29 12:37:45 +0000

    added param id

--- webpresent/templates/content/audience-applet.php
+++ webpresent/templates/content/audience-applet.php
@@ -4,6 +4,8 @@
 	echo '	<param name="archive" value="'.$this->presentationsURL.'/presentations-client.jar"/>';
 	echo '	<param name="host" value="localhost"/>';
         echo '  <param name="port" value="1962"/>';
+        echo '  <param name="thumbnail" value="'.$this->objConfig->getcontentBasePath().'webpresent_thumbnails/'.$id.'"/>';
+        echo '  <param name="id" value="'.$id.'"/>';
 	echo "</applet> ";
-       echo '</center>';
+        echo '</center>';
 ?>
