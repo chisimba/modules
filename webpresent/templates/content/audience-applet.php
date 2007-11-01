cf86439e04cd1cb07393b9328bae9bae64a1b489
David Wafula <davidwaf@gmail.com>
2007-11-01 12:12:28 +0000

    resized applet

--- webpresent/templates/content/audience-applet.php
+++ webpresent/templates/content/audience-applet.php
@@ -1,6 +1,6 @@
 <?php
         echo '<center>';
-	echo '<applet code="avoir.realtime.presentations.client.ClientViewer" width="720" height="418">';
+	echo '<applet code="avoir.realtime.presentations.client.ClientViewer" width="720" height="500">';
 	echo '	<param name="archive" value="'.$this->presentationsURL.'/presentations-client.jar"/>';
 	echo '	<param name="host" value="localhost"/>';
         echo '  <param name="port" value="1962"/>';
--- webpresent/templates/content/presenter-applet.php
+++ webpresent/templates/content/presenter-applet.php
@@ -8,7 +8,7 @@
          
         echo '<center>';
         //echo '<p>'.$homeLink->show().'</p>';
-	echo '<applet code="avoir.realtime.presentations.client.presenter.ui.MainFrame" width="720" height="418" align="middle">';
+	echo '<applet code="avoir.realtime.presentations.client.presenter.ui.MainFrame" width="780" height="438" align="middle">';
 	echo '	<param name="archive" value="'.$this->presentationsURL.'/presentations-client.jar"/> ';
 	echo '	<param name="host" value="localhost"/>';
         echo '  <param name="port" value="1962"/>';
