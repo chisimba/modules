fe51df2000bd792a2305cd8af2600cc3f60bd995
Derek Keats <derek@dkeats.com>
2007-11-05 20:57:41 +0000

    Added a configuration file to be used in all service classes

---
+++ flashremote/resources/flashremote_config_inc.php
@@ -0,0 +1,23 @@
+<?php
+/**
+*
+* Chisimba basepath configuration for running flash remoting services
+*
+* Created on 05 Nov 2007
+*
+* To change the template for this generated file go to
+* Window - Preferences - PHPeclipse - PHP - Code Templates
+*
+* Chisimba class to
+*
+* @author Derek keats
+* @package flashremote
+*
+*/
+define('kewl_entry_point_run', TRUE);
+$GLOBALS['savedir'] = getcwd();
+
+// Define the basepath for Chisimba app. This should point to your app directory
+// within your operating system's filesystem.
+define('CHISIMBA_BASEPATH', '/home/dkeats/Desktop/eclipse-workspace/chisimba_framework/app/');
+?>
