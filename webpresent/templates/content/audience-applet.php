1b455bbf0d803205fba5aa2324e3936e541e31b0
David Wafula <davidwaf@gmail.com>
2007-11-13 11:03:14 +0000

    updated  applet size

--- webpresent/templates/content/audience-applet.php
+++ webpresent/templates/content/audience-applet.php
@@ -1,11 +1,11 @@
 <?php
-        echo '<center>';
-	echo '<applet code="avoir.realtime.presentations.client.ClientViewer" width="720" height="500">';
+    echo '<center>';
+	echo '<applet code="avoir.realtime.presentations.client.ClientViewer" width="75%" height="500">';
 	echo '	<param name="archive" value="'.$this->presentationsURL.'/presentations-client.jar"/>';
 	echo '	<param name="host" value="localhost"/>';
-        echo '  <param name="port" value="1962"/>';
-        echo '  <param name="thumbnail" value="'.$this->objConfig->getcontentBasePath().'webpresent_thumbnails/'.$id.'"/>';
-        echo '  <param name="id" value="'.$id.'"/>';
+    echo '  <param name="port" value="3128"/>';
+    echo '  <param name="thumbnail" value="'.$this->objConfig->getcontentBasePath().'webpresent_thumbnails/'.$id.'"/>';
+    echo '  <param name="id" value="'.$id.'"/>';
 	echo "</applet> ";
-        echo '</center>';
+	echo '</center>';
 ?>
--- webpresent/templates/content/presenter-applet.php
+++ webpresent/templates/content/presenter-applet.php
@@ -1,19 +1,10 @@
 <?php
-
-        //$homeLink = new link ($this->uri(array('action'=>'home')));
-       // $homeLink->link = 'Home';
-        $this->homeLink=& $this->newObject('link', 'htmlelements');
-        $homeLink->link = 'Home';
-        echo '  <param name="contentBasePath" value="'.$this->objConfig->getcontentBasePath().'webpresent/'.$id.'"/>';
-         
-        echo '<center>';
-        //echo '<p>'.$homeLink->show().'</p>';
-	echo '<applet code="avoir.realtime.presentations.client.presenter.ui.MainFrame" width="780" height="500" align="middle">';
-	echo '	<param name="archive" value="'.$this->presentationsURL.'/presentations-client.jar,'.$this->presentationsURL.'/video.jar"/> ';
+    echo '<center>';
+    echo '<applet code="avoir.realtime.presentations.client.presenter.ui.PresenterFrame" width="75%" height="600" align="middle">';
+	echo '	<param name="archive" value="'.$this->presentationsURL.'/presentations-client.jar"/> ';
 	echo '	<param name="host" value="localhost"/>';
-        echo '  <param name="port" value="1962"/>';
-        echo '  <param name="contentBasePath" value="'.$this->objConfig->getcontentBasePath().'webpresent/'.$id.'"/>';
-        echo '  <param name="files" value="'.$files.'"/>';
-	echo "</applet> ";
-        echo '</center>';
+    echo '  <param name="port" value="3128"/>';
+    echo '  <param name="contentBasePath" value="'.$this->objConfig->getcontentBasePath().'webpresent/'.$id.'"/>';
+    echo "</applet> ";
+    echo '</center>';
 ?>
