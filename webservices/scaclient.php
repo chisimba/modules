55cfb496eb958cb53169625b54a73e159793bedc
paulscott <unknown@unknown.com>
2007-11-13 10:32:15 +0000

    sca test

--- webservices/scaclient.php
+++ webservices/scaclient.php
@@ -1,21 +1,20 @@
 <?php
 include 'SCA/SCA.php';
 
-$smd = file_get_contents('http://127.0.0.1/chisimba_framework/app/packages/webservices/server.php?wsdl');
-file_put_contents('HelloService.wsdl', $smd);
+$name = 'William Shakespeare';
+
+//$smd = file_get_contents('http://fsiu.uwc.ac.za/chisimba_modules/webservices/server.php?wsdl');
+//file_put_contents('HelloService.wsdl', $smd);
 $service = SCA::getService('server.php');
-
-var_dump($service);
-echo $service->lookup('William Shakespeare');
-
-//var_dump($service->getBlogPosts(54));
-
+//var_dump($service);
+echo $service->lookup($name);
 echo "<br />";
 echo $service->hello('Paul');
-
-$s = new SoapClient('http://127.0.0.1/chisimba_framework/app/packages/webservices/server.php?wsdl', array('location' =>
+echo "<br />";
+echo "<br />";
+$s = new SoapClient('http://fsiu.uwc.ac.za/chisimba_modules/webservices/server.php?wsdl', array('location' =>
 'http://fsiu.uwc.ac.za/chisimba_modules/webservices/server.php?wsdl'));
-var_dump($s->lookup(array('name' => 'William Shakespeare')));
-var_dump($s);
+var_dump($s->hello(array('name' => $name)));
+//var_dump($s);
 
 ?>
