19467d8d58e6be72275967779684d2e526e8502c
David Wafula <davidwaf@gmail.com>
2007-11-14 12:44:53 +0000

    added function to start server automatically if not running, as a background process

--- webpresent/controller.php
+++ webpresent/controller.php
@@ -204,8 +204,8 @@ class webpresent extends controller
 	
 	private function startServer()
     {
-       
-    $cmd = "/usr/lib/jvm/java-1.5.0-sun/bin/java  -Xms128m -Xmx256m -cp .:". $this->objConfig->getModulePath()."/realtime/resources/presentations/presentations-server.jar avoir.realtime.presentations.server.Server 3128 >/dev/null &";
+       ///usr/lib/jvm/java-1.5.0-sun/bin/
+    $cmd = "java  -Xms128m -Xmx256m -cp .:". $this->objConfig->getModulePath()."/realtime/resources/presentations/presentations-server.jar avoir.realtime.presentations.server.Server 3128 >/dev/null &";
 	//echo $cmd;
     system($cmd,$return_value);
     
