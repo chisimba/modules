832bc3d74178789b325d886c75e58d469d664cea
paulscott <unknown@unknown.com>
2007-11-30 08:36:57 +0000

    fixed up formatting

--- blog/classes/block_lastten_class_inc.php
+++ blog/classes/block_lastten_class_inc.php
@@ -89,7 +89,7 @@ class block_lastten extends object
     public function init() 
     {
         $this->objLanguage = &$this->getObject('language', 'language');
-        $this->blogOps = &$this->getObject('blogposts', 'blog');
+        $this->blogPosts = &$this->getObject('blogposts', 'blog');
         $this->display = $this->blogPosts->showLastTenPosts();
         $this->title = $this->objLanguage->languageText("mod_blog_block_latestblogs", "blog");
     }
