1aab50c18a408dddc064c6bef40b8faac15ebcdd
David Wafula <davidwaf@gmail.com>
2007-12-03 11:47:57 +0000

    fullname bug fix

--- webpresent/templates/content/audience-applet.php
+++ webpresent/templates/content/audience-applet.php
@@ -1,6 +1,6 @@
 <?php
     echo '<center>';
-	echo '<applet code="avoir.realtime.presentations.client.ClientViewer" width="75%" height="500">';
+		echo '<applet code="avoir.realtime.presentations.client.viewer.ClientViewer" width="75%" height="600">';
 	echo '	<param name="archive" value="'.$this->presentationsURL.'/presentations-client.jar"/>';
 	echo '	<param name="host" value="localhost"/>';
     echo '  <param name="port" value="3128"/>';
@@ -9,6 +9,7 @@
 	echo '  <param name="invokedThroughWebpresent" value="true"/>';
 	echo '  <param name="user" value="'.$this->objUser->userName().'"/>';
 	echo '  <param name="isLoggedIn" value="'.$this->objUser->isLoggedIn().'"/>';
+	echo '  <param name="fullname" value="'.$this->objUser->fullname().'"/>';
 	echo "</applet> ";
 	echo '</center>';
 ?>
--- webpresent/templates/content/presenter-applet.php
+++ webpresent/templates/content/presenter-applet.php
@@ -1,6 +1,6 @@
 <?php
     echo '<center>';
-    echo '<applet code="avoir.realtime.presentations.client.presenter.ui.PresenterFrame" width="75%" height="600" align="middle">';
+    echo '<applet code="avoir.realtime.presentations.client.presenter.PresenterFrame" width="75%" height="600" align="middle">';
 	echo '	<param name="archive" value="'.$this->presentationsURL.'/presentations-client.jar"/> ';
 	echo '	<param name="host" value="localhost"/>';
     echo '  <param name="port" value="3128"/>';
@@ -8,6 +8,7 @@
     echo '  <param name="user" value="'.$this->objUser->userName().'"/>';
 	echo '  <param name="slideId" value="'.$id.'"/>';
 	echo '  <param name="jodconverterPath" value="'.$jodconverterPath.'"/>';
-    echo "</applet> ";
+	echo '  <param name="fullname" value="'.$this->objUser->fullname().'"/>'; 
+   echo "</applet> ";
     echo '</center>';
 ?>
