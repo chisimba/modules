c33b715fb48a601a361bb79a5f7f2b1d15ad4019
Derek Keats <derek@dkeats.com>
2008-01-09 07:34:43 +0000

    Added ohloh badge block Added MODULE_STATUS to register.conf

---
+++ blockalicious/classes/block_ohloh_class_inc.php
@@ -0,0 +1,37 @@
+<?
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run'])
+{
+	die("You cannot view this page directly");
+}
+// end security check
+
+/**
+*
+* The class provides a hello world block to demonstrate
+* how to use blockalicious
+*
+* @author Derek Keats
+*
+*/
+class block_helloworld extends object
+{
+    var $title;
+
+    /**
+    * Constructor for the class
+    */
+    function init()
+    {
+        //Set the title -
+        $this->title='Ohloh: Chisimba';
+    }
+
+    /**
+    * Method to output a block with information on how help works
+    */
+    function show()
+	{
+       return "<script type=\"text/javascript\" src=\"http://www.ohloh.net/projects/5263/widgets/project_thin_badge\"></script>";
+    }
+}
