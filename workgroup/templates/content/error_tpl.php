1108bf8d399b38d1d4fa781d66e2adfac77189af
David Wafula <davidwaf@gmail.com>
2008-01-09 13:39:51 +0000

    changed text display to use register.conf

--- workgroup/templates/content/error_tpl.php
+++ workgroup/templates/content/error_tpl.php
@@ -3,6 +3,6 @@
         'context' => '', 
         'context' => ''
     );
-    echo $this->objLanguage->code2txt("phrase_notincontext", $rep);
-	//echo "<p>".$this->objLanguage->languageText("phrase_notincontext")."</p>";
+    //echo $this->objLanguage->code2txt("phrase_notincontext", $rep);
+	echo "<p>".$this->objLanguage->languageText("phrase_notincontext")."</p>";
 ?>
