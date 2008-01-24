8e4b290f716e0bb45ff3ecda75a1c2aedb53d1ca
nic <unknown@unknown.com>
2008-01-24 11:56:29 +0000

    template for no permissions

---
+++ cmsadmin/templates/content/cms_nopermissions_tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This template is used when user's without permissions attempt to edit content
+ */
+
+echo "<span class='error'>".$this->objLanguage->languageText('mod_cmsadmin_nopermissions','cmsadmin')."</span>";
+
+
+?>
