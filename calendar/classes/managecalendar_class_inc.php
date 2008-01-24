72e0fa00394acb870eda4f93450eb20f4b814c04
Jeremy O'Connor <joconnor@uwc.ac.za>
2008-01-24 10:01:39 +0000

    Bug.

--- calendar/classes/managecalendar_class_inc.php
+++ calendar/classes/managecalendar_class_inc.php
@@ -20,8 +20,8 @@ if (!$GLOBALS['kewl_entry_point_run'])
 class managecalendar extends object
 {
     var $eventsList; // an array to store current events
-    
-    
+
+
     /**
     * Constructor method to define the table
     */
@@ -29,11 +29,11 @@ class managecalendar extends object
         $this->objCalendar =& $this->getObject('dbcalendar', 'calendarbase');
         $this->objLanguage = &$this->getObject('language', 'language');
         $this->eventsList = NULL;
-        
+
         // Set the calling module
         $this->objCalendar->module = 'calendar';
     }
-	
+
 	/**
 	* Method to set the permission to edit/delete context events
 	* @param boolean $permission TRUE if user has permission to edit context events - else FALSE
@@ -42,15 +42,15 @@ class managecalendar extends object
 	{
 		$this->objCalendar->editDeletePermission[1] = $permission;
 	}
-	
+
 	function setEventsTag($eventsTag)
 	{
 		$this->objCalendar->eventsTag = $eventsTag;
 	}
-    
+
     /**
     * Method to get the details of a single event by providing the record Id
-    * 
+    *
     * @param string $id Record Id of the Event
     * @return array record
     */
@@ -60,8 +60,8 @@ class managecalendar extends object
     }
 
     /**
-    * Method to insert a single day event into the database. 
-    * 
+    * Method to insert a single day event into the database.
+    *
     * @param string $date - Date of the Event
     * @param string $eventtitle - Title of the Event
     * @param string $eventdetails - Details of the event
@@ -91,13 +91,13 @@ class managecalendar extends object
                 $timeFrom,
                 $timeTo
             );
-        
+
         return $lastInsert;
     }
-	
+
 	/**
-    * Method to insert a single day event for a context into the database. 
-    * 
+    * Method to insert a single day event for a context into the database.
+    *
     * @param string $date - Date of the Event
     * @param string $eventtitle - Title of the Event
     * @param string $eventdetails - Details of the event
@@ -128,10 +128,10 @@ class managecalendar extends object
             );
         return $lastInsert;
     }
-    
+
     /**
     * Method to insert a multi day event into the database.
-    * 
+    *
     * @param string $date - Date of the Event
     * @param string $date2 - Date when the event ends
     * @param string $multidaystart - Record ID of the Start (First Day) of the multiday event
@@ -149,11 +149,11 @@ class managecalendar extends object
         if ($dateFirstEntry == NULL) {
             $dateFirstEntry = strftime('%Y-%m-%d %H:%M:%S', mktime());
         }
-        
+
         if ($dateLastModified == NULL) {
             $dateLastModified = strftime('%Y-%m-%d %H:%M:%S', mktime());
         }
-        
+
         $lastInsert = $this->objCalendar->insertMultiDayEvent(
                 $date, // Date of Event
                 $date2, // End Date
@@ -170,13 +170,13 @@ class managecalendar extends object
                 $dateLastModified, // date of last entry
                 $eventStartId
             );
-        
+
         return $lastInsert;
     }
-	
+
 	/**
     * Method to insert a multi day event into the database.
-    * 
+    *
     * @param string $date - Date of the Event
     * @param string $date2 - Date when the event ends
     * @param string $multidaystart - Record ID of the Start (First Day) of the multiday event
@@ -192,15 +192,15 @@ class managecalendar extends object
     */
     function insertMultiDayContextEvent ($date, $date2, $eventtitle, $eventdetails, $eventurl, $contextCode, $userFirstEntry, $userLastModified = NULL, $dateFirstEntry = NULL, $dateLastModified = NULL, $eventStartId=NULL)
     {
-        
+
         if ($dateFirstEntry == NULL) {
             $dateFirstEntry = strftime('%Y-%m-%d %H:%M:%S', mktime());
         }
-        
+
         if ($dateLastModified == NULL) {
             $dateLastModified = strftime('%Y-%m-%d %H:%M:%S', mktime());
         }
-        
+
         $lastInsert = $this->objCalendar->insertMultiDayEvent(
                 $date, // Date of Event
                 $date2, // End Date
@@ -217,13 +217,13 @@ class managecalendar extends object
                 $dateLastModified, // date of last entry
                 $eventStartId
             );
-        
+
         return $lastInsert;
     }
-    
+
     /**
     * Method to update an event
-    * 
+    *
     * @param string $id - Record If of the event
     * @param string $multidayevent - A flag to indicate whether this is a multiday event or not
     * @param string $date - Date of the Event
@@ -239,29 +239,29 @@ class managecalendar extends object
         if ($multidayevent == 0) {
             $multiday_startid = NULL;
         }
-        
+
         $this->objCalendar->updateSingle(
-                $id, 
+                $id,
                 $multidayevent, // update
-                $date, 
+                $date,
                 $multiday_startid, // update
-                $eventtitle, 
-                $eventdetails, 
-                $eventurl, 
+                $eventtitle,
+                $eventdetails,
+                $eventurl,
                 0, // userorcontext
                 NULL, // Context
                 NULL, // workgroup
                 0, // show users
-                $user, 
+                $user,
                 strftime('%Y-%m-%d %H:%M:%S', mktime()) // date LAST entry
             );
-        
+
         return;
     }
-	
+
 	/**
     * Method to update an event
-    * 
+    *
     * @param string $id - Record If of the event
     * @param string $multidayevent - A flag to indicate whether this is a multiday event or not
     * @param string $date - Date of the Event
@@ -278,86 +278,86 @@ class managecalendar extends object
         if ($multidayevent == 0) {
             $multiday_startid = NULL;
         }
-        
+
         $this->objCalendar->updateSingle(
-                $id, 
+                $id,
                 $multidayevent, // update
-                $date, 
+                $date,
                 $multiday_startid, // update
-                $eventtitle, 
-                $eventdetails, 
-                $eventurl, 
+                $eventtitle,
+                $eventdetails,
+                $eventurl,
                 1, // userorcontext
                 $contextCode, // Context
                 NULL, // workgroup
                 0, // show users
-                $user, 
+                $user,
                 strftime('%Y-%m-%d %H:%M:%S', mktime()) // date LAST entry
             );
-        
+
         return;
     }
-    
+
     /**
     * Method to delete a single event
-    * 
+    *
     * @param string $id Record Id of the Event
     * @param string $userId User Id of the person to whom the event belongs
     */
     function deleteSingle($id, $userId)
     {
         $event = $this->objCalendar->getSingle($id);
-        
+
         if ($event['userFirstEntry'] != $userId || $event['userorcontext'] != 0) {
             return;
         } else {
             return $this->objCalendar->deleteSingle($id);
         }
     }
-    
+
     /**
     * Method to delete a multi day event event
-    * 
+    *
     * @param string $multiEventId Record Id of the start of the multiday event
     */
     function deleteBatch($multiEventId)
     {
         $this->objCalendar->deleteBatch($multiEventId);
-        return;	
+        return;
     }
-    
+
     /**
     * Method to delete a multi day event (children)
-    * 
+    *
     * @param string $multiEventId Record Id of the start of the multiday event
     */
     function deleteMultiEventsChild($multiEventId)
     {
         $this->objCalendar->deleteMultiEventsChild($multiEventId);
-        return;	
+        return;
     }
-    
-    
+
+
     /**
     * Method to get the start of a multi-day event
-    * 
+    *
     * @param string $start_id Record Id of the start date
     */
     function getStartMultiDayEvent($start_id)
     {
         return $this->objCalendar->getStartMultiDayEvent($start_id);
     }
-    
+
     /**
     * Method to get the end of a multi-day event
-    * 
+    *
     * @param string $start_id Record Id of the start date
     */
     function getLastMultiDayEvent($start_id)
     {
         return $this->objCalendar->getLastMultiDayEvent($start_id);
     }
-	
+
 	/**
 	* Method to generate a calendar
 	* @param string $userId Record Id of the User if it needs to include User Events
@@ -369,33 +369,33 @@ class managecalendar extends object
 	function getEventsCalendar($user=NULL, $context=NULL, $month, $year)
 	{
 		$sql = $this->objCalendar->getCalendarSQL($user, $context, $month, $year);
-		
-echo $context;
+
+//echo $context;
 
 
 		return $this->objCalendar->generateCalendar('user', $user, $month, $year, $sql);
 	}
-    
+
     /**
     * Method to show a small calendar with list of events. Used for the Personal Space module.
     */
     function show()
     {
         $objUser =& $this->getObject('user', 'security');
-        
-        
+
+
         $events2 = $this->objCalendar->getEvents('user', $objUser->userId(), (date('Y-m-').'01'), NULL, 10);
-        
+
         $title = '<h1>'.$this->objLanguage->languageText('word_calendar').'</h1>';
-        
+
         $eventsList = $this->objCalendar->generateSmallListing ($events2, 'calendar');
-        
+
         $calendar = $this->objCalendar->generateSmallCalendar('user', $objUser->userId());
-        
+
         $uri = $this->uri(NULL, 'calendar');
-        
+
         $link = '<br /><p><a href="'.$uri.'">'.$this->objLanguage->languageText('word_calendar').'</a></p>';
-        
+
         return $title.' '.$calendar.'<br /><br />'.$eventsList.$link ;
     }
 
