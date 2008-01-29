1007734f862997f5c8fd23767a91f7e01a2797ef
David Wafula <davidwaf@gmail.com>
2008-01-29 11:54:47 +0000

    Added Files:  	realtime-classroom-applet_tpl.php

---
+++ templates/content/realtime-classroom-applet_tpl.php
@@ -0,0 +1,11 @@
+
+<?php
+
+    echo '<center>';
+       echo '<applet code=avoir.realtime.classroom.RealtimeClassroomApplet.class width=460 height=160>';
+	echo '<param name=archive value="'.$this->presentationsURL.'/avoir-realtime-classroom-0.1.jar">';
+        echo 'alt="Your browser understands the &lt;APPLET&gt; tag but isn\'t running the applet, for some reason."
+        Your browser is completely ignoring the &lt;APPLET&gt; tag! ';     
+      echo '</applet>';
+    echo '</center>';
+?>
