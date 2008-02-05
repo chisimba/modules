6256bfc913d32798f2da5e8a24810d6b74d3efd3
David Wafula <davidwaf@gmail.com>
2008-02-05 17:16:01 +0000

    added support for images and presentations

--- realtime/templates/content/realtime-classroom_tpl.php
+++ realtime/templates/content/realtime-classroom_tpl.php
@@ -7,54 +7,72 @@ if (isset ($noContextCode))
 <!-- START FILE MANAGER FILE CHOOSER CODE-->
 <script type="text/javascript">
 
-	function openWindow(theURL,winName,features) { 
-	  newwindow=window.open(theURL,winName,features);
-       if (window.focus) {
-       	newwindow.focus()
-	   }
-    } 
+   function openWindow(theURL,winName,features) {
+    newwindow=window.open(theURL,winName,features);
+     if (window.focus) {
+      newwindow.focus()
+    }
+   } 
 
     /*
      * Function that is called by Java applet when user wants to select an image.
      */
     function openImageFileChooser() {
-		openWindow('<?php echo str_replace('amp;', '', $this->uri(array('action'=>'selectrealtimeimagewindow', 'name'=>'selectimage'), 'filemanager')); ?>','new','toolbar=no, menubar=no, width=600, height=400, resizable=yes, scrollbars=auto, toolbar=no top=200 screenY=200 left=300 screenY=300');
+      openWindow('<?php echo str_replace('amp;', '', $this->uri(array('action'=>'selectrealtimeimagewindow', 'name'=>'selectimage'), 'filemanager')); ?>','new','toolbar=no, menubar=no, width=600, height=400, resizable=yes, scrollbars=auto, toolbar=no top=200 screenY=200 left=300 screenY=300');
     }
 
-	/*
-	 * Function that is called by file manager image chooser popup when a file has been selected.
-	 */    
-    function callFunctionFromParent()
-	{
-	    var imageURL = document.getElementById('hidden_selectimage').value;
-	    document.getElementById("whiteboardapplet").imageSelected(imageURL);
-	}
-</script>
-<div style="display:none;">
-<?php
+    /*
+     * Function that is called by Java applet when user wants to select a presentation.
+     */
+    function openPresentationFileChooser() {
+      openWindow('<?php echo str_replace('amp;', '', $this->uri(array('action'=>'selectpresentationwindow', 'name'=>'selectpresentation'), 'filemanager')); ?>','new','toolbar=no, menubar=no, width=600, height=400, resizable=yes, scrollbars=auto, toolbar=no top=200 screenY=200 left=300 screenY=300');
+    }
 
-	$objSelectFile = $this->getObject('selectrealtimeimage', 'filemanager');
-	$objSelectFile->name = 'selectimage';
-	$this->setVar('pageSuppressXML', TRUE);
-	echo $objSelectFile->show();
+    /*
+     *Function that is called by file manager image chooser popup when a file has been selected.
+     */    
+    function passImageUrlToApplet(imageURL)
+    {
+       // alert(imageURL);
+    // var imageURL = document.getElementById('hidden_selectimage').value;
+    document.RealtimeClassroomApplet.selectImage(imageURL);
+    }
+
+    /*
+     *Function that is called by file manager presentation chooser popup when a file has been selected.
+     */    
+    function passPresentationUrlToApplet(presentationURL,presentationPath,presentationId)
+    {
+     //var presentationURL = document.getElementById('hidden_selectpresentation').value;
+    document.RealtimeClassroomApplet.selectPresentation(presentationURL,presentationPath,presentationId);
+    }
+    </script>
+   <div style="display:none;">
+   <?php
+
+    $objSelectFile = $this->getObject('selectfile', 'filemanager');
+    $objSelectFile->name = 'selectimage';
+    $this->setVar('pageSuppressXML', TRUE);
+    echo $objSelectFile->show();
 ?>
 </div>
 
 <!-- END FILE MANAGER FILE CHOOSER CODE-->
 <?
-$modPath=$this->objAltConfig->getModulePath();
-$replacewith="";
-$docRoot=$_SERVER['DOCUMENT_ROOT'];
-$appletPath=str_replace($docRoot,$replacewith,$modPath);
-$appletCodeBase="http://" . $_SERVER['HTTP_HOST']."/".$appletPath.'/realtime/resources/';
-//echo $appletCodeBase;
-echo '<applet codebase="'.$appletCodeBase.'"';
-echo 'code="avoir.realtime.classroom.RealtimeClassroomApplet.class"';
-echo 'archive="avoir-realtime-classroom-0.1.jar,avoir-realtime-common-0.1.jar,avoir-whiteboard-client-0.1.jar" width="700" height="500">';
-echo '<param name=userName value="'.$this->objUser->userName().'"';
-echo '<param name=fullname value="'.$this->objUser->fullname().'">';
-echo '<param name=port value="1981">';
-      echo '</applet>';
+    $modPath=$this->objAltConfig->getModulePath();
+    $replacewith="";
+    $docRoot=$_SERVER['DOCUMENT_ROOT'];
+    $appletPath=str_replace($docRoot,$replacewith,$modPath);
+    $appletCodeBase="http://" . $_SERVER['HTTP_HOST']."/".$appletPath.'/realtime/resources/';
+    //echo $appletCodeBase;
+    echo '<applet codebase="'.$appletCodeBase.'"';
+    echo 'code="avoir.realtime.classroom.RealtimeClassroomApplet.class" name ="RealtimeClassroomApplet"';
+    echo 'archive="avoir-realtime-classroom-0.1.jar,avoir-realtime-common-0.1.jar,avoir-whiteboard-client-0.1.jar" width="100%" height="600">';
+    echo '<param name=userName value="'.$this->objUser->userName().'">';
+    echo '<param name=fullname value="'.$this->objUser->fullname().'">';
+
+    echo '<param name=port value="1981">';
+    echo '</applet>';
          
 
 }
