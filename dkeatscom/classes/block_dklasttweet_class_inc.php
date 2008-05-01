3624717f339e818676fe7156b132ff43b8b7a14c
Derek Keats <derek@dkeats.com>
2008-05-01 10:31:56 +0000

    Adding a helper module for the dkeats.com website

---
+++ dkeatscom/classes/block_dklasttweet_class_inc.php
@@ -0,0 +1,47 @@
+<?
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run'])
+{
+	die("You cannot view this page directly");
+}
+// end security check
+
+/**
+*
+* The class provides a hello world block to demonstrate
+* how to use blockalicious
+*
+* @author Derek Keats
+*
+*/
+class block_dklasttweet extends object
+{
+    var $title;
+
+    /**
+    * Constructor for the class
+    */
+    function init()
+    {
+        $this->objLanguage = $this->getObject('language', 'language');
+        $this->objUser = $this->getObject('user', 'security');
+        $this->title=$this->objLanguage->languageText("mod_twitter_lasttweet", "twitter");
+    }
+
+    /**
+    * Method to output a Tweet block
+    */
+    function show()
+	{
+        $objConfig = $this->getObject('dbsysconfig', 'sysconfig');
+        $userName= $objConfig->getValue('twittername', 'dkeats.com');
+        $password = $objConfig->getValue('twitterpassword', 'dkeats.com');
+        if ($userName!==NULL && $password !==NULL) {
+            $objTwitterRemote = $this->getObject('twitterremote', 'twitter');
+            $objTwitterRemote->initializeConnection($userName, $password);
+            return $objTwitterRemote->showStatus(TRUE, FALSE);
+        } else {
+            return $this->objLanguage->languageText("mod_twitter_nologonshort", "twitter");
+        }
+    }
+}
