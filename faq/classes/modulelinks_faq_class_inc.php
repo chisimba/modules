bc94b8b7b21a9201f1cb991b7d8df9ec1a669753
bvanrensburg <unknown@unknown.com>
2008-05-20 14:11:52 +0000

    created site map for faq module

--- faq/classes/modulelinks_faq_class_inc.php
+++ faq/classes/modulelinks_faq_class_inc.php
@@ -11,16 +11,49 @@ if (!$GLOBALS['kewl_entry_point_run'])
 
 class modulelinks_faq extends object
 {
-
     public function init()
     {
         $this->_objDBFaqEntries = & $this->newObject('dbfaqentries','faq');
         $this->_objDBCategories = & $this->newObject('dbfaqcategories','faq');
+        $this->objDbContext = &$this->getObject('dbcontext','context');
+        $this->contextCode = $this->objDbContext->getContextCode();
+    	// If we are not in a context...
+        if ($this->contextCode == null) {
+            $this->contextId = "root";
+        }
+        else {
+            $this->contextId = $this->contextCode;
+        }
     }
     
     public function show()
     {
+		$rootNode = new treenode (array('link'=>$this->uri(NULL, 'faq'), 'text'=>'FAQ'));
         
+        $nodesArray = array();
+        
+        $categories = $this->_objDBCategories->getContextCategories($this->contextId);
+        
+        foreach ($categories as $category)
+        {
+            $node =& new treenode(array('link'=>$this->uri(array('action'=>'view', 'id'=>$category['id'])), 'text'=>$category['categoryname']));
+            
+            $nodesArray['category'.$category['id']] =& $node;
+            $rootNode->addItem($nodesArray['category'.$category['id']]);
+            
+            $entries = $this->_objDBFaqEntries->getEntries($this->contextId, $category['id']);
+            
+            foreach ($entries as $entry)
+            {
+                $node =& new treenode(array('link'=>$this->uri(array('action'=>'view', 'id'=>$category['id'])), 'text'=>$entry['question']));
+                
+                $nodesArray['entry'.$entry['id']] =& $node;
+                $nodesArray['category'.$category['id']]->addItem($nodesArray['entry'.$entry['id']]);
+            }
+
+        }
+
+        return $rootNode;
     }
     
     /**
@@ -50,8 +83,10 @@ class modulelinks_faq extends object
           }
          
           return $bigArr;
+          
+          
 */
-         }
+    }
     
 }
 
