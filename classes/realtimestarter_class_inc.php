fe19164b2c896b4c388c0277ee44e1b767579bde
David Wafula <davidwaf@gmail.com>
2008-06-19 14:57:43 +0000

    removed dependency on chameleon..can work with any server

--- classes/realtimestarter_class_inc.php
+++ classes/realtimestarter_class_inc.php
@@ -12,64 +12,23 @@
             $this->objConfig = $this->getObject('altconfig', 'config');
 
         }
-
-        /**
-         * Starts the slide server with own id
-         */
-        function startSlidesServer()
-        {
-    
-            $objSysConfig = $this->getObject('dbsysconfig', 'sysconfig');
-            $slideServerId=$this->objConfig->serverName();
-            $cmd = "java -Xms64m -Xmx128m -cp ".    
-            $this->objConfig->getModulePath().
-    "/realtime/resources/realtime-base-0.1.jar:".$this->objConfig->getModulePath().
-    "/realtime/resources/realtime-launcher-0.1.jar avoir.realtime.tcp.base.SlidesServer ".$slideServerId."  >/dev/null &";
-            system($cmd,$return_value);
-        }
-
+   
         /**
          * Generates a URL to be used to replace the filter
          * @param <type> $id
          * @param <type> $agenda
          * @return <type>
          */
-        public function generateURL($id,$agenda){
-
-            $userLevel;
-            $isLoggedIn='false';
-
-            if ($this->objUser->isAdmin())
-            {
-                $this->userLevel = 'admin';
-            }
-            elseif ($this->objUser->isLecturer())
-            {
-                $this->userLevel = 'lecturer';
-            }
-            elseif ($this->objUser->isStudent())
-            {
-                $this->userLevel = 'student';
-            } else
-            {
-                $this->userLevel = 'guest';
-            }
-            $isLoggedIn =$this->objUser->isLoggedIn();
-            $resourcesPath="/var/www/app/chisimba_modules/realtime/resources";
-            $this->startSlidesServer();
-            $objSysConfig = $this->getObject('dbsysconfig', 'sysconfig');
-            $slideServerId=$this->objConfig->serverName();
-            $appletCodeBase='http://chameleon.uwc.ac.za/app/chisimba_modules/realtime/resources/';
-            $slidesDir='http://chameleon.uwc.ac.za/app/usrfiles/webpresent/';
+        public function generateURL($id,$agenda,$resourcesPath,$appletCodeBase,$slidesDir,$username,$fullnames,$userLevel,$slideServerId){
             $url='<center>';
             $url.='<applet codebase="'.$appletCodeBase.'"';
             $url.='code="avoir.realtime.tcp.launcher.RealtimeLauncher" name ="Avoir Realtime Applet"';
      
             $url.='archive="realtime-launcher-0.1.jar" width="100%" height="600">';
-            $url.='<param name=userName value="'.$this->objUser->userName().'">';
-            $url.='<param name=isLocalhost value="true">';
-            $url.='<param name=fullname value="'.$this->objUser->fullname().'">';
-            $url.='<param name=userLevel value="'.$this->userLevel.'">';
+            $url.='<param name=userName value="'.$username.'">';
+            $url.='<param name=isLocalhost value="false">';
+            $url.='<param name=fullname value="'.$fullnames.'">';
+            $url.='<param name=userLevel value="'.$userLevel.'">';
             $url.='<param name=uploadURL value="'.$uploadURL.'">';
             $url.='<param name=chatLogPath value="'.$chatLogPath.'">';
             $url.='<param name=siteRoot value="'.$siteRoot.'">';
@@ -81,12 +40,13 @@
             $url.='<param name=resourcesPath value="'.$resourcesPath.'">';
             $url.='<param name=sessionId value="'.$id.'">';
             $url.='<param name=sessionTitle value="'.$agenda.'">';
-            $url.='<param name=slideServerId value="gen12Srv15Nme3">';
+            $url.='<param name=slideServerId value="'.$slideServerId.'">';
     
             $url.='<param name=isSessionPresenter value="false">';
             $url.='</applet>';
     
-
+            $url.='</center>';
+            
             return $url;
         } 
     }
