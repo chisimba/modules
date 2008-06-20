e9a9824465411beb213a588324406cf311c52c4c
David Wafula <davidwaf@gmail.com>
2008-06-20 15:55:30 +0000

    updated the super node ips

--- resources/java/chisimba_realtime_base/src/avoir/realtime/tcp/base/TCPClient.java
+++ resources/java/chisimba_realtime_base/src/avoir/realtime/tcp/base/TCPClient.java
@@ -84,7 +84,7 @@ public class TCPClient {
      */
     protected ObjectOutputStream writer;
     private boolean slideServerReplying = false;
-     private String SUPERNODE_HOST = "196.21.45.85";
+    private String SUPERNODE_HOST = "196.21.45.85";
     private int SUPERNODE_PORT = 80;
     //private String SUPERNODE_HOST = "localhost";
     //private int SUPERNODE_PORT = 22225;
