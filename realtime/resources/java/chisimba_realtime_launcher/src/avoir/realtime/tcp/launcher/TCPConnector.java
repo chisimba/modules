35b13426dbfa5de2559b2d9fff9ffb8d8fbafa59
David Wafula <davidwaf@gmail.com>
2008-06-27 08:55:57 +0000

    increased version number

--- realtime/resources/java/chisimba_realtime_launcher/src/avoir/realtime/tcp/launcher/TCPConnector.java
+++ realtime/resources/java/chisimba_realtime_launcher/src/avoir/realtime/tcp/launcher/TCPConnector.java
@@ -50,10 +50,10 @@ public class TCPConnector {
      */
     protected ObjectOutputStream writer;
     private boolean slideServerReplying = false;
-    //private String SUPERNODE_HOST = "196.21.45.85";
-    //private int SUPERNODE_PORT = 80;
-    private String SUPERNODE_HOST = "localhost";
-    private int SUPERNODE_PORT = 22225;
+    private String SUPERNODE_HOST = "196.21.45.85";
+    private int SUPERNODE_PORT = 80;
+    //private String SUPERNODE_HOST = "localhost";
+    //private int SUPERNODE_PORT = 22225;
     //everything is encrypted here
     private SSLSocketFactory dfactory;
     private SSLSocket socket;
