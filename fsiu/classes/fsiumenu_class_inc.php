4d44664a6dba82c210140c592c01f6a4883754b8
nonnie <unknown@unknown.com>
2008-07-03 09:42:57 +0000

    Cleared some bug

--- fsiu/classes/fsiumenu_class_inc.php
+++ fsiu/classes/fsiumenu_class_inc.php
@@ -45,17 +45,17 @@ class fsiumenu extends object
         $str = '<div id="sanordmenu">';
         
         /** SHow link to login
-          *if (!$this->objUser->isLoggedIn()) {
-          *  $objIcon = $this->newObject('geticon', 'htmlelements');
-          *  $objIcon->setIcon('loginlink');
-          *  $objIcon->alt = 'Login';
-          *  $objIcon->title = 'Login';
-          *  $objIcon->align = 'top';
-          *  $this->loadClass('link','htmlelements');
-          *  $link = new link ($this->uri(array('action'=>'error', 'message'=>'needlogin'), *'security'));
-          *  $link->link = $objIcon->show();
-          *  $str .=  '<div style="float:right;">'.$link->show().'</div>';
-        } */
+          if (!$this->objUser->isLoggedIn()) {
+            $objIcon = $this->newObject('geticon', 'htmlelements');
+            $objIcon->setIcon('loginlink');
+            $objIcon->alt = 'Login';
+            $objIcon->title = 'Login';
+            $objIcon->align = 'top';
+            $this->loadClass('link','htmlelements');
+            $link = new link ($this->uri(array('action'=>'error', 'message'=>'needlogin'), 'security'));
+            $link->link = $objIcon->show();
+            
+        }*/
         
         $str .= '<ul>';
         
