22c04c5a28901688e818a8d711ec38c2577efe62
paulscott <unknown@unknown.com>
2008-08-25 07:56:33 +0000

    "added block"

---
+++ remotepopularity/classes/block_remotepopularity_class_inc.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Block class for remote popularity
+ *
+ * Block functions for remote popularity module
+ *
+ * PHP version 5
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * @version    CVS: $Id:
+ * @package    remotepopularity
+ * @subpackage blocks
+ * @author     Paul Scott <pscott@uwc.ac.za>
+ * @copyright  2006-2008 AVOIR
+ * @license    http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @link       http://avoir.uwc.ac.za
+ * @see        remotepopularity
+ */
+// security check - must be included in all scripts
+if (!
+/**
+ * Description for $GLOBALS
+ * @global unknown $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+/**
+ * A block to show the pie chart of remote downloads.
+ *
+ * @category  Chisimba
+ * @author    Paul Scott
+ * @version   0.1
+ * @copyright 2006-2008 AVOIR
+ *
+ */
+class block_remotepopularity extends object
+{
+    /**
+     * The title of the block
+     *
+     * @var    object
+     * @access public
+     */
+    public $title;
+    /**
+     * graph in a box
+     *
+     * @var    object
+     * @access public
+     */
+    public $display;
+    
+    /**
+     * Language elements
+     *
+     * @var    object
+     * @access public
+     */
+    public $objLanguage;
+    
+    /**
+     * Standard init function
+     *
+     * Instantiate language and user objects and create title
+     *
+     * @return NULL
+     */
+    public function init() 
+    {
+    	$this->objLanguage = $this->getObject('language', 'language');
+        $objFlashGraph = $this->getObject('flashgraph', 'utilities');
+ 		$objFlashGraph->dataSource = $this->uri(array('action'=>'getlastdata', 'number' => 5));
+ 		$objFlashGraph->width = "100%";
+ 		//$objFlashGraph->height = "200";
+ 		$graph = $objFlashGraph->show(); 
+        $this->display = $graph;
+        $this->title = $this->objLanguage->languageText("mod_rempop_tofive", "remotepopularity");
+    }
+    /**
+     * Standard block show method.
+     *
+     * @return string $this->display block rendered
+     */
+    public function show() 
+    {
+    	$popLink = new link ($this->uri(NULL, 'remotepopularity'));
+        $popLink->link = $this->objLanguage->languageText('mod_rempop_linktext', 'remotepopularity');
+        
+        return $this->display.'<p>'.$popLink->show().'</p>';
+        //return $this->display;
+    }
+}
+?>
