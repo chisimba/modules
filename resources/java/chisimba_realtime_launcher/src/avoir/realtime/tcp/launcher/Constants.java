8ded21ad27c4cb5f6b649199fff940a09d51b0dc
David Wafula <davidwaf@gmail.com>
2008-08-30 20:56:13 +0000

    added APPLET and WEBSTART constants

--- resources/java/chisimba_realtime_launcher/src/avoir/realtime/tcp/launcher/Constants.java
+++ resources/java/chisimba_realtime_launcher/src/avoir/realtime/tcp/launcher/Constants.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package avoir.realtime.tcp.launcher;
 
 /**
@@ -12,7 +11,9 @@ package avoir.realtime.tcp.launcher;
 public class Constants {
 
     public static final String WIN_REALTIME_HOME = "c:\\avoir-realtime-0.1\\";
-     public static final String REALTIME_HOME = System.getProperty("user.home") + "/avoir-realtime-0.1";
+    public static final String REALTIME_HOME = System.getProperty("user.home") + "/avoir-realtime-0.1";
+    public static int APPLET = 0;
+    public static int WEBSTART = 1;
 
     public static String getRealtimeHome() {
         if (System.getProperty("os.name").toUpperCase().startsWith("WINDOWS")) {
@@ -21,5 +22,4 @@ public class Constants {
             return REALTIME_HOME;
         }
     }
-
 }
