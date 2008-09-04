5c87e1c1b2684dfd4701a9b4bff869a179adf4ff
paulscott <unknown@unknown.com>
2008-09-04 05:24:26 +0000

    "im module to handle jabber/gtalk requests"

---
+++ im/controller.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * IM controller class
+ * 
+ * Class to control the IM module
+ * 
+ * PHP version 5
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @category  chisimba
+ * @package   im
+ * @author    Paul Scott <pscott@uwc.ac.za>
+ * @copyright 2008 Paul Scott
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   CVS: $Id: $
+ * @link      http://avoir.uwc.ac.za
+ * @see       xmpphp
+ */
+
+class im extends controller 
+{
+	/**
+    *
+    * Standard constructor method to retrieve the action from the
+    * querystring, and instantiate the user and lanaguage objects
+    *
+    */
+  public  function init()
+    {
+    	try {
+        	$this->objUser =  $this->getObject("user", "security");
+        	//Create an instance of the language object
+        	$this->objLanguage = $this->getObject("language", "language");
+    	}
+    	catch (customException $e)
+    	{
+    		customException::cleanUp();
+    		exit;
+    	}
+    }
+
+    /**
+    * Standard dispatch method to handle adding and saving
+    * of comments
+    *
+    * @access public
+    * @param void
+    * @return void
+    */
+ 	public  function dispatch()
+    {
+        $action = $this->getParam('action');
+    	switch ($action) {
+            case null:
+            	echo "booyakasha!";
+            	break;
+            			
+            default:
+            	die("unknown action");
+            	break;
+        }
+    }
+	
+}
