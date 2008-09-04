3b74c7e09d4d1ed35b830281a47bffc92fd89869
paulscott <unknown@unknown.com>
2008-09-04 09:06:17 +0000

    "basic table to hold messages"

---
+++ im/sql/tbl_im.sql
@@ -0,0 +1,30 @@
+<?php
+// Table Name
+$tablename = 'tbl_im';
+
+//Options line for comments, encoding and character set
+$options = array('comment' => 'table to hold IM messages from users', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
+
+// Fields
+$fields = array(
+	'id' => array(
+		'type' => 'text',
+		'length' => 32
+		),
+	'msgtype' => array(
+		'type' => 'text',
+		'length' => 255,
+		),
+	'msgfrom' => array(
+		'type' => 'text',
+		'length' => 255,
+		),
+	'msgbody' => array(
+		'type' => 'clob',
+		),
+	'when' => array(
+		'type' => 'timestamp',
+		),
+	);
+
+?>
