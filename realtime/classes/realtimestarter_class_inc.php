d0bb299536455074af56592de6dcb538e19cc0c9
David Wafula <davidwaf@gmail.com>
2008-09-12 21:07:40 +0000

    updated the generated jnlp

--- realtime/classes/realtimestarter_class_inc.php
+++ realtime/classes/realtimestarter_class_inc.php
@@ -1,7 +1,7 @@
 <?php
     class realtimestarter extends object{
-   
-        public  $objConfig;  
+
+        public  $objConfig;
         /**
          * Constructor
          */
@@ -12,7 +12,7 @@
             $this->objConfig = $this->getObject('altconfig', 'config');
 
         }
-   
+
         /**
          * Generates a URL to be used to replace the filter
          * @param <type> $id
@@ -23,7 +23,7 @@
             $url='<center>';
             $url.='<applet codebase="'.$appletCodeBase.'"';
             $url.='code="avoir.realtime.tcp.launcher.RealtimeLauncher" name ="Avoir Realtime Applet"';
-     
+
             $url.='archive="realtime-launcher-0.1.jar" width="100%" height="600">';
             $url.='<param name=userName value="'.$username.'">';
             $url.='<param name=isLocalhost value="false">';
@@ -41,66 +41,82 @@
             $url.='<param name=sessionId value="'.$id.'">';
             $url.='<param name=sessionTitle value="'.$agenda.'">';
             $url.='<param name=slideServerId value="'.$slideServerId.'">';
-    
+
             $url.='<param name=isSessionPresenter value="false">';
             $url.='</applet>';
-    
+
             $url.='</center>';
-            
+
             return $url;
         }
 
 
-public function generateJNLP(
-    $fileBase,
-    $appletCodeBase,
-    $supernodeHost,
-    $superNodePort,
-    $username,$fullnames,
-    $isPresenter,
-    $sessionId,
-    $userDetails,
-    $userImagePath){
+        public function generateJNLP(
+            $type,
+            $fileBase,
+            $appletCodeBase,
+            $supernodeHost,
+            $superNodePort,
+            $username,$fullnames,
+            $isPresenter,
+            $sessionId,
+            $sessionTitle,
+            $userDetails,
+            $userImagePath,
+            $isLoggedIn,
+            $siteRoot,
+            $resourcesPath,
+            $userLevel,
+            $chatLogPath,
+            $filePath,
+            $slideServerId){
 
 
-  
-    $jnlpFile = $fileBase.'/'.$username.'_chisimba_classroom.jnlp';
-    $fh = fopen($jnlpFile, 'w') or die("can't open file");
+            $jnlpFile = $fileBase.'/'.$type.'_'.$username.'_chisimba_classroom.jnlp';
+            $fh = fopen($jnlpFile, 'w') or die("can't open file");
 
-   fwrite($fh,'<?xml version="1.0" encoding="utf-8"?>');
-   fwrite($fh,'<jnlp spec="1.0+" ');
-   fwrite($fh,      'codebase="'.$appletCodeBase.'" ');
-   fwrite($fh,     ' href="'.$username.'_chisimba_classroom.jnlp">');
-   fwrite($fh,   '<information>');
-   fwrite($fh,    ' <title>Realtime Classroom</title>');
-   fwrite($fh,    ' <vendor>AVOIR</vendor>');
-   fwrite($fh,    ' <description>Realtime Classroom</description>');
-   fwrite($fh,   ' <homepage href="http://avoir.uwc.ac.za"/>');
-   fwrite($fh,    ' <description kind="short">Realtime Virtual Classroom</description>');
-   fwrite($fh,    ' <icon href="images/logo.png"/> ');
-   fwrite($fh,    ' <icon kind="splash" href="images/splash_realtime.png"/> ');
-   fwrite($fh,    ' <offline-allowed/>');
-   fwrite($fh,   '</information>');
-   fwrite($fh, '<resources>     ');
-   fwrite($fh,  	'<jar href="realtime-launcher-1.0.1.jar"/>   ');
-   fwrite($fh,	'<j2se version="1.5+"');
-   fwrite($fh,	 '     href="http://java.sun.com/products/autodl/j2se"/>');
-   fwrite($fh,   '</resources>');
-   fwrite($fh,  '<application-desc main-class="avoir.realtime.tcp.launcher.RealtimeLauncher">');
-   fwrite($fh,   ' <argument>'.$supernodeHost.'</argument>');
-   fwrite($fh,   ' <argument>'.$superNodePort.'</argument>');
-   fwrite($fh,   ' <argument>'.$username.'</argument>');
-   fwrite($fh,   ' <argument>'.$fullnames.'</argument>');
-   fwrite($fh,   ' <argument>'.$isPresenter.'</argument>');
-   fwrite($fh,   ' <argument>'.$sessionId.'</argument>');
-   fwrite($fh,   ' <argument>'.$userDetails.'</argument>');
-   fwrite($fh,   ' <argument>'.$userImagePath.'</argument>');
-    fwrite($fh,   '</application-desc>');
-   fwrite($fh,'<security>');
-   fwrite($fh,'  <all-permissions/>');
-   fwrite($fh,'</security> ');
-   fwrite($fh,'</jnlp>');
-   fclose($fh); 
+            fwrite($fh,'<?xml version="1.0" encoding="utf-8"?>');
+            fwrite($fh,'<jnlp spec="1.0+" ');
+            fwrite($fh,      'codebase="'.$appletCodeBase.'" ');
+            fwrite($fh,     ' href="'.$type.'_'.$username.'_chisimba_classroom.jnlp">');
+            fwrite($fh,   '<information>');
+            fwrite($fh,    ' <title>Realtime Classroom</title>');
+            fwrite($fh,    ' <vendor>AVOIR</vendor>');
+            fwrite($fh,    ' <description>Realtime Classroom</description>');
+            fwrite($fh,   ' <homepage href="http://avoir.uwc.ac.za"/>');
+            fwrite($fh,    ' <description kind="short">Realtime Virtual Classroom</description>');
+            fwrite($fh,    ' <icon href="images/logo.png"/> ');
+            fwrite($fh,    ' <icon kind="splash" href="images/splash_realtime.png"/> ');
+            fwrite($fh,    ' <offline-allowed/>');
+            fwrite($fh,   '</information>');
+            fwrite($fh, '<resources>     ');
+            fwrite($fh,  	'<jar href="realtime-launcher-1.0.1.jar"/>   ');
+            fwrite($fh,	'<j2se version="1.5+"');
+            fwrite($fh,	 '     href="http://java.sun.com/products/autodl/j2se"/>');
+            fwrite($fh,   '</resources>');
+            fwrite($fh,  '<application-desc main-class="avoir.realtime.tcp.launcher.RealtimeLauncher">');
+            fwrite($fh,   ' <argument>'.$supernodeHost.'</argument>');
+            fwrite($fh,   ' <argument>'.$superNodePort.'</argument>');
+            fwrite($fh,   ' <argument>'.$username.'</argument>');
+            fwrite($fh,   ' <argument>'.$fullnames.'</argument>');
+            fwrite($fh,   ' <argument>'.$isPresenter.'</argument>');
+            fwrite($fh,   ' <argument>'.$sessionId.'</argument>');
+            fwrite($fh,   ' <argument>'.$sessionTitle.'</argument>');
+            fwrite($fh,   ' <argument>'.$userDetails.'</argument>');
+            fwrite($fh,   ' <argument>'.$userImagePath.'</argument>');
+            fwrite($fh,   ' <argument>'.$isLoggedIn.'</argument>');
+            fwrite($fh,   ' <argument>'.$siteRoot.'</argument>');
+            fwrite($fh,   ' <argument>'.$resourcesPath.'</argument>');
+            fwrite($fh,   ' <argument>'.$userLevel.'</argument>');
+            fwrite($fh,   ' <argument>'.$chatLogPath.'</argument>');
+            fwrite($fh,   ' <argument>'.$filePath.'</argument>');
+            fwrite($fh,   ' <argument>'.$slideServerId.'</argument>');
+            fwrite($fh,   '</application-desc>');
+            fwrite($fh,'<security>');
+            fwrite($fh,'  <all-permissions/>');
+            fwrite($fh,'</security> ');
+            fwrite($fh,'</jnlp>');
+            fclose($fh);
+        }
     }
-}
 ?>
