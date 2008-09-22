06f022173687c1f5133ffc22a089103edd2554f5
David Wafula <davidwaf@gmail.com>
2008-09-22 14:10:36 +0000

    initial code

---
+++ resources/java/chisimba_realtime_base/src/avoir/realtime/tcp/common/MessageCode.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avoir.realtime.tcp.common;
+
+/**
+ *
+ * @author developer
+ */
+public class MessageCode {
+
+    public static final int CLASSROOM_SPECIFIC = 1;
+    public static final int WEBPRESENT_SPECIFIC = 2;
+    public static final int ALL = 3;
+}
