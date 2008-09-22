353fa90f2c67e8ca5739c1cb92ef71528743c74e
David Wafula <davidwaf@gmail.com>
2008-09-22 14:16:09 +0000

    implemented new interface methds

--- realtime/resources/java/chisimba_realtime_pluginmanager/src/avoir/realtime/tcp/pluginmanager/PluginManager.java
+++ realtime/resources/java/chisimba_realtime_pluginmanager/src/avoir/realtime/tcp/pluginmanager/PluginManager.java
@@ -1,5 +1,19 @@
 /*
- * This is used as a bridge to use the plugin interface to load external jar files
+ *  Copyright (C) GNU/GPL AVOIR 2008
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  * 
  */
 package avoir.realtime.tcp.pluginmanager;
@@ -8,11 +22,13 @@ import avoir.realtime.tcp.launcher.RealtimePlugin;
 import javax.swing.JApplet;
 import javax.swing.JPanel;
 import avoir.realtime.tcp.base.RealtimeBase;
+import java.applet.AppletContext;
+import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 
 /**
- *
- * @author developer
+ * Interface that allows to use reflection to load up real applets
+ * @author David Wafula
  */
 public class PluginManager implements RealtimePlugin {
 
@@ -30,16 +46,25 @@ public class PluginManager implements RealtimePlugin {
             boolean isSlidesHost,
             String siteRoot,
             String slideServerId,
-            String resourcesPath) {
+            String resourcesPath,
+            AppletContext appletContext) {
 
 
         return base.init(userLevel, fullname, userName, host,
                 port, isPresenter, sessionId, slidesDir, isSlidesHost,
-                siteRoot, slideServerId, resourcesPath, false);
+                siteRoot, slideServerId, resourcesPath, false, appletContext);
     }
 
     public void setSupernodeHost(String host) {
         base.setSupernodeHost(host);
+    }
+
+    public void setUserDetails(String userDetails) {
+        base.setUserDetails(userDetails);
+    }
+
+    public void setUserImagePath(String userImagePath) {
+        base.setUserImagePath(userImagePath);
     }
 
     public void setSupernodePort(int port) {
@@ -65,9 +90,20 @@ public class PluginManager implements RealtimePlugin {
         return new JPanel();
     }
 
-    public JPanel createClassroomBase(String host,int port,int mode) {
-        base.setMODE(mode);
-        return base.initAsClassroom(host,port);
+    public JPanel createClassroomBase(String host, int port, int mode, String username,
+            String fullnames,
+            boolean isPresenter,
+            String sessionId, String userLevel,
+            String slidesDir,
+            String siteRoot,
+            String slidesServerId,
+            String resourcesPath, JFrame parent) {
+        //base.setMODE(mode);
+        return base.initAsClassroom(host, port, username, fullnames, isPresenter, sessionId, userLevel,
+                slidesDir,
+                siteRoot,
+                slidesServerId,
+                resourcesPath, parent);
     }
 
     public void removeUser(String arg0, String arg1) {
