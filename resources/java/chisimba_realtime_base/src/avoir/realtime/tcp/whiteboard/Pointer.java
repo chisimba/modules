4ad860b55d7be0558f116a9b6162656e56cdb941
David Wafula <davidwaf@gmail.com>
2008-09-22 14:12:18 +0000

    wb new classes

---
+++ resources/java/chisimba_realtime_base/src/avoir/realtime/tcp/whiteboard/Pointer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package avoir.realtime.tcp.whiteboard;
+
+import java.awt.Point;
+import javax.swing.ImageIcon;
+
+
+ public   class Pointer {
+
+        Point point;
+        ImageIcon icon;
+
+        public Pointer(Point point, ImageIcon icon) {
+            this.point = point;
+            this.icon = icon;
+        }
+
+        public ImageIcon getIcon() {
+            return icon;
+        }
+
+        public void setIcon(ImageIcon icon) {
+            this.icon = icon;
+        }
+
+        public Point getPoint() {
+            return point;
+        }
+
+        public void setPoint(Point point) {
+            this.point = point;
+        }
+    }
