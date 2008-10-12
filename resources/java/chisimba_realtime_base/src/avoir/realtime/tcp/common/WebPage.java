8859775a488bd2b42187ecc560fa0feeb5ff1bbf
David Wafula <davidwaf@gmail.com>
2008-10-12 16:26:10 +0000

    webpage

---
+++ resources/java/chisimba_realtime_base/src/avoir/realtime/tcp/common/WebPage.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avoir.realtime.tcp.common;
+
+/**
+ *
+ * @author developer
+ */
+public class WebPage {
+
+    private String id;
+    private String url;
+    private int index;
+
+    public WebPage(String id, String url, int index) {
+        this.id = id;
+        this.url = url;
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
