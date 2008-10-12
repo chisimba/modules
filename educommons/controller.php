dfd91e28866ea5da6d331351ceffe26755a0dbfe
charlvn <unknown@unknown.com>
2008-10-12 21:43:39 +0000

    Added educommons module - very much work in progress

---
+++ educommons/controller.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * eduCommons controller class
+ * 
+ * Class to control the eduCommons module
+ * 
+ * PHP version 5
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @category  chisimba
+ * @package   educommons
+ * @author    Charl van Niekerk <charlvn@charlvn.za.net>
+ * @copyright 2008 Charl van Niekerk
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id$
+ * @link      http://avoir.uwc.ac.za
+ */
+
+class educommons extends controller
+{
+	protected $objSpie;
+
+	/**
+	 * Standard constructor to load the necessary resources
+	 * and populate the new object's instance variables
+	 * @access public
+	 */
+	public function init()
+	{
+		$this->objSpie = $this->getObject('spie', 'feed');
+	}
+
+	/**
+	 * Standard dispatch method to handle the various possible actions
+	 * @access public
+	 */
+	public function dispatch()
+	{
+		$action = $this->getParam('action');
+		switch ($action) {
+			default:
+				$objContextContentChapters = $this->getObject('db_contextcontent_chapters', 'contextcontent');
+				$objContextContentChapters->addChapter('', 'Test', 'Hello');
+				break;
+				$this->objSpie->startPie('http://free.uwc.ac.za/freecourseware/biodiversity-conservation-biology/conservation-biology/RSS');
+				$items = $this->objSpie->get_items();
+				foreach ($items as $item) {
+					echo $item->get_title();
+					echo $item->get_link();
+					echo $item->get_content();
+				}
+		}
+	}
+}
+
+?>
