5e694fb018e9209146740643a6d41da82be8e295
David Wafula <davidwaf@gmail.com>
2008-10-12 16:26:45 +0000

    for closing tabs

---
+++ realtime/resources/java/chisimba_realtime_base/src/avoir/realtime/tcp/common/packet/RemoveDocumentPacket.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avoir.realtime.tcp.common.packet;
+
+/**
+ *
+ * @author developer
+ */
+public class RemoveDocumentPacket implements RealtimePacket {
+
+    private String sessionId;
+    private String id;
+    private int type;
+
+    public RemoveDocumentPacket(String sessionId, String id, int type) {
+        this.sessionId = sessionId;
+        this.id = id;
+        this.type = type;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
