51db8e171eb517524e91d5a913449bb54ed6b1c3
David Wafula <davidwaf@gmail.com>
2008-10-15 06:56:25 +0000

    stop sharing screen app

---
+++ realtime/resources/java/chisimba_realtime_base/src/avoir/realtime/tcp/common/packet/StopScreenSharing.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avoir.realtime.tcp.common.packet;
+
+/**
+ *
+ * @author developer
+ */
+public class StopScreenSharing implements RealtimePacket {
+
+    public String getId() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String getSessionId() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setId(String id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
