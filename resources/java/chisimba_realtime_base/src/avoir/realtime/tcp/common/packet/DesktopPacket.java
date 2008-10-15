22dd9d524805de312bffc764c6cf172113783f0d
David Wafula <davidwaf@gmail.com>
2008-10-15 06:54:59 +0000

    for app sharing

---
+++ resources/java/chisimba_realtime_base/src/avoir/realtime/tcp/common/packet/DesktopPacket.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avoir.realtime.tcp.common.packet;
+
+import avoir.realtime.tcp.base.appsharing.ScreenScrapeData;
+
+/**
+ *
+ * @author developer
+ */
+public class DesktopPacket implements RealtimePacket {
+
+    private ScreenScrapeData data;
+    private String sessionId;
+
+    public DesktopPacket(ScreenScrapeData data, String sessionId) {
+        this.data = data;
+        this.sessionId = sessionId;
+    }
+
+    public String getId() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setId(String id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public ScreenScrapeData getData() {
+        return data;
+    }
+
+    public void setData(ScreenScrapeData data) {
+        this.data = data;
+    }
+}
