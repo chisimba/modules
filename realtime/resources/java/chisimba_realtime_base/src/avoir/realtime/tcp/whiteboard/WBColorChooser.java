e0e33fb72fbbb36da81afd05f0a274911e4075c8
David Wafula <davidwaf@gmail.com>
2008-10-15 07:12:06 +0000

    color chooser

---
+++ realtime/resources/java/chisimba_realtime_base/src/avoir/realtime/tcp/whiteboard/WBColorChooser.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avoir.realtime.tcp.whiteboard;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class WBColorChooser extends JPanel
+        implements ChangeListener {
+
+    protected JColorChooser tcc;
+    private static JFrame frame = new JFrame("Color Chooser");
+    private WhiteboardSurface wb;
+
+    public WBColorChooser(final WhiteboardSurface wb) {
+        super(new BorderLayout());
+        this.wb = wb;
+
+        //Set up color chooser for setting text color
+        tcc = new JColorChooser(wb.getColour());
+        tcc.getSelectionModel().addChangeListener(this);
+        tcc.setBorder(BorderFactory.createTitledBorder(
+                "Choose Text Color"));
+
+        JPanel cPanel = new JPanel();
+        JButton okButton = new JButton("OK");
+        okButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+                wb.setColour(tcc.getColor());
+                wb.setTextColor(tcc.getColor());
+                frame.dispose();
+            }
+        });
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+                frame.dispose();
+            }
+        });
+        cPanel.add(okButton);
+        cPanel.add(cancelButton);
+
+        add(tcc, BorderLayout.CENTER);
+        add(cPanel, BorderLayout.SOUTH);
+    }
+
+    public void stateChanged(ChangeEvent e) {
+        Color newColor = tcc.getColor();
+        wb.setTextColor(tcc.getColor());
+        wb.setColour(newColor);
+    }
+
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    public static void createAndShowGUI(WhiteboardSurface wb) {
+        //Create and set up the window.
+
+        //Create and set up the content pane.
+        JComponent newContentPane = new WBColorChooser(wb);
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
