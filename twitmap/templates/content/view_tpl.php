12f5247644268b6dd815a664c072caf5c6962a6b
paulscott <unknown@unknown.com>
2008-10-22 12:11:25 +0000

    changed template to echo raw KML

--- twitmap/templates/content/view_tpl.php
+++ twitmap/templates/content/view_tpl.php
@@ -1,19 +1,19 @@
 <?php
-$cssLayout = &$this->newObject('csslayout', 'htmlelements');
-$objSideBar = $this->newObject('usermenu', 'toolbar');
+//$cssLayout = &$this->newObject('csslayout', 'htmlelements');
+//$objSideBar = $this->newObject('usermenu', 'toolbar');
 
 // Set columns to 3
-$cssLayout->setNumColumns(2);
-$leftMenu = NULL;
-$leftCol = NULL;
-$middleColumn = NULL;
+//$cssLayout->setNumColumns(2);
+//$leftMenu = NULL;
+//$leftCol = NULL;
+//$middleColumn = NULL;
 
-$middleColumn .= $kml;
+//$middleColumn .= $kml;
+echo $kml;
+//$leftCol .= $objSideBar->show();
 
-$leftCol .= $objSideBar->show();
-
-$cssLayout->setMiddleColumnContent($middleColumn);
-$cssLayout->setLeftColumnContent($leftCol); //$leftMenu->show());
+//$cssLayout->setMiddleColumnContent($middleColumn);
+//$cssLayout->setLeftColumnContent($leftCol); //$leftMenu->show());
 // $cssLayout->setRightColumnContent($rightSideColumn);
-echo $cssLayout->show();
+//echo $cssLayout->show();
 ?>
