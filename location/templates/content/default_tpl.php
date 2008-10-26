11d18a1173955a2aa98b9a9c55adb215931b8f64
charlvn <unknown@unknown.com>
2008-10-26 13:48:11 +0000

    Location Module: Added language items

--- location/templates/content/default_tpl.php
+++ location/templates/content/default_tpl.php
@@ -4,6 +4,9 @@
 $this->loadClass('htmlheading', 'htmlelements');
 $this->loadClass('href', 'htmlelements');
 
+// Load the language class
+$objLanguage = $this->getObject('language', 'language');
+
 // Set up the CSS layout
 $cssLayout = $this->newObject('csslayout', 'htmlelements');
 $cssLayout->setNumColumns(2);
@@ -15,26 +18,46 @@ $cssLayout->setLeftColumnContent($leftColumn);
 
 // Set up the heading
 $header = new htmlHeading();
-$header->str = 'Current Location';//$this->objLanguage->languageText('mod_im_recentmessages', 'im');
+$header->str = $objLanguage->languageText('mod_location_currentlocation', 'location');
 $header->type = 1;
 $middleColumn = $header->show();
 
+// Get user location language strings
+$nameLabel = $objLanguage->languageText('mod_location_locationname', 'location');
+$longitudeLabel = $objLanguage->languageText('mod_location_longitude', 'location');
+$latitudeLabel = $objLanguage->languageText('mod_location_latitude', 'location');
+
+// Get user location variable values
+$nameValue = $this->getVar('locationName');
+$longitudeValue = $this->getVar('locationLongitude');
+$latitudeValue = $this->getVar('locationLatitude');
+
+// Add location information to middle column
 $middleColumn .= '<ul>';
-$middleColumn .= '<li>' . $this->getVar('locationName') . '</li>';
-$middleColumn .= '<li>' . $this->getVar('locationLongitude') . '</li>';
-$middleColumn .= '<li>' . $this->getVar('locationLatitude') . '</li>';
+$middleColumn .= "<li>$nameLabel: $nameValue</li>";
+$middleColumn .= "<li>$longitudeLabel: $longitudeValue</li>";
+$middleColumn .= "<li>$latitudeLabel: $latitudeValue</li>";
 $middleColumn .= '</ul>';
 
+// Add twitter information and controls to middle column
 if ($this->getVar('locationTwitter')) {
-    $uri = $this->uri(array('module'=>'location', 'action'=>'disabletwitter'));
-    $href = new href($uri, 'Disable');
-    $middleColumn .= '<p>Twitter is currently enabled. '.$href->show().'</p>';
+    $linkUri = $this->uri(array('module'=>'location', 'action'=>'disabletwitter'));
+    $linkText = $objLanguage->languageText('mod_location_disable', 'location');
+    $linkHref = new href($linkUri, $linkText);
+    $link = $linkHref->show();
+    $text = $objLanguage->languageText('mod_location_twitterenabled', 'location');
+    $middleColumn .= "<p>$text $link</p>";
 } else {
-    $uri = $this->uri(array('module'=>'location', 'action'=>'enabletwitter'));
-    $href = new href($uri, 'Enable');
-    $middleColumn .= '<p>Twitter is currently disabled. '.$href->show().'</p>';
+    $linkUri = $this->uri(array('module'=>'location', 'action'=>'enabletwitter'));
+    $linkText = $objLanguage->languageText('mod_location_enable', 'location');
+    $linkHref = new href($linkUri, $linkText);
+    $link = $linkHref->show();
+    $text = $objLanguage->languageText('mod_location_twitterdisabled', 'location');
+    $middleColumn .= "<p>$text $link</p>";
 }
 
+// Finally add the middle column to the CSS layout
 $cssLayout->setMiddleColumnContent($middleColumn);
 
+// And output the whole lot!
 echo $cssLayout->show();
