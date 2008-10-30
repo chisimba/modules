0c18fd2d92b6b28b2840fbfb769dece5cd25a834
wnitsckie <unknown@unknown.com>
2008-10-30 11:58:32 +0000

    added userid

--- im/templates/content/counsilors_tpl.php
+++ im/templates/content/counsilors_tpl.php
@@ -8,10 +8,10 @@ foreach ($users as $user)
 	$name = $user['firstname']." ".$user['surname'];
 	if($objDBIMUser->isCounsilor($user['id']))
 	{
-		$objLink->href = $this->uri(array("action" => "removecounsilor", "userid" => $user['id']));
+		$objLink->href = $this->uri(array("action" => "removecounsilor", "userid" => $user['userid']));
 		$objLink->link = "remove";
 	}else{
-		$objLink->href = $this->uri(array("action" => "addcounsilor", "userid" => $user['id']));
+		$objLink->href = $this->uri(array("action" => "addcounsilor", "userid" => $user['userid']));
 		$objLink->link = "add";
 	}
 	
