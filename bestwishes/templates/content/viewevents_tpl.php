92e323ff005d6522932eceeaa185b35af611da06
matnatalis <unknown@unknown.com>
2008-10-31 06:49:47 +0000

    This is the best wishes module

---
+++ bestwishes/templates/content/viewevents_tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+*@ Author Emmanuel Natalis
+*@ University of dar es salaam
+*@ 2008
+*/
+$this->objDbbestwishes=$this->getObject('dbbestwishes','bestwishes');
+echo $this->objDbbestwishes->viewUsersEvents();
+?>
