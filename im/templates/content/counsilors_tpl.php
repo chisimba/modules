9a9c66ddbeeee044d63a643a775f160740acfd67
charlvn <unknown@unknown.com>
2008-11-02 12:07:38 +0000

    IM Module: Converted tabs to spaces.

--- im/templates/content/counsilors_tpl.php
+++ im/templates/content/counsilors_tpl.php
@@ -4,18 +4,18 @@ $objDBIMUser = $this->getObject('dbimusers', 'im');
 $objLink = $this->getObject('link','htmlelements');
 foreach ($users as $user)
 {
-	
-	$name = $user['firstname']." ".$user['surname'];
-	if($objDBIMUser->isCounsilor($user['userid']))
-	{
-		$objLink->href = $this->uri(array("action" => "removecounsilor", "userid" => $user['userid']));
-		$objLink->link = "remove";
-	}else{
-		$objLink->href = $this->uri(array("action" => "addcounsilor", "userid" => $user['userid']));
-		$objLink->link = "add";
-	}
-	
-	echo $name."   ".$objLink->show().'<br/>';
+    
+    $name = $user['firstname']." ".$user['surname'];
+    if($objDBIMUser->isCounsilor($user['userid']))
+    {
+        $objLink->href = $this->uri(array("action" => "removecounsilor", "userid" => $user['userid']));
+        $objLink->link = "remove";
+    }else{
+        $objLink->href = $this->uri(array("action" => "addcounsilor", "userid" => $user['userid']));
+        $objLink->link = "add";
+    }
+    
+    echo $name."   ".$objLink->show().'<br/>';
 
 }
 
