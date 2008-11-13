8646542eb1fe04d83fe0e79346b998aa86c4e2e5
nic <unknown@unknown.com>
2008-11-13 10:03:29 +0000

    implement cab loader for stats module

--- stats/controller.php
+++ stats/controller.php
@@ -1,55 +1,125 @@
 <?php
-/* -------------------- stats class extends controller ----------------*/
-// security check - must be included in all scripts
-if (!$GLOBALS['kewl_entry_point_run'])
-{
+/**
+ * Stats tests and tutorials Chisimba
+ * 
+ * Statistics module to load authorware tests and tutorials within Chisimba framework
+ * 
+ * PHP version 5
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @category  Chisimba
+ * @package   stats
+ * @author    Nic Appleby <nappleby@uwc.ac.za>
+ * @copyright 2008 AVOIR
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id$
+ * @link      http://avoir.uwc.ac.za
+ */
+
+if (!
+/**
+ * Description for $GLOBALS
+ * @global entry point $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+$GLOBALS['kewl_entry_point_run']) {
     die("You cannot view this page directly");
 }
-// end security check
                                                                                                                                              
 /**
-* Controller class for the stats module
-* @copyright 2004 KEWL.NextGen
-* @author James Scoble
-*
-* $Id: controller.php
-*/
+ * stats class
+ * 
+ * stats module controller for Chisimba
+ * 
+ * @category  Chisimba
+ * @package   stats
+ * @author    Nic Appleby <nappleby@uwc.ac.za>
+ * @copyright 2008 AVOIR
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id$
+ * @link      http://avoir.uwc.ac.za
+ */
 
-class stats extends controller
-{
-
-    public function init()
-    {
-        $this->objConfig= $this->getObject('altconfig', 'config');
-        $this->objUser= $this->getObject('user', 'security');
-        $this->userId=$this->objUser->userId();
-        $this->objLanguage=  $this->getObject('language','language');
-    }
+class stats extends controller {
+    
     /**
-    * This is the main method in the class
-    * It calls other functions depending on the value of $action
-    * @param string $action
+     * User object 
+     * @var object holds user details
+     * @access public
+     */
+    public $objUser;
+    
+    /**
+     * Language object
+     * @var object used to fetch language items
+     * @access public
+     */
+    public $objLanguage;
+    
+    /**
+     * Logger object
+     * @var object used to log module usage
+     * @access public
+     */
+    public $objLogger;
+
+    /**
+    * Init method
+    * 
+    * Standard Chisimba Init() method
+    * 
+    * @return void  
+    * @access public
     */
-    public function dispatch($action=Null)
-    {
-        switch ($action)
-        {
+    public function init() {
+        $this->objUser = $this->getObject('user', 'security');
+        $this->objLanguage = $this->getObject('language','language');
+        
+        //Log this module call
+        $this->objLog = $this->newObject('logactivity', 'logger');
+        $this->objLog->log();
+        
+        $this->setLayoutTemplate('stats_layout_tpl.php');
+    }
+    
+    /**
+    * Main dispatch function controlls execution based on
+    * the action parameter from the get or post variable
+    * 
+    * @param string $action the action to take
+    * @return string the name of the template to display
+    * @access public
+    */
+    public function dispatch($action) {
+        switch ($action) {
 
         default:
-            return ('default_tpl.php');
+            return "default_tpl.php";
         }
     }
 
 
     /** 
     * Method to determine if the user has to be logged in or not
+    * in order to access the module
+    *
+    * @return void
+    * @access public
     */
-    public function requiresLogin() // overides that in parent class
-    {
-        $action=$this->getParam('action','NULL');
-        if ($action===NULL){
-            return FALSE;
-        }
+    public function requiresLogin() {
+        
         return TRUE;
     }
     
--- stats/templates/content/default_tpl.php
+++ stats/templates/content/default_tpl.php
@@ -1,5 +1,46 @@
-<?
-$display=$this->newObject('statsdisplay');
-print $display->defaultMessage();
-?>
+<?php
+/**
+ * Stats tests and tutorials Chisimba
+ * 
+ * Deafult view template for stats module
+ * 
+ * PHP version 5
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @category  Chisimba
+ * @package   stats
+ * @author    Nic Appleby <nappleby@uwc.ac.za>
+ * @copyright 2008 AVOIR
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id$
+ * @link      http://avoir.uwc.ac.za
+ */
 
+$note = "Before you can start with tutorial 1,  you must download the Web Player.
+    It is only necessary to do this once.";
+
+$objPop = $this->getObject('windowpop','htmlelements');
+$objPop->set('location',$this->getResourceURI("cabload.html"));
+$objPop->set('linktext','Click here');
+$objPop->set('width','550');
+$objPop->set('height','350');
+$objPop->putJs();
+
+$install = $objPop->show()." to install the player. Please note this will only work with Internet Explorer.";
+
+echo "$note<br />$install";
+$login = $this->getResourceURI('login.aam');
+echo "<br /><br /><iframe width='100%' height='100%' scrolling='no' frameborder='0' src='$login'></iframe>";
+?>
