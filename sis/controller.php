31fb30b80e818e9b61656a9c0faf39418ec03e2d
James Scoble <jscoble@uwc.ac.za>
2008-11-26 14:21:09 +0000

    Very first elements of this module. Not a working module yet.

---
+++ sis/controller.php
@@ -0,0 +1,91 @@
+<?php
+/* -------------------- sis class extends controller ----------------*/
+                                                                                                                                             
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run'])
+{
+    die("You cannot view this page directly");
+}
+// end security check
+
+
+class sis extends controller
+{
+    public $objConfig;
+    public $objLanguage;
+    public $objSISforms;
+    
+    /**
+    * ye olde standard init()
+    */
+    public function init()
+    {
+        $this->objConfig = $this->getObject('altconfig','config');
+        $this->objLanguage = $this->getObject('language','language');
+        $this->objSISforms = $this->getObject('studentforms','sis');
+    }
+
+    /**
+    * 
+    *
+    */
+    public function dispatch($action) 
+    {
+        switch ($action)
+        {
+            case "myprofile":
+                return $this->myProfile();
+            case "saveprofile":
+                return $this->saveProfile();
+            case 'newstudent':
+                return $this->showStudent();
+            case 'savestudent':
+                return $this->saveStudent();
+            default:
+                return $this->showDefault();
+        }
+    }
+    
+
+    /**
+    * Default action
+    * @returns string $template
+    */
+    public function showDefault()
+    { 
+       return "default_tpl.php";
+    }
+
+    /**
+    
+    /**
+    * Calls classes to get form info
+    * @returns string $template
+    */
+    public function showStudent()
+    { 
+       $this->string=$this->objSISforms->studentInput();
+       return "default_tpl.php";
+    }
+
+    /**
+    * Calls classes to save form info
+    * @returns string $template
+    */
+    public function saveStudent()
+    {
+       $paramNames=array();
+       $paramvals=array();
+       foreach ($paramNames as $line)
+       {
+          $paramvals[$line]=$this->getParam($line);
+       }
+    
+       return "default_tpl.php";
+    }
+    
+
+
+}
+
+?>
---
+++ sis/templates/content/default_tpl.php
@@ -0,0 +1,7 @@
+<?
+
+if (isset($this->string)){
+    print $this->string;
+} else {
+   print "default template for new Student Information Systems module. Under construction.";
+}
