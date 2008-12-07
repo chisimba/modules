48f98ab6ba09c2019cf0d67fdc163c5163970df6
charlvn <unknown@unknown.com>
2008-12-07 20:09:29 +0000

    eduCommons Module: Added labels to form.

--- educommons/templates/content/upload_tpl.php
+++ educommons/templates/content/upload_tpl.php
@@ -7,12 +7,18 @@ $header->type = 1;
 $header->str = 'eduCommons IMS Package Import';
 echo $header->show();
 
-$objUpload = $this->newObject('uploadinput', 'filemanager');
+$objFileLabel = $this->newObject('label', 'htmlelements');
+$objFileLabel->label('IMS Package', 'input_fileupload');
 
-$objDropdown = $this->newObject('dropdown', 'htmlelements');
-$objDropdown->dropdown('context');
+$objFileUpload = $this->newObject('uploadinput', 'filemanager');
+
+$objContextLabel = $this->newObject('label', 'htmlelements');
+$objContextLabel->label('Context', 'input_context');
+
+$objContextDropdown = $this->newObject('dropdown', 'htmlelements');
+$objContextDropdown->dropdown('context');
 foreach ($contexts as $context) {
-    $objDropdown->addOption($context['contextcode']);
+    $objContextDropdown->addOption($context['contextcode']);
 }
 
 $objButton = new button('upload', 'Upload');
@@ -22,8 +28,8 @@ $uri = $this->uri(array('action'=>'upload'));
 
 $objForm = new form('upload', $uri);
 $objForm->extra = ' ENCTYPE=\'multipart/form-data\'';
-$objForm->addToForm('<p>'.$objUpload->show().'</p>');
-$objForm->addToForm('<p>'.$objDropdown->show().'</p>');
+$objForm->addToForm('<p>'.$objFileLabel->show().': '.$objFileUpload->show().'</p>');
+$objForm->addToForm('<p>'.$objContextLabel->show().': '.$objContextDropdown->show().'</p>');
 $objForm->addToForm('<p>'.$objButton->show().'</p>');
 
 echo $objForm->show();
