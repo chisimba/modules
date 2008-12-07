9fcf1507c0cbecaefcf295d3551ca515f2f554a5
charlvn <unknown@unknown.com>
2008-12-07 21:22:02 +0000

    eduCommons Module: Further improvements to the template.

--- educommons/templates/content/upload_tpl.php
+++ educommons/templates/content/upload_tpl.php
@@ -18,6 +18,9 @@ $objFileLabel->label($fileText, 'input_fileupload');
 // Create the file upload field.
 $objFileUpload = $this->newObject('uploadinput', 'filemanager');
 
+// Generate the file upload markup to go into the form.
+$fileMarkup = '<p>' . $objFileLabel->show() . ': ' . $objFileUpload->show() . '</p>';
+
 // Create the label for the context dropdown box.
 $objContextLabel = $this->newObject('label', 'htmlelements');
 $objContextLabel->label($contextText, 'input_context');
@@ -30,9 +33,15 @@ foreach ($contexts as $context) {
     $objContextDropdown->addOption($context['contextcode']);
 }
 
+// Generate the context dropdown markup to go into the form.
+$contextMarkup = '<p>' . $objContextLabel->show() . ': ' . $objContextDropdown->show() . '</p>';
+
 // Create the upload submit button.
-$objButton = new button('upload', $buttonText);
-$objButton->setToSubmit();
+$objUploadButton = new button('upload', $buttonText);
+$objUploadButton->setToSubmit();
+
+// Generate the upload submit button markup to go into the form.
+$uploadMarkup = '<p>' . $objUploadButton->show() . '</p>';
 
 // Generate the form action URI.
 $uri = $this->uri(array('action'=>'upload'));
@@ -41,8 +50,8 @@ $uri = $this->uri(array('action'=>'upload'));
 $objForm = new form('upload', $uri);
 $objForm->extra = ' ENCTYPE=\'multipart/form-data\'';
 
-$objForm->addToForm('<p>'.$objFileLabel->show().': '.$objFileUpload->show().'</p>');
-$objForm->addToForm('<p>'.$objContextLabel->show().': '.$objContextDropdown->show().'</p>');
-$objForm->addToForm('<p>'.$objButton->show().'</p>');
+$objForm->addToForm($fileMarkup);
+$objForm->addToForm($contextMarkup);
+$objForm->addToForm($uploadMarkup);
 
 echo $objForm->show();
