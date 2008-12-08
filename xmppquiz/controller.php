38dba44daf24eac6351e758a63b8278acca28e2b
paulscott <unknown@unknown.com>
2008-12-08 09:19:45 +0000

    xmpp quiz module for cs4fn project. This is still just a module stub

---
+++ xmppquiz/controller.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * XMPP Quiz controller class
+ *
+ * Controller class for the XMPP Quiz module
+ *
+ * PHP version 5
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * @category  Chisimba
+ * @package   xmppquiz
+ * @author    Paul Scott <pscott@uwc.ac.za>
+ * @copyright 2008 Paul Scott
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id: $
+ * @link      http://avoir.uwc.ac.za
+ */
+// security check - must be included in all scripts
+if (! /**
+ * Description for $GLOBALS
+ * @global entry point $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+$GLOBALS ['kewl_entry_point_run']) {
+    die ( "You cannot view this page directly" );
+}
+// end security check
+
+
+/**
+ * XMPP Quiz controller class
+ *
+ * XMPP Quiz controller class
+ *
+ * @category  Chisimba
+ * @package   xmppquiz
+ * @author    Paul Scott <pscott@uwc.ac.za>
+ * @copyright 2008 Paul Scott
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   Release: @package_version@
+ * @link      http://avoir.uwc.ac.za
+ */
+class xmppquiz extends controller {
+
+}
+?>
