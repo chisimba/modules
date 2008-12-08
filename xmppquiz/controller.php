eaa646e71ac28de3821f033c7ccb18dc09689624
paulscott <unknown@unknown.com>
2008-12-08 10:34:05 +0000

    added in default stuff and generic xmpp connection stuff

--- xmppquiz/controller.php
+++ xmppquiz/controller.php
@@ -65,6 +65,17 @@ class xmppquiz extends controller {
         $this->objConfig = $this->getObject('altconfig', 'config');
         $this->objSysConfig = $this->getObject('dbsysconfig', 'sysconfig');
         $this->objImOps = $this->getObject('imops', 'im');
+
+        // Get the sysconfig variables for the Jabber user to set up the connection.
+        $this->objSysConfig = $this->getObject ( 'dbsysconfig', 'sysconfig' );
+        $this->jserver = $this->objSysConfig->getValue ( 'jabberserver', 'im' );
+        $this->jport = $this->objSysConfig->getValue ( 'jabberport', 'im' );
+        $this->juser = $this->objSysConfig->getValue ( 'jabberuser', 'im' );
+        $this->jpass = $this->objSysConfig->getValue ( 'jabberpass', 'im' );
+        $this->jclient = $this->objSysConfig->getValue ( 'jabberclient', 'im' );
+        $this->jdomain = $this->objSysConfig->getValue ( 'jabberdomain', 'im' );
+        $this->timeLimit = $this->objSysConfig->getValue ( 'imtimelimit', 'im' );
+        $this->conn = new XMPPHP_XMPP ( $this->jserver, intval ( $this->jport ), $this->juser, $this->jpass, $this->jclient, $this->jdomain, $printlog = FALSE, $loglevel = XMPPHP_Log::LEVEL_ERROR );
     }
 
     public function dispatch($action = NULL) {
@@ -75,7 +86,13 @@ class xmppquiz extends controller {
 
             default :
                 // wha!
-                $this->objImOps->sendMessage ( 'pscott209@gmail.com', 'Hope this works!' );
+                //$this->objImOps->sendMessage ( 'pscott209@gmail.com', 'Hope this works!' );
+                $conn2 = new XMPPHP_XMPP ( $this->jserver, intval ( $this->jport ), $this->juser, $this->jpass, $this->jclient, $this->jdomain, $printlog = FALSE, $loglevel = XMPPHP_Log::LEVEL_ERROR );
+                $conn2->connect ();
+
+                $conn2->processUntil ( 'session_start' );
+                $conn2->message ( 'pscott209@gmail.com', "yo, from the xmpp quiz..." );
+                $conn2->disconnect ();
                 echo "hello?"; die();
                 break;
         }
