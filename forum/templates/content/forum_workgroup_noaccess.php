2866b60f6822cbc6f5b21e489b20ad1eb1af34cb
charlvn <unknown@unknown.com>
2008-12-14 22:01:14 +0000

    Forum Module: Fixed missing language items.

--- forum/templates/content/forum_workgroup_noaccess.php
+++ forum/templates/content/forum_workgroup_noaccess.php
@@ -5,22 +5,22 @@ $this->loadClass('link', 'htmlelements');
 
 $header = new htmlheading();
 $header->type=1;
-$header->str=$this->objLanguage->languageText('mod_forum_accessdeniedworkgroup');
+$header->str=$this->objLanguage->languageText('mod_forum_accessdeniedworkgroup', 'forum');
 echo $header->show();
 
 echo '<p>';
-echo $this->objLanguage->languageText('mod_forum_accessdeniedworkgroupmessage');
+echo $this->objLanguage->languageText('mod_forum_accessdeniedworkgroupmessage', 'forum');
 echo '</p>';
 echo '<p>';
 $prevPage = new link ('javascript: history.go(-1)');
-$prevPage->link = $this->objLanguage->languageText('mod_forum_backtoprevpage');
+$prevPage->link = $this->objLanguage->languageText('mod_forum_backtoprevpage', 'forum');
 
 echo $prevPage->show();
 
 echo ' / ';
 
 $backtoForumLink = new link ($this->uri(NULL));
-$backtoForumLink->link = $this->objLanguage->languageText('mod_forum_backtoforumsincontent').' '.$contextTitle;
+$backtoForumLink->link = $this->objLanguage->languageText('mod_forum_backtoforumsincontent', 'forum').' '.$contextTitle;
 
 echo $backtoForumLink->show();
 
