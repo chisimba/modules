8f7fffc7b3c6168b9346b78075acaf225ec2bfa4
charlvn <unknown@unknown.com>
2008-12-14 22:48:54 +0000

    Brightkite Module: Added brightkite library class.

---
+++ brightkite/classes/brightkiteops_class_inc.php
@@ -0,0 +1,35 @@
+<?php
+
+// security check - must be included in all scripts
+if (!
+/**
+ * Description for $GLOBALS
+ * @global unknown $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+/**
+ * Library class to provide easy access to the Brightkite API.
+ *
+ * @author    Charl van Niekerk <charlvn@charlvn.za.net>
+ * @copyright GNU/GPL, AVOIR
+ * @package   brightkite
+ * @access    public
+ * @version   $Id$
+ */
+class locationops extends object
+{
+    /**
+     * Standard constructor to load the necessary resources
+     * and populate the new object's instance variables
+     */
+    public function init()
+    {
+    }
+}
+
+?>
