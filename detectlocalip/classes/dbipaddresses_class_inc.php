f3365275d6e91ba54ef27554194e06f1c6ef96e0
charlvn <unknown@unknown.com>
2008-12-29 21:18:46 +0000

    Detect Local IP Module: Removed CVS from @version.

--- detectlocalip/classes/dbipaddresses_class_inc.php
+++ detectlocalip/classes/dbipaddresses_class_inc.php
@@ -30,7 +30,7 @@
  * @author    zDerek Keats derek@dkeats.com
  * @copyright 2007 AVOIR
  * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
- * @version   CVS: $Id: db_MODULECODE.php,v 1.1 2007-11-25 09:13:27 dkeats Exp $
+ * @version   $Id: db_MODULECODE.php,v 1.1 2007-11-25 09:13:27 dkeats Exp $
  * @link      http://avoir.uwc.ac.za
  */
 
--- detectlocalip/controller.php
+++ detectlocalip/controller.php
@@ -31,7 +31,7 @@
  * @author    Derek Keats derek@dkeats.com
  * @copyright 2007 AVOIR
  * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
- * @version   CVS: $Id: controller.php,v 1.4 2007-11-25 09:13:27 dkeats Exp $
+ * @version   $Id: controller.php,v 1.4 2007-11-25 09:13:27 dkeats Exp $
  * @link      http://avoir.uwc.ac.za
  */
  
