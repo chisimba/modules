03e1a7e0040df6b61bc98d2d3b612574173add43
charlvn <unknown@unknown.com>
2008-12-29 21:42:08 +0000

    Remote Popularity Module: Fixed subversion identifiers.

--- remotepopularity/classes/block_remotepopularity_class_inc.php
+++ remotepopularity/classes/block_remotepopularity_class_inc.php
@@ -19,7 +19,7 @@
  * Free Software Foundation, Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * @version    $Id:
+ * @version    $Id$
  * @package    remotepopularity
  * @subpackage blocks
  * @author     Paul Scott <pscott@uwc.ac.za>
