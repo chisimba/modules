57834b2f95fa08fa522166a7dbf55f4bf5a73830
charlvn <unknown@unknown.com>
2008-12-29 21:38:50 +0000

    Writing Tools Module: Removed CVS from @version.

--- writingtools/controller.php
+++ writingtools/controller.php
@@ -27,7 +27,7 @@
  * @author    Derek Keats dkeats@uwc.ac.za
  * @copyright 2007 AVOIR
  * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
- * @version   CVS: $Id: controller.php,v 1.4 2007-11-25 09:13:27 dkeats Exp $
+ * @version   $Id: controller.php,v 1.4 2007-11-25 09:13:27 dkeats Exp $
  * @link      http://avoir.uwc.ac.za
  */
 
