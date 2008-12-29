a50a8c5b5ac71fd84b0e28859624b9db3fcc7bc6
charlvn <unknown@unknown.com>
2008-12-29 21:36:08 +0000

    RDFGen Module: Corrected subversion identifiers.

--- rdfgen/classes/rdf_class_inc.php
+++ rdfgen/classes/rdf_class_inc.php
@@ -13,7 +13,7 @@
  * @author    Paul Scott <pscott@uwc.ac.za>
  * @copyright 2008 Paul Scott
  * @license   gpl
- * @version   $id$
+ * @version   $Id$
  * @link      http://avoir.uwc.ac.za
  */
 // security check - must be included in all scripts
