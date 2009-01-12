7f164800befc39df394c4c6a03bb57843f6b4897
charlvn <unknown@unknown.com>
2009-01-12 00:07:54 +0000

    Skinsuploader module: Corrected code formatting in upload skin template.

--- skinsuploader/templates/content/uploadSkin_tpl.php
+++ skinsuploader/templates/content/uploadSkin_tpl.php
@@ -1,11 +1,11 @@
 <?php
-    /**
-    * Main template file for the skins upload module
-    * Displays data and forms
-    */
-    
-    print "<div align='center'>";
-    
+/**
+* Main template file for the skins upload module
+* Displays data and forms
+*/
+
+print "<div align='center'>";
+
 /**
 * Upload template for skinsuploader module
 * Allows upload of skins
@@ -13,39 +13,39 @@
 * and modified by Jarrett L. Jordaan
 */
 
-    //Creating the form
-    $form=&$this->newObject('form','htmlelements');
-    $form->extra=' enctype="multipart/form-data" ';
-    $form->name='importusers';
-    $paramArray = array('action' => 'uploadSkin');
-    $form->setAction($this->uri($paramArray,'skinsuploader'));
+//Creating the form
+$form = $this->newObject('form','htmlelements');
+$form->extra=' enctype="multipart/form-data" ';
+$form->name='importusers';
+$paramArray = array('action' => 'uploadSkin');
+$form->setAction($this->uri($paramArray,'skinsuploader'));
 
-    //the file input
-    $fileInput=&$this->newObject('textinput','htmlelements');
-    $fileInput->fldType='file';
-    $fileInput->label=$this->objLanguage->languageText("word_upload");
-    $fileInput->name='upload';
-    $fileInput->size=60;
+//the file input
+$fileInput = $this->newObject('textinput','htmlelements');
+$fileInput->fldType='file';
+$fileInput->label=$this->objLanguage->languageText("word_upload");
+$fileInput->name='upload';
+$fileInput->size=60;
 
-    //the submit button
-    $objElement = new button('CSV');
-    $objElement->setToSubmit();
-    $objElement->setValue($this->objLanguage->languageText("word_upload"));
+//the submit button
+$objElement = new button('CSV');
+$objElement->setToSubmit();
+$objElement->setValue($this->objLanguage->languageText("word_upload"));
 
-    //add the objects to the form
-    $form->setDisplayType(1);
-    $form->addToForm($fileInput);
-    $form->addToForm($objElement);
+//add the objects to the form
+$form->setDisplayType(1);
+$form->addToForm($fileInput);
+$form->addToForm($objElement);
 
-    //Heading
-    $objHeading=&$this->newObject('htmlheading','htmlelements');
-    $objHeading->str=$this->objLanguage->languageText("word_upload_heading");
-    $objHeading->type=3;
-    $strCenter=$objHeading->show();
-    $strCenter.=$form->show();
+//Heading
+$objHeading = $this->newObject('htmlheading','htmlelements');
+$objHeading->str=$this->objLanguage->languageText("word_upload_heading");
+$objHeading->type=3;
+$strCenter=$objHeading->show();
+$strCenter.=$form->show();
 
-    print $strCenter."\n"; 
-    
-    print"</div>\n";
-    
+print $strCenter."\n";
+
+print"</div>\n";
+
 ?>
