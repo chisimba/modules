5c6b92962e94f73a2836165bf349edb5a5ab1716
charlvn <unknown@unknown.com>
2009-02-08 20:31:08 +0000

    Modaldialogue Module: Converted tabs to spaces and added skin script manipulation.

--- modaldialogue/controller.php
+++ modaldialogue/controller.php
@@ -2,12 +2,16 @@
 
 class modaldialogue extends controller
 {
-	public function init()
-	{
-	}
-	
-	public function dispatch()
-	{
-	    return 'main_tpl.php';
-	}
+    protected $objSkin;
+
+    public function init()
+    {
+        $this->objSkin = $this->getObject('skin', 'skin');
+    }
+    public function dispatch()
+    {
+        $this->objSkin->setVar('SUPPRESS_PROTOTYPE', true);
+        $this->objSkin->setVar('JQUERY_VERSION', '1.2.6');
+        return 'main_tpl.php';
+    }
 }
