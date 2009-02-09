5163ae6ece44ffa97e2cba8c6457dbb31dcc318c
charlvn <unknown@unknown.com>
2009-02-09 02:38:53 +0000

    eduCommons Moduke: Redirect to contextcontent module instead of contextadmin.

--- educommons/controller.php
+++ educommons/controller.php
@@ -110,7 +110,7 @@ class educommons extends controller
                 $data = $this->objImport->parseIms($manifest);
                 $this->objImport->addCourses($data, $context);
                 $this->objImport->addPages($data, $directory, $context);
-                $this->nextAction(null, null, 'contextadmin');
+                $this->nextAction(null, null, 'contextcontent');
                 break;
             default:
                 $contexts = $this->objContext->getListOfContext();
