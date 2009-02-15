900a73f512f767fa76365fa6f583364ebd648fca
charlvn <unknown@unknown.com>
2009-02-15 23:02:27 +0000

    Modaldialogue Module: Added docblock to class jqdialogue.

--- modaldialogue/classes/jqdialogue_class_inc.php
+++ modaldialogue/classes/jqdialogue_class_inc.php
@@ -1,5 +1,34 @@
 <?php
 
+/**
+ * jqdialogue class
+ * 
+ * Class to be used for generating jQuery UI Dialogues.
+ * 
+ * PHP version 5
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @category  chisimba
+ * @package   htmlelements
+ * @author    Joke van Niekerk <jokevn@jokevn.za.net> and Charl van Niekerk <charlvn@charlvn.za.net>
+ * @copyright 2009 FSIU
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id$
+ * @link      http://avoir.uwc.ac.za/
+ * @see       http://docs.jquery.com/UI/Dialog
+ */
 class jqdialogue extends object
 {
     /**
