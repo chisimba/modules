a0b6f611027703139fdf85c0a2b695966df9a764
charlvn <unknown@unknown.com>
2009-02-17 22:22:39 +0000

    Blog Module: Added ability to the blogtermsdialogue class to set its own title.

--- blog/classes/blogtermsdialogue_class_inc.php
+++ blog/classes/blogtermsdialogue_class_inc.php
@@ -4,13 +4,20 @@ $GLOBALS['_globalObjEngine']->loadClass('jqdialogue', 'htmlelements');
 
 class blogtermsdialogue extends jqdialogue
 {
+    protected $objLanguage;
+
     public function init()
     {
+        parent::init();
+        $this->objLanguage = $this->getObject('language', 'language');
+        $this->title = $this->objLanguage->languageText('mod_blog_terms_title', 'blog');
     }
 
     public function show()
     {
-        $this->content .= 'Test';
+        if ($this->content) {
+            $this->content .= 'Test';
+        }
         return parent::show();
     }
 }
