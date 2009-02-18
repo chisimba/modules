eaff7da48944fc568fd5ddd9ce6501cdc355f628
charlvn <unknown@unknown.com>
2009-02-18 01:14:44 +0000

    Blog Module: Added header phpdoc blocks to blogtermsdialogue.

--- blog/classes/blogtermsdialogue_class_inc.php
+++ blog/classes/blogtermsdialogue_class_inc.php
@@ -1,7 +1,57 @@
 <?php
 
+/**
+ * blogtermsdialogue class
+ * 
+ * Class to be used for generating jQuery UI Dialogues for accepting blog terms.
+ * 
+ * PHP version 5
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @category  chisimba
+ * @package   blog
+ * @author    Joke van Niekerk <jokevn@jokevn.za.net> and Charl van Niekerk <charlvn@charlvn.za.net>
+ * @copyright 2009 FSIU
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id$
+ * @link      http://avoir.uwc.ac.za/
+ * @see       http://docs.jquery.com/UI/Dialog
+ */
+
+// security check - must be included in all scripts
+if (!
+/**
+ * Description for $GLOBALS
+ * @global unknown $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+
 $GLOBALS['_globalObjEngine']->loadClass('jqdialogue', 'htmlelements');
 
+/**
+ * Class to create dialogues using the jQuery UI for accepting blog terms.
+ * 
+ * @package   blog
+ * @category  chisimba
+ * @copyright 2009 FSIU
+ * @license   GNU GPL
+ * @author    Joke van Niekerk and Charl van Niekerk
+ */
 class blogtermsdialogue extends jqdialogue
 {
     /**
