fe66782291a12537b65de817ad538bb080a56a9d
David Wafula <davidwaf@gmail.com>
2009-02-18 07:44:10 +0000

    added presence monitor: tracks users idle state

--- java_src/chisimba_realtime/src/avoir/realtime/classroom/SessionTimer.java
+++ java_src/chisimba_realtime/src/avoir/realtime/classroom/SessionTimer.java
@@ -4,6 +4,8 @@
  */
 package avoir.realtime.classroom;
 
+import avoir.realtime.classroom.packets.PresencePacket;
+import avoir.realtime.common.PresenceConstants;
 import java.util.TimerTask;
 
 /**
@@ -17,10 +19,26 @@ public class SessionTimer extends TimerTask {
     int mins = 0;
     int hrs = 0;
     private ClassroomMainFrame mf;
+    private int idleTime = 0;
+    private int ORANGE_IDLE_TIME =  3 * 60;
+    private int RED_IDLE_TIME =  5 * 60;
+    private boolean sendRed = false;
+    private boolean sendOrange = false;
 
     public SessionTimer(ClassroomMainFrame mf) {
         super();
         this.mf = mf;
+    }
+
+    public int getIdleTime() {
+        return idleTime;
+    }
+
+    public void setIdleTime(int idleTime) {
+        this.idleTime = 0;// idleTime;
+
+        sendOrange = false;
+        sendOrange = false;
     }
 
     public boolean isPaused() {
@@ -33,6 +51,16 @@ public class SessionTimer extends TimerTask {
 
     public void run() {
         if (!paused) {
+            if (idleTime > ORANGE_IDLE_TIME && idleTime < RED_IDLE_TIME && !sendOrange) {
+                mf.getTcpConnector().sendPacket(new PresencePacket(mf.getUser().getSessionId(), PresenceConstants.USER_IDLE_ICON, PresenceConstants.USER_ORANGE_IDLE, mf.getUser().getUserName()));
+                sendOrange = true;
+            }
+            if (idleTime > RED_IDLE_TIME && !sendRed) {
+                mf.getTcpConnector().sendPacket(new PresencePacket(mf.getUser().getSessionId(), PresenceConstants.USER_IDLE_ICON, PresenceConstants.USER_RED_IDLE, mf.getUser().getUserName()));
+                sendRed = true;
+            }
+
+            idleTime++;
             if (secs == 60) {
                 mins++;
                 secs = 0;
