31a27763a4551ab416e542ce41baeafdc9f393e3
rosina <unknown@unknown.com>
2009-02-24 15:13:26 +0000

    ahis module
    added templates

---
+++ ahis/templates/content/active_addsample_tpl.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * ahis Active Survaillance add new samples screen Template
+ *
+ * Template for capturing active surveillance new samples of herd 
+ * 
+ * PHP version 5
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @category  Chisimba
+ * @package   ahis
+ * @author    Rosina Ntow <rntow@ug.edu.gh>
+ * @copyright 2009 AVOIR
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id: active_herdview_tpl.php 
+ * @link      http://avoir.uwc.ac.za
+ */
+// security check - must be included in all scripts
+if (!
+/**
+ * Description for $GLOBALS
+ * @global entry point $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+
+$objHeading = $this->getObject('htmlheading','htmlelements');
+$objHeading->str = $this->objLanguage->languageText('phrase_active')." ".$this->objLanguage->languageText('word_add')."  ".$this->objLanguage->languageText('word_samples');
+$objHeading->type = 2;
+
+
+$this->loadClass('textinput','htmlelements');
+$this->loadClass('textarea','htmlelements');
+$this->loadClass('dropdown','htmlelements');
+$this->loadClass('button','htmlelements');
+$this->loadClass('layer','htmlelements');
+
+
+$addButton = new button('add', $this->objLanguage->languageText('word_add'));
+$addButton->setToSubmit();
+$backUri = $this->uri(array('action'=>'active_sampleview'));
+$backButton = new button('back', $this->objLanguage->languageText('word_back'), "javascript: document.location='$backUri'");
+
+$inputDate = $this->getObject('datepicker','htmlelements');
+$inputDate->setDefaultDate($calendardate);
+
+//$testDate = $this->newObject('datepicker','htmlelements');
+//$testDate->setName('dateTest');
+//$testDate->setDefaultDate($calenderdate);
+
+
+$speciesDrop = new dropdown('species');
+$speciesDrop->addFromDB($arraytesttype, 'name', 'id');
+$speciesDrop->setSelected($species);
+$ageDrop = new dropdown('age');
+$ageDrop->addFromDB($arraytesttype, 'name', 'id');
+$ageDrop->setSelected($age);
+$sexDrop = new dropdown('sex');
+$sexDrop->addFromDB($arraytesttype, 'name', 'id');
+$sexDrop->setSelected($sex);
+$sampletypeDrop = new dropdown('sampletype');
+$sampletypeDrop->addFromDB($arraytesttype, 'name', 'id');
+$sampletypeDrop->setSelected($sampletype);
+$testtypeDrop = new dropdown('testtype');
+$testtypeDrop->addFromDB($arraytesttype, 'name', 'id');
+$testtypeDrop->setSelected($testttype);
+$testresultDrop = new dropdown('testresult');
+$testresultDrop->addFromDB($arraytesttype, 'name', 'id');
+$testresultDrop->setSelected($testresult);
+$vachistoryDrop = new dropdown('vachistory');
+$vachistoryDrop->addFromDB($arraytesttype, 'name', 'id');
+$vachistoryDrop->setSelected($vachistory);
+
+
+$specArea = new textarea('spec',$spec,0,25);
+
+$sampleidBox = new textinput('sampleid', $sampleid);
+$animalidBox = new textinput('animalid', $animalid);
+$ageidBox = new textinput('ageid', $ageid);
+$numberBox = new textinput('number', $number);
+$remarksBox = new textinput('remarks', $remarks);
+
+$objTable = $this->getObject('htmltable','htmlelements');
+$objTable->cellspacing = 2;
+$objTable->width = NULL;
+$objTable->cssClass = 'min50';
+
+
+$objTable->startRow();
+
+$objTable->addCell($this->objLanguage->languageText('phrase_sampleid'));
+$objTable->addCell($sampleidBox->show());
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell($this->objLanguage->languageText('phrase_animalid'));
+$objTable->addCell($animalidBox->show());
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell($this->objLanguage->languageText('word_species'));
+$objTable->addCell($speciesDrop->show());
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell($this->objLanguage->languageText('word_age'));
+$objTable->addCell($ageDrop->show());
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell($this->objLanguage->languageText('word_sex'));
+$objTable->addCell($sexDrop->show());
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell($this->objLanguage->languageText('phrase_sampletype'));
+$objTable->addCell($sampletypeDrop->show());
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell($this->objLanguage->languageText('phrase_testtype'));
+$objTable->addCell($testtypeDrop->show());
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell($this->objLanguage->languageText('phrase_dateoftest'));
+$objTable->addCell($inputDate->show());
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell($this->objLanguage->languageText('phrase_testresult'));
+$objTable->addCell($testresultDrop->show());
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell($this->objLanguage->languageText('word_specification'));
+$objTable->addCell($specArea->show());
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell($this->objLanguage->languageText('phrase_vaccinationhistory'));
+$objTable->addCell($vachistoryDrop->show());
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell($this->objLanguage->languageText('word_number'));
+$objTable->addCell($numberBox->show());
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell($this->objLanguage->languageText('word_remarks'));
+$objTable->addCell($remarksBox->show());  
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell('&nbsp');
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell($backButton->show());
+$objTable->addCell($addButton->show());
+$objTable->endRow();
+
+
+$this->loadClass('form','htmlelements');
+$objForm = new form('reportForm', $this->uri(array('action' => 'active_sampleview')));
+$objForm->addToForm($objTable->show());
+
+$objLayer = new layer();
+$objLayer->addToStr($objHeading->show()."<hr class='ahis' />".$objForm->show());
+$objLayer->align = 'center';
+
+echo $objLayer->show();
+
+?>
---
+++ ahis/templates/content/active_sampleview_tpl.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * ahis Active Survaillance Samples screen Template
+ *
+ * Template for capturing active surveillance sample data
+ * 
+ * PHP version 5
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @category  Chisimba
+ * @package   ahis
+ * @author    Rosina Ntow <rntow@ug.edu.gh>
+ * @copyright 2009 AVOIR
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id: active_herdview_tpl.php 
+ * @link      http://avoir.uwc.ac.za
+ */
+// security check - must be included in all scripts
+if (!
+/**
+ * Description for $GLOBALS
+ * @global entry point $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+
+
+$objHeading = $this->getObject('htmlheading','htmlelements');
+$objHeading->str = $this->objLanguage->languageText('phrase_active')." ".$this->objLanguage->languageText('word_samples');
+$objHeading->type = 2;
+
+
+$this->loadClass('layer','htmlelements');
+$this->loadClass('dropdown','htmlelements');
+$this->loadClass('textinput','htmlelements');
+
+$objTable = $this->getObject('htmltable','htmlelements');
+
+
+$addButton = new button('add', $this->objLanguage->languageText('word_add'));
+$addButton->setToSubmit();
+$backUri = $this->uri(array('action'=>'sero_surveillance'));
+$backButton = new button('cancel', $this->objLanguage->languageText('word_back'), "javascript: document.location='$backUri'");
+$nextUri = $this->uri(array('action'=>'sero_surveillance'));
+$nextButton = new button('cancel', $this->objLanguage->languageText('word_next'), "javascript: document.location='$nextUri'");
+
+$campBox = new textinput('campBox',$campBox);
+$diseaseBox = new textinput('diseaseBox',$diseaseBox);
+
+
+$officerDrop = new dropdown('officerId');
+$officerDrop->addFromDB($userList, 'name', 'userid');
+$officerDrop->setSelected($officerId);
+$officerDrop->extra = 'disabled';
+
+
+$objTable->cellspacing = 2;
+$objTable->width = NULL;
+$objTable->cssClass = 'min50';
+
+
+$objTable->startRow();
+$objTable->addCell("<h6>".$this->objLanguage->languageText('word_campaign')." ".$this->objLanguage->languageText('word_name').": </h6>");
+$objTable->addCell($campBox->show());
+$objTable->addCell('&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp');
+$objTable->addCell('&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp');
+$objTable->addCell("<h6>".$this->objLanguage->languageText('word_disease').": </h6>");
+$objTable->addCell($diseaseBox->show());
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell($this->objLanguage->languageText('mod_ahis_reportofficer','ahis').": $tab");
+$objTable->addCell($officerDrop->show());
+$objTable->addCell('');
+$objTable->addCell('');
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell($this->objLanguage->languageText('word_samples').": $tab");
+$objTable->addCell('');
+$objTable->addCell('');
+$objTable->endRow();
+
+$objLayer = new layer();
+$objLayer->addToStr($objHeading->show()."<hr class='ahis' />".$objTable->show());
+$objLayer->align = 'center';
+
+echo $objLayer->show();
+
+
+$objTable = new htmlTable();
+$objTable->cellpadding =4;
+$objTable->cellspacing = 2;
+$objTable->width = '60%';
+$objTable->cssClass = 'min50';
+
+$objTable->startRow();
+$objTable->addCell($this->objLanguage->languageText('phrase_sampleid'),'','','','heading');
+
+$objTable->addCell($this->objLanguage->languageText('phrase_dateoftesting'), '', '', '', 'heading');
+
+$objTable->addCell($this->objLanguage->languageText('word_species'), '', '', '', 'heading');
+$objTable->addCell($this->objLanguage->languageText('word_age'), '', '', '', 'heading');
+$objTable->addCell($this->objLanguage->languageText('phrase_sampletype'), '', '', '', 'heading');
+$objTable->addCell($this->objLanguage->languageText('phrase_typeoftest'), '', '', '', 'heading');
+$objTable->addCell($this->objLanguage->languageText('word_number'), '', '', '', 'heading');
+$objTable->addCell($this->objLanguage->languageText('phrase_vaccinationhistory'), '', '', '', 'heading');
+$objTable->addCell($this->objLanguage->languageText('word_action'), '', '', '', 'heading');
+
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell('');
+$objTable->addCell('');
+$objTable->addCell('');
+$objTable->addCell('');
+$objTable->addCell('');
+$objTable->addCell('');
+$objTable->startRow();
+$objTable->addCell('&nbsp');
+$objTable->endRow();
+$objTable->startRow();
+$objTable->addCell($backButton->show());
+$objTable->addCell($addButton->show());
+$objTable->addCell($nextButton->show());
+$objTable->endRow();
+$this->loadClass('form','htmlelements');
+$objForm = new form('reportForm', $this->uri(array('action' => 'active_addsample')));
+$objForm->addToForm($objTable->show());
+
+$objLayer = new layer();
+$objLayer->addToStr($objForm->show());
+$objLayer->align = 'center';
+
+echo $objLayer->show();
+
+?>
