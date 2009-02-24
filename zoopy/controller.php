d60f6e431c70f0302321edb640100edbda537132
charlvn <unknown@unknown.com>
2009-02-24 11:09:20 +0000

    Zoopy Module: Added php docblocks to controller.

--- zoopy/controller.php
+++ zoopy/controller.php
@@ -2,15 +2,39 @@
 
 class zoopy extends controller
 {
+    /**
+     * Object of the dbsysconfig class in the sysconfig module.
+     *
+     * @access protected
+     * @var object
+     */
     protected $objSysConfig;
+
+    /**
+     * Object of the zoopylib class in the zoopy module.
+     *
+     * @access protected
+     * @var object
+     */
     protected $objZoopyLib;
 
+    /**
+     * Initialises the instance variables.
+     *
+     * @access public
+     */
     public function init()
     {
         $this->objSysConfig = $this->getObject('dbsysconfig', 'sysconfig');
         $this->objZoopyLib = $this->getObject('zoopylib', 'zoopy');
     }
 
+    /**
+     * Gets the name of the template to be used.
+     *
+     * @access public
+     * @return string Name of the template.
+     */
     public function dispatch()
     {
         $uri = $this->objSysConfig->getValue('mod_zoopy_feed_uri', 'zoopy');
@@ -21,6 +45,12 @@ class zoopy extends controller
         return 'main_tpl.php';
     }
 
+    /**
+     * Determines of the user needs to be logged on in order to view the page.
+     *
+     * @access public
+     * @return boolean True if the user needs to be logged in, false otherwise.
+     */
     public function requiresLogin()
     {
         return false;
