283704065b7ad5fee875de753209861317770eb5
charlvn <unknown@unknown.com>
2009-02-24 11:20:55 +0000

    ZoopyModule: Added phpdocblock headers to controller.

--- zoopy/controller.php
+++ zoopy/controller.php
@@ -1,5 +1,62 @@
 <?php
 
+/**
+ * Zoopy controller class
+ * 
+ * Class to control the zoopy module
+ * 
+ * PHP version 5
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @category  chisimba
+ * @package   zoopy
+ * @author    Charl van Niekerk <charlvn@charlvn.za.net>
+ * @copyright 2009 Charl van Niekerk
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id$
+ * @link      http://avoir.uwc.ac.za
+ * @link      http://www.zoopy.com/
+ */
+
+// security check - must be included in all scripts
+if (!
+/**
+ * Description for $GLOBALS
+ * @global unknown $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+/**
+ * Zoopy controller class
+ *
+ * Class to control the zoopy module.
+ *
+ * @category  Chisimba
+ * @package   zoopy
+ * @author    Charl van Niekerk <charlvn@charlvn.za.net>
+ * @copyright 2009 Charl van Niekerk
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   Release: @package_version@
+ * @link      http://avoir.uwc.ac.za
+ * @link      http://www.zoopy.com/
+ */
+
 class zoopy extends controller
 {
     /**
