fb01125f77b9d78e027bccf62f724727a8f9c401
David Wafula <davidwaf@gmail.com>
2009-03-05 16:24:50 +0000

    impelemented question and slide show navigators, and customizable filechoosrs

--- realtime/java_src/chisimba_realtime/src/avoir/realtime/filetransfer/FileReceiverManager.java
+++ realtime/java_src/chisimba_realtime/src/avoir/realtime/filetransfer/FileReceiverManager.java
@@ -27,6 +27,7 @@ import avoir.realtime.common.packet.SessionImgPacket;
 import avoir.realtime.common.packet.UploadMsgPacket;
 import avoir.realtime.common.packet.XmlQuestionPacket;
 import avoir.realtime.common.BuilderSlide;
+import avoir.realtime.common.XmlUtil;
 import avoir.realtime.survey.Value;
 import java.awt.Color;
 import java.io.File;
@@ -142,7 +143,7 @@ public class FileReceiverManager {
                     base.setQuestionImage(image);
                 }
                 if (p.getFileType() == Constants.SLIDE_SHOW_VIEW) {
-                    ArrayList<BuilderSlide> list = readXmlSlideShowFile(filename);
+                    ArrayList<BuilderSlide> list = XmlUtil.readXmlSlideShowFile(filename);
                     /*    base.showSlideViewer();
                     base.getSlideShowViewerFrame().setSlides(list);
                      */
@@ -192,7 +193,7 @@ public class FileReceiverManager {
 
                 }
                 if (p.getFileType() == Constants.SLIDE_SHOW_VIEW) {
-                    ArrayList<BuilderSlide> list = readXmlSlideShowFile(filename);
+                    ArrayList<BuilderSlide> list = XmlUtil.readXmlSlideShowFile(filename);
                     //base.showSlideViewer();
                     //base.getSlideShowViewerFrame().setSlides(list);
                     base.getWhiteBoardSurface().setSlides(list);
@@ -228,212 +229,6 @@ public class FileReceiverManager {
             ex.printStackTrace();
         }
         return null;
-    }
-
-    private ArrayList<BuilderSlide> readXmlSlideShowFile(String path) {
-        ArrayList<BuilderSlide> slides = new ArrayList<BuilderSlide>();
-        try {
-            DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-            Document doc = docBuilder.parse(new File(path));
-            doc.getDocumentElement().normalize();
-
-
-            NodeList slideList = doc.getElementsByTagName("slide");
-            ArrayList<BuilderSlide> values = new ArrayList<BuilderSlide>();
-            for (int i = 0; i < slideList.getLength(); i++) {
-                Node node = slideList.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
-
-                    NodeList titlevals = element.getElementsByTagName("title");
-                    Element titleElement = (Element) titlevals.item(0);
-                    NodeList titleNodes = titleElement.getChildNodes();
-                    String title = ((Node) titleNodes.item(0)).getNodeValue();
-
-                    NodeList textvals = element.getElementsByTagName("text");
-                    Element textElement = (Element) textvals.item(0);
-                    NodeList textNodes = textElement.getChildNodes();
-                    String text = ((Node) textNodes.item(0)).getNodeValue();
-
-
-
-
-                    String imageData = "";
-
-                    try {
-                        NodeList imagevals = element.getElementsByTagName("image-data");
-                        Element imageElement = (Element) imagevals.item(0);
-                        NodeList imageNodes = imageElement.getChildNodes();
-                        imageData = ((Node) imageNodes.item(0)).getNodeValue();
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                    byte[] data = Base64.decode(imageData);
-
-                    NodeList textsvals = element.getElementsByTagName("text-size");
-                    Element textsElement = (Element) textsvals.item(0);
-                    NodeList textsNodes = textsElement.getChildNodes();
-                    String texts = ((Node) textsNodes.item(0)).getNodeValue();
-                    int textSize = 18;
-                    try {
-                        textSize = Integer.parseInt(texts.trim());
-                    } catch (NumberFormatException ex) {
-                        ex.printStackTrace();
-                    }
-
-                    NodeList colorList = doc.getElementsByTagName("text-color");
-                    Color color = Color.BLUE;
-                    for (int k = 0; k < colorList.getLength(); k++) {
-                        Node colorNode = colorList.item(k);
-                        if (colorNode.getNodeType() == Node.ELEMENT_NODE) {
-                            Element colorElement = (Element) colorNode;
-
-                            NodeList rvals = colorElement.getElementsByTagName("red");
-                            Element rElement = (Element) rvals.item(0);
-                            NodeList rNodes = rElement.getChildNodes();
-                            String rs = ((Node) rNodes.item(0)).getNodeValue();
-                            int red = 0;
-                            try {
-                                red = Integer.parseInt(rs.trim());
-                            } catch (NumberFormatException ex) {
-                                ex.printStackTrace();
-                            }
-                            NodeList gvals = element.getElementsByTagName("green");
-                            Element gElement = (Element) gvals.item(0);
-                            NodeList gNodes = gElement.getChildNodes();
-                            String gs = ((Node) gNodes.item(0)).getNodeValue();
-                            int green = 0;
-                            try {
-                                green = Integer.parseInt(gs.trim());
-                            } catch (NumberFormatException ex) {
-                                ex.printStackTrace();
-                            }
-
-                            NodeList bvals = element.getElementsByTagName("blue");
-                            Element bElement = (Element) bvals.item(0);
-                            NodeList bNodes = bElement.getChildNodes();
-                            String bs = ((Node) bNodes.item(0)).getNodeValue();
-                            int blue = 0;
-
-                            try {
-                                blue = Integer.parseInt(bs.trim());
-                            } catch (NumberFormatException ex) {
-                                ex.printStackTrace();
-                            }
-                            color = new Color(red, green, blue);
-                        }
-                    }
-
-
-                    XmlQuestionPacket qpacket = readXmlElememnt((Element) node);
-                    BuilderSlide slide = new BuilderSlide(title, text, color, textSize, qpacket, new ImageIcon(data), null);
-                    slides.add(slide);
-
-                }
-            }
-            return slides;
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return null;
-        }
-    }
-
-    private XmlQuestionPacket readXmlElememnt(Element doc) {
-        try {
-            String name = "";
-            NodeList qn = doc.getElementsByTagName("question-str");
-            for (int i = 0; i < qn.getLength(); i++) {
-                Node node = qn.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
-                    NodeList vals = element.getChildNodes();
-                    name = ((Node) vals.item(0)).getNodeValue();
-                }
-            }
-            NodeList slides = doc.getElementsByTagName("answer");
-            ArrayList<Value> values = new ArrayList<Value>();
-            for (int i = 0; i < slides.getLength(); i++) {
-                Node node = slides.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
-
-                    NodeList optvals = element.getElementsByTagName("option");
-                    Element optionElement = (Element) optvals.item(0);
-                    NodeList optNodes = optionElement.getChildNodes();
-                    String opt = ((Node) optNodes.item(0)).getNodeValue();
-
-                    NodeList valvals = element.getElementsByTagName("value");
-                    Element valElement = (Element) valvals.item(0);
-                    NodeList valNodes = valElement.getChildNodes();
-                    String val = ((Node) valNodes.item(0)).getNodeValue();
-
-                    NodeList ca = element.getElementsByTagName("correct-answer");
-                    Element caElement = (Element) ca.item(0);
-                    NodeList caNodes = caElement.getChildNodes();
-                    String castr = ((Node) caNodes.item(0)).getNodeValue();
-
-                    values.add(new Value(opt, val, new Boolean(castr).booleanValue()));
-
-                }
-            }
-
-            NodeList essay = doc.getElementsByTagName("type");
-            int type = Constants.ESSAY_QUESTION;
-            for (int i = 0; i < essay.getLength(); i++) {
-                Node node = essay.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
-                    NodeList vals = element.getChildNodes();
-                    type = Integer.parseInt(((Node) vals.item(0)).getNodeValue());
-                }
-            }
-
-            NodeList sender = doc.getElementsByTagName("sender");
-            String theSender = "";
-            for (int i = 0; i < sender.getLength(); i++) {
-                Node node = sender.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
-                    NodeList vals = element.getChildNodes();
-                    theSender = ((Node) vals.item(0)).getNodeValue();
-                }
-            }
-            NodeList id = doc.getElementsByTagName("id");
-            String theId = "";
-            for (int i = 0; i < id.getLength(); i++) {
-                Node node = id.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
-                    NodeList vals = element.getChildNodes();
-                    theId = ((Node) vals.item(0)).getNodeValue();
-                }
-            }
-            NodeList im = doc.getElementsByTagName("image-path");
-            String imagePath = "";
-            for (int i = 0; i < im.getLength(); i++) {
-                Node node = im.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
-                    NodeList vals = element.getChildNodes();
-                    imagePath = ((Node) vals.item(0)).getNodeValue();
-                }
-            }
-
-            if (imagePath.equals("null")) {
-                return new XmlQuestionPacket(name, values, type, theSender, theId, null,
-                        null, imagePath);
-            }
-            ImageIcon image = new ImageIcon(Base64.decode(imagePath));
-            if (imagePath != null || !imagePath.equals("null")) {
-                image = new ImageIcon(imagePath);
-            }
-            return new XmlQuestionPacket(name, values, type, theSender, theId, null,
-                    image, imagePath);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return null;
-        }
     }
 
     private void processFlash(final String filepath, final String id, final String sessionId) {
