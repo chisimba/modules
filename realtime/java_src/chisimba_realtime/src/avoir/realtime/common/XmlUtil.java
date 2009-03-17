d5c78c9e8d4d57046ca53761c9adf41bc85fe8e3
David Wafula <davidwaf@gmail.com>
2009-03-17 14:30:02 +0000

    added the clear function for question/slide manager

--- realtime/java_src/chisimba_realtime/src/avoir/realtime/common/XmlUtil.java
+++ realtime/java_src/chisimba_realtime/src/avoir/realtime/common/XmlUtil.java
@@ -41,6 +41,7 @@ public class XmlUtil {
                     Element element = (Element) node;
                     String title = readString(element, "title");
                     String text = readString(element, "text");
+                    String url=readString(element, "url");
                     String imageData = readString(element, "image-data");
                     byte[] data = Base64.decode(imageData);
                     int textSize = readInt(element, "text-size");
@@ -61,40 +62,38 @@ public class XmlUtil {
                     }
 
                     int index = readInt(element, "index");
-                    String qname = readString(doc, "question-str");
-                    int type = readInt(doc, "type");
-                    String qsender = readString(doc, "sender");
-                    String qid = readString(doc, "id");
-                    String qimagePath = readString(doc, "image-path");
-                    NodeList answersList = doc.getElementsByTagName("answers");
+                    String qname = readString(element, "question-str");
+                    int type = readInt(element, "type");
+                    String qsender = readString(element, "sender");
+                    String qid = readString(element, "id");
+                    String qimageData = readString(element, "image-data");
+                    NodeList answersList = doc.getElementsByTagName("answer");
                     ArrayList<Value> values = new ArrayList<Value>();
-                    for (int k = 0; k < colorList.getLength(); k++) {
+                    for (int k = 0; k < answersList.getLength(); k++) {
                         Node answerNode = answersList.item(k);
                         if (node.getNodeType() == Node.ELEMENT_NODE) {
                             Element answerElement = (Element) answerNode;
                             String opt = readString(answerElement, "option");
                             String value = readString(answerElement, "value");
-                            String correctAnswer = readString(answerElement, "corrent-answer");
+                            String correctAnswer = readString(answerElement, "correct-answer");
                             values.add(new Value(opt, value, new Boolean(correctAnswer).booleanValue()));
+                            
                         }
                     }
-                    XmlQuestionPacket qpacket = null;
-                    if (qimagePath.equals("null")) {
-                        qpacket = new XmlQuestionPacket(qname, values, type, qsender, qid,
-                                new File(path).getName(),
-                                null, qimagePath);
-                    }
-                    ImageIcon image = new ImageIcon();
-                    if (qimagePath != null || !qimagePath.equals("null")) {
-                        image = new ImageIcon(qimagePath);
-                        qpacket = new XmlQuestionPacket(qname, values, type, qsender, qid,
-                                new File(path).getName(),
-                                image, qimagePath);
-                    }
 
+
+                    XmlQuestionPacket qpacket = null;
+                    ImageIcon icon = null;
+                    if (qimageData != null) {
+                        byte[] xdata = Base64.decode(qimageData);
+                        icon = new ImageIcon(xdata);
+                    }
+                    qpacket = new XmlQuestionPacket(qname, values, type, qsender, qid,
+                            new File(path).getName(),
+                            icon, "");
 
                     BuilderSlide slide = new BuilderSlide(title, text, color, textSize, qpacket,
-                            new ImageIcon(data), null, index, textXPos, textYPos);
+                            new ImageIcon(data), null, index, textXPos, textYPos,url);
                     slides.add(slide);
 
                 }
@@ -199,21 +198,18 @@ public class XmlUtil {
             int type = readInt(doc, "type");
             String theSender = readString(doc, "sender");
             String theId = readString(doc, "id");
-            String imagePath = readString(doc, "image-path");
-
-            if (imagePath.equals("null")) {
+            String imageData = readString(doc, "image-data");
+            if (imageData != null) {
+                byte[] data = Base64.decode(imageData);
+                ImageIcon icon = new ImageIcon(data);
                 return new XmlQuestionPacket(name, values, type, theSender, theId, new File(path).getName(),
-                        null, imagePath);
+                        icon, null);
+
             }
-            ImageIcon image = new ImageIcon();
-            if (imagePath != null || !imagePath.equals("null")) {
-                image = new ImageIcon(imagePath);
-            }
-            return new XmlQuestionPacket(name, values, type, theSender, theId, new File(path).getName(),
-                    image, imagePath);
         } catch (Exception ex) {
             ex.printStackTrace();
             return null;
         }
+        return null;
     }
 }
