985e3be2ea111ce02638e5dbfc6d2b5afa02c456
David Wafula <davidwaf@gmail.com>
2009-03-17 14:31:37 +0000

    added the clear function for question/slide manager

--- java_src/chisimba_realtime/src/avoir/realtime/instructor/SlideShowNavigator.java
+++ java_src/chisimba_realtime/src/avoir/realtime/instructor/SlideShowNavigator.java
@@ -15,9 +15,12 @@ import avoir.realtime.common.Constants;
 import avoir.realtime.common.ImageUtil;
 import avoir.realtime.common.RealtimeFile;
 import avoir.realtime.common.XmlUtil;
+import avoir.realtime.common.packet.DeleteFilePacket;
 import avoir.realtime.common.packet.FileDownloadRequest;
 import avoir.realtime.common.packet.FileVewRequestPacket;
-import avoir.realtime.common.packet.QuestionPacket;
+
+import avoir.realtime.common.packet.SlideShowPopulateRequest;
+import avoir.realtime.common.packet.UpdateSlideShowIndexPacket;
 import avoir.realtime.common.packet.XmlQuestionPacket;
 import avoir.realtime.instructor.whiteboard.Classroom;
 import java.awt.BorderLayout;
@@ -29,8 +32,10 @@ import java.awt.event.MouseEvent;
 import java.io.File;
 import java.util.ArrayList;
 import javax.swing.JButton;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -41,7 +46,7 @@ import javax.swing.tree.TreeSelectionModel;
 import javax.swing.event.TreeModelEvent;
 import javax.swing.event.TreeModelListener;
 
-public class SlideShowNavigator extends JPanel {
+public class SlideShowNavigator extends JPanel implements ActionListener {
 
     protected DefaultMutableTreeNode rootNode;
     protected DefaultTreeModel treeModel;
@@ -53,14 +58,28 @@ public class SlideShowNavigator extends JPanel {
     private JButton addButton = new JButton(ImageUtil.createImageIcon(this, "/icons/add.png"));
     private JButton refreshButton = new JButton(ImageUtil.createImageIcon(this, "/icons/refresh.png"));
     private String mode = "slide-show";
+    private JPopupMenu popup = new JPopupMenu();
+    private JMenuItem newItem = new JMenuItem("New");
+    private JMenuItem editItem = new JMenuItem("Edit");
+    private JMenuItem deleteItem = new JMenuItem("Delete");
 
     public SlideShowNavigator(Classroom xmf) {
         super(new BorderLayout());
         this.mf = xmf;
         rootNode = new DefaultMutableTreeNode("Slide Show Navigator");
         treeModel = new DefaultTreeModel(rootNode);
-
+        popup.add(newItem);
+        popup.add(editItem);
+        popup.add(deleteItem);
         tree = new JTree(treeModel);
+        newItem.addActionListener(this);
+        newItem.setActionCommand("new");
+        deleteItem.addActionListener(this);
+        deleteItem.setActionCommand("delete");
+        editItem.addActionListener(this);
+        editItem.setActionCommand("edit");
+
+
         //    tree.setEditable(true);
         tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
         tree.setShowsRootHandles(true);
@@ -69,10 +88,23 @@ public class SlideShowNavigator extends JPanel {
             @Override
             public void mouseClicked(MouseEvent e) {
                 TreePath parentPath = tree.getSelectionPath();
-
+                if (e.getButton() == e.BUTTON3) {
+                    if (selectedNodeIsEditable()) {
+                        popup.show(SlideShowNavigator.this, e.getX(), e.getY());
+                    }
+                    return;
+                }
                 selectedNode = (DefaultMutableTreeNode) (parentPath.getLastPathComponent());
                 Object obj = selectedNode.getUserObject();
+                if (obj instanceof String) {
+                    String str = (String) obj;
+                    if (str.equals("Clear")) {
+                        mf.getWhiteboard().setCurrentSlide(null, 0, 0, true);
+                        mf.getWhiteboard().setSlideUrl(null);
+                        mf.getWhiteboard().setSlideText(null);
 
+                    }
+                }
                 if (obj instanceof BuilderSlide) {
                     BuilderSlide slide = (BuilderSlide) obj;
                     mf.getWhiteboard().setSlideTextXPos(slide.getTextXPos());
@@ -80,17 +112,23 @@ public class SlideShowNavigator extends JPanel {
                     mf.getWhiteboard().setSlideText(slide.getText());
                     mf.getWhiteboard().setTextColor(slide.getTextColor());
                     mf.getWhiteboard().setCurrentSlide(slide.getImage(), slide.getIndex(), 0, true);
+                    mf.getWhiteboard().setSlideUrl(slide.getUrl());
                     XmlQuestionPacket qn = slide.getQuestion();
 
                     if (qn.getType() != -1) {
-                        mf.initAnswerFrame(qn.getQuestionPacket(), qn.getName(),true);
+                        mf.initAnswerFrame(qn.getQuestionPacket(), qn.getName(), false);
                     }
+                    mf.getTcpConnector().sendPacket(new UpdateSlideShowIndexPacket(mf.getUser().getSessionId(), slide.getIndex()));
 
                 }
                 if (e.getClickCount() == 2) {
                     if (obj instanceof RealtimeFile) {
                         RealtimeFile file = (RealtimeFile) obj;
                         String targetPath = file.getPath();
+                        mf.getTcpConnector().sendPacket(
+                                new SlideShowPopulateRequest(mf.getUser().getSessionId(),
+                                file.getFileName()));
+
                         File f = new File(Constants.REALTIME_HOME + "/classroom/documents/" + mf.getUser().getSessionId() + "/" + new File(targetPath).getName());
                         if (f.exists()) {
                             if (mode.equals("slide-show")) {
@@ -99,7 +137,8 @@ public class SlideShowNavigator extends JPanel {
                             }
                             if (mode.equals("question-manager")) {
                                 XmlQuestionPacket pac = XmlUtil.readXmlQuestionFile(f.getAbsolutePath());
-                                mf.initAnswerFrame(pac.getQuestionPacket(), pac.getName(),true);
+                                mf.initAnswerFrame(pac.getQuestionPacket(), pac.getName(), false);
+                                mf.getTcpConnector().sendPacket(pac.getQuestionPacket());
                             }
                         } else {
                             if (mode.equals("slide-show")) {
@@ -127,12 +166,7 @@ public class SlideShowNavigator extends JPanel {
         addButton.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
-                if (mode.equals("slide-show")) {
-                    mf.showSlideBuilder();
-                }
-                if (mode.equals("question-manager")) {
-                    mf.showQuestionsManager();
-                }
+                processAdd();
             }
         });
         // closeButton.setContentAreaFilled(false);
@@ -159,7 +193,7 @@ public class SlideShowNavigator extends JPanel {
                     mf.getTcpConnector().sendPacket(new FileVewRequestPacket(targetPath));
                 }
                 if (mode.equals("question-manager")) {
-                 //   mf.getTcpConnector().setFileManagerMode("question-list");
+                    //   mf.getTcpConnector().setFileManagerMode("question-list");
                     String targetPath = mf.getUser().getUserName() + "/questions";
                     mf.getTcpConnector().sendPacket(new FileVewRequestPacket(targetPath));
                 }
@@ -183,12 +217,107 @@ public class SlideShowNavigator extends JPanel {
         add(scrollPane, BorderLayout.CENTER);
     }
 
+    private void processAdd() {
+        if (mode.equals("slide-show")) {
+            mf.showSlideBuilder();
+        }
+        if (mode.equals("question-manager")) {
+            mf.showQuestionsManager();
+        }
+    }
+
+    private boolean selectedNodeIsEditable() {
+        TreePath currentSelection = tree.getSelectionPath();
+
+        if (currentSelection != null) {
+            DefaultMutableTreeNode currentNode = (DefaultMutableTreeNode) (currentSelection.getLastPathComponent());
+            MutableTreeNode parent = (MutableTreeNode) (currentNode.getParent());
+            if (parent != null) {
+                Object obj = currentNode.getUserObject();
+                if (obj instanceof RealtimeFile) {
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+
+    private void processEdit() {
+        TreePath currentSelection = tree.getSelectionPath();
+        if (currentSelection != null) {
+            DefaultMutableTreeNode currentNode = (DefaultMutableTreeNode) (currentSelection.getLastPathComponent());
+            MutableTreeNode parent = (MutableTreeNode) (currentNode.getParent());
+            if (parent != null) {
+                Object obj = currentNode.getUserObject();
+                if (obj instanceof RealtimeFile) {
+                    RealtimeFile file = (RealtimeFile) obj;
+                    String targetPath = file.getPath();
+                    File f = new File(Constants.REALTIME_HOME + "/classroom/documents/" + mf.getUser().getSessionId() + "/" + new File(targetPath).getName());
+                    if (f.exists()) {
+                        if (mode.equals("slide-show")) {
+                            ArrayList<BuilderSlide> list = XmlUtil.readXmlSlideShowFile(f.getAbsolutePath());
+                            mf.showSlideBuilder();
+                            mf.getSlideBuilderManager().setMode("edit-slide");
+                            mf.getSlideBuilderManager().setSlides(list, f.getName());
+                        }
+                        if (mode.equals("question-manager")) {
+                            XmlQuestionPacket pac = XmlUtil.readXmlQuestionFile(f.getAbsolutePath());
+                            mf.showQuestionsManager();
+                            mf.getSurveyManagerFrame().setMode("edit");
+                            mf.getSurveyManagerFrame().setQuestion(pac);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void processDelete() {
+        TreePath currentSelection = tree.getSelectionPath();
+        if (currentSelection != null) {
+            DefaultMutableTreeNode currentNode = (DefaultMutableTreeNode) (currentSelection.getLastPathComponent());
+            MutableTreeNode parent = (MutableTreeNode) (currentNode.getParent());
+            if (parent != null) {
+                Object obj = currentNode.getUserObject();
+                if (obj instanceof RealtimeFile) {
+                    RealtimeFile file = (RealtimeFile) obj;
+                    int n = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete " + file.getFileName() + "?", "Confirm Delete", JOptionPane.YES_NO_OPTION);
+                    if (n == JOptionPane.YES_OPTION) {
+                        String localCopy = Constants.REALTIME_HOME + "/classroom/documents/" + file.getFileName();
+                        new File(localCopy).delete();
+                        mf.getTcpConnector().sendPacket(new DeleteFilePacket(file.getPath()));
+
+                    }
+                }
+            }
+        }
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("new")) {
+            processAdd();
+        }
+        if (e.getActionCommand().equals("delete")) {
+            processDelete();
+        }
+        if (e.getActionCommand().equals("edit")) {
+            processEdit();
+        }
+    }
+
+    public SlideShowNavigator() {
+    }
+
     public void setRootText(String text) {
         rootNode.setUserObject(text);
+
     }
 
     public void setFiles(ArrayList<RealtimeFile> files) {
         clear();
+        addObject(null, "Clear");
         for (int i = 0; i < files.size(); i++) {
             if (!files.get(i).getFileName().equals("..")) {
                 addObject(null, files.get(i), true);
@@ -209,7 +338,7 @@ public class SlideShowNavigator extends JPanel {
             selectedNode.removeAllChildren();
             treeModel.reload();
             for (int i = 0; i < slides.size(); i++) {
-                addObject(selectedNode, slides.get(i));
+                addObject(selectedNode, slides.get(i), true);
             }
         }
     }
