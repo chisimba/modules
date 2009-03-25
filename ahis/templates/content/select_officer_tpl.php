09bec1295bd19f7ad54f63df09e765ce70f0cd49
nic <unknown@unknown.com>
2009-03-25 09:43:01 +0000

    fixed userid/username mix up

--- ahis/templates/content/select_officer_tpl.php
+++ ahis/templates/content/select_officer_tpl.php
@@ -49,7 +49,7 @@ $this->loadClass('layer','htmlelements');
 
 
 $inputOfficer = new dropdown('officerId');
-$inputOfficer->addFromDB($userList, 'userid', 'name');
+$inputOfficer->addFromDB($userList, 'name', 'userid');
 $inputOfficer->setSelected($officerId);
 if (!$this->objUser->isAdmin()) {
     $inputOfficer->extra = 'disabled';
