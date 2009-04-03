f1af685583e9bc92d761ef2d159cba5c9c105726
cmert <unknown@unknown.com>
2009-04-03 10:15:05 +0000

    Added sql tables

--- librarysearch/templates/content/librarysearch_result_tpl.php
+++ librarysearch/templates/content/librarysearch_result_tpl.php
@@ -26,8 +26,8 @@ $script = <<<STARTSEARCH
         //targetDiv
         //alert (workflow);
 
-        //var theUrl = "?module=librarysearch&action=execworkflow&id=" + sourceId + '&search_key=' + searchKey + '&subject_cluster=' + subjectCluster;
-        var theUrl = "http://www.google.com";
+        var theUrl = "?module=librarysearch&action=execworkflow&id=" + sourceId + '&search_key=' + searchKey + '&subject_cluster=' + subjectCluster;
+        //var theUrl = "http://localhost/fresh";
         //alert(theUrl);
         jQuery.ajax({
             url: theUrl,
@@ -36,7 +36,7 @@ $script = <<<STARTSEARCH
             beforeSend: function() {
             },
             success: function(data) {
-                alert(data);
+                //alert(data);
                 jQuery('#' + divId).replaceWith(data);
             },
             error: function(data) {
