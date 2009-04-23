a75a17a627dcb74b7177decc272693ac07de1a98
David Wafula <davidwaf@gmail.com>
2009-04-23 18:47:17 +0000

    added direct way of adding left blocks

--- news/templates/layout/blocks_layout_tpl.php
+++ news/templates/layout/blocks_layout_tpl.php
@@ -4,20 +4,20 @@
 
 // Add JavaScript if User can update blocks
 if ($objUser->isAdmin()) {
-    
+
     $objIcon = $this->newObject('geticon', 'htmlelements');
     $objIcon->setIcon('up');
     $upIcon = $objIcon->show();
-    
-    
+
+
     $objIcon->setIcon('down');
     $downIcon = $objIcon->show();
-    
+
     $objIcon->setIcon('delete');
     $deleteIcon = $objIcon->show();
-?>
+    ?>
 <script type="text/javascript">
-// <![CDATA[
+    // <![CDATA[
     upIcon = '<?php echo $upIcon; ?>';
     downIcon = '<?php echo $downIcon; ?>';
     deleteIcon = '<?php echo $deleteIcon; ?>';
@@ -31,13 +31,15 @@ if ($objUser->isAdmin()) {
     pageType = '<?php echo $pageType; ?>';
     pageId = '<?php echo $pageTypeId; ?>';
 
-// ]]>
+    // ]]>
 </script>
 <?php
-    $this->appendArrayVar('headerParams', $this->getJavaScriptFile('jquery/jquery.livequery.js', 'htmlelements'));
-    echo $this->getJavaScriptFile('newsblocks.js');
+$this->appendArrayVar('headerParams', $this->getJavaScriptFile('jquery/jquery.livequery.js', 'htmlelements'));
+echo $this->getJavaScriptFile('newsblocks.js');
 } // End Addition of JavaScript
 
+$objBlocks = $this->getObject('dbmoduleblocks', 'modulecatalogue');
+$wideBlocks = $objBlocks->getBlocks('wide', 'site|user|postlogin');
 
 
 $leftContent = $this->objNewsMenu->generateMenu();
@@ -46,99 +48,160 @@ $leftContent .= $this->objNewsStories->getFeedLinks();
 $adminOptions = array();
 
 if ($this->isValid('managecategories')) {
-    $newsCategoriesLink = new link ($this->uri(array('action'=>'managecategories')));
-    $newsCategoriesLink->link = 'Manage News Categories';
-    $adminOptions[] = '<li>'.$newsCategoriesLink->show().'</li>';
+$newsCategoriesLink = new link ($this->uri(array('action'=>'managecategories')));
+$newsCategoriesLink->link = 'Manage News Categories';
+$adminOptions[] = '<li>'.$newsCategoriesLink->show().'</li>';
 }
 
 if ($this->isValid('addstory')) {
-    $addNewsStoryLink = new link ($this->uri(array('action'=>'addstory')));
-    $addNewsStoryLink->link = 'Add News Story';
-    $adminOptions[] = '<li>'.$addNewsStoryLink->show().'</li>';
+$addNewsStoryLink = new link ($this->uri(array('action'=>'addstory')));
+$addNewsStoryLink->link = 'Add News Story';
+$adminOptions[] = '<li>'.$addNewsStoryLink->show().'</li>';
 }
 
 if (count($adminOptions) > 0) {
 
-    $leftContent .= '<h3>News Options</h3>';
+$leftContent .= '<h3>News Options</h3>';
 
-    $leftContent .= '<ul>';
+$leftContent .= '<ul>';
 
-    foreach ($adminOptions as $option)
-    {
-        $leftContent .= $option;
-    }
+foreach ($adminOptions as $option)
+{
+    $leftContent .= $option;
+}
 
-    $leftContent .= '</ul>';
+$leftContent .= '</ul>';
 
 }
 
 if ($objUser->isAdmin()) {
 
-    $smallBlocksDropDown = new dropdown ('rightblocks');
-    $smallBlocksDropDown->cssId = 'ddrightblocks';
-    $smallBlocksDropDown->addOption('', $objLanguage->languageText('phrase_selectone', 'context', 'Select One').'...');
+$rightSmallBlocksDropDwon = new dropdown ('rightblocks');
+$rightSmallBlocksDropDwon->cssId = 'ddrightblocks';
+$rightSmallBlocksDropDwon->addOption('', $objLanguage->languageText('phrase_selectone', 'context', 'Select One').'...');
+
+$leftSmallBlocksDropDwon = new dropdown ('leftblocks');
+$leftSmallBlocksDropDwon->cssId = 'ddleftblocks';
+$leftSmallBlocksDropDwon->addOption('', $objLanguage->languageText('phrase_selectone', 'context', 'Select One').'...');
+
+
+foreach ($smallDynamicBlocks as $smallBlock)
+{
+    $rightSmallBlocksDropDwon->addOption('dynamicblock|'.$smallBlock['id'].'|'.$smallBlock['module'], htmlentities($smallBlock['title']));
+    $leftSmallBlocksDropDwon->addOption('dynamicblock|'.$smallBlock['id'].'|'.$smallBlock['module'], htmlentities($smallBlock['title']));
+
+}
+
+foreach ($smallBlocks as $smallBlock)
+{
+    $block = $this->newObject('block_'.$smallBlock['blockname'], $smallBlock['moduleid']);
+    $title = $block->title;
+
+    $rightSmallBlocksDropDwon->addOption('block|'.$smallBlock['blockname'].'|'.$smallBlock['moduleid'], htmlentities($title));
+    $leftSmallBlocksDropDwon->addOption('block|'.$smallBlock['blockname'].'|'.$smallBlock['moduleid'], htmlentities($title));
+
+}
+    $wideBlocksDropDown = new dropdown ('middleblocks');
+    $wideBlocksDropDown->cssId = 'ddmiddleblocks';
+    $wideBlocksDropDown->addOption('', $objLanguage->languageText('phrase_selectone', 'context', 'Select One').'...');
+
+    $objDynamicBlocks = $this->getObject('dynamicblocks', 'blocks');
+    $wideDynamicBlocks = $objDynamicBlocks->getWideSiteBlocks();
     
-    
-    foreach ($smallDynamicBlocks as $smallBlock)
+    foreach ($wideDynamicBlocks as $wideBlock)
     {
-        $smallBlocksDropDown->addOption('dynamicblock|'.$smallBlock['id'].'|'.$smallBlock['module'], htmlentities($smallBlock['title']));
+        $wideBlocksDropDown->addOption('dynamicblock|'.$wideBlock['id'].'|'.$wideBlock['module'], htmlentities($wideBlock['title']));
     }
-    
-    foreach ($smallBlocks as $smallBlock)
+
+    foreach ($wideBlocks as $wideBlock)
     {
-        $block = $this->newObject('block_'.$smallBlock['blockname'], $smallBlock['moduleid']);
+        $block = $this->newObject('block_'.$wideBlock['blockname'], $wideBlock['moduleid']);
         $title = $block->title;
-        
-        $smallBlocksDropDown->addOption('block|'.$smallBlock['blockname'].'|'.$smallBlock['moduleid'], htmlentities($title));
+
+        $wideBlocksDropDown->addOption('block|'.$wideBlock['blockname'].'|'.$wideBlock['moduleid'], htmlentities($title));
     }
-    
-    $rightBlocks = $smallBlocksDropDown->show();
-    
-    $button = new button ('addrightblock', $objLanguage->languageText('mod_prelogin_addblock', 'system', 'Add Block'));
-    $button->cssId = 'rightbutton';
-    
-    $rightButton = $button->show();
-    
-    
-    $editOnButton = new button ('editonbutton', $objLanguage->languageText('mod_context_turneditingon', 'context', 'Turn Editing On'));
-    $editOnButton->cssId = 'editmodeswitchbutton';
-    $editOnButton->setOnClick("switchEditMode();");
+
+ 
+$rightBlocks = $rightSmallBlocksDropDwon->show();
+$leftBlocks = $leftSmallBlocksDropDwon->show();
+
+$button = new button ('addrightblock', $objLanguage->languageText('mod_prelogin_addblock', 'system', 'Add Block'));
+$button->cssId = 'rightbutton';
+$rightButton = $button->show();
+
+$button = new button ('addleftblock', $objLanguage->languageText('mod_prelogin_addblock', 'system', 'Add Block'));
+$button->cssId = 'leftbutton';
+$leftButton = $button->show();
+
+
+
+$editOnButton = new button ('editonbutton', $objLanguage->languageText('mod_context_turneditingon', 'context', 'Turn Editing On'));
+$editOnButton->cssId = 'editmodeswitchbutton';
+$editOnButton->setOnClick("switchEditMode();");
 }
 
 
 $right = '';
 
 if (isset($rightContent)) {
-    $right .= $rightContent;
+$right .= $rightContent;
 }
 
 if ($objUser->isAdmin()) {
-    $right .= '<div id="editmode">'.$editOnButton->show().'</div>';
+$right .= '<div id="editmode">'.$editOnButton->show().'</div>';
 }
 
 
 $right .= '<div id="rightblocks">'.$rightBlocksStr.'</div>';
 
-if ($objUser->isAdmin()) {
-    
-    $header = new htmlheading();
-    $header->type = 3;
-    $header->str = $objLanguage->languageText('mod_context_addablock', 'context', 'Add a Block');
+$left = '';
 
-    $right .= '<div id="rightaddblock">'.$header->show().$rightBlocks;
-    $right .= '<div id="rightpreview"><div id="rightpreviewcontent"></div> '.$rightButton.' </div>';
-    $right .= '</div>';
+if (isset($leftContent)) {
+$left .= $leftContent;
 }
 
+
+
+$left .= '<div id="leftblocks">'.$leftBlocksStr.'</div>';
+
+if ($objUser->isAdmin()) {
+
+$header = new htmlheading();
+$header->type = 3;
+$header->str = $objLanguage->languageText('mod_context_addablock', 'context', 'Add a Block');
+
+$right .= '<div id="rightaddblock">'.$header->show().$rightBlocks;
+$right .= '<div id="rightpreview"><div id="rightpreviewcontent"></div> '.$rightButton.' </div>';
+$right .= '</div>';
+
+$left .= '<div id="leftaddblock">'.$header->show().$leftBlocks;
+$left .= '<div id="leftpreview"><div id="leftpreviewcontent"></div> '.$leftButton.' </div>';
+$left .= '</div>';
+
+
+}
+
+$button = new button ('addmiddleblock', $objLanguage->languageText('mod_prelogin_addblock', 'system', 'Add Block'));
+$button->cssId = 'middlebutton';
+$objContextBlocks = $this->getObject('dbcontextblocks', 'context');
+$middleBlocks = $objContextBlocks->getContextBlocks($this->contextCode, 'middle');
+
+$cssLayout->middleColumnContent = '<div id="middleblocks">'.$middleBlocks.'</div>';
+
+if ($objUser->isAdmin()) {
+    $cssLayout->middleColumnContent .= '<div id="middleaddblock">'.$wideBlocksDropDown->show();
+    $cssLayout->middleColumnContent .= '<div id="middlepreview"><div id="middlepreviewcontent"></div> '.$button->show().' </div>';
+    $cssLayout->middleColumnContent .= '</div>';
+}
 $cssLayout = $this->getObject('csslayout', 'htmlelements');
-$cssLayout->setLeftColumnContent($leftContent);
+$cssLayout->setLeftColumnContent($left);
 $cssLayout->setMiddleColumnContent($this->getContent());
 
 if ($right == '<div id="rightblocks"></div>') {
-    $cssLayout->setNumColumns(2);
+$cssLayout->setNumColumns(2);
 } else {
-    $cssLayout->setNumColumns(3);
-    $cssLayout->setRightColumnContent($right);
+$cssLayout->setNumColumns(3);
+$cssLayout->setRightColumnContent($right);
 }
 
 echo $cssLayout->show();
