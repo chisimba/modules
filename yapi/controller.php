6f4c3585d63e93cf479d43a7cccd16130de96b5e
paulscott <unknown@unknown.com>
2009-05-05 08:53:29 +0000

    "abstracted to usable classes"

--- yapi/controller.php
+++ yapi/controller.php
@@ -53,87 +53,18 @@ $GLOBALS ['kewl_entry_point_run']) {
  * @link      http://avoir.uwc.ac.za
  */
 class yapi extends controller {
-    public $objLanguage;
-    public $objConfig;
-    public $objSysConfig;
-
-    private $ysession;
-    private $consumerKey;
-    private $consumerKeySecret;
-    private $applicationId;
-
-    public $yuser;
-    public $profile;
-    public $presence;
-    public $connections;
-    public $updates;
-    public $connectionUpdates;
-
+    public $objOps;
 
     public function init() {
-        include ($this->getResourcePath ( 'lib/Yahoo.inc', 'yapi' ));
-        $this->objLanguage = $this->getObject('language', 'language');
-        $this->objConfig = $this->getObject('altconfig', 'config');
-        // Get the sysconfig variables for the Yahoo API user to set up the connection.
-        $this->objSysConfig = $this->getObject ( 'dbsysconfig', 'sysconfig' );
-
-        // All these need to come from dbsysconfig...
-        // Make sure you obtain application keys before continuing by visiting:
-        // http://developer.yahoo.com/dashboard/
-
-        // Your consumer key goes here.
-        $this->consumerKey = $this->objSysConfig->getValue('consumer_key', 'yapi');
-
-        // Your consumer key secret goes here.
-        $this->consumerKeySecret = $this->objSysConfig->getValue('consumer_secret', 'yapi');
-
-        // Your application ID goes here.
-        $this->applicationId = $this->objSysConfig->getValue('application_id', 'yapi');
-
-        // Get a session first. If the viewer isn't sessioned yet, this call
-        // will redirect them to log in and authorize your application to
-        $this->ysession = YahooSession::requireSession($this->consumerKey, $this->consumerKeySecret,
-        $this->applicationId);
-
-        // Get the currently sessioned user. That means the user who is
-        // currently viewing this page.
-        $this->yuser = $this->ysession->getSessionedUser();
-
-        // Load the profile for the current user.
-        $this->profile = $this->yuser->loadProfile();
-
-        // Fetch the presence for the current user.
-        $this->presence = $this->yuser->getPresence();
-
-        // Access the connection list for the current user.
-        $start = 0; $count = 100; $total = 0;
-        $this->connections = $this->yuser->getConnections($start, $count, $total);
-
-        // Retrieve the updates for the current user.
-        $this->updates = $this->yuser->listUpdates();
-
-        // Retrieve the updates for the connections of the current user.
-        $this->connectionUpdates = $this->yuser->listConnectionUpdates();
+        $this->objOps = $this->getObject('yapiops');
     }
 
     public function dispatch($action = NULL) {
         switch ($action) {
-            case "view" :
-                // Do nothing
-                // TODO: do something here...
-                break;
-
             default :
-                echo "Profile: ".$this->print_html($this->profile)."<br />";
-                echo "Presence ".$this->print_html($this->presence)."<br />";
-                echo "Connections: ".$this->print_html($this->connections)."<br />";
-                echo "<h2>Updates</h2>"."<br />";
-                echo "<h3>Yours</h3>"."<br />";
-                echo $this->print_html($this->updates)."<br />";
-                echo "<h3>Your Connections</h3>"."<br />";
-                echo $this->print_html($this->connectionUpdates)."<br />";
-
-                die();
+                $query = $this->getParam('yquery', "select * from social.profile where guid=me");
+                $res = $this->objOps->executeYQL($query);
+                echo $this->objOps->debugOutput($res);
                 break;
         }
     }
