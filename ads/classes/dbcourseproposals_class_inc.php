cabb7c59f22642eb75eec2dde6d2f8d4dd4aa4cc
David Wafula <davidwaf@gmail.com>
2009-06-25 07:44:48 +0000

    added form2 implementation

---
+++ ads/classes/dbcourseproposals_class_inc.php
@@ -0,0 +1,46 @@
+<?php
+/* 
+ * Responsibl for insterting, updating and deleting course propaosals table
+ */
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']){
+    die('You cannot view this page directly');
+}
+class dbcourseproposals extends dbTable{
+
+    public function init()
+    {
+        parent::init('tbl_course_proposals');  //super
+        $this->table = 'tbl_course_proposals';
+        $this->objUser = $this->getObject ( 'user', 'security' );
+
+    }
+
+    public function addCourseProposal($title){
+
+        $data = array(
+            'title' => $title,
+            'userid' => $this->objUser->userId(),
+            'creation_date' => strftime('%Y-%m-%d %H:%M:%S', mktime()),
+            'status' =>'0',
+        );
+
+        $courseProposalId = $this->insert($data);
+        return $courseProposalId;
+    }
+
+    public function getCourseProposals($userid)
+    {
+        $sql="select * from " .$this->table;//." where userid = '".$userid."'";
+        $rows=$this->getArray($sql);
+        return $rows;
+    }
+
+    public function getCourseProposal($id)
+    {
+        $sql="select * from " .$this->table." where id = '".$id."'";
+        $rows=$this->getArray($sql);
+        return $rows;
+    }
+}
+?>
