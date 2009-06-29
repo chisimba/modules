e5de953939139f82b82fbd3ec411a24dc81fe24f
pwando <unknown@unknown.com>
2009-06-29 13:10:11 +0000

    added a class that contains general worksheet functions

--- worksheet/classes/functions_worksheet_class_inc.php
+++ worksheet/classes/functions_worksheet_class_inc.php
@@ -22,10 +22,13 @@ class functions_worksheet extends object
     public function init()
     {
     	$this->objWorksheet =& $this->getObject('dbworksheet', 'worksheet');	
+    	$this->objWorksheetResults =& $this->getObject('dbworksheetresults', 'worksheet');	
 	$this->loadClass('htmlheading', 'htmlelements');
 	$this->loadClass('link', 'htmlelements');
         $this->objLanguage = &$this->getObject('language', 'language');
         $this->objContext = &$this->newObject('dbcontext', 'context');
+        $this->objIcon= $this->newObject('geticon','htmlelements');
+	$objPopup=&$this->loadClass('windowpop','htmlelements');
     }
     
     /**
@@ -34,27 +37,8 @@ class functions_worksheet extends object
      *@param string $contextCode
      * @return array
      */
-    public function displayWorksheets($contextCode)
+    public function displayWorksheets($contextCode, $userId=Null)
     { 
-/*          
-		$bigArr = array();
-        
-        $worksheets = $this->objWorksheet->getWorksheetsInContext($contextCode);	
-		//print_r($worksheets);
- 		foreach ($worksheets as $worksheet)
-       {
-              $newArr = array();    
-              $newArr['menutext'] = $worksheet['name'];
-              $newArr['description'] = $worksheet['description'];
-              $newArr['itemid'] = $worksheet['id'];
-              $newArr['moduleid'] = 'worksheet';
-              $newArr['params'] = array('action' => 'selectforanswer','id'=>$worksheet['id']);
-              $bigArr[] = $newArr;
-              //echo $bigArr;
-        }
-          
-        return $bigArr;
-*/
 	//Get worksheets
 	$worksheets = $this->objWorksheet->getWorksheetsInContext($contextCode);
 
@@ -70,25 +54,54 @@ class functions_worksheet extends object
 	    $table->endRow();
 	} else {
 		$table->startHeaderRow();
-		    $table->addHeaderCell($this->objLanguage->languageText('mod_worksheet_worksheetname', 'worksheet', 'Worksheet Name'));
-		    $table->addHeaderCell($this->objLanguage->languageText('mod_worksheet_questions', 'worksheet', 'Questions'));
-		    $table->addHeaderCell($this->objLanguage->languageText('mod_worksheet_activitystatus', 'worksheet', 'Activity Status'));
-		    $table->addHeaderCell($this->objLanguage->languageText('mod_worksheet_percentage', 'worksheet', 'Percentage'));
-		    $table->addHeaderCell($this->objLanguage->languageText('mod_worksheet_totalmark', 'worksheet', 'Total Mark'));
-		    $table->addHeaderCell($this->objLanguage->languageText('mod_worksheet_closingdate', 'worksheet', 'Closing Date'));
+		    $table->addHeaderCell($this->objLanguage->languageText('mod_worksheet_worksheetname', 'worksheet', 'Worksheet Name'),'20%');
+		    $table->addHeaderCell($this->objLanguage->languageText('mod_worksheet_questions', 'worksheet', 'Questions'),'13%');
+		    $table->addHeaderCell($this->objLanguage->languageText('mod_worksheet_closingdate', 'worksheet', 'Closing Date'),'15%');
+		    $table->addHeaderCell($this->objLanguage->languageText('mod_worksheet_datecompleted', 'worksheet', 'Date Completed'),'15%');
+		    $table->addHeaderCell($this->objLanguage->languageText('mod_worksheet_mark', 'worksheet', 'Mark'),'10%');
+		    $table->addHeaderCell($this->objLanguage->languageText('word_view', 'worksheet', 'View'),'13%');
 		$table->endHeaderRow();		
 		foreach ($worksheets as $worksheet)
 		{
-		    $table->startRow();
-			$link = new link ($this->uri(array('action'=>'worksheetinfo', 'id'=>$worksheet['id'])));
-			$link->link = $worksheet['name'];
-			$table->addCell($link->show());
-			$table->addCell($worksheet['questions']);
-			$table->addCell($this->objWorksheet->getStatusText($worksheet['activity_status']));
-			$table->addCell($worksheet['percentage']);
-			$table->addCell($worksheet['total_mark']);
-			$table->addCell($worksheet['closing_date']);
-		    $table->endRow();
+		 $wkshtResults = $this->objWorksheetResults->getWorksheetResult($userId, $worksheet['id']);
+			 if($wkshtResults['userid']==$userId){
+			 if(!empty($wkshtResults['mark'])){
+			   $theMark = round(($wkshtResults['mark']/$worksheet['total_mark']*100),2)." %";
+			 }else{
+			   $theMark = "";
+			 }
+			    $table->startRow();
+				$link = new link ($this->uri(array('action'=>'worksheetinfo', 'id'=>$worksheet['id'])));
+				$link->link = $worksheet['name'];
+				if(!empty($theMark)){
+					$this->objIcon->title=$this->objLanguage->languageText('word_view', 'worksheet', 'View');
+				    	$this->objIcon->setIcon('comment_view');
+				   	$commentIcon = $this->objIcon->show();
+
+					$objPopup = new windowpop();
+				    	$objPopup->set('location',$this->uri(array('action'=>'viewworksheet','id'=>$worksheet['id']),"eportfolio"));
+				    	$objPopup->set('linktext',$commentIcon);
+				    	$objPopup->set('width','800');
+				    	$objPopup->set('height','400');
+				    	$objPopup->set('left','200');
+				    	$objPopup->set('top','200');
+				    	$objPopup->set('scrollbars','yes');
+				    	$objPopup->set('resizable','yes');
+				    	$objPopup->putJs(); // you only need to do this once per page
+				}
+				
+				$table->addCell($worksheet['name']);
+				$table->addCell($worksheet['questions']);
+				$table->addCell($worksheet['closing_date']);
+				$table->addCell($wkshtResults['last_modified']);
+				$table->addCell($theMark);
+				if(!empty($theMark)){
+				  $table->addCell('<p>'.$objPopup->show().'</p>');
+				}else{
+				  $table->addCell("&nbsp;");
+				}
+			    $table->endRow();
+			}
 		}
      }
 	    return $header->show().$table->show();
