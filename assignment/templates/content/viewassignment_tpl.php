c3a69390170ffe2b27638d3ead5ece64777c7dc0
James Scoble <jscoble@uwc.ac.za>
2009-07-07 12:18:42 +0000

    added .docx support

--- assignment/templates/content/viewassignment_tpl.php
+++ assignment/templates/content/viewassignment_tpl.php
@@ -221,7 +221,7 @@ if ($this->isValid('markassignments')) {
             $form->extra = 'enctype="multipart/form-data"';
             
             $objUpload = $this->newObject('uploadinput', 'filemanager');
-            $objUpload->restrictFileList = array('doc', 'odt', 'rtf', 'txt');
+            $objUpload->restrictFileList = array('doc', 'odt', 'rtf', 'txt', 'docx');
             
             $button = new button('submitform', $this->objLanguage->languageText('mod_assignment_uploadassignment', 'assignment', 'Upload Assignment'));
             $button->setToSubmit();;
@@ -243,7 +243,7 @@ if ($this->isValid('markassignments')) {
             $objSelectFile = $this->newObject('selectfile', 'filemanager');
             
             $objSelectFile->name = 'assignment';
-            $objSelectFile->restrictFileList = array('doc', 'odt', 'rtf', 'txt');
+            $objSelectFile->restrictFileList = array('doc', 'odt', 'rtf', 'txt', 'docx');
             
             $button = new button('submitform', 'Submit Assignment');
             $button->setToSubmit();;
