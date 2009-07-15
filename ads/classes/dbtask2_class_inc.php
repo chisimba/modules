81a4194da5252dec8280619fbb73707ca51b582e
Gorthaur <unknown@unknown.com>
2009-07-15 19:38:24 +0000

    Experienced some problems, so updating again.

--- ads/classes/dbtask2_class_inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-        class dbtask2 extends dbtable {
-                public function init() {
-                        parent::init("tbl_tabletask2E");
-                        parent::init("tbl_tabletask2F");
-                }// end init()
-        } // end class dbtest
-
-?>
