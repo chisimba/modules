b285598485ab844a220e0f86e2b1bf812fdbd26a
David Wafula <davidwaf@gmail.com>
2009-07-16 11:26:51 +0000

    modified scripts for extj proposal status (nguni)

--- ads/resources/scripts/check-radio.js
+++ ads/resources/scripts/check-radio.js
@@ -8,26 +8,8 @@ Ext.onReady(function(){
 
     Ext.QuickTips.init();
     
-    // turn on validation errors beside the field globally
     Ext.form.Field.prototype.msgTarget = 'side';
 
-
-    /*====================================================================
-     * Individual checkbox/radio examples
-     *====================================================================*/
-    
-    // Using checkbox/radio groups will generally be easier and more flexible than
-    // using individual checkbox and radio controls, but this shows that you can
-    // certainly do so if you only need a single control, or if you want to control  
-    // exactly where each check/radio goes within your layout.
-    
-    /*====================================================================
-     * RadioGroup examples
-     *====================================================================*/
-    // NOTE: These radio examples use the exact same options as the checkbox ones
-    // above, so the comments will not be repeated.  Please see comments above for
-    // additional explanation on some config options.
-    
     var radioGroup = {
             xtype: 'radiogroup',
             itemCls: 'x-check-group-alt',
@@ -58,27 +40,12 @@ Ext.onReady(function(){
         buttons: [{
             text: 'Save',
             handler: function(){
-                //var O = this.ownerCt;
                 if (fp.getForm().isValid()) {
                     if (fp.url)
                         fp.getForm().getEl().dom.action = fp.url + "&id=" + id;
-                    /*if (fp.baseParams) {
-                        for (i in fp.baseParams) {
-                            fp.add({
-                                xtype: 'hidden',
-                                name: i,
-                                value: fp.baseParams[i]
-                            })
-                        }
-                        fp.doLayout();
-                    }*/
+                    
                     fp.getForm().submit();
-                }
-
-               /*if(fp.getForm().isValid()){
-                   Ext.Msg.alert('Submitted Values', 'The following will be sent to the server: <br />'+
-                        fp.getForm().getValues(true).replace(/&/g,', '));
-                }*/
+                }   
             }
         },{
             text: 'Reset',
