f726e34e4174c71dab466739fc68308889dff0ab
David Wafula <davidwaf@gmail.com>
2009-07-17 06:16:11 +0000

    script for calculating hours updated (nguni)

--- ads/resources/scripts/calc.js
+++ ads/resources/scripts/calc.js
@@ -1,108 +1,100 @@
 /* javascript */
 jQuery(document).ready(function() {
-       
+   
+   // on page load, do calculations
+   checkInput5();
+   checkInput6();
+   checkInput8();
+   checkInput9();
+
+   jQuery("#input_D5_1").blur(function() {
+       checkInput5();
+       checkInput6();
+   });
+   jQuery("#input_D5_2").blur(function() {
+       checkInput5();
+       checkInput6();
+   });
+   jQuery("#input_D5_3").blur(function() {
+       checkInput5();
+   });
+   jQuery("#input_D5_4").blur(function() {
+       checkInput5();
+   });
    jQuery("#input_D5_5").blur(function() {
-       // check all the input fields from 1 to 8
+       checkInput5();
+   });
+
+   jQuery("#input_D5_6").blur(function() {
+       checkInput6();
+   });
+   
+   jQuery("#input_D5_7").blur(function() {
+       checkInput8();
+   });
+   jQuery("#input_D5_8").blur(function() {
+       checkInput8();
+   });
+   jQuery("#input_D5_9").blur(function(){
+       checkInput9();
+   });
+});
+
+function checkInput5() {
+   /*
+    * state = 0: action on page load
+    * state = 1: action on input blur
+    */
+    // check all the input fields from 1 to 8
        var inp1 = parseInt(jQuery("#input_D5_1").val()),
            inp2 = parseInt(jQuery("#input_D5_2").val()),
            inp3 = parseInt(jQuery("#input_D5_3").val()),
            inp4 = parseInt(jQuery("#input_D5_4").val()),
            inp5 = parseInt(jQuery("#input_D5_5").val());
 
-       if(isNaN(inp1) || inp1.length == 0) {
-           alert("Please enter a number for a.");
-           jQuery("#input_D5_1").focus();
-       }
-       else if (isNaN(inp2) || inp2.length == 0) {
-           alert("Please enter a number for b.");
-           jQuery("#input_D5_2").focus();
+       if(isNaN(inp1)) {inp1 = 0;}
+       if(isNaN(inp2)) {inp2 = 0;}
+       if(isNaN(inp3)) {inp3 = 0;}
+       if(isNaN(inp4)) {inp4 = 0;}
+       if(isNaN(inp5)) {inp5 = 0;}
+       // do the calculations
+       var totalContactTime = inp1*(inp2 + inp3 + inp4 + inp5);
+       jQuery("#totContTime").text(totalContactTime);
+}
 
-       }
-       else if (isNaN(inp3) || inp3.length == 0) {
-           alert("Please enter a number for c.");
-           jQuery("#input_D5_3").focus();
+function checkInput6() {
+    var inp1 = parseInt(jQuery("#input_D5_1").val()),
+        inp2 = parseInt(jQuery("#input_D5_2").val()),
+        inp6 = parseInt(jQuery("#input_D5_6").val());
 
-       }
-       else if (isNaN(inp4) || inp4.length == 0) {
-           alert("Please enter a number for d.");
-           jQuery("#input_D5_4").focus();
+    if(isNaN(inp1)) {inp1 = 0;}
+    if(isNaN(inp2)) {inp2 = 0;}
+    if(isNaN(inp6)) {inp6 = 0;}
+    // calculate the total notional study
+    var totNotStudy = inp1*inp2*inp6;
+    jQuery("#totNotTime").text(totNotStudy);
+}
+       
+function checkInput8() {
+    var inp7 = parseFloat(jQuery("#input_D5_7").val()),
+        inp8 = parseFloat(jQuery("#input_D5_8").val());
 
-       }
-       else if (isNaN(inp5) || inp5.length == 0) {
-           alert("Please enter a number for e.");
-           jQuery("#input_D5_5").focus();
+    if(isNaN(inp7)) {inp7 = 0;}
+    if(isNaN(inp8)) {inp8 = 0;}
+    
+    // calculate the total notional study
+    var totExamTime = inp7*inp8;
+    jQuery("#totExamTime").text(totExamTime);
+}
 
-       }
-       else {
-           // do the calculations
-           var totalContactTime = inp1*(inp2 + inp3 + inp4 + inp5);
-           jQuery("#totContTime").text(totalContactTime);
-       }
+function checkInput9() {
+    // get the total notional hours (excluding studying)
+    var totNotHrs1 = parseInt(jQuery("#totNotTime").text()),
+        totNotHrs2 = parseInt(jQuery("#input_D5_9").val());
 
-   });
+    if(isNaN(totNotHrs1)) {totNotHrs1 = 0;}
+    if(isNaN(totNotHrs2)) {totNotHrs2 = 0;}
 
-   jQuery("#input_D5_6").blur(function() {
-       var inp1 = parseInt(jQuery("#input_D5_1").val()),
-           inp2 = parseInt(jQuery("#input_D5_2").val()),
-           inp6 = parseInt(jQuery("#input_D5_6").val());
-
-      if(isNaN(inp1) || inp1.length == 0) {
-           alert("Please enter a number for a.");
-           jQuery("#input_D5_1").focus();
-       }
-       else if (isNaN(inp2) || inp2.length == 0) {
-           alert("Please enter a number for b.");
-           jQuery("#input_D5_2").focus();
-
-       }
-       else if (isNaN(inp6) || inp6.length == 0) {
-           alert("Please enter a number for f.");
-           jQuery("#input_D5_6").focus();
-
-       }
-       else {
-           // calculate the total notional study
-           var totNotStudy = inp1*inp2*inp6;
-           jQuery("#totNotTime").text(totNotStudy);
-       }
-   });
-
-   jQuery("#input_D5_8").blur(function() {
-       var inp7 = parseFloat(jQuery("#input_D5_7").val()),
-           inp8 = parseFloat(jQuery("#input_D5_8").val());
-
-      if(isNaN(inp7) || inp7.length == 0) {
-           alert("Please enter a number for g.");
-           jQuery("#input_D5_7").focus();
-       }
-       else if ( isNaN(inp8) || inp8.length == 0) {
-           alert("Please enter a number for h.");
-           jQuery("#input_D5_8").focus();
-
-       }
-       else {
-           // calculate the total notional study
-           var totExamTime = inp7*inp8;
-           jQuery("#totExamTime").text(totExamTime);
-       }
-   });
-
-   jQuery("#input_D5_9").blur(function(){
-       // get the total notional hours (excluding studying)
-       var totNotHrs1 = parseInt(jQuery("#totNotTime").text()),
-           totNotHrs2 = parseInt(jQuery("#input_D5_9").val());
-
-       if(isNaN(totNotHrs1) || totNotHrs1.length == 0) {
-           alert("Total Notional Hours excluding exams have not yet been calculated.");
-       }
-       else if(isNaN(totNotHrs2) || totNotHrs2.length == 0) {
-           alert("Please enter a number for i.");
-           jQuery("#input_D5_9").focus();
-       }
-       else {
-           // calculate the total notional hours and display on form
-           jQuery("#totNotTime2").text(totNotHrs1 + totNotHrs2);
-       }
-   });
-});
-
+    var totNotHrs = totNotHrs1 + totNotHrs2;
+    jQuery("#totNotTime2").text(totNotHrs);
+}
