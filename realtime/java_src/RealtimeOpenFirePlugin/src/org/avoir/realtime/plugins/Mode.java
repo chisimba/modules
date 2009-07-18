5ebb5c16022fab9b80ec0f03989c1cf3c1323238
David Wafula <davidwaf@gmail.com>
2009-07-18 18:38:02 +0000

    added handler for mic holders

--- realtime/java_src/RealtimeOpenFirePlugin/src/org/avoir/realtime/plugins/Mode.java
+++ realtime/java_src/RealtimeOpenFirePlugin/src/org/avoir/realtime/plugins/Mode.java
@@ -121,5 +121,5 @@ public class Mode {
     public static final String REQUEST_ROOM_MEMBERS= "request-room-members";
     public static final String ROOM_MEMBERS= "room-members";
     public static final String ITEM_HISTORY_FROM_SERVER = "item-history-from-server";
-
+    public static final String MIC_HOLDER = "mic-holder";
 }
