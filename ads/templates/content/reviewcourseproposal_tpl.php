5bc31dbd93c3dd9e9a34d6acdbc9d8cdd991bb3f
jerezar <unknown@unknown.com>
2009-07-20 09:23:38 +0000

    Added ext style form

--- ads/templates/content/reviewcourseproposal_tpl.php
+++ ads/templates/content/reviewcourseproposal_tpl.php
@@ -1,4 +1,36 @@
 <?php
+ $extbase = '<script language="JavaScript" src="'.$this->getResourceUri('scripts/ext-base.js').'" type="text/javascript"></script>';
+    $extall = '<link rel="stylesheet" type="text/css" href="'.$this->getResourceUri('scripts/ext-all.css').'"/>';
+    $extalldebug = '<script language="JavaScript" src="'.$this->getResourceUri('scripts/ext-all-debug.js').'" type="text/javascript"></script>';
+    $mainjs = '<script language="JavaScript" src="'.$this->getResourceUri('scripts/review.js').'" type="text/javascript"></script>';
+    $styleSheet="
+    <style type=\"text/css\">
+        .x-check-group-alt {
+            background: #D1DDEF;
+            border-top:1px dotted #B5B8C8;
+            border-bottom:1px dotted #B5B8C8;
+        }
+        div#myForm {
+            padding: 0 3em;
+        }
+    </style>
+    ";
+
+    //append to the top of the page
+    $this->appendArrayVar('headerParams', $extbase);
+    $this->appendArrayVar('headerParams', $extall);
+    $this->appendArrayVar('headerParams', $extalldebug);
+    $this->appendArrayVar('headerParams', $mainjs);
+    $this->appendArrayVar('headerParams', $styleSheet);
+
+    // display the extj radio form
+    echo '<div id="myForm">';
+    echo '<div id="form-ct"></div></div>';
+    echo '<input type="hidden" name="id" id="id" value="'.$this->getParam("id").'">';
+/*
+ *
+ */
+/*
 $this->loadClass('htmltable','htmlelements');
 $this->loadClass('form','htmlelements');
 $this->loadClass('button','htmlelements');
@@ -24,6 +56,7 @@ $objTable->endRow();
 $objTable->row_attributes=' height="10"';
 $buttons='';
 /************** Build form **********************/
+/*
 $saveButton = new button('save',$objLanguage->languageText('mod_ads_savereview','ads'));
 $saveButton->setToSubmit();
 
@@ -53,4 +86,6 @@ $cssLayout->setMiddleColumnContent($rightSideColumn);
 
 //Output the content to the page
 echo $cssLayout->show();
+ * 
+ */
 ?>
