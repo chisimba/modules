7c84bb73f2fe06d0da700a744b358c93172e89a7
David Wafula <davidwaf@gmail.com>
2009-07-20 09:17:47 +0000

    removed duplicates

--- java_src/realtime1.0.2/deploy/deploy.sh
+++
@@ -1,6 +0,0 @@
-#/dwaf/core-software/apache-ant-1.7.1/bin/ant deploy
-cp ../dist/realtime102.jar .
-jarsigner -keystore keystore realtime102.jar  avoir
-#cp lib/realtime-xmpp.jar /var/www/chisimba-realtime-1.0.2/
-cp realtime102.jar /var/www/chisimba-realtime/
-#scp lib/realtime-xmpp.jar  pscott@kim.wits.ac.za:/home/pscott
