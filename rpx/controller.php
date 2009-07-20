8c47ad2bb2155f68fbf827e497c7ab6c42d2361a
charlvn <unknown@unknown.com>
2009-07-20 21:19:55 +0000

    RPX Module: Renamed objConfig property to objAltConfig in controller class.

--- rpx/controller.php
+++ rpx/controller.php
@@ -2,11 +2,11 @@
 
 class rpx extends controller
 {
-    public $objConfig;
+    protected $objAltConfig;
 
     public function init()
     {
-        $this->objConfig = $this->getObject('altconfig', 'config');
+        $this->objAltConfig = $this->getObject('altconfig', 'config');
     }
 
     public function dispatch()
