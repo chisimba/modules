301eeb86ddaae4a411418a87833e29389ef7ee11
charlvn <unknown@unknown.com>
2009-07-20 21:13:58 +0000

    RPX Module: Added objConfig property to controller class.

--- rpx/controller.php
+++ rpx/controller.php
@@ -2,8 +2,11 @@
 
 class rpx extends controller
 {
+    protected $objConfig;
+
     public function init()
     {
+        $this->objConfig = $this->getObject('altconfig', 'config');
     }
 
     public function dispatch()
