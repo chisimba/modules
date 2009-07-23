ef97851d2fe9f336615e99571d62c1ffaaf6fd8f
David Wafula <davidwaf@gmail.com>
2009-07-23 21:27:06 +0000

    downgraded to 2 users to speak simultaneously same time, and assigning admin rights to users

--- realtime/java_src/RealtimeOpenFirePlugin/avoir-realtime/deploy.sh
+++ realtime/java_src/RealtimeOpenFirePlugin/avoir-realtime/deploy.sh
@@ -1,4 +1,3 @@
-cp /dwaf/core-projects/realtime-xmpp/dist/realtime-xmpp.jar /dwaf/core-projects/RealtimeOpenFirePlugin/avoir-realtime/lib
 cp ../dist/RealtimeOpenFirePlugin.jar /dwaf/core-projects/RealtimeOpenFirePlugin/avoir-realtime/lib
 jar cvf avoirrealtime.jar .
 cp avoirrealtime.jar /dwaf/core-software/openfire/plugins/
