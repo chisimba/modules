316eaa9948b431100b2383c4fa1ad957f73925d2
charlvn <unknown@unknown.com>
2009-07-28 12:24:28 +0000

    RPX Module: Various modifications to the db class.

--- rpx/classes/dbrpxidentifiers_class_inc.php
+++ rpx/classes/dbrpxidentifiers_class_inc.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * Location IM dbtable derived class
+ * RPX Identifier dbtable derived class.
  * 
- * Class to interact with the database for the location module
+ * Class to interact with the database for the RPX module.
  * 
  * PHP version 5
  * 
@@ -21,34 +21,28 @@
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  * 
  * @category  chisimba
- * @package   location
- * @author    Charl van Niekerk <charlvn@charlvn.za.net>
- * @copyright 2008 Charl van Niekerk
+ * @package   rpx
+ * @author    Charl van Niekerk <charlvn@charlvn.com>
+ * @copyright 2009 Charl van Niekerk.
  * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
- * @version   $Id: dblocation_class_inc.php 11975 2008-12-29 22:10:39Z charlvn $
+ * @version   $Id$
  * @link      http://avoir.uwc.ac.za
  * @see       api
  */
-class dblocation extends dbTable 
+class dbrpxidentifiers extends dbTable 
 {
     protected $objUser;
     protected $userId;
 
     private $id;
-    private $longitude;
-    private $latitude;
-    private $name;
-    private $fireEagleToken;
-    private $fireEagleSecret;
-    private $twitter;
+    private $identifier;
 
     /**
-     * Constructor
-     *
+     * The standard class constructor.
      */
     public function init()
     {
-        parent::init('tbl_location');
+        parent::init('tbl_rpx_identifiers');
 
         $this->objUser = $this->getObject('user', 'security');
         $this->userId = $this->objUser->userId();
@@ -61,92 +55,26 @@ class dblocation extends dbTable
 
     private function populate($row) {
         if ($row && is_array($row) && count($row)) {
-            $this->id = $row['id'];
-            $this->longitude = $row['longitude'];
-            $this->latitude = $row['latitude'];
-            $this->name = $row['name'];
-            $this->fireEagleToken = $row['fireeagle_token'];
-            $this->fireEagleSecret = $row['fireeagle_secret'];
-            $this->twitter = (bool) $row['twitter'];
+            $this->id         = $row['id'];
+            $this->identifier = $row['identifier'];
         }
     }
 
-    public function getLongitude()
+    public function getIdentifier()
     {
-        return $this->longitude;
+        return $this->identifier;
     }
 
-    public function setLongitude($longitude)
+    public function setIdentifier($identifier)
     {
-        $this->longitude = $longitude;
-    }
-
-    public function getLatitude()
-    {
-        return $this->latitude;
-    }
-
-    public function setLatitude($latitude)
-    {
-        $this->latitude = $latitude;
-    }
-
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
-    public function getFireEagleToken()
-    {
-        return $this->fireEagleToken;
-    }
-
-    public function setFireEagleToken($fireEagleToken)
-    {
-        $this->fireEagleToken = $fireEagleToken;
-    }
-
-    public function getFireEagleSecret()
-    {
-        return $this->fireEagleSecret;
-    }
-
-    public function setFireEagleSecret($fireEagleSecret)
-    {
-        $this->fireEagleSecret = $fireEagleSecret;
-    }
-
-    public function getTwitter()
-    {
-        return $this->twitter;
-    }
-
-    public function setTwitter($twitter)
-    {
-        $this->twitter = $twitter;
-    }
-
-    public function loadByFireEagleToken($token)
-    {
-        $row = $this->getRow('fireeagle_token', $token);
-        $this->populate($row);
+        $this->identifier = $identifier;
     }
 
     public function put()
     {
         $row = array();
-        $row['userid'] = $this->userId;
-        $row['longitude'] = $this->longitude;
-        $row['latitude'] = $this->latitude;
-        $row['name'] = $this->name;
-        $row['fireeagle_token'] = $this->fireEagleToken;
-        $row['fireeagle_secret'] = $this->fireEagleSecret;
-        $row['twitter'] = (int) $this->twitter;
+        $row['userid']     = $this->userId;
+        $row['identifier'] = $this->identifier;
         if ($this->id) {
             $this->update('id', $this->id, $row);
         } else {
@@ -154,5 +82,3 @@ class dblocation extends dbTable
         }
     }
 }
-
-?>
