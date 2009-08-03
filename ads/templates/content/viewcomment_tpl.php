6360c1267a4652421b00001019dd85abe9d02359
nguni52 <unknown@unknown.com>
2009-08-03 10:11:04 +0000

    added view comment template page

---
+++ ads/templates/content/viewcomment_tpl.php
@@ -0,0 +1,56 @@
+<?php
+
+    // security check - must be included in all scripts
+    if (!$GLOBALS['kewl_entry_point_run'])
+    {
+        die("You cannot view this page directly");
+    }
+    // end security check
+
+    // scripts for extjs
+    $extbase = '<script language="JavaScript" src="'.$this->getResourceUri('ext-3.0-rc2/adapter/ext/ext-base.js','htmlelements').'" type="text/javascript"></script>';
+    $extalljs = '<script language="JavaScript" src="'.$this->getResourceUri('ext-3.0-rc2/ext-all.js','htmlelements').'" type="text/javascript"></script>';
+    $extallcss = '<link rel="stylesheet" type="text/css" href="'.$this->getResourceUri('ext-3.0-rc2/resources/css/ext-all.css','htmlelements').'"/>';
+    
+    $this->appendArrayVar('headerParams', $extbase);
+    $this->appendArrayVar('headerParams', $extalljs);
+    $this->appendArrayVar('headerParams', $extallcss);
+
+    // Create an instance of the css layout class
+    $cssLayout = & $this->newObject('csslayout', 'htmlelements');// Set columns to 2
+    $cssLayout->setNumColumns(2);
+
+    $postLoginMenu  = $this->newObject('postloginmenu','toolbar');
+    $cssLayout->setLeftColumnContent($postLoginMenu->show());
+    $rightSideColumn =  '<div id="commentsBody"></div>';
+    $cssLayout->setMiddleColumnContent($rightSideColumn);
+
+    echo $cssLayout->show();
+
+    $data = $this->objComment->getComments($this->id, $version);
+    $myRows = $this->objComment->getNumComments($this->id, $version);
+    
+    $myComments = "";
+    $count = 1;
+    foreach($data as $data) {
+        $myComments .= "{title: 'Comment ".$count."', html: '".$data['comment']."'}";
+        if($count != $myRows) {
+            $myComments .= ",";
+        }
+        $count += 1;
+    }
+
+    $mainjs = "Ext.onReady(function(){
+                    var tabs = new Ext.TabPanel({
+                        renderTo: 'commentsBody',
+                        width:450,
+                        height: 200,
+                        activeTab: 0,
+                        frame:true,
+                        defaults:{autoHeight: true},
+                        items:[".$myComments."]
+                    });
+                });";
+
+    echo "<script type=\"text/javascript\">".$mainjs."</script>";
+?>
