bc59fc3ccbf0e90539514b9d52b4b57201aaa2a7
Jeremy O'Connor <joconnor@uwc.ac.za>
2009-08-11 10:04:57 +0000

    Ticket #3430: Student cannot select file in filemanager when submitting an .mp3 file as an assignment.

--- assignment/templates/content/viewassignment_tpl.php
+++ assignment/templates/content/viewassignment_tpl.php
@@ -22,10 +22,10 @@ $header->str = $assignment['name'];
 if ($this->isValid('edit')) {
     $editLink = new link ($this->uri(array('action'=>'edit', 'id'=>$assignment['id'])));
     $editLink->link = $editIcon;
-    
+
     $deleteLink = new link ($this->uri(array('action'=>'delete', 'id'=>$assignment['id'], 'return'=>'view')));
     $deleteLink->link = $deleteIcon;
-    
+
     $header->str .= ' '.$editLink->show().'&nbsp;'.$deleteLink->show();
 }
 
@@ -78,10 +78,10 @@ echo '<hr />'.$htmlHeader->show();
 // If Lecturer, show list of assignments
 if ($this->isValid('markassignments')) {
     $submissions = $this->objAssignmentSubmit->getStudentSubmissions($assignment['id']);
-    
-    
-    
-    
+
+
+
+
     $table = $this->newObject('htmltable', 'htmlelements');
     $table->startHeaderRow();
     $table->addHeaderCell($this->objLanguage->code2Txt('mod_assignment_studname', 'assignment', NULL, '[-readonly-] Name'));
@@ -89,23 +89,23 @@ if ($this->isValid('markassignments')) {
     $table->addHeaderCell($this->objLanguage->languageText('mod_assignment_mark', 'assignment', 'Mark'));
     $table->addHeaderCell($this->objLanguage->languageText('mod_assignment_comment', 'assignment', 'Comment'));
     $table->endHeaderRow();
-    
+
     if (count($submissions) == 0) {
         $table->startRow();
         $table->addCell($this->objLanguage->languageText('mod_assignment_noassignmentssubmitted', 'assignment', 'No Assignments Submitted Yet'), NULL, NULL, NULL, 'noRecordsMessage', ' colspan="4"');
         $table->endRow();
     } else {
-        
+
         foreach ($submissions as $submission)
         {
             $table->startRow();
-            
+
             $link = new link ($this->uri(array('action'=>'viewsubmission', 'id'=>$submission['id'])));
             $link->link = $this->objUser->fullName($submission['userid']);
-            
+
             $table->addCell($link->show());
             $table->addCell($objDateTime->formatDate($submission['datesubmitted']));
-            
+
             if ($submission['mark'] == NULL) {
                 $table->addCell('<em>'.$this->objLanguage->languageText('mod_assignment_notmarked', 'assignment', 'Not Marked').'</em>');
                 $table->addCell('<em>'.$this->objLanguage->languageText('mod_assignment_notmarked', 'assignment', 'Not Marked').'</em>');
@@ -113,24 +113,24 @@ if ($this->isValid('markassignments')) {
                 $table->addCell($submission['mark']);
                 $table->addCell($objTrimStr->strTrim($submission['commentinfo'], 50));
             }
-            
+
             $table->endRow();
         }
     }
-    
+
     echo $table->show();
-    
+
     // Show Student Views
 } else {
-    
+
     $submissions = $this->objAssignmentSubmit->getStudentAssignment($this->objUser->userId(), $assignment['id']);
-    
-    
+
+
     if (count($submissions) == 0) {
-        
+
     } else if (count($submissions) == 0) {
     } else {
-        
+
         $table = $this->newObject('htmltable', 'htmlelements');
         $table->startHeaderRow();
         $table->addHeaderCell($this->objLanguage->languageText('mod_assignment_submissions', 'assignment', 'Submissions'));
@@ -138,7 +138,7 @@ if ($this->isValid('markassignments')) {
         $table->addHeaderCell($this->objLanguage->languageText('mod_assignment_mark', 'assignment', 'Mark'));
         //$table->addHeaderCell($this->objLanguage->languageText('mod_assignment_comment', 'assignment', 'Comment'));
         $table->endHeaderRow();
-        
+
         $objFile = $this->getObject('dbfile', 'filemanager');
   /*
   * Creating the link to view assignment results
@@ -147,21 +147,21 @@ if ($this->isValid('markassignments')) {
         {
 
         $table->startRow();
-            
+
             $link = new link ($this->uri(array('action'=>'viewsubmission', 'id'=>$submission['id'])));
 
-            
+
             $link->link = $this->objLanguage->languageText('mod_assignment_viewscoremark','assignment');
             $table->addCell($link->show());
-               
+
             $table->addCell($submission['datesubmitted']);
-            
+
             if($assignment['closing_date'] > date('Y-m-d H:i')) {
                 $table->addCell('<em>'.$this->objLanguage->languageText('mod_assignment_notmarked', 'assignment', 'Not Marked').'</em>');
                 $table->addCell('<em>'.$this->objLanguage->languageText('mod_assignment_notmarked', 'assignment', 'Not Marked').'</em>');
-                
+
             } else {
-            
+
                 if ($submission['mark'] == NULL) {
                     $table->addCell('<em>'.$this->objLanguage->languageText('mod_assignment_notmarked', 'assignment', 'Not Marked').'</em>');
                     $table->addCell('<em>'.$this->objLanguage->languageText('mod_assignment_notmarked', 'assignment', 'Not Marked').'</em>');
@@ -173,87 +173,87 @@ if ($this->isValid('markassignments')) {
                    // $table->addCell($objTrimStr->strTrim($submission['commentinfo'], 50));
                 }
             }
-            
+
             $table->endRow();
         }
-        
+
         echo $table->show();
     }
-    
-    
-    
-    
+
+
+
+
     if ($this->objAssignmentSubmit->checkOkToSubmit($this->objUser->userId(), $assignment['id'])) {
         $hiddenInput = new hiddeninput('id', $assignment['id']);
-        
-        
+
+
         $header = new htmlHeading();
         $header->type = 1;
         $header->str = $this->objLanguage->languageText('mod_assignment_submitassignment', 'assignment', 'Submit Assignment');
         echo '<hr />'.$header->show();
-        
+
         // Display by Assignment Type
         if($assignment['closing_date'] < date('Y-m-d H:i')){
             echo '<div class="noRecordsMessage">'.$this->objLanguage->languageText('mod_assignment_assignmentclosed', 'assignment', 'Assignment Closed').'</div>';
         } else if ($assignment['format'] == '0') { // Online Assignment
-            
+
             $form = new form ('addassignment', $this->uri(array('action'=>'submitonlineassignment')));
-            
+
             $htmlArea = $this->newObject('htmlarea', 'htmlelements');
             $htmlArea->name = 'text';
             $htmlArea->width = '100%';
-            
+
             $button = new button('submitform', $this->objLanguage->languageText('mod_assignment_submitassignment', 'assignment', 'Submit Assignment'));
             $button->setToSubmit();;
-            
+
             $form->addToForm($hiddenInput->show().$htmlArea->show().'<br />'.$button->show());
-            
+
             echo $form->show();
-            
+
         } else { // Upload Assignment
             $header = new htmlHeading();
             $header->str = $this->objLanguage->languageText('mod_filemanager_uploadnewfile', 'filemanager', $this->objLanguage->languageText('mod_filemanager_uploadnewfile', 'filemanager', 'Upload new file'));
             $header->type = 4;
-            
+
             echo $header->show();
-            
+
             $form = new form('addassignmentbyupload', $this->uri(array('action'=>'uploadassignment')));
             $form->extra = 'enctype="multipart/form-data"';
-            
+
             $objUpload = $this->newObject('uploadinput', 'filemanager');
-            $objUpload->restrictFileList = array('doc', 'odt', 'rtf', 'txt', 'docx');
-            
+            $objUpload->restrictFileList = array('doc', 'odt', 'rtf', 'txt', 'docx', 'mp3');
+
             $button = new button('submitform', $this->objLanguage->languageText('mod_assignment_uploadassignment', 'assignment', 'Upload Assignment'));
             $button->setToSubmit();;
-            
+
             $form->addToForm($hiddenInput->show().$objUpload->show().'<br />'.$button->show());
-            
-            
+
+
             echo $form->show();
-            
+
             $header = new htmlHeading();
             $header->str = $this->objLanguage->languageText('mod_filemanager_chooseexisting', 'filemanager', 'Choose existing file from file manager');
             $header->type = 4;
-            
+
             echo $header->show();
-            
+
             $form = new form('submitassignment', $this->uri(array('action'=>'submitassignment')));
-            
-            
+
+
             $objSelectFile = $this->newObject('selectfile', 'filemanager');
-            
+
             $objSelectFile->name = 'assignment';
-            $objSelectFile->restrictFileList = array('doc', 'odt', 'rtf', 'txt', 'docx');
-            
+            $objSelectFile->restrictFileList = array('doc', 'odt', 'rtf', 'txt', 'docx', 'mp3');
+
             $button = new button('submitform', 'Submit Assignment');
             $button->setToSubmit();;
-            
+
             $form->addToForm($hiddenInput->show().$objSelectFile->show().'<br />'.$button->show());
-            
-            
+
+
             echo $form->show();
         }
-        
+
     }
 }
 
