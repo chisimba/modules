53faf646743017ebcb0cde75714a748956a0e090
nguni52 <unknown@unknown.com>
2009-08-19 15:03:57 +0000

    added course history listing using extjs trees, forms and json

--- ads/classes/getdata_class_inc.php
+++ ads/classes/getdata_class_inc.php
@@ -40,6 +40,30 @@
         id:'ver8',
         leaf:true
     }]
+}, {
+    text:'Control Engineering',
+    expanded: true,
+    children:[{
+        text:'18/08/2009_1',
+        id:'ver1',
+        leaf:true
+    },{
+        text:'18/08/2009_2',
+        id:'ver2',
+        leaf:true
+    },{
+        text:'17/08/2009_1',
+        id:'ver3',
+        leaf:true
+    },{
+        text:'16/08/2009_1',
+        id:'ver4',
+        leaf:true
+    },{
+        text:'15/08/2009_1',
+        id:'ver5',
+        leaf:true
+    }]
 }]";
             
             return $str;
