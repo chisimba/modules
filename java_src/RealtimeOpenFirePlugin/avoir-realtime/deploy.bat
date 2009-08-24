0cbff36ffc05ab636d225c3ae54ea3ee48bb6aeb
David Wafula <davidwaf@gmail.com>
2009-08-24 19:46:19 +0000

    deply script for windows

---
+++ java_src/RealtimeOpenFirePlugin/avoir-realtime/deploy.bat
@@ -0,0 +1,4 @@
+copy ../dist/RealtimeOpenFirePlugin.jar /dwaf/projects/RealtimeOpenFirePlugin/avoir-realtime/lib
+jar cvf avoirrealtime.jar .
+copy avoirrealtime.jar /dwaf/software/openfire/plugins/
+
--- java_src/RealtimeOpenFirePlugin/avoir-realtime/deploy.sh
+++ java_src/RealtimeOpenFirePlugin/avoir-realtime/deploy.sh
@@ -1,4 +1,4 @@
-cp ../dist/RealtimeOpenFirePlugin.jar /dwaf/core-projects/RealtimeOpenFirePlugin/avoir-realtime/lib
+cp ../dist/RealtimeOpenFirePlugin.jar /dwaf/projects/RealtimeOpenFirePlugin/avoir-realtime/lib
 jar cvf avoirrealtime.jar .
-cp avoirrealtime.jar /dwaf/core-software/openfire/plugins/
+cp avoirrealtime.jar /dwaf/software/openfire/plugins/
 #scp /dwaf/core-software/openfire/plugins/avoirrealtime.jar pscott@kim.wits.ac.za:/home/pscott
