f6a7623020846d1a28bf8ef5c077a1ed88eb9fbe
David Wafula <davidwaf@gmail.com>
2009-08-30 18:07:13 +0000

    added paged grid search

--- ads/classes/dbcoursecomments_class_inc.php
+++ ads/classes/dbcoursecomments_class_inc.php
@@ -1,39 +1,66 @@
 <?php
 
-    // security check - must be included in all scripts
-    if (!$GLOBALS['kewl_entry_point_run']){
-        die('You cannot view this page directly');
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']){
+    die('You cannot view this page directly');
+}
+class dbcoursecomments extends dbTable{
+
+    public function init()
+    {
+        parent::init('tbl_ads_course_comments');  //super
+        $this->table = 'tbl_ads_course_comments';
+        $this->objUser = $this->getObject( 'user', 'security' );
+        $this->objDocumentStore = $this->getObject('dbdocument');
     }
-    class dbcoursecomments extends dbTable{
 
-        public function init()
-        {
-            parent::init('tbl_ads_course_comments');  //super
-            $this->table = 'tbl_ads_course_comments';
-            $this->objUser = $this->getObject( 'user', 'security' );
-        }
+    public function addComment($courseid, $comment,$status) {
+        $version = $this->objDocumentStore->getMaxVersion($courseid);
+        $data = array('courseid'=>$courseid, 'comment'=>$comment, 'version'=>$version,'status'=>$status);
+        $this->insert($data);
+    }
 
-        public function addComment($courseid, $comment) {
-            $sql = "select max(version) version from tbl_documentstore where coursecode = '".$courseid."'";
-            $data = $this->getArray($sql);
-            $version = $data[0]['version'];
+    public function getComments($courseid, $version) {
+        $sql = "select comment from ".$this->table." where courseid = '".$courseid."' and version = ".$version;
+        $data = $this->getArray($sql);
+
+        return $data;
+    }
+    public function getAllComments($courseid) {
+        $statuscodes=  array(
+              "0"=> 'New',
+              "1"=>'APO Comment',
+              "2"=>'Library comment',
+              "3"=>'Subsidy comment',
+              "4"=>'Faculty subcommittee',
+              "5"=>'Faculty',
+              "6"=> 'APDC');
+        $sql = "select * from ".$this->table." where courseid = '".$courseid."' order by status";
+        $data = $this->getArray($sql);
+        $comments="";
+        $unformatedComments="";
+        $status=0;
+        foreach($data as $xc){
             
-            $data = array('courseid'=>$courseid, 'comment'=>$comment, 'version'=>$version);
-            $this->insert($data);
+            if($status != $xc['status']){
+                $status= $xc['status'];
+                $unformatedComments.='<h3>'.$statuscodes[$status].'</h3>';
+            }
+            $unformatedComments.=$xc['comment'].'<br/>---------------------<br/>';
         }
-
-        public function getComments($courseid, $version) {
-            $sql = "select comment from ".$this->table." where courseid = '".$courseid."' and version = ".$version;
-            $data = $this->getArray($sql);
-
-            return $data;
-        }
-
-        public function getNumComments($courseid, $version) {
-            $sql = "select count(comment) counter from ".$this->table." where courseid = '".$courseid."' and version = ".$version;
-            $data = $this->getArray($sql);
-
-            return $data[0]['counter'];
-        }
+        
+        $order   = array("\r\n", "\n", "\r");
+        $replace = '<br />';
+        // Processes \r\n's first so they aren't converted twice.
+        $comments = str_replace($order, $replace, $unformatedComments);
+        return $comments;
     }
+
+    public function getNumComments($courseid, $version) {
+        $sql = "select count(comment) counter from ".$this->table." where courseid = '".$courseid."' and version = ".$version;
+        $data = $this->getArray($sql);
+
+        return $data[0]['counter'];
+    }
+}
 ?>
