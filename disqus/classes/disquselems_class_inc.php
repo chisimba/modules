dea2cd697e0640dcc0263d755223b8ba6a64438e
David Wafula <davidwaf@gmail.com>
2009-09-05 00:10:21 +0000

    added a developer option

--- disqus/classes/disquselems_class_inc.php
+++ disqus/classes/disquselems_class_inc.php
@@ -134,21 +134,30 @@ class disquselems extends dbtable
     */
     public function addWidget()
     {
-        $ret = "<script type='text/javascript'>
-//<[CDATA[
+
+       $objSysConfig = $this->getObject('dbsysconfig', 'sysconfig');
+       $developermode=$objSysConfig->getValue('mod_disqus_developermode', 'disqus');
+       $user=$objSysConfig->getValue('mod_disqus_defaultuser', 'disqus');
+
+       $div='<div id="disqus_thread"></div><script type="text/javascript" src="http://disqus.com/forums/'.$user.'/embed.js"></script><noscript><a href="http://witsfossad.disqus.com/?url=ref">View the discussion thread.</a></noscript><a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>';
+
+
+$ret = "<script type=\"text/javascript\">
+var disqus_developer=".$developermode.";
+//<![CDATA[
 (function() {
-        var links = document.getElementsByTagName('a');
-        var query = '?';
-        for(var i = 0; i < links.length; i++) {
-            if(links[i].href.indexOf('#disqus_thread') >= 0) {
-                query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
-            }
-        }
-        document.write('<script type=\"text/javascript\" src=\"http://disqus.com/forums/dkeats/get_num_replies.js' + query + '\"></' + 'script>');
-    })();
+	var links = document.getElementsByTagName('a');
+	var query = '?';
+	for(var i = 0; i < links.length; i++) {
+	if(links[i].href.indexOf('#disqus_thread') >= 0) {
+		query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
+	}
+	}
+	document.write('<script charset=\"utf-8\" type=\"text/javascript\" src=\"http://disqus.com/forums/".$user."/get_num_replies.js' + query + '\"></' + 'script>');
+})();
 //]]>
 </script>";
-        return $ret;
+        return $ret.$div;
     }
 
 }
