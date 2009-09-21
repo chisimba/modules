158da892932557160fdb3959ae956eb8934f8340
sylm <unknown@unknown.com>
2009-09-21 08:26:30 +0000

    layout fixes (added s)

--- realtime/java_src/realtime-xmpp-classic/src/org/avoir/realtime/gui/main/MainFrame.java
+++ realtime/java_src/realtime-xmpp-classic/src/org/avoir/realtime/gui/main/MainFrame.java
@@ -1285,7 +1285,7 @@ public class MainFrame extends javax.swing.JFrame {
         toolsMenu.add(screenViewerMenuItem);
 
         screenShareSizeMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F9, 0));
-        screenShareSizeMenuItem.setText("Screen Share Option");
+        screenShareSizeMenuItem.setText("Screen Share Options");
         screenShareSizeMenuItem.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 screenShareSizeMenuItemActionPerformed(evt);
