658ab78b0b1de118b59b191c61444b48c9d84021
musnit <unknown@unknown.com>
2009-09-29 01:45:36 +0000

    added processfileview method

--- java_src/realtime-xmpp-classic/src/org/avoir/realtime/gui/AnswerNavigator.java
+++ java_src/realtime-xmpp-classic/src/org/avoir/realtime/gui/AnswerNavigator.java
@@ -5,6 +5,9 @@
 
 package org.avoir.realtime.gui;
 
+import java.util.ArrayList;
+
+import org.avoir.realtime.common.RealtimeFile;
 import org.avoir.realtime.gui.main.MainFrame;
 import org.avoir.realtime.net.RPacketListener;
 
@@ -17,6 +20,28 @@ public class AnswerNavigator extends Navigator{
     public AnswerNavigator(MainFrame xmf) {
         super(xmf, "answers", "Answers Navigator");
           RPacketListener.addAnswerNavigatorFileVewListener(this);
+    }
+    
+    public void processFileView(ArrayList<RealtimeFile> files) {
+        clear();
+        addAccessTypes();
+
+        for (int i = 0; i < files.size(); i++) {
+
+            if (files.get(i).isPublicAccessible()) {
+            	javax.swing.tree.DefaultMutableTreeNode questionnode = addObject(publicNode, files.get(i), true);
+            	ArrayList<RealtimeFile> files2 = files.get(i).getFiles();
+            	for (int j = 0; j < files2.size(); j++) {
+                	addObject(questionnode, files2.get(j), true);
+            	}
+            } else {
+            	javax.swing.tree.DefaultMutableTreeNode questionnode = addObject(privateNode, files.get(i), true);
+            	ArrayList<RealtimeFile> files2 = files.get(i).getFiles();
+            	for (int j = 0; j < files2.size(); j++) {
+                	addObject(questionnode, files2.get(j), true);
+            	}
+            }
+        }
     }
 
 }
