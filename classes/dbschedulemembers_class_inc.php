0e28fef6dc1095957368f3bb8509020f795184b3
David Wafula <davidwaf@gmail.com>
2009-10-07 10:33:00 +0000

    added preview and email notification links

--- classes/dbschedulemembers_class_inc.php
+++ classes/dbschedulemembers_class_inc.php
@@ -14,7 +14,22 @@ class dbschedulemembers extends dbTable{
         $this->table = 'tbl_realtime_schedule_members';
         $this->objUser = $this->getObject ( 'user', 'security' );
     }
+    public function sendMail($to,$fromemail,$id){
+        $linkUrl = $this->uri(array(array('action'=>'showevent','id'=>$id)));
+        $link=' '. str_replace("amp;", "", $linkUrl);
 
+        $body= "Thank you for registering for $fromemail. To view this event, please click here : $link";
+        $subject="$fromemail registration";
+
+        $objMailer = $this->getObject('email', 'mail');
+        $objMailer->setValue('to', array($to));
+        $objMailer->setValue('from', $fromemail);
+        $objMailer->setValue('fromName', $fromemail);
+        $objMailer->setValue('subject', $subject);
+        $objMailer->setValue('body', $body);
+
+        $objMailer->send();
+    }
     public function addRoomMember(
         $userid,
         $sessionid
@@ -28,10 +43,16 @@ class dbschedulemembers extends dbTable{
 
             return FALSE;
         }else{
-
+            $sessiondata=$this->getSessionMeta($sessionid);
             $scheduleId = $this->insert($data);
+            $this->sendMail($this->objUser->email($userid), $sessiondata['event_title'],$scheduleId);
             return $scheduleId;
         }
+    }
+
+    public function getSessionMeta($id){
+        $data=$this->getRow('id',$id,'tbl_simpleregistrationevents');
+        return $data;
     }
     public function sessionExists($userid,$sessionid)
     {
@@ -51,6 +72,8 @@ class dbschedulemembers extends dbTable{
         $rows=$this->getArray($sql);
         return $rows;
     }
+
+
     public function getScheduleMembers($sessionid)
     {
         $sql="select * from " .$this->table." where sessionid ='".$sessionid."'";
