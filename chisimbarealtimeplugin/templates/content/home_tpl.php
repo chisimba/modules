0e5611ac259420c1a812b1ed2ab00ee4ad3a328a
David Wafula <davidwaf@gmail.com>
2009-10-08 14:39:36 +0000

    fixed the ie bug

--- chisimbarealtimeplugin/templates/content/home_tpl.php
+++ chisimbarealtimeplugin/templates/content/home_tpl.php
@@ -85,7 +85,7 @@ $addButton->setId('add-session');
 $addButtonShow=$this->objUser->isLecturer()? $addButton->show():"";
 $toolbar = $this->getObject('contextsidebar','context');
 $cssLayout->setLeftColumnContent($toolbar->show());
-$rightSideColumn = '<h1>'.$this->objContext->getTitle($this->contextcode).'</h1><div id="grouping-grid">'.$content.$addButtonShow.'<br/><br /></div>';
+$rightSideColumn = .$content.'<h1>'.$this->objContext->getTitle($this->contextcode).'</h1><div id="grouping-grid">'.$addButtonShow.'<br/><br /></div>';
 $cssLayout->setMiddleColumnContent($rightSideColumn);
 $submitUrl = $this->uri(array('action' => 'saveschedule'));
 $data='';
@@ -199,11 +199,59 @@ Ext.grid.data = [
     ".$data."
 
 ];
-
+var hours=
+[
+        ['00:00 am'],
+        ['01:30 am'],
+        ['02:00 am'],
+        ['02:30 am'],
+        ['03:00 am'],
+        ['03:30 am'],
+        ['04:00 am'],
+        ['04:30 am'],
+        ['05:00 am'],
+        ['05:30 am'],
+        ['06:00 am'],
+        ['06:30 am'],
+        ['07:00 am'],
+        ['07:30 am'],
+        ['08:00 am'],
+        ['08:30 am'],
+        ['09:00 am'],
+        ['09:30 am'],
+        ['10:00 am'],
+        ['10:30 am'],
+        ['11:00 am'],
+        ['11:30 am'],
+        ['12:00 pm'],
+        ['12:30 pm'],
+        ['01:00 pm'],
+        ['01:30 pm'],
+        ['02:00 pm'],
+        ['02:30 pm'],
+        ['03:00 pm'],
+        ['03:30 pm'],
+        ['04:00 pm'],
+        ['04:30 pm'],
+        ['05:00 pm'],
+        ['05:30 pm'],
+        ['06:00 pm'],
+        ['06:30 pm'],
+        ['07:00 pm'],
+        ['07:30 pm'],
+        ['08:00 pm'],
+        ['08:30 pm'],
+        ['09:00 pm'],
+        ['09:30 pm'],
+        ['10:00 pm'],
+        ['10:30 pm'],
+        ['11:00 pm'],
+        ['11:30 pm']
+];
 
     var timefromstore = new Ext.data.ArrayStore({
         fields: ['timefrom'],
-        data : Ext.datedata.hours // from states.js
+        data : hours
     });
     var timeFromField = new Ext.form.ComboBox({
         store: timefromstore,
@@ -221,7 +269,7 @@ Ext.grid.data = [
     });
     var timetostore = new Ext.data.ArrayStore({
         fields: ['timeto'],
-        data : Ext.datedata.hours // from states.js
+        data : hours // from states.js
     });
     var timeToField = new Ext.form.ComboBox({
         store: timetostore,
