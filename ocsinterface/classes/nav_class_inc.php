9b1a00fbf84461309f33a57dd895a8f998133b12
David Wafula <davidwaf@gmail.com>
2009-11-06 06:52:22 +0000

    initial code for the ocsinterface module

---
+++ ocsinterface/classes/nav_class_inc.php
@@ -0,0 +1,40 @@
+<?php
+
+class nav extends object {
+
+    public function init() {
+        $this->objLanguage = $this->getObject('language', 'language');
+        $this->objUser = $this->getObject('user', 'security');
+        $this->loadclass('link','htmlelements');
+        $this->objDbSysconfig = $this->getObject('dbsysconfig', 'sysconfig');
+        $this->storyparser=$this->getObject('storyparser');
+    }
+
+    public function getLeftContent() {
+
+        $sidecatid=$this->objDbSysconfig->getValue('SIDE_NAV_CATEGORY','ocsinterface');
+        $sidenavs=$this->storyparser->getStoryByCategory($sidecatid);
+        $list=array();
+
+        foreach($sidenavs as $nav) {
+            $sidelink = new link ($this->uri(array('action'=>'viewstory', 'storyid'=>$nav['id']),"ocsinterface"));
+            $sidelink->link=   $nav['title'];
+            $list[] = $sidelink->show();
+        }
+
+        $desc=
+            '<ul id="nav-secondary">';
+        $cssClass = '';
+        foreach($list as $element) {
+            if(strtolower($element) == strtolower($toSelect)) {
+                $cssClass = ' class="active" ';
+            }
+            $desc.='<li $cssClass>'.$element.'</li>';
+        }
+        $desc.='</ul>';
+
+        return $desc;
+    }
+}
+
+?>
