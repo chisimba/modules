b8a4f25301b611d5f73dbdc52bd4689bbbffd8ac
David Wafula <davidwaf@gmail.com>
2009-11-08 22:14:29 +0000

    added article support

--- jukskei/classes/viewerutils_class_inc.php
+++ jukskei/classes/viewerutils_class_inc.php
@@ -280,7 +280,7 @@ cell content can go in here
             <ul class="paneltabs">
 
              <font style="font-family:Arial;font-size:24;">  '.$navbar.'</font><hr>
-             <b style="font-family:Arial;font-size:24;">'.$storylink->show().'&nbsp; |&nbsp;</b><br/>
+             <b style="font-family:Arial;font-size:24;">Topic: '.$storylink->show().'</b><br/>
              <b style="font-family:Arial;font-size:24;">'.$data['title'].'&nbsp; |&nbsp;</b>
              <font style="font-family:Arial;font-size:18;">  '.$articlenav.'</font>
              '.$this->objWashout->parseText($data['maintext']).'
