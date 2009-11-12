7923c965390056b975e3594e64eaeec4a26aec0a
David Wafula <davidwaf@gmail.com>
2009-11-12 08:59:05 +0000

    added washout

--- ocsinterface/classes/nav_class_inc.php
+++ ocsinterface/classes/nav_class_inc.php
@@ -14,11 +14,12 @@ class nav extends object {
 
         $sidecatid=$this->objDbSysconfig->getValue('SIDE_NAV_CATEGORY','ocsinterface');
         $sidenavs=$this->storyparser->getStoryByCategory($sidecatid);
+        $objWashout = $this->getObject('washout', 'utilities');
         $list=array();
 
         foreach($sidenavs as $nav) {
             $sidelink = new link ($this->uri(array('action'=>'viewstory', 'storyid'=>$nav['id']),"ocsinterface"));
-            $sidelink->link=   $nav['title'];
+            $sidelink->link=   $objWashout->parseText($nav['title']);
             $list[] = $sidelink->show();
         }
 
