808b29b81ff9f4c51400dd5aa83554dd5a1a57a3
David Wafula <davidwaf@gmail.com>
2009-11-12 23:37:49 +0000

    initial efl code

---
+++ efl/controller.php
@@ -0,0 +1,18 @@
+<?php
+class EFL extends controller{
+	public $objLanguage;
+	
+	function init(){
+		
+		  //Instantiate the language object
+  		$this->objLanguage = $this->getObject('language', 'language');
+	}
+
+
+	function dispatch(){
+		
+		return "essayedit_tpl.php";
+	}
+
+}
+?>
