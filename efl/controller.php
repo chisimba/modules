f17ce20c8d51a48a39738d8a08dac179cc2c625c
muthathi <unknown@unknown.com>
2009-11-13 09:54:07 +0000

    created a simple text editor

--- efl/controller.php
+++ efl/controller.php
@@ -1,16 +1,16 @@
 <?php
-class EFL extends controller{
+class efl extends controller{
 	public $objLanguage;
-	
+
 	function init(){
-		
+
 		  //Instantiate the language object
   		$this->objLanguage = $this->getObject('language', 'language');
 	}
 
 
 	function dispatch(){
-		
+
 		return "essayedit_tpl.php";
 	}
 
--- efl/templates/content/essayedit_tpl.php
+++ efl/templates/content/essayedit_tpl.php
@@ -1,5 +1,16 @@
 <?php
-$objEditForm = $this->getObject('editform', 'EFL');
+
+$extbase = '<script language="JavaScript" src="'.$this->getResourceUri('ext-3.0-rc2/adapter/ext/ext-base.js','htmlelements').'" type="text/javascript"></script>';
+$extalljs = '<script language="JavaScript" src="'.$this->getResourceUri('ext-3.0-rc2/ext-all.js','htmlelements').'" type="text/javascript"></script>';
+$extallcss = '<link rel="stylesheet" type="text/css" href="'.$this->getResourceUri('ext-3.0-rc2/resources/css/ext-all.css','htmlelements').'"/>';
+$mainjs = '<script src="'.$this->getResourceUri('js/editor.js').'" type="text/javascript"></script>';
+
+$this->appendArrayVar('headerParams', $extbase);
+$this->appendArrayVar('headerParams', $extalljs);
+$this->appendArrayVar('headerParams', $extallcss);
+$this->appendArrayVar('headerParams', $mainjs);
+
+echo "<div id='texteditor'></div>";
+$objEditForm = $this->getObject('editform', 'efl');
 echo $objEditForm->show();
 ?>
-
