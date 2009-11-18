d66b9694efc6f36efaa0ca14d91384f2a0c820cc
David Wafula <davidwaf@gmail.com>
2009-11-18 09:41:57 +0000

    uses old applet tags, no working yet

--- efl/templates/content/essaymarker_tpl.php
+++ efl/templates/content/essaymarker_tpl.php
@@ -1,39 +1,25 @@
 <?php
 
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-header("Cache-Control: no-store, no-cache, must-revalidate");
-header("Cache-control: post-check=0, pre-check=0, false");
-header("Pragma: no-cache");
-header("Content-Type: application/x-java-jnlp-file");
+
 $objAltConfig = $this->getObject('altconfig','config');
 $modPath=$objAltConfig->getModulePath();
 $replacewith="";
 $docRoot=$_SERVER['DOCUMENT_ROOT'];
 $resourcePath=str_replace($docRoot,$replacewith,$modPath);
-$codebase="http://" . $_SERVER['HTTP_HOST']."/".$resourcePath.'/efl/resources/';
-?>
+$codebase="http://" . $_SERVER['HTTP_HOST'].$resourcePath.'/efl/resources';
 
 
-<jnlp spec="1.0+" codebase="'.$codebase.'">
-    <information>
-        <title>EFL</title>
-        <vendor>WITS eLearn</vendor>
-        <description>EFL</description>
-        <homepage href="http://www.wits.ac.za"/>
-        <description kind="short">EFL</description>
-        <icon href=<?php echo "$codebase/images/logo.png";?>/>
-        <icon kind="splash" href=<?php echo "$codebase/images/splash_realtime.png";?>/>
-        <offline-allowed/>
-    </information>
-    <resources>
-        <j2se version="1.5+" />
-        <jar href="efl.jar" />
-    </resources>
-    <application-desc main-class="efla.EflaApp">
-    </application-desc>
-    <security>
-        <all-permissions/>
-    </security>
-</jnlp>
+$str='<applet
+      codebase="'.$codebase.'"
+      archive="jefla.jar"
+      code="efla.EflaView"
+      width=1024
+      height=768>
+
+</applet>
+
+
+';
+echo $str;
+
 ?>
