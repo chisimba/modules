882e403b2ecb348f31e93ef53501977fee6234c4
David Wafula <davidwaf@gmail.com>
2010-01-10 11:57:30 +0000

    added extra left nav

--- speak4free/classes/nav_class_inc.php
+++ speak4free/classes/nav_class_inc.php
@@ -6,7 +6,7 @@ class nav extends object {
     }
 
     public  function show() {
-        $twobtalent=new link($this->uri(array('action'=>'viewstory','category'=> 'twobtalent')));
+        /*      $twobtalent=new link($this->uri(array('action'=>'viewstory','category'=> 'twobtalent')));
         $twobtalent->extra=' id= "twobtalent"';
         $twobtalent->link='<span>two be talent</span>';
 
@@ -21,12 +21,30 @@ class nav extends object {
         $onlinegal=new link($this->uri(array('action'=>'viewstory','category'=> 'onlinegal')));
         $onlinegal->extra=' id= "onlinegal"';
         $onlinegal->link='<span>onlinegal</span>';
+        */
 
-        $str='<br/><br/><ul>';
+        $aboutus=new link($this->uri(array('action'=>'viewstory','category'=> 'aboutus')));
+        $aboutus->link='<h3>About Us</h3>';
+
+        $mission=new link($this->uri(array('action'=>'viewstory','category'=> 'mission')));
+        $mission->link='<h3>Our Mission</h3>';
+
+        $values=new link($this->uri(array('action'=>'viewstory','category'=> 'values')));
+        $values->link='<h3>Values</h3>';
+
+        $strategicboard=new link($this->uri(array('action'=>'viewstory','category'=> 'strategicboard')));
+        $strategicboard->link='<h3>Strategic Board</h3>';
+
+        $contactus=new link($this->uri(array('action'=>'viewstory','category'=> 'contactus')));
+        $contactus->link='<h3>Contact Us</h3>';
+
+        $str='<br/><br/>
+            <ul id="xnav-secondary">';
         $str.='<li>'.$aboutus->show().'</li>';
-        $str.='<li>'.$twobtalent->show().'</li>';
-        $str.='<li>'.$poems->show().'</li>';
-        $str.='<li>'.$onlinegal->show().'</li>';
+        $str.='<li>'.$mission->show().'</li>';
+        $str.='<li>'.$values->show().'</li>';
+        $str.='<li>'.$strategicboard->show().'</li>';
+        $str.='<li>'.$contactus->show().'</li>';
         $str.="</ul>";
 
         return $str;
