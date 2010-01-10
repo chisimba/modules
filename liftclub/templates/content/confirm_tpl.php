a464388eaf28b3bbf496c875e7bd210d41f8973f
pwando <unknown@unknown.com>
2010-01-10 07:43:13 +0000

    modified template to conform to standards - echo just

--- liftclub/templates/content/confirm_tpl.php
+++ liftclub/templates/content/confirm_tpl.php
@@ -1,18 +1,19 @@
 <?php
 
-echo '<h1>'.$this->objLanguage->languageText('mod_userregistration_registrationsuccess', 'userregistration', 'You have successfully registered on the site').'</h1>';
+$str = '<h1>'.$this->objLanguage->languageText('mod_userregistration_registrationsuccess', 'userregistration', 'You have successfully registered on the site').'</h1>';
 
 $objBizCard = $this->getObject('userbizcard', 'useradmin');
 $objBizCard->setUserArray($user);
 
-echo $objBizCard->show();
+$str .= $objBizCard->show();
 
-echo '<p>'.$this->objLanguage->languageText('mod_userregistration_emailsent', 'userregistration', 'An email has been sent to your email address with your details').':</p>';
+$str .= '<p>'.$this->objLanguage->languageText('mod_userregistration_emailsent', 'userregistration', 'An email has been sent to your email address with your details').':</p>';
 
-echo '<ul>';
-echo '<li><strong>'.$this->objLanguage->languageText('word_username', 'system').'</strong>: '.$user['username'].'</li>';
+$str .= '<ul>';
+$str .= '<li><strong>'.$this->objLanguage->languageText('word_username', 'system').'</strong>: '.$user['username'].'</li>';
 //echo '<li><strong>'.$this->objLanguage->languageText('word_password', 'system').'</strong>: ***** </li>';
-echo '</ul>';
+$str .= '</ul>';
 
-echo '<br /><br />';
+$str .= '<br /><br />';
+echo $str;
 ?>
