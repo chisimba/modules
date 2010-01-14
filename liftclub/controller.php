eba9920534783e7fed4db97cd4204abbc346bf82
pwando <unknown@unknown.com>
2010-01-14 21:38:01 +0000

    Worked on modify registration

--- liftclub/controller.php
+++ liftclub/controller.php
@@ -817,7 +817,7 @@ class liftclub extends controller
             return $this->nextAction(NULL);
         }
         // Generate User Id
-        $id = $this->getParam('id');
+        $id = $this->objUser->userId();
 
         $username = $this->getParam('register_username');
         $password = $this->getParam('register_password');
