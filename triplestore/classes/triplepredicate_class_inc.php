48705e0aaa6e4c7d8066f532548afaef6084dac0
charlvn <unknown@unknown.com>
2010-02-06 03:09:20 +0000

    Added docblock.

--- triplestore/classes/triplepredicate_class_inc.php
+++ triplestore/classes/triplepredicate_class_inc.php
@@ -1,5 +1,60 @@
 <?php
 
+/**
+ * Triplestore predicate data access class
+ * 
+ * Class to facilitate interaction with the framework triplestore.
+ * 
+ * PHP version 5
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @category  chisimba
+ * @package   triplestore
+ * @author    Charl van Niekerk <charlvn@charlvn.com>
+ * @copyright 2010 Charl van Niekerk
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id$
+ * @link      http://avoir.uwc.ac.za/
+ */
+
+// security check - must be included in all scripts
+if (!
+/**
+ * Description for $GLOBALS
+ * @global unknown $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+/**
+ * Triplestore predicate data access class
+ * 
+ * Class to facilitate interaction with the framework triplestore.
+ * 
+ * @category  chisimba
+ * @package   triplestore
+ * @author    Charl van Niekerk <charlvn@charlvn.com>
+ * @copyright 2010 Charl van Niekerk
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id$
+ * @link      http://avoir.uwc.ac.za/
+ */
+
 class triplepredicate extends object implements Iterator
 {
     protected $objects;
