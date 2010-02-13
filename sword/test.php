90e50a2fae9324a22a4ac96b3bdc7ffb75bbed75
charlvn <unknown@unknown.com>
2010-02-13 15:10:46 +0000

    Add file.

--- sword/test.php
+++ sword/test.php
@@ -11,5 +11,8 @@ $package->setTitle('Test Title');
 $package->setAbstract('Test Abstract');
 $package->create();
 
+// Add a file to the package
+$package->addFile($filename, $mimetype);
+
 $client = new SWORDAPPClient();
 $client->deposit('http://localhost:8080/sword/collection:open', 'fedoraAdmin', 'charlvn', 'Charl van Niekerk', 'tmp/test.zip', 'http://purl.org/net/sword-types/METSDSpaceSIP', 'application/zip', false, true);
