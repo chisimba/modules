b5176a6036f37a7e336c5c1e431711826b934a8a
charlvn <unknown@unknown.com>
2010-02-21 14:02:44 +0000

    Added class docblocks and set Id property and corrected punctuation.

--- phirehose/controller.php
+++ phirehose/controller.php
@@ -1,5 +1,59 @@
 <?php
 
+/**
+ * Phirehose controller class
+ * 
+ * Handles HTTP requests for the Phirehose module.
+ * 
+ * PHP version 5
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @category  chisimba
+ * @package   phirehose
+ * @author    Charl van Niekerk <charlvn@charlvn.com>
+ * @copyright 2010 Charl van Niekerk
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id$
+ * @link      http://avoir.uwc.ac.za/
+ */
+
+// security check - must be included in all scripts
+if (!
+/**
+ * Description for $GLOBALS
+ * @global unknown $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+/**
+ * Phirehose controller class
+ *
+ * Handles HTTP requests for the Phirehose module.
+ *
+ * @category  Chisimba
+ * @package   phirehose
+ * @author    Charl van Niekerk <charlvn@charlvn.com>
+ * @copyright 2010 Charl van Niekerk
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id$
+ * @link      http://avoir.uwc.ac.za/
+ */
 class phirehose extends controller
 {
     /**
@@ -111,7 +165,7 @@ class phirehose extends controller
      * Allows all methods without login.
      *
      * @access public
-     * @param  string  $action The name of the action
+     * @param  string  $action The name of the action.
      * @return boolean Always returns FALSE.
      */
     public function requiresLogin($action)
