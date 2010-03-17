dd5f56d5451ec8755bbcc96283fc182491fd99f5
charlvn <unknown@unknown.com>
2010-03-17 10:53:51 +0000

    Moved logic out into botops class.

--- bot/controller.php
+++ bot/controller.php
@@ -2,34 +2,22 @@
 
 class bot extends controller
 {
+    protected $objBotops;
     protected $objLanguage;
     protected $objMultisearch;
 
     public function init()
     {
+        $this->objBotops      = $this->getObject('botops', 'bot');
         $this->objLanguage    = $this->getObject('language', 'language');
         $this->objMultisearch = $this->getObject('multisearchops', 'multisearch');
     }
 
     public function dispatch()
     {
-        $body      = $this->getParam('body');
-        $command   = strtolower(strtok($body, ' '));
-        $arguments = strtok('');
-
-        switch ($command) {
-            case 'search':
-                $query    = $this->objMultisearch->buildQuery($arguments, 2);
-                $results  = $this->objMultisearch->doQuery($query);
-                $text     = $this->objMultisearch->formatQuery($results, 'plaintext');
-                $response = implode('', $text);
-                break;
-            default:
-                $response = $this->objLanguage->languageText('mod_bot_invalidcommand', 'bot');
-        }
-
+        $message = $this->getParam('body');
         header('Content-Type: text/plain; charset=UTF-8');
-        echo $response;
+        echo $this->objBotops->process($response);
     }
 
     /**
