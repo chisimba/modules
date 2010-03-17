b458a6666486185ef0292ed584ee077e75f94d2b
charlvn <unknown@unknown.com>
2010-03-17 10:51:34 +0000

    Added botops

---
+++ bot/classes/botops_class_inc.php
@@ -0,0 +1,23 @@
+<?php
+
+class botops extends object
+{
+    public function process($message)
+    {
+        $command   = strtolower(strtok($message, ' '));
+        $arguments = strtok('');
+
+        switch ($command) {
+            case 'search':
+                $query    = $this->objMultisearch->buildQuery($arguments, 2);
+                $results  = $this->objMultisearch->doQuery($query);
+                $text     = $this->objMultisearch->formatQuery($results, 'plaintext');
+                $response = implode('', $text);
+                break;
+            default:
+                $response = $this->objLanguage->languageText('mod_bot_invalidcommand', 'bot');
+        }
+
+        return $response;
+    }
+}
