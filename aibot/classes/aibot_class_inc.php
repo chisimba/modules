4fc7aaf62c29abaaaff45710ba2a119a195e75c1
charlvn <unknown@unknown.com>
2010-03-18 13:41:59 +0000

    Added enabled option to aibot.

--- aibot/classes/aibot_class_inc.php
+++ aibot/classes/aibot_class_inc.php
@@ -2,6 +2,7 @@
 
 class aibot extends object
 {
+    protected $enabled;
     protected $objClient;
     protected $objSysConfig;
 
@@ -10,15 +11,21 @@ class aibot extends object
         require_once $this->getPearResource('XML/RPC.php');
 
         $this->objSysConfig = $this->getObject('dbsysconfig', 'sysconfig');
-        $url = parse_url($this->objSysConfig->getValue('url', 'aibot'));
-        $objClient = new XML_RPC_Client($url['path'], $url['host'], $url['port']);
+        $this->enabled      = (boolean) $this->objSysConfig->getValue('enabled', 'aibot');
+        $url                = parse_url($this->objSysConfig->getValue('url', 'aibot'));
+        $this->objClient    = new XML_RPC_Client($url['path'], $url['host'], $url['port']);
     }
 
     public function chat($text)
     {
-        $params = array(new XML_RPC_Value($text, 'string'));
-        $message = new XML_RPC_Message('chat', $params);
+        $params   = array(new XML_RPC_Value($text, 'string'));
+        $message  = new XML_RPC_Message('chat', $params);
         $response = $client->send($message);
         var_dump($response);
+    }
+
+    public function isEnabled()
+    {
+        return $this->enabled;
     }
 }
