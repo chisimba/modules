f3966bab0f401f64f9e0c22d339559e146bc41a8
David Wafula <davidwaf@gmail.com>
2010-04-08 09:39:03 +0000

    added sample stream

--- fossilstream/templates/content/home_tpl.php
+++ fossilstream/templates/content/home_tpl.php
@@ -1,5 +1,11 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <?php
+
+
+$objWashout = $this->getObject('washout', 'utilities');
+
+$vid='[FLV: url= http://localhost/chisimba/somefile.flv, width=800,
+	 height=600]';
 $objH1 = $this->newObject('htmlheading', 'htmlelements');
 $objH2 = $this->newObject('htmlheading', 'htmlelements');
 $cssLayout = $this->getObject('csslayout', 'htmlelements');
@@ -12,7 +18,9 @@ $objH1->str="The Fossil";
 $objH2->type=2;
 $objH2->str="Coming soon...";
 $cssLayout->setLeftColumnContent($leftContent);
-$cssLayout->setMiddleColumnContent($objH1->show(). $objH2->show());
+$content='[IFRAME]http://146.141.9.151/flv/player2/flash.html|400|324[/IFRAME]';
+$content=$objWashout->parseText($content);
+$cssLayout->setMiddleColumnContent($objH1->show(). $objH2->show().$content);
 
 echo $cssLayout->show();
 ?>
