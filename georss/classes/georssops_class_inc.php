84f2741a4bbe7bc740d024b2b9f7d8f5b782dbe7
paulscott <unknown@unknown.com>
2010-04-17 15:25:04 +0000

    removed flickr

--- georss/classes/georssops_class_inc.php
+++ georss/classes/georssops_class_inc.php
@@ -61,8 +61,8 @@ class georssops extends object
             
             var newl = new OpenLayers.Layer.GeoRSS( 'GeoRSS', '".$this->getResourceUri('georss.xml', 'georss')."' );
             map.addLayer(newl);
-            var newl2 = new OpenLayers.Layer.GeoRSS( 'My Flickr GeoRSS', '".$this->getResourceUri('flickr.xml', 'georss')."' );
-            map.addLayer(newl2);
+            //var newl2 = new OpenLayers.Layer.GeoRSS( 'My Flickr GeoRSS', '".$this->getResourceUri('flickr.xml', 'georss')."' );
+            //map.addLayer(newl2);
             
             map.setCenter(new OpenLayers.LonLat(0,0), 2);
             
