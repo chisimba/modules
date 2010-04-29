f52cd6b57983f47e4bacd1b374e338c0d6bcc491
qfenama <unknown@unknown.com>
2010-04-29 08:13:41 +0000

    change faq list according to ticket #3980

--- faq/templates/content/view_tpl.php
+++ faq/templates/content/view_tpl.php
@@ -7,7 +7,28 @@
     $this->loadHTMLElement("link");
     $this->loadHTMLElement("hiddeninput");
     $this->loadHTMLElement("label");
-//
+	
+	//Add the jquery stuff to th header
+	$jqr = $this->getJavaScriptFile('1.4.2/jquery-1.4.2.min.js', 'jquery');
+
+	//$jqr .= $this->getJavaScriptFile('jquery-script.js', 'faq');
+	$jqr .= '<script type="text/javascript">
+		$(document).ready(function() {
+	
+	    $(\'.wrapperDarkBkg\').hide();
+
+		$(\'a\').click(function(){
+	
+	    	var id = this.id;
+	
+			//$(\'.wrapperDarkBkg\').hide();
+
+			$(\'#content\'+id).toggle("slow");	
+			}); 
+		}); </script>';		
+
+	$this->appendArrayVar('headerParams', $jqr);
+
     //Use to check for admin user:
     $isAdmin = $this->objUser->isAdmin();
 
@@ -94,107 +115,105 @@
         // show using an ordered list
         echo '<ol>';
         foreach ($list as $element) {
-            echo "<li><a href=\"#".$element["id"]."\">";
+            echo "<li><a href=\"#".$element["id"]."\" id=\"".$element["id"]."\">";
             echo /*$index . " : " . */ nl2br($element["question"]);
             echo "</a></li>";
-            $index++;
+            
+		//Link of answers as a div
+
+		// Anchor tag for link to top of page.
+				//echo "<div id=\"".$element["id"]."\"></div>";
+				$found = true;
+			   ?> 
+				<!--<div style="background-color: #008080; padding:5px;">-->
+				<!--<div style="background-color: #000080; padding:5px;">-->
+				
+		<?php
+				echo '<div class="wrapperDarkBkg" id="content'.$element["id"].'">';
+				echo "<b>" . $index . ": " . "</b>" . $objLanguage->languageText("word_answer","system").": ";
+		?>
+				<!--<div style="background-color: #FFFFFF; padding:5px;">-->
+				<div class="wrapperLightBkg">
+		<?php
+				echo "<p>";
+				echo "<b>" . "</b>" . nl2br($element["answer"]);
+				echo "</p>";
+				//echo $objLanguage->languageText("faq_postedby") . " : " . $objUser->fullname($element["userId"]) . "&nbsp;" . $element["dateLastUpdated"] . "<br/>";
+				echo "&nbsp;";
+
+				if ($isAdmin || $isLecturer) {
+				    // Edit an entry.
+				    $icon = $this->getObject('geticon','htmlelements');
+				    $icon->setIcon('edit');
+				    $icon->alt = "Edit";
+				    $icon->align=false;
+				    echo "<a href=\"" .
+				            $this->uri(array(
+				                'module'=>'faq',
+				                'action'=>'edit',
+				                'category'=>$categoryId,
+				                'id' => $element["id"]
+				    ))
+				    . "\">".$icon->show()."</a>";
+				    echo "&nbsp;";
+				    // Delete an entry.
+				    $objConfirm=&$this->newObject('confirm','utilities');
+				    $icon = $this->getObject('geticon','htmlelements');
+				    $icon->setIcon('delete');
+				    $icon->alt = "Delete";
+				    $icon->align=false;
+				    $objConfirm->setConfirm(
+				        $icon->show(),
+				        $this->uri(array(
+				            'action'=>'deleteconfirm',
+				            'category'=>$categoryId,
+				            'id'=>$element["id"]
+				        )),
+				        $objLanguage->languageText('faq_suredelete'));
+				    echo $objConfirm->show();
+
+				    // Scroll down one entry.
+				    if($element["nextid"] != null) {
+					    $index = $index + 1;
+					    $icon = $this->getObject('geticon','htmlelements');
+					    $icon->setIcon('down');
+					    $icon->alt = "Down";
+					    $icon->align=false;
+					    echo "<a href=\"#" .$element["nextid"]. "\">".$icon->show()."</a>";
+					    echo "&nbsp;";
+					    $index--;
+				    }
+				    
+				    if($index > 1) {
+				    // Scroll up one entry.
+				    $index = $index - 1;
+				    $icon = $this->getObject('geticon','htmlelements');
+				    $icon->setIcon('up');
+				    $icon->alt = "Up";
+				    $icon->align=false;
+				    echo "<a href=\"#" .$element["previd"]. "\">".$icon->show()."</a>";
+				    echo "&nbsp;";
+				    $index++;
+				    }
+				}
+		?>
+				</div>
+				</div>
+				<!--</div>-->
+		<?php
+				//echo "<br/>";
+				//echo "<br/>";
+				$index++;
+
         }
         echo '</ol>';
-        echo "<br/>";
-    }
-
-    // List the questions and answers.
-    $index = 1;
-    $found = false;
-    foreach ($list as $element) {
-        // Anchor tag for link to top of page.
-        echo "<a id=\"".$element["id"]."\"></a>";
-        $found = true;
-       // var_dump($element["id"]);
-?>
-        <!--<div style="background-color: #008080; padding:5px;">-->
-        <!--<div style="background-color: #000080; padding:5px;">-->
-        <div class="wrapperDarkBkg">
-<?php
-        echo "<b>" . $index . ": " . "</b>" . nl2br($element["question"]);
-?>
-        <!--<div style="background-color: #FFFFFF; padding:5px;">-->
-        <div class="wrapperLightBkg">
-<?php
-        echo "<p>";
-        echo "<b>" . "</b>" . nl2br($element["answer"]);
-        echo "</p>";
-        //echo $objLanguage->languageText("faq_postedby") . " : " . $objUser->fullname($element["userId"]) . "&nbsp;" . $element["dateLastUpdated"] . "<br/>";
-        echo "&nbsp;";
-        if ($isAdmin || $isLecturer) {
-            // Edit an entry.
-            $icon = $this->getObject('geticon','htmlelements');
-            $icon->setIcon('edit');
-            $icon->alt = "Edit";
-            $icon->align=false;
-            echo "<a href=\"" .
-                    $this->uri(array(
-                        'module'=>'faq',
-                        'action'=>'edit',
-                        'category'=>$categoryId,
-                        'id' => $element["id"]
-            ))
-            . "\">".$icon->show()."</a>";
-            echo "&nbsp;";
-            // Delete an entry.
-            $objConfirm=&$this->newObject('confirm','utilities');
-            $icon = $this->getObject('geticon','htmlelements');
-            $icon->setIcon('delete');
-            $icon->alt = "Delete";
-            $icon->align=false;
-            $objConfirm->setConfirm(
-                $icon->show(),
-                $this->uri(array(
-                    'action'=>'deleteconfirm',
-                    'category'=>$categoryId,
-                    'id'=>$element["id"]
-                )),
-                $objLanguage->languageText('faq_suredelete'));
-            echo $objConfirm->show();
-
-            // Scroll down one entry.
-            if($element["nextid"] != null) {
-	            $index = $index + 1;
-	            $icon = $this->getObject('geticon','htmlelements');
-	            $icon->setIcon('down');
-	            $icon->alt = "Down";
-	            $icon->align=false;
-	            echo "<a href=\"#" .$element["nextid"]. "\">".$icon->show()."</a>";
-	            echo "&nbsp;";
-	            $index--;
-            }
-            
-            if($index > 1) {
-            // Scroll up one entry.
-            $index = $index - 1;
-            $icon = $this->getObject('geticon','htmlelements');
-            $icon->setIcon('up');
-            $icon->alt = "Up";
-            $icon->align=false;
-            echo "<a href=\"#" .$element["previd"]. "\">".$icon->show()."</a>";
-            echo "&nbsp;";
-            $index++;
-            }
-        }
-?>
-        </div>
-        </div>
-        <!--</div>-->
-<?php
-        echo "<br/>";
-        echo "<br/>";
-        $index++;
-    }
+        //echo "<br/>";
+	}
     // If no entries then display message.
     if (!$found) {
         echo "<div class=\"noRecordsMessage\">" . $objLanguage->languageText("faq_noentries","faq") . "</div>";
     }
-
+	echo "<br/>";
     $link = new link ($this->uri(NULL));
     $link->link = $objLanguage->languageText("mod_faq_faqhome","faq", 'FAQ Home');
     echo $link->show();
