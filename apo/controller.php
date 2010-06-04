84f248b804f16686f0c0433741ddb08fcf9bf784
David Wafula <davidwaf@gmail.com>
2010-06-04 08:46:11 +0000

    has functionality for apo skin

---
+++ apo/controller.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ *
+ *  This module contains specialized functionality for controlling
+ * the rendering of apo skin
+ *
+ * m
+ *
+ * @author David Wafula
+ * @package apo
+ *
+ * PHP version 5
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+// security check - must be included in all scripts
+if (!
+/**
+ * The $GLOBALS is an array used to control access to certain constants.
+ * Here it is used to check if the file is opening in engine, if not it
+ * stops the file from running.
+ *
+ * @global entry point $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ *
+ */
+$GLOBALS['kewl_entry_point_run'])
+{
+        die("You cannot view this page directly");
+}
+// end security check
+
+class apo extends controller
+{
+
+    public function init()
+    {
+        $this->objUser = $this->getObject('user', 'security');
+        $this->objLanguage = $this->getObject('language', 'language');
+    }
+
+
+    /**
+     *
+     * The standard dispatch method for the twitter module.
+     * The dispatch method uses methods determined from the action
+     * parameter of the  querystring and executes the appropriate method,
+     * returning its appropriate template. This template contains the code
+     * which renders the module output.
+     *
+     */
+    public function dispatch()
+    {
+        //Get action from query string and set default to view
+        $action=$this->getParam('action', 'default');
+        /*
+        * Convert the action into a method (alternative to
+        * using case selections)
+        */
+        $method = $this->__getMethod($action);
+        /*
+        * Return the template determined by the method resulting
+        * from action
+        */
+        return $this->$method();
+    }
+
+
+    /*------------- BEGIN: Set of methods to replace case selection ------------*/
+    
+    /**
+    *
+    * If you have a token, it allows you to edit it using
+    * userparamsadmin, else it allows you to create it using
+    * userparamsadmin
+    * 
+    * @return string The contents of the dump template
+    * @access private
+    *
+    */
+    private function __default()
+    {
+        return "home_tpl.php";
+    }
+
+
+    /**
+    *
+    * Method to check if a given action is a valid method
+    * of this class preceded by double underscore (__). If it __action
+    * is not a valid method it returns FALSE, if it is a valid method
+    * of this class it returns TRUE.
+    *
+    * @access private
+    * @param string $action The action parameter passed byref
+    * @return boolean TRUE|FALSE
+    *
+    */
+    private function __validAction(& $action)
+    {
+        if (method_exists($this, "__".$action)) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    /**
+    *
+    * Method to convert the action parameter into the name of
+    * a method of this class.
+    *
+    * @access private
+    * @param string $action The action parameter passed byref
+    * @return stromg the name of the method
+    *
+    */
+    function __getMethod(& $action)
+    {
+        if ($this->__validAction($action)) {
+            return "__" . $action;
+        } else {
+            return "__actionError";
+        }
+    }
+
+    /*------------- END: Set of methods to replace case selection ------------*/
+
+
+
+    /**
+    *
+    * This is a method to determine if the user has to
+    * be logged in or not. Note that this is an example,
+    * and if you use it view will be visible to non-logged in
+    * users. Delete it if you do not want to allow annonymous access.
+    * It overides that in the parent class
+    *
+    * @return boolean TRUE|FALSE
+    *
+    */
+    public function requiresLogin()
+    {
+        return FALSE;
+     }
+}
+?>
---
+++ apo/templates/content/home_tpl.php
@@ -0,0 +1,34 @@
+<?php
+$this->setVar('canvas', 'apo');
+
+//html > body #tools_index .firefox tools > div #wrap > div #nav2 .globalnav > a #jqt1 .active
+$content='
+<div id="tools_index">
+<div id="tools">
+<div id="wrap">
+<div id="nav2">
+
+<div id="globalnav">
+<ul>
+<div id="jqt1">
+Home
+</div>
+<div id="jqt2">
+Software Development
+</div>
+</ul>
+</div>
+
+</div>
+<div id="content">
+
+<div id="scroll">
+APO
+</div>
+</div>
+</div>
+</div>
+</div>
+';
+echo $content;
+?>
