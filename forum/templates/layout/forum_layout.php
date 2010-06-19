5a7f0d11283efc22eaff5c9bac5fe8bb864d8f93
Derek Keats <derek@dkeats.com>
2010-06-19 18:58:40 +0000

    Fixed layout

--- forum/templates/layout/forum_layout.php
+++ forum/templates/layout/forum_layout.php
@@ -1,23 +1,7 @@
 <?php
-
-$middleColumn = '<div style="padding: 5px;">';
-if($this->contextObject->isInContext())
-{
-    //$objContextUtils = $this->getObject('utilities','context');
-    //echo $objContextUtils->getHiddenContextMenu('forum','none');
-     
-}
-$middleColumn .= $this->getContent(); 
-$middleColumn .= '</div>';
-
 $middleColumn = $this->getVar('middleColumn');
-
 $cssLayout = $this->newObject('csslayout', 'htmlelements');
 $cssLayout->setNumColumns(1);
 $cssLayout->setMiddleColumnContent($middleColumn);
-
 echo $cssLayout->show();
-
-
 ?>
-
