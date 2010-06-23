57666d3bbfcff80282fb158ea0c9a97cc04cccee
pwando <unknown@unknown.com>
2010-06-23 14:23:21 +0000

    added sql files

---
+++ gradebook2/sql/tbl_gradebook2_grades.sql
@@ -0,0 +1,36 @@
+<?php
+/**
+ *
+ * Table for storing learner grades
+ *
+ */
+
+/* Set the table name */
+ 
+$tablename = 'tbl_gradebook2_grades';
+
+/* Options line for comments, encoding and character set */
+
+   $options = array(
+    'comment' => 'Table for tbl_gradebook2_grades',
+    'collate' => 'utf8_general_ci',
+    'character_set' => 'utf8');
+/* Create the table fields */
+   $fields = array(
+    'id' => array(
+       'type' => 'text',
+       'length' => 32,
+       'notnull' => 1
+       ),
+   'learnerid' => array(
+       'type' => 'text',
+       'length' => 32,
+       'notnull' => TRUE
+       ),
+   'totalgrade' => array(
+       'type' => 'text',
+       'length' => 32,
+       'notnull' => 1
+       )
+);
+?>
