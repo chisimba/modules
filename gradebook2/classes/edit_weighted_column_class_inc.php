a045beab8b876db1efe8ff251a6da348d6c72303
pwando <unknown@unknown.com>
2010-07-01 17:31:39 +0000

    building addedit weighted column form class

--- gradebook2/classes/edit_weighted_column_class_inc.php
+++ gradebook2/classes/edit_weighted_column_class_inc.php
@@ -14,15 +14,78 @@ class edit_weighted_column extends object
         $this->loadClass('label', 'htmlelements');
         //Load the textarea class
         $this->loadClass('textarea','htmlelements');
-        //Load the button class
-        $this->loadClass('button', 'htmlelements'); 
         //Load the radio button class
         $this->loadClass('radio', 'htmlelements'); 
+        //Load the dropdown button class
+        $this->loadClass('dropdown', 'htmlelements'); 
+        //Load the button class
+        $this->loadClass('button', 'htmlelements'); 
     }
     private function buildForm()
     {
-
+        //Load the form elements
         $this->loadElements();
+        //Create the form
+        $objForm = new form('weighted_column', $this->getFormAction());
+        //----------TEXT INPUT 1--------------
+        //Create a new textinput for the column name
+        $objColumname = new textinput('column_name');
+        //Create a new label for the text labels
+        $columnameLabel = new label($this->objLanguage->languageText("mod_gradebook2_columname","gradebook2"),"column_name");
+        $objForm->addToForm($objColumname->show() . "<br />");
+        $objForm->addToForm($columnameLabel->show());
+        //----------TEXT INPUT 2--------------
+        //Create a new textinput for the display name
+        $objDisplayname = new textinput('display_name');
+        //Create a new label for the text labels
+        $displaynameLabel = new label($this->objLanguage->languageText("mod_gradebook2_displayname","gradebook2"),"display_name");
+        $objForm->addToForm($objDisplayname->show() . "<br />");
+        $objForm->addToForm($displaynameLabel->show());
+        //----------TEXTAREA--------------
+        //Create a new textarea for the description text
+        $objDescriptiontxt = new textarea('description');
+        $descriptionLabel = new label($this->objLanguage->languageText("mod_gradebook2_description","gradebook2"),"description");
+        $objForm->addToForm($descriptionLabel->show() . "<br />");
+        $objForm->addToForm($objDescriptiontxt->show() . "<br />");
+        //----------DROP DOWN 1--------------
+        //Create a new dropdown for the primary display
+        $objPrimaryDisplay = new dropdown('primary_display');
+        //Add percentage option
+        $objPrimaryDisplay->addOption('%', $this->objLanguage->languageText("mod_gradebook2_percentage","gradebook2"));
+        //Create a new label for the text labels
+        $primarydisplayLabel = new label($this->objLanguage->languageText("mod_gradebook2_primarydisplay","gradebook2"),"primary_display");
+        $objForm->addToForm($objPrimaryDisplay->show() . "<br />");
+        $objForm->addToForm($primarydisplayLabel->show());
+        //----------DROP DOWN 2--------------
+        //Create a new dropdown for the secondary display
+        $objSecondaryDisplay = new dropdown('secondary_display');
+        //Add percentage option
+        $objSecondaryDisplay->addOption('%', $this->objLanguage->languageText("mod_gradebook2_percentage","gradebook2"));
+        //Create a new label for the text labels
+        $secondarydisplayLabel = new label($this->objLanguage->languageText("mod_gradebook2_secondarydisplay","gradebook2"),"secondary_display");
+        $objForm->addToForm($objSecondaryDisplay->show() . "<br />");
+        $objForm->addToForm($secondarydisplayLabel->show());
+        //----------DROP DOWN 3--------------
+        //Create a new dropdown for the grading period
+        $objGradingPeriod = new dropdown('grading_period');
+        //Add Grading period option
+        $objGradingPeriod->addOption('2008/2009', '2008/2009');
+        //Create a new label for the text labels
+        $gradingPeriodLabel = new label($this->objLanguage->languageText("mod_gradebook2_gradingperiod","gradebook2"),"grading_period");
+        $objForm->addToForm($objGradingPeriod->show() . "<br />");
+        $objForm->addToForm($gradingPeriodLabel->show());
+        
+        //----------SUBMIT BUTTON--------------
+        //Create a button for submitting the form
+        $objButton = new button('save');
+        // Set the button type to submit
+        $objButton->setToSubmit();
+        // Use the language object to label button 
+        // with the word save
+        $objButton->setValue(' '.$this->objLanguage->languageText("mod_gradebook2_submit", "gradebook2").' ');
+        $objForm->addToForm($objButton->show());
+        
+        return $objForm->show();
     }
     private function getFormAction()
     {
