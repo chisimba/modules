dffa00dacb25e29452e5f27073b6d28730d8aca7
jacqui <unknown@unknown.com>
2010-07-08 19:46:56 +0000

    added column 'commentdata'

--- wicid/sql/tbl_wicid_formdata.sql
+++ wicid/sql/tbl_wicid_formdata.sql
@@ -4,6 +4,7 @@
     $fields = array('id' => array('type' => 'text','length' => 32, 'notnull'=>TRUE),
                     'formname' => array('type' => 'text', 'notnull'=>TRUE),
                     'formdata' => array('type' => 'text', 'notnull'=>TRUE),
+                    'commentdata' => array('type' => 'text', 'notnull'=>TRUE),
                     'docid' => array('type' => 'text','length' => 32, 'notnull'=>TRUE),
                     'userid' => array('type' => 'text','length' => 30, 'notnull'=>TRUE)
                     );
