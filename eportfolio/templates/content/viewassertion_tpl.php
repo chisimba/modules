e960f4fc2d48d0b88349950e500f56a125b93a19
pwando <unknown@unknown.com>
2010-07-11 17:37:27 +0000

    modified form to use new function

--- eportfolio/templates/content/viewassertion_tpl.php
+++ eportfolio/templates/content/viewassertion_tpl.php
@@ -8,42 +8,7 @@ $this->loadClass("htmltable", 'htmlelements');
 //reflectId
 $singleView = $this->objGetall->viewSingleAssertion($assertionId);
 echo $singleView;
-$form = new form("add", $this->uri(array(
-    'module' => 'eportfolio',
-    'action' => 'postcomment',
-    'prevaction' => 'singleassertion',
-    'eportpartidvarname' => 'assertionId',
-    'eportfoliopartid' => $assertionId
-)));
-$objHeading = &$this->getObject('htmlheading', 'htmlelements');
-$objHeading->type = 3;
-$objHeading->str = $objLanguage->languageText("mod_eportfolio_postcomment", 'eportfolio');
-//table object
-$reflecTable = &$this->newObject("htmltable", "htmlelements");
-$reflecTable->width = '100%';
-//$reflecTable->attributes = " align='left' border='0'";
-$reflecTable->cellspacing = '5';
-//row for author comments
-$reflecTable->startRow();
-$reflecTable->addCell($objHeading->show());
-$reflecTable->endRow();
-//new comment text field
-$textinput = new textarea("newcomment", '');
-$form->addRule('newcomment', 'Please type a comment', 'required');
-$reflecTable->startRow();
-$reflecTable->addCell($textinput->show());
-$reflecTable->endRow();
-//Submit button
-$button = new button("submit", $objLanguage->languageText("word_submit", "system"));
-$button->setToSubmit();
-$reflecTable->startRow();
-$reflecTable->addCell($button->show());
-$reflecTable->endRow();
-$reflecTable->startRow();
-$reflecTable->addCell("&nbsp;");
-$reflecTable->endRow();
-$form->addToForm($reflecTable->show());
-echo $form->show();
+echo $this->objGetall->viewPartForm('singleassertion', 'assertionId', $assertionId );
 //Get Object
 $this->objIcon = &$this->newObject('geticon', 'htmlelements');
 $objLayer3 = $this->newObject('layer', 'htmlelements');
