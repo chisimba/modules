4dfd34cdbd19f6d848e534d59776c09d989b32e5
pwando <unknown@unknown.com>
2010-07-11 17:43:52 +0000

    applied function on form

--- eportfolio/templates/content/viewassertion_tpl.php
+++ eportfolio/templates/content/viewassertion_tpl.php
@@ -1,20 +1,5 @@
 <?php
-// Load classes.
-$this->loadClass("form", "htmlelements");
-$this->loadClass("textinput", "htmlelements");
-$this->loadClass('textarea', 'htmlelements');
-$this->loadClass("button", "htmlelements");
-$this->loadClass("htmltable", 'htmlelements');
-//reflectId
-$singleView = $this->objGetall->viewSingleAssertion($assertionId);
-echo $singleView;
+echo $this->objGetall->viewSingleAssertion($assertionId);
 echo $this->objGetall->viewPartForm('singleassertion', 'assertionId', $assertionId );
-//Get Object
-$this->objIcon = &$this->newObject('geticon', 'htmlelements');
-$objLayer3 = $this->newObject('layer', 'htmlelements');
-$this->objIcon->setIcon('close');
-$this->objIcon->extra = " onclick='javascript:window.close()'";
-$objLayer3->align = 'center';
-$objLayer3->str = $this->objIcon->show();
-echo $objLayer3->show();
+echo $this->objGetall->getCloseBtn();
 ?>
