236bb677212b2bd45b670acc8d3863b551790618
charlvn <unknown@unknown.com>
2010-07-19 23:35:08 +0000

    Added postinstall hook (untested - conference code).

---
+++ wurfl/patches/installscripts_class_inc.php
@@ -0,0 +1,15 @@
+<?php
+
+class wurfl_installscripts
+{
+    public function postinstall()
+    {
+        $zip = new ZipArchive();
+
+        if ($zip->open($this->getResourcePath('wurfl-2.0.18.xml.zip')) === TRUE) {
+            $zip->extractTo($this->getResourcePath(''));
+            $zip->close();
+        }
+    }
+}
+?>
