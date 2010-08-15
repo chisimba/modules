1d63b0f40764cfd56a21eb9d29d7b66af05048a5
charlvn <unknown@unknown.com>
2010-08-15 15:25:14 +0000

    Wrote code to add tweets into the tweetstore.

---
+++ tweetstore/controller.php
@@ -0,0 +1,38 @@
+<?php
+
+class tweetstore extends controller
+{
+    private $objJson;
+    private $objMongo;
+    private $objSysConfig;
+    private $whitelist;
+
+    public function init()
+    {
+        $this->objJson = $this->getObject('json', 'utilities');
+        $this->objMongo = $this->getObject('mongoops', 'mongo');
+        $this->objSysConfig = $this->getObject('dbsysconfig', 'sysconfig');
+
+        $collection = $this->objSysConfig->getValue('collection', 'tweetstore');
+        $this->objMongo->setCollection($collection);
+
+        $whitelist = $this->objSysConfig->getValue('whitelist', 'tweetstore');
+        $this->whitelist = explode('|', $whitelist);
+        if (!in_array($_SERVER['SERVER_ADDR'], $this->whitelist)) {
+            $this->whitelist[] = $_SERVER['SERVER_ADDR'];
+        }
+    }
+
+    public function dispatch()
+    {
+        switch ($this->getParam('action')) {
+            case 'add':
+                if (in_array($_SERVER['REMOTE_ADDR'], $this->whitelist)) {
+                    $json = file_get_contents('php://input');
+                    $data = $this->objJson($json);
+                    $this->objMongo->insert($data);
+                }
+                break;
+        }
+    }
+}
