9d20e98832229563d454c00218d1772345c028f2
charlvn <unknown@unknown.com>
2010-08-15 22:15:31 +0000

    Added configuration.

--- redis/classes/redisops_class_inc.php
+++ redis/classes/redisops_class_inc.php
@@ -4,10 +4,21 @@ class redisops extends object
 {
     private $objRedis;
 
+    /**
+     * Instance of the dbsysconfig class of the sysconfig module.
+     *
+     * @access private
+     * @var    object
+     */
+    private $objSysConfig;
+
     public function init()
     {
+        $this->objSysConfig = $this->getObject('dbsysconfig', 'sysconfig');
+        $host = $this->objSysConfig->getValue('host', 'redis');
+        $port = $this->objSysConfig->getValue('port', 'redis');
         include $this->getResourcePath('redis.php');
-        $this->objRedis = new php_redis('127.0.0.1', 6379);
+        $this->objRedis = new php_redis($host, $port);
     }
 
     public function get($key)
