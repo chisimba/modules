51c6b85db79115ded375e1f2ccd57ab0db01c0ac
nic <unknown@unknown.com>
2010-08-17 10:50:30 +0000

    fixed display of blocks

--- cms/templates/layout/cms_layout_tpl.php
+++ cms/templates/layout/cms_layout_tpl.php
@@ -60,7 +60,13 @@ switch($currentAction){
 // Add left blocks    
 if(!empty($leftPageBlocks)) {
     foreach($leftPageBlocks as $pbks) {
-        $blockId = $pbks['blockid'];
+        
+		if($objModule->checkIfRegistered('textblock')) {
+			$blockDisplay = $this->newObject('textblockbase', 'textblock');
+			$blockDisplay->setData($pbks['blockid']);
+			$leftSide .= $blockDisplay->show();
+		}
+		/*$blockId = $pbks['blockid'];
         $blockToShow = $objDbBlocks->getBlock($blockId);
 
 		
@@ -71,7 +77,6 @@ if(!empty($leftPageBlocks)) {
 		$cssClass = 'featurebox';
 
 		//If a textblock is being used then check the show title fields
-		if($objModule->checkIfRegistered('textblock')) {
 			$txtBlockArr = $objTextBlock->getBlock($blockId);
 
 			if (isset($txtBlockArr['show_title']) && $txtBlockArr['show_title'] != '1') {
@@ -92,6 +97,7 @@ if(!empty($leftPageBlocks)) {
 		//TODO: Add support for hiding fields to core block module
 
 		$leftSide .= $objBlocks->showBlock($blockToShow['blockname'], $blockToShow['moduleid'], NULL, 20, TRUE, $showToggle, 'default', $showTitle, $cssClass, $cssId);
+		*/
     }
 }
 
@@ -114,7 +120,12 @@ $rightSide = '';
 if(!empty($pageBlocks)) {
     $hasBlocks = TRUE;
     foreach($pageBlocks as $pbks) {
-        $blockId = $pbks['blockid'];
+		if($objModule->checkIfRegistered('textblock')) {
+			$blockDisplay = $this->newObject('textblockbase', 'textblock');
+			$blockDisplay->setData($pbks['blockid']);
+			$rightSide .= $blockDisplay->show();
+		}
+        /*$blockId = $pbks['blockid'];
         $blockToShow = $objDbBlocks->getBlock($blockId);
 
 		$showToggle = TRUE;
@@ -145,10 +156,8 @@ if(!empty($pageBlocks)) {
 		//TODO: Add support for hiding fields to core block module
 
 		$rightSide .= $objBlocks->showBlock($blockToShow['blockname'], $blockToShow['moduleid'], NULL, 20, TRUE, $showToggle, 'default', $showTitle, $cssClass, $cssId);
+		*/
     }
-}
-if ($objModule) {
-	
 }
 
 $cssLayout = $this->newObject('csslayout', 'htmlelements');
