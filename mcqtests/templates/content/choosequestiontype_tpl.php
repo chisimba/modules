dd1d0e8dbc766b162299ca18288d0765ed5a0819
nguni52 <unknown@unknown.com>
2010-08-24 07:42:58 +0000

    fixed bug for pagination

--- mcqtests/templates/content/choosequestiontype_tpl.php
+++ mcqtests/templates/content/choosequestiontype_tpl.php
@@ -14,6 +14,20 @@ $this->loadclass('link','htmlelements');
 $this->loadClass('label', 'htmlelements');
 $this->loadClass('fieldsetex', 'htmlelements');
 
+/*$extbase = '<script language="JavaScript" src="'.$this->getResourceUri('ext-3.0-rc2/adapter/ext/ext-base.js','htmlelements').'" type="text/javascript"></script>';
+$extalljs = '<script language="JavaScript" src="'.$this->getResourceUri('ext-3.0-rc2/ext-all.js','htmlelements').'" type="text/javascript"></script>';
+$extallcss = '<link rel="stylesheet" type="text/css" href="'.$this->getResourceUri('ext-3.0-rc2/resources/css/ext-all.css','htmlelements').'"/>';*/
+$mainjs = '<script language="JavaScript" src="'.$this->getResourceUri('js/main.js').'" type="text/javascript"></script>';
+/*$mcqdb = '<script language="JavaScript" src="'.$this->getResourceUri('js/mcqdb.js').'" type="text/javascript"></script>';
+$buttoncss = '<link rel="stylesheet" type="text/css" href="'.$this->getResourceUri('css/buttons.css').'"/>';
+
+$this->appendArrayVar('headerParams', $extallcss);
+$this->appendArrayVar('headerParams', $extbase);
+$this->appendArrayVar('headerParams', $extalljs);*/
+$this->appendArrayVar('headerParams', $mainjs);
+/*$this->appendArrayVar('headerParams', $mcqdb);
+$this->appendArrayVar('headerParams', $buttoncss);
+*/
 $this->dbQuestions = $this->newObject('dbquestions');
 
 
@@ -31,6 +45,17 @@ if (empty($questions)) {
     $count = 0;
 }
 
+
+$mainForm = '<div id="mainform">';
+echo '<strong><h1>'.$test['name'].'</h1></strong>';
+
+/*$existingQuestions = new dropdown('existingQ');
+$existingQuestions->setId("existingQ");
+$existingQuestions->addOption('-', '[-Select Method-]');
+$existingQuestions->addOption('newQ', 'New questions');
+$existingQuestions->addOption('oldQ', 'Choose from database');
+$existingQuestionsLabel = new label ('Select Method ', 'existingQ');*/
+
 $batchOptions = new dropdown('qnoption');
 $batchOptions->setId("qnoption");
 $batchOptions->addOption('-', '[-Select question type-]');
@@ -38,56 +63,19 @@ $batchOptions->addOption('mcq', 'MCQ questions');
 $batchOptions->addOption('freeform', 'Free form test entry questions');
 $batchLabel = new label ('Select question type ', 'input_qnoptionlabel');
 
-echo '<strong><h1>'.$test['name'].'</h1></strong>';
 $fd=$this->getObject('fieldsetex','htmlelements');
 
+$fd->addLabel('<strong>'.$existingQuestionsLabel->show().'</strong>'.$existingQuestions->show());
 $fd->addLabel('<strong>'.$batchLabel->show().'</strong>'.$batchOptions->show());
 $fd->setLegend('Select question type');
 $formmanager=$this->getObject('formmanager');
 
 $questionContentStr='<div id="addquestion">'.$formmanager->createAddQuestionForm($test).'</div>';
 $questionContentStr.='<div id="freeform">'.$formmanager->createAddFreeForm($test).'</div>';
-
+//$questionContentStr.='<div id="dbquestions">'.$formmanager->createDatabaseQuestions($oldQuestions).'</div>';
+//$questionContentStr.='<div id="mcqGrid"></div>';
 $fd->addLabel($questionContentStr);
-echo $fd->show();
-
-
+$mainForm .= $fd->show().'</div>';
+echo $mainForm;
 
 ?>
-<script type="text/javascript" language="javascript">
-    //<![CDATA[
-
-    jQuery(document).ready(function() {
-
-        jQuery('#freeform').hide();
-        jQuery('#addquestion').hide();
-        jQuery("#qnoption").change(function(){
-            var val=this.value;
-
-            if(val == 'freeform'){
-                jQuery('#freeform').show();
-                jQuery('#addquestion').hide();
-            }else if(val == 'mcq'){
-                jQuery('#addquestion').show();
-                jQuery('#freeform').hide();
-            }else{
-                jQuery('#freeform').hide();
-                jQuery('#addquestion').hide();
-            }
-
-        });
-    });
-
-    function processQuestionType()
-    {
-        if (document.getElementById('input_qnoption').value == '-')
-        {
-            alert('Please select an action');
-            document.getElementById('input_qnoption').focus();
-        }  else {
-            //document.getElementById('form_qnform').submit();
-            document.getElementById('input_qnoptionlabel').textContent='Updated!';
-        }
-    }
-    //]]>
-</script>
