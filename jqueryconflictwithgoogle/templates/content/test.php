c2e1e7131d43ad20785f68c76260c9c572a8b164
Derek Keats <derek@dkeats.com>
2010-09-07 15:05:06 +0000

    Fixed the coding bugs that were causing modal windows not to work in Chrome.

--- jqueryconflictwithgoogle/templates/content/test.php
+++ jqueryconflictwithgoogle/templates/content/test.php
@@ -1,4 +1,4 @@
-<script type=""  src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+
 <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
 <script type=""  src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
 
