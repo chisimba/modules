8b2bb02aa8c3adfaa4a9245377ad8ed9dc56814e
charlvn <unknown@unknown.com>
2010-09-08 22:38:16 +0000

    Converted tabs to spaces.

--- blogcomments/controller.php
+++ blogcomments/controller.php
@@ -62,23 +62,23 @@ class blogcomments extends controller
     */
   public  function init()
     {
-    	try {
-    		$this->objDbcomm = $this->getObject('dbblogcomments');
-    		$this->objComm = $this->getObject('commentapi');
-        	//Retrieve the action parameter from the querystring
-        	$this->action = $this->getParam('action', Null);
-        	//Create an instance of the User object
-        	$this->objUser =  & $this->getObject("user", "security");
-        	//Create an instance of the language object
-        	$this->objLanguage = &$this->getObject("language", "language");
+        try {
+            $this->objDbcomm = $this->getObject('dbblogcomments');
+            $this->objComm = $this->getObject('commentapi');
+            //Retrieve the action parameter from the querystring
+            $this->action = $this->getParam('action', Null);
+            //Create an instance of the User object
+            $this->objUser =  & $this->getObject("user", "security");
+            //Create an instance of the language object
+            $this->objLanguage = &$this->getObject("language", "language");
                 // Create an instance of the modules object.
                 $this->objModules = $this->getObject('modules', 'modulecatalogue');
-    	}
-    	catch (customException $e)
-    	{
-    		customException::cleanUp();
-    		exit;
-    	}
+        }
+        catch (customException $e)
+        {
+            customException::cleanUp();
+            exit;
+        }
     }
 
     /**
@@ -89,103 +89,103 @@ class blogcomments extends controller
     * @param void
     * @return void
     */
- 	public  function dispatch()
+     public  function dispatch()
     {
         $action = $this->getParam('action');
-    	switch ($action) {
+        switch ($action) {
             case null:
-            	$this->setVarByRef('errmsg', $this->objLanguage->languageText("mod_blogcomments_phrase_noaction", "blogcomments"));
-            	return 'noaction_tpl.php';
-            	break;
+                $this->setVarByRef('errmsg', $this->objLanguage->languageText("mod_blogcomments_phrase_noaction", "blogcomments"));
+                return 'noaction_tpl.php';
+                break;
             case 'add':
-            	//check if the user is logged in
-				if($this->objUser->isLoggedIn() == TRUE)
-				{
-					$this->setVar('pageSuppressToolbar', TRUE);
-                	$this->setVar('pageSuppressBanner', TRUE);
-                	$this->setVar('pageSuppressIM',TRUE);
-                	//Suppress footer in the page (keep it simple)
-                	$this->setVar('suppressFooter', TRUE);
-                	return "input_tpl.php";
+                //check if the user is logged in
+                if($this->objUser->isLoggedIn() == TRUE)
+                {
+                    $this->setVar('pageSuppressToolbar', TRUE);
+                    $this->setVar('pageSuppressBanner', TRUE);
+                    $this->setVar('pageSuppressIM',TRUE);
+                    //Suppress footer in the page (keep it simple)
+                    $this->setVar('suppressFooter', TRUE);
+                    return "input_tpl.php";
                 }
                 else {
-                	return 'notloggedin_tpl.php';
+                    return 'notloggedin_tpl.php';
                 }
                 break;
 
             case 'addtodb':
-            	//$this->requiresLogin(FALSE);
-            	if(!$this->objUser->isLoggedIn())
-            	{
-            		$captcha = $this->getParam('request_captcha');
-            	}
-            	$addinfo['useremail'] = $this->getParam('email');
-            	$addinfo['postuserid'] = $this->getParam('userid');
-            	$addinfo['postid'] = $this->getParam('postid');
-            	$addinfo['table'] = $this->getParam('table');
-            	$addinfo['mod'] = $this->getParam('mod');
-            	$addinfo['aurl'] = $this->getParam('url');
-            	$addinfo['commentauthor'] = $this->getParam('commentauthor');
-            	$addinfo['ctype'] = $this->getParam('type');
-            	$addinfo['comment'] = $this->getParam('comment');
-            	$addinfo = $this->objComm->addToDb($addinfo);
-            	//print_r($addinfo);
-            	//check that the captcha is correct
-            	if(!$this->objUser->isLoggedIn())
-            	{
-          			if (md5(strtoupper($captcha)) != $this->getParam('captcha') || empty($captcha))
-          			{
-          				//get a timeoutmessage and display it...
-          				$tmsg = $this->getObject('timeoutmessage', 'htmlelements');
-          				$tmsg->setMessage = $this->objLanguage->languageText("mod_blogcomments_badcaptcha", "blogcomments");
-          				$msg = $tmsg->show();
-          				$this->setVarByRef('msg', $msg);
-          				$this->nextAction('viewsingle',array('postid' => $addinfo['postid'], 'userid' => $this->objUser->userId(), 'comment' => $addinfo['comment'], 'useremail' => $addinfo['useremail']), $addinfo['mod']);
-          				exit;
-          			}
-          		}
-            	//print_r($addinfo);die();
-            	$this->objDbcomm->addComm2Db($addinfo);
+                //$this->requiresLogin(FALSE);
+                if(!$this->objUser->isLoggedIn())
+                {
+                    $captcha = $this->getParam('request_captcha');
+                }
+                $addinfo['useremail'] = $this->getParam('email');
+                $addinfo['postuserid'] = $this->getParam('userid');
+                $addinfo['postid'] = $this->getParam('postid');
+                $addinfo['table'] = $this->getParam('table');
+                $addinfo['mod'] = $this->getParam('mod');
+                $addinfo['aurl'] = $this->getParam('url');
+                $addinfo['commentauthor'] = $this->getParam('commentauthor');
+                $addinfo['ctype'] = $this->getParam('type');
+                $addinfo['comment'] = $this->getParam('comment');
+                $addinfo = $this->objComm->addToDb($addinfo);
+                //print_r($addinfo);
+                //check that the captcha is correct
+                if(!$this->objUser->isLoggedIn())
+                {
+                      if (md5(strtoupper($captcha)) != $this->getParam('captcha') || empty($captcha))
+                      {
+                          //get a timeoutmessage and display it...
+                          $tmsg = $this->getObject('timeoutmessage', 'htmlelements');
+                          $tmsg->setMessage = $this->objLanguage->languageText("mod_blogcomments_badcaptcha", "blogcomments");
+                          $msg = $tmsg->show();
+                          $this->setVarByRef('msg', $msg);
+                          $this->nextAction('viewsingle',array('postid' => $addinfo['postid'], 'userid' => $this->objUser->userId(), 'comment' => $addinfo['comment'], 'useremail' => $addinfo['useremail']), $addinfo['mod']);
+                          exit;
+                      }
+                  }
+                //print_r($addinfo);die();
+                $this->objDbcomm->addComm2Db($addinfo);
 
-            	$this->nextAction('viewsingle',array('postid' => $addinfo['postid'], 'userid' => $this->objUser->userId()), $addinfo['mod']);
-            	
+                $this->nextAction('viewsingle',array('postid' => $addinfo['postid'], 'userid' => $this->objUser->userId()), $addinfo['mod']);
+                
             case 'updatecomment':
-            	$commid = $this->getParam('commid');
-            	$edits = nl2br($this->getParam('newcomment'));
-            	echo $this->objDbcomm->updateComment($commid, $edits);
-            	break;
-            	
+                $commid = $this->getParam('commid');
+                $edits = nl2br($this->getParam('newcomment'));
+                echo $this->objDbcomm->updateComment($commid, $edits);
+                break;
+                
             case 'deletecomment':
-            	$commentid = $this->getParam('commentid');
-            	$postid = $this->getParam('postid');
-            	$module = 'blog';
-            	$this->objDbcomm->deletecomment($commentid);
-            	
-            	$this->nextAction('viewsingle',array('postid' => $postid), $module);
+                $commentid = $this->getParam('commentid');
+                $postid = $this->getParam('postid');
+                $module = 'blog';
+                $this->objDbcomm->deletecomment($commentid);
+                
+                $this->nextAction('viewsingle',array('postid' => $postid), $module);
             
             case 'moderate':
-            	$this->setVar('pageSuppressXML', TRUE);
-            	// Case to moderate comments based on a user id. 
-            	$userid = $this->objUser->userId();
-            	// grab all comments made against this userid...
-            	$comm4me = $this->objDbcomm->grabCommentsByUser($userid);
-            	// grab all comments made by me...
-            	$meemail = $this->objUser->email($userid); 
-            	// grab comments made by me from blogcomments table
-            	$mycomments = $this->objDbcomm->getMyComments($meemail);
-            	// set the two datasets as refs and send to the template
-            	$this->setVarByRef('mycomments', $mycomments);
-            	$this->setVarByRef('comm4me', $comm4me);     	
-            	// Interface to handle the moderation.
-            	return 'moderation_tpl.php';
-            	break;
-            	
+                $this->setVar('pageSuppressXML', TRUE);
+                // Case to moderate comments based on a user id. 
+                $userid = $this->objUser->userId();
+                // grab all comments made against this userid...
+                $comm4me = $this->objDbcomm->grabCommentsByUser($userid);
+                // grab all comments made by me...
+                $meemail = $this->objUser->email($userid); 
+                // grab comments made by me from blogcomments table
+                $mycomments = $this->objDbcomm->getMyComments($meemail);
+                // set the two datasets as refs and send to the template
+                $this->setVarByRef('mycomments', $mycomments);
+                $this->setVarByRef('comm4me', $comm4me);         
+                // Interface to handle the moderation.
+                return 'moderation_tpl.php';
+                break;
+                
             case 'getcomments':
                 $itemid = $this->getParam('itemid', 'test');
                 $data = $this->objDbcomm->grabComments($itemid);
                 $data = array_reverse($data);
                 $response = $this->getParam("jsoncallback") . "(" . json_encode($data) . ")";
-	            echo $response;
+                echo $response;
                 break;
                 
             case 'savecomment':
@@ -195,10 +195,10 @@ class blogcomments extends controller
                 $ins = array('userid' => $this->objUser->userId(), 'comment_author' => $name, 'comment_content' => $comment, 'comment_parentid' => $itemid, 'comment_date' => time()); 
                 $this->objDbcomm->insert($ins);
                 break;
-            		
+                    
             default:
-            	die("unknown action");
-            	break;
+                die("unknown action");
+                break;
         }
     }
     
