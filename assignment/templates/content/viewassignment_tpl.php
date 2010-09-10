441bb183c065b5a4324e2bba5e9b9e7931657946
Jeremy O'Connor <joconnor@uwc.ac.za>
2010-09-10 12:35:24 +0000

    Allow .pptx files to be uploaded.

--- assignment/templates/content/viewassignment_tpl.php
+++ assignment/templates/content/viewassignment_tpl.php
@@ -236,7 +236,7 @@ if ($this->isValid('markassignments')) {
             $form->extra = 'enctype="multipart/form-data"';
 
             $objUpload = $this->newObject('uploadinput', 'filemanager');
-            $objUpload->restrictFileList = array('doc', 'odt', 'rtf', 'txt', 'docx', 'mp3', 'ppt');
+            $objUpload->restrictFileList = array('doc', 'odt', 'rtf', 'txt', 'docx', 'mp3', 'ppt', 'pptx');
 
             $button = new button('submitform', $this->objLanguage->languageText('mod_assignment_uploadassignment', 'assignment', 'Upload Assignment'));
             $button->setToSubmit();
@@ -259,7 +259,7 @@ if ($this->isValid('markassignments')) {
             $objSelectFile = $this->newObject('selectfile', 'filemanager');
 
             $objSelectFile->name = 'assignment';
-            $objSelectFile->restrictFileList = array('doc', 'odt', 'rtf', 'txt', 'docx', 'mp3', 'ppt');
+            $objSelectFile->restrictFileList = array('doc', 'odt', 'rtf', 'txt', 'docx', 'mp3', 'ppt', 'pptx');
 
             $button = new button('submitform', 'Submit Assignment');
             $button->setToSubmit();
