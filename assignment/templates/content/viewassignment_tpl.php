49d21b71dd55c7cf461aa06752329fece0a04be7
Jeremy O'Connor <joconnor@uwc.ac.za>
2010-09-15 12:17:33 +0000

    Allow .mp3 and .pdf files to be uploaded.

--- assignment/templates/content/viewassignment_tpl.php
+++ assignment/templates/content/viewassignment_tpl.php
@@ -236,7 +236,7 @@ if ($this->isValid('markassignments')) {
             $form->extra = 'enctype="multipart/form-data"';
 
             $objUpload = $this->newObject('uploadinput', 'filemanager');
-            $objUpload->restrictFileList = array('doc', 'odt', 'rtf', 'txt', 'docx', 'mp3', 'ppt', 'pptx');
+            $objUpload->restrictFileList = array('doc', 'odt', 'rtf', 'txt', 'docx', 'mp3', 'ppt', 'pptx', 'mp3', 'pdf');
 
             $button = new button('submitform', $this->objLanguage->languageText('mod_assignment_uploadassignment', 'assignment', 'Upload Assignment'));
             $button->setToSubmit();
@@ -259,7 +259,7 @@ if ($this->isValid('markassignments')) {
             $objSelectFile = $this->newObject('selectfile', 'filemanager');
 
             $objSelectFile->name = 'assignment';
-            $objSelectFile->restrictFileList = array('doc', 'odt', 'rtf', 'txt', 'docx', 'mp3', 'ppt', 'pptx');
+            $objSelectFile->restrictFileList = array('doc', 'odt', 'rtf', 'txt', 'docx', 'mp3', 'ppt', 'pptx', 'mp3', 'pdf');
 
             $button = new button('submitform', 'Submit Assignment');
             $button->setToSubmit();
