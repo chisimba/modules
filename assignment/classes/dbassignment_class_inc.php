48e8da9aec02759e6425fd87c387b0662e5ae97e
Jeremy O'Connor <joconnor@uwc.ac.za>
2010-09-15 08:58:08 +0000

    Added debugging code.

--- assignment/classes/dbassignment_class_inc.php
+++ assignment/classes/dbassignment_class_inc.php
@@ -245,8 +245,11 @@ class dbassignment extends dbtable {
      */
     public function updateAssignment($id, $name, $description, $resubmit, $format, $mark, $percentage, $opening_date, $closing_date, $assesment_type, $emailAlert, $filename_conversion) {
 
+        trigger_error('$filename_conversion='.$filename_conversion);
+
         $id = $this->update('id', $id, array(
                     'name' => $name,
+
                     'description' => $description,
                     'resubmit' => $resubmit,
                     'format' => $format,
