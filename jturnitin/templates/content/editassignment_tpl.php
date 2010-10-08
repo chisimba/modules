586ce38ec76a4b53b162a228899eeb8f098019ed
David Wafula <davidwaf@gmail.com>
2010-10-08 00:18:02 +0000

    turnit module using simple java sockets instead of curl to connect to turnitin

---
+++ jturnitin/templates/content/editassignment_tpl.php
@@ -0,0 +1,35 @@
+<?php
+
+$objSysConfig  = $this->getObject('altconfig','config');
+$objExtJS = $this->getObject('extjs','ext');
+
+$objExtJS->show();
+$ext = "";
+$this->appendArrayVar('headerParams', '
+	        	<script type="text/javascript">
+	        		var storeUri = "'.str_replace('&amp;','&',$this->uri(array('action' => 'json_getstudentassessments', 'module' => 'turnitin'))).'";
+	        		var baseuri = "'.$objSysConfig->getsiteRoot().'index.php";
+                                var ainst="'.$instructions.'";
+                                var title="'.$title.'";
+                                var dstart="'.$datestart.'";
+                                var dend="'.$dateend.'";
+                                var assignmentid="'.$id.'";
+                                var sviewreports="'.$sviewreports.'";
+                                var generate="'.$generate.'";
+                                var repository="'.$repository.'";
+                                var searchpapers="'.$searchpapers.'";
+                                var searchinternet="'.$searchinternet.'";
+                                var searchjournals="'.$searchjournals.'";
+                                var searchinstitution="'.$searchinstitution.'";
+                                var latesubmissions="'.$latesubmissions.'";
+
+                                   
+	        	</script>');
+
+$ext .=$this->getJavaScriptFile('editassignment.js', 'turnitin');
+$ext .=$this->getJavaScriptFile('editadvancedassigoptions.js', 'turnitin');
+$ext .=$this->getJavaScriptFile('editadvancedassigoptions2.js', 'turnitin');
+$this->appendArrayVar('headerParams', $ext);
+echo '<div id="surface"></div>';
+
+?>
