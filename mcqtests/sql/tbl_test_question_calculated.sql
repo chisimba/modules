6ff19c914d2554f9bbd882752f45060370a2875f
nguni52 <unknown@unknown.com>
2010-10-11 06:36:39 +0000

    numerical, short answer, matching questions and description addition

---
+++ mcqtests/sql/tbl_test_question_calculated.sql
@@ -0,0 +1,34 @@
+<?php
+$tablename = 'tbl_test_question_calculated';
+$options = array('comment' => 'This table lists the calculated questions', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
+$fields = array(
+    'id' => array(
+        'type' => 'text',
+        'length' => 32,
+        ),
+    'questionid' => array(
+        'type' => 'text',
+        'length' => 10
+        ),
+    'answer' => array(
+        'type' => 'text',
+        'length' => 10
+        ),
+    'tolerance' => array(
+        'type' => 'text',
+        'length' => 20
+        ),
+    'tolerancetype' => array(
+        'type' => 'text',
+        'length' => 10
+        ),
+    'correctanswerlength' => array(
+        'type' => 'text',
+        'length' => 10
+        ),
+    'correctanswerformat' => array(
+        'type' => 'text',
+        'length' => 10
+        )
+);
+?>
