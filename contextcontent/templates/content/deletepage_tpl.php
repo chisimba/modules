009da6501efbc069f441e9ecc79aa2785bc4dba4
David Wafula <davidwaf@gmail.com>
2010-10-12 08:43:14 +0000

    Added defualt lang items on delete page button

--- contextcontent/templates/content/deletepage_tpl.php
+++ contextcontent/templates/content/deletepage_tpl.php
@@ -24,7 +24,7 @@ $radio->setBreakSpace('</p><p>');
 
 $form->addToForm('<p >'.$radio->show().'</p>');
 
-$button = new button ('confirm', $this->objLanguage->languageText('mod_contextcontent_confirmdelcontextpages','contextcontent'));
+$button = new button ('confirm', $this->objLanguage->languageText('mod_contextcontent_confirmdelcontextpages','contextcontent','Delete'));
 $button->setToSubmit();
 
 $hiddeninput = new hiddeninput('id', $page['id']);
