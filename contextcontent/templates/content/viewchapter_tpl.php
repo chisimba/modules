f45cab695bb6cc88c1ecdee8d7d70bd597586130
David Wafula <davidwaf@gmail.com>
2010-10-12 09:39:21 +0000

    added addpage link on viewchapter

--- contextcontent/templates/content/viewchapter_tpl.php
+++ contextcontent/templates/content/viewchapter_tpl.php
@@ -1,28 +1,33 @@
 <?php
+
 $objFile = $this->getObject('dbfile', 'filemanager');
 $objHead = $this->newObject('htmlheading', 'htmlelements');
 $this->loadClass('link', 'htmlelements');
+
+//A link to adding a page
+$addPageLink = new link($this->uri(array('action' => 'addpage', 'context' => $this->contextCode, 'chapter' =>  $chapterId)));
+$addPageLink->link = $this->objLanguage->languageText('mod_contextcontent_addcontextpages', 'contextcontent');
 
 $nextPage = "";
 $chapterList = "";
 $editChapter = "";
 
-if($firstPage != FALSE) {
+if ($firstPage != FALSE) {
     //Create Next Page Link
-    $link = new link ($this->uri(array('action'=>'viewpage', 'id'=>$firstPage, 'prevchapterid'=>$chapterId), 'contextcontent'));
-    $link->link = $this->objLanguage->languageText('mod_contextcontent_nextpage','learningcontent','Next Page').': '.htmlentities($page['menutitle']).' &#187;';
+    $link = new link($this->uri(array('action' => 'viewpage', 'id' => $firstPage, 'prevchapterid' => $chapterId), 'contextcontent'));
+    $link->link = $this->objLanguage->languageText('mod_contextcontent_nextpage', 'learningcontent', 'Next Page') . ': ' . htmlentities($page['menutitle']) . ' &#187;';
     $nextPage = $link->show();
 }
 
 //Create Return to Chapter List Link
-$link = new link ($this->uri(array('action'=>'showcontextchapters', 'chapterId'=>$chapter['id'], 'prevchapterid'=>$chapterId), 'contextcontent'));
-$link->link = '&#171; '.$this->objLanguage->languageText('mod_contextcontent_returntochapterlist','learningcontent','Return to Chapter List');
+$link = new link($this->uri(array('action' => 'showcontextchapters', 'chapterId' => $chapter['id'], 'prevchapterid' => $chapterId), 'contextcontent'));
+$link->link = '&#171; ' . $this->objLanguage->languageText('mod_contextcontent_returntochapterlist', 'learningcontent', 'Return to Chapter List');
 $chapterList = $link->show();
 
 if ($this->isValid('addpage')) {
     //Create Edit Chapter Link
-    $link = new link ($this->uri(array('action'=>'editchapter', 'id'=>$chapterId, 'prevaction'=>'viewchapter'), 'contextcontent'));
-    $link->link = $this->objLanguage->languageText('mod_contextcontent_editchapter','learningcontent','Edit Chapter');
+    $link = new link($this->uri(array('action' => 'editchapter', 'id' => $chapterId, 'prevaction' => 'viewchapter'), 'contextcontent'));
+    $link->link = $this->objLanguage->languageText('mod_contextcontent_editchapter', 'learningcontent', 'Edit Chapter');
     $editChapter = $link->show();
 }
 
@@ -30,18 +35,20 @@ $table = $this->newObject('htmltable', 'htmlelements');
 //$table->border='1';
 $table->startRow();
 $table->cssClass = "pagenavigation";
-$table->addCell($chapterList, '33%', 'top');
-$table->addCell($editChapter, '33%', 'top');
-$table->addCell($nextPage, '33%', 'top', 'right');
+$table->addCell($chapterList, '25%', 'top');
+$table->addCell($editChapter, '25%', 'top');
+$table->addCell($addPageLink->show(),'25%', 'top');
+$table->addCell($nextPage, '25%', 'top', 'right');
 $table->endRow();
 
 $topTable = $this->newObject('htmltable', 'htmlelements');
 //$topTable->border='1';
 $topTable->startRow();
 $topTable->cssClass = "toppagenavigation";
-$topTable->addCell($chapterList, '33%', 'top');
-$topTable->addCell($editChapter, '33%', 'top');
-$topTable->addCell($nextPage, '33%', 'top', 'right');
+$topTable->addCell($chapterList, '25%', 'top');
+$topTable->addCell($editChapter, '25%', 'top');
+$topTable->addCell($addPageLink->show(),'25%', 'top');
+$topTable->addCell($nextPage, '25%', 'top', 'right');
 $topTable->endRow();
 
 $objWashout = $this->getObject('washout', 'utilities');
@@ -50,14 +57,13 @@ $content = "";
 
 $introheader = new htmlheading();
 $introheader->type = 3;
-$introheader->str =$chapter['chaptertitle'].'&nbsp;&nbsp;-&nbsp;&nbsp;'. $this->objLanguage->languageText('mod_contextcontent_aboutchapter_introduction', 'contextcontent', 'Introduction)');
+$introheader->str = $chapter['chaptertitle'] . '&nbsp;&nbsp;-&nbsp;&nbsp;' . $this->objLanguage->languageText('mod_contextcontent_aboutchapter_introduction', 'contextcontent', 'Introduction)');
 
-$content.= $introheader->show().$objWashout->parseText($chapter['introduction']);
+$content.= $introheader->show() . $objWashout->parseText($chapter['introduction']);
 
 if ($this->isValid('addpage')) {
-    echo '<div id="tablenav">'.$topTable->show() . $content . '<hr />' . $table->show().'</div>';
+    echo '<div id="tablenav">' . $topTable->show() . $content . '<hr />' . $table->show() . '</div>';
 } else {
-    echo '<div id="tablenav">'.$topTable->show() . $content . '<hr />' . $table->show().'</div>';
+    echo '<div id="tablenav">' . $topTable->show() . $content . '<hr />' . $table->show() . '</div>';
 }
-
 ?>
