be21e0dcb83d8609e8aa4ef77dc0d30a0eac65b2
nguni52 <unknown@unknown.com>
2010-10-15 13:48:59 +0000

    matching and numerical questions modifications

--- mcqtests/templates/content/editnumericalquestion_tpl.php
+++ mcqtests/templates/content/editnumericalquestion_tpl.php
@@ -9,10 +9,15 @@ $this->setLayoutTemplate('mcqtests_layout_tpl.php');
 
 $numericalformmanager = $this->getObject('numerical_question');
 
-$testid = $test['id'];
-$edit = true;
+$testid = $this->getParam('id');
+if($mode == 'edit') {
+    $edit = true;
+    $questionId = $this->getParam('questionId');
+} else {
+    $edit = false;
+}
 // display the form for editing this question
-$questionContentStr.='<div id="numericalquestions">'.$numericalformmanager->numericalQForm($testid, $data, $edit).'</div>';
+$questionContentStr.='<div id="numericalquestions">'.$numericalformmanager->numericalQForm($testid, $data, $edit, $questionId).'</div>';
 
 echo $questionContentStr;
 ?>
