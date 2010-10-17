815f87dc54e1926c9c0f3e4deec1b2a77b8cf701
David Wafula <davidwaf@gmail.com>
2010-10-17 08:01:05 +0000

    added flag to check if user is in context or not

--- assignment/templates/content/assignment_home_tpl.php
+++ assignment/templates/content/assignment_home_tpl.php
@@ -1,34 +1,33 @@
 <?php
 
-$openLabel = $this->objLanguage->languageText('mod_assignment_open','assignment');
-$closedLabel = $this->objLanguage->languageText('mod_assignment_closed','assignment');
-$viewLabel = $this->objLanguage->languageText('mod_assignment_view','assignment');
-$uploadLabel = $this->objLanguage->languageText('mod_assignment_upload','assignment');
-$onlineLabel = $this->objLanguage->languageText('mod_assignment_online','assignment');
+$openLabel = $this->objLanguage->languageText('mod_assignment_open', 'assignment');
+$closedLabel = $this->objLanguage->languageText('mod_assignment_closed', 'assignment');
+$viewLabel = $this->objLanguage->languageText('mod_assignment_view', 'assignment');
+$uploadLabel = $this->objLanguage->languageText('mod_assignment_upload', 'assignment');
+$onlineLabel = $this->objLanguage->languageText('mod_assignment_online', 'assignment');
 
 // Set up html elements
-$this->loadClass('htmltable','htmlelements');
-$this->loadClass('htmlheading','htmlelements');
-$this->loadClass('link','htmlelements');
-$this->loadClass('button','htmlelements');
-$objIcon = $this->newObject('geticon','htmlelements');
-$objTimeOut = $this->newObject('timeoutMessage','htmlelements');
+$this->loadClass('htmltable', 'htmlelements');
+$this->loadClass('htmlheading', 'htmlelements');
+$this->loadClass('link', 'htmlelements');
+$this->loadClass('button', 'htmlelements');
+$objIcon = $this->newObject('geticon', 'htmlelements');
+$objTimeOut = $this->newObject('timeoutMessage', 'htmlelements');
 
 $objTrim = $this->getObject('trimstr', 'strings');
 $createButton = new button('submit', $this->objLanguage->languageText('mod_assignment_createassignments', 'assignment', 'Create Assignment'));
 $createButton->setToSubmit();
 
 $objHead = new htmlheading();
-$objHead->str=$this->objLanguage->languageText('mod_assignment_assignments', 'assignment', 'Assignments');
-$objHead->type=1;
+$objHead->str = $this->objLanguage->languageText('mod_assignment_assignments', 'assignment', 'Assignments');
+$objHead->type = 1;
 
 if ($this->isValid('add')) {
 
     //$objIcon->setIcon('add');
-    $link = new link ($this->uri(array('action'=>'add')));
+    $link = new link($this->uri(array('action' => 'add')));
     $link->link = $createButton->show();
-    $objHead->str .= ' '.$link->show();
-    
+    $objHead->str .= ' ' . $link->show();
 }
 
 echo $objHead->show();
@@ -36,15 +35,15 @@ echo $objHead->show();
 $objTable = $this->newObject('htmltable', 'htmlelements');
 
 $objTable->startHeaderRow();
-$objTable->addHeaderCell($this->objLanguage->languageText('word_name', 'system', 'Name'),'20%');
+$objTable->addHeaderCell($this->objLanguage->languageText('word_name', 'system', 'Name'), '20%');
 $objTable->addHeaderCell($this->objLanguage->languageText('mod_assignment_assignmenttype', 'assignment', 'Assignment Type'), '13%');
 //$objTable->addHeaderCell($this->objLanguage->languageText('word_description', 'system', 'Description'));
-$objTable->addHeaderCell(ucfirst($this->objLanguage->code2Txt('mod_assignment_lecturer', 'assignment', NULL, '[-author-]')),'15%');
-$objTable->addHeaderCell($this->objLanguage->languageText('mod_assignment_closingdate', 'assignment', 'Closing Date'),'15%');
-$objTable->addHeaderCell($this->objLanguage->languageText('word_status', 'system', 'Status'),'8%');
+$objTable->addHeaderCell(ucfirst($this->objLanguage->code2Txt('mod_assignment_lecturer', 'assignment', NULL, '[-author-]')), '15%');
+$objTable->addHeaderCell($this->objLanguage->languageText('mod_assignment_closingdate', 'assignment', 'Closing Date'), '15%');
+$objTable->addHeaderCell($this->objLanguage->languageText('word_status', 'system', 'Status'), '8%');
 
 if ($this->isValid('edit') && count($assignments) > 0) {
-    $objTable->addHeaderCell('&nbsp;','60');
+    $objTable->addHeaderCell('&nbsp;', '60');
 }
 
 $objTable->endHeaderRow();
@@ -54,10 +53,8 @@ if (count($assignments) == 0) {
 
 
     $objTable->startRow();
-    $objTable->addCell($this->objLanguage->languageText('mod_assignment_noassignments', 'assignment', 'No Assignments'),'','','','noRecordsMessage','colspan="6"');
+    $objTable->addCell($this->objLanguage->languageText('mod_assignment_noassignments', 'assignment', 'No Assignments'), '', '', '', 'noRecordsMessage', 'colspan="6"');
     $objTable->endRow();
-
-
 } else {
 
     $i = 0;
@@ -72,44 +69,52 @@ if (count($assignments) == 0) {
     $counter = 0;
 
 
-    foreach ($assignments as $assignment)
-    {
-        $class = ($i++%2 == 0) ? 'odd' : 'even';
+    foreach ($assignments as $assignment) {
+        $class = ($i++ % 2 == 0) ? 'odd' : 'even';
 
-        if($assignment['closing_date'] > date('Y-m-d H:i')) {
-            if(($assignment['opening_date'] < date('Y-m-d H:i')) || $assignment['opening_date'] == NULL) {
+        if ($assignment['closing_date'] > date('Y-m-d H:i')) {
+            if (($assignment['opening_date'] < date('Y-m-d H:i')) || $assignment['opening_date'] == NULL) {
                 $status = $openLabel;
             } else {
                 $status = $this->objLanguage->languageText('mod_assignment_notopenforentry', 'assignment', 'Not Open for Entry');
             }
-
         } else {
             $status = $closedLabel;
         }
 
-        $objLink = new link($this->uri(array('action'=>'view', 'id'=>$assignment['id'])));
-        $objLink->title = $viewLabel.' '.$assignment['name'];
+        $objLink = new link($this->uri(array('action' => 'view', 'id' => $assignment['id'])));
+        $objLink->title = $viewLabel . ' ' . $assignment['name'];
         $objLink->link = $assignment['name'];
 
 
         // Display whether the assignment is online or uploadable
-        if($assignment['format'] == 1){
+        if ($assignment['format'] == 1) {
             $format = $uploadLabel;
-        }else{
+        } else {
             $format = $onlineLabel;
         }
 
         $okToShow = FALSE;
 
-        if(($assignment['opening_date'] < date('Y-m-d H:i')) || $assignment['opening_date'] == NULL) {
+        if (($assignment['opening_date'] < date('Y-m-d H:i')) || $assignment['opening_date'] == NULL) {
             $okToShow = TRUE;
         }
 
-        if($assignment['visibility'] == '0'){
-            $groups=$this->objAssignmentGroups->getWorkgroups($assignment['id']);
-            if(count($groups)< 1){
-                $okToShow=FALSE;
+        if ($assignment['visibility'] == '0') {
+            $okToShow = FALSE;
+            $groups = $this->objAssignmentGroups->getWorkgroups($assignment['id']);
+            $mc = $this->getObject('modules', 'modulecatalogue');
+            if ($mc->checkIfRegistered('workgroup')) {
+                $objUser = $this->getObject('user', 'security');
+                $objGroups = $this->getObject('dbWorkgroupUsers', 'workgroup');
+                foreach ($groups as $group) {
+
+                    if ($objGroups->memberOfWorkGroup($objUser->userid(), $group['workgroup_id'])) {
+                        $okToShow = TRUE;
+                    }
+                }
             }
+           
         }
         if ($this->isValid('edit')) {
             $okToShow = TRUE;
@@ -120,21 +125,21 @@ if (count($assignments) == 0) {
             $counter++;
 
             $objTable->startRow();
-            $objTable->addCell($objLink->show(),'20%','','',$class);
-            $objTable->addCell($format,'13%','','',$class);
+            $objTable->addCell($objLink->show(), '20%', '', '', $class);
+            $objTable->addCell($format, '13%', '', '', $class);
             //$objTable->addCell($objTrim->strTrim(strip_tags($assignment['description']), 50),'','','',$class);
-            $objTable->addCell($this->objUser->fullname($assignment['userid']),'15%','','',$class);
-            $objTable->addCell($this->objDate->formatDate($assignment['closing_date']),'15%','','',$class);
-            $objTable->addCell($status,'8%','','',$class);
+            $objTable->addCell($this->objUser->fullname($assignment['userid']), '15%', '', '', $class);
+            $objTable->addCell($this->objDate->formatDate($assignment['closing_date']), '15%', '', '', $class);
+            $objTable->addCell($status, '8%', '', '', $class);
 
             if ($this->isValid('edit')) {
-                $editLink = new link ($this->uri(array('action'=>'edit', 'id'=>$assignment['id'])));
+                $editLink = new link($this->uri(array('action' => 'edit', 'id' => $assignment['id'])));
                 $editLink->link = $editIcon;
 
-                $deleteLink = new link ($this->uri(array('action'=>'delete', 'id'=>$assignment['id'])));
+                $deleteLink = new link($this->uri(array('action' => 'delete', 'id' => $assignment['id'])));
                 $deleteLink->link = $deleteIcon;
 
-                $objTable->addCell($editLink->show().'&nbsp;'.$deleteLink->show(), '60');
+                $objTable->addCell($editLink->show() . '&nbsp;' . $deleteLink->show(), '60');
             }
             $objTable->endRow();
         }
@@ -142,7 +147,7 @@ if (count($assignments) == 0) {
 
     if ($counter == 0) {
         $objTable->startRow();
-        $objTable->addCell($this->objLanguage->languageText('mod_assignment_noassignments', 'assignment', 'No Assignments'),'','','','noRecordsMessage','colspan="6"');
+        $objTable->addCell($this->objLanguage->languageText('mod_assignment_noassignments', 'assignment', 'No Assignments'), '', '', '', 'noRecordsMessage', 'colspan="6"');
         $objTable->endRow();
     }
 }
@@ -151,19 +156,17 @@ echo $objTable->show();
 
 
 if ($this->isValid('add')) {
-    $link = new link ($this->uri(array('action'=>'add')));
+    $link = new link($this->uri(array('action' => 'add')));
     $link->link = $this->objLanguage->languageText('mod_assignment_addassignment', 'assignment', 'Add Assignment');
 
-    echo '<p>'.$link->show().'</p>';
+    echo '<p>' . $link->show() . '</p>';
 }
 
 
 if ($this->objUser->isContextStudent($this->contextCode)) {
-	$this->objLink->link($this->uri(array('action'=>'displaylist')));
-	$this->objLink->link=$this->objLanguage->languageText('mod_assignment_submittedassignmentslist', 'assignment');
+    $this->objLink->link($this->uri(array('action' => 'displaylist')));
+    $this->objLink->link = $this->objLanguage->languageText('mod_assignment_submittedassignmentslist', 'assignment');
 
-	echo $this->objLink->show();
+    echo $this->objLink->show();
 }
-
-
 ?>
