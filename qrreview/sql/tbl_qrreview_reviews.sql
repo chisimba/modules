7bff1e4dc7454cfeb2812dde05f71e995c6e6772
paulscott <unknown@unknown.com>
2010-10-22 04:38:53 +0000

    table

---
+++ qrreview/sql/tbl_qrreview_reviews.sql
@@ -0,0 +1,43 @@
+<?php
+// Table Name
+$tablename = 'tbl_qrreview_reviews';
+
+//Options line for comments, encoding and character set
+$options = array('comment' => 'Reviews and ratings table', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
+
+// Fields
+$fields = array(
+	'id' => array(
+		'type' => 'text',
+		'length' => 32
+		),
+    'prodid' => array(
+		'type' => 'text',
+		'length' => 50,
+		),
+	'prodrate' => array(
+		'type' => 'text',
+		'length' => 10,
+		),
+	'prodcomm' => array(
+		'type' => 'clob',
+		),
+    'phone' => array(
+        'type' => 'text',
+        'length' => 12,
+    ),	
+	'creationdate' => array(
+		'type' => 'text',
+		'length' => 80,
+		),		
+	);
+//create other indexes here...
+
+$name = 'prodid';
+
+$indexes = array(
+                'fields' => array(
+                	'prodid' => array(),
+                )
+        );
+?>
