39fae2f2c2332e4e2cd950886aaadd557767b265
charlvn <unknown@unknown.com>
2010-10-28 12:25:48 +0000

    Added caption and made various improvements.

--- html5elements/classes/html5table_class_inc.php
+++ html5elements/classes/html5table_class_inc.php
@@ -6,14 +6,23 @@ class html5table extends object
      * Generates an HTML5 table.
      *
      * @access public
-     * @param  array  $data    The table contents.
-     * @param  array  $headers The column headers.
+     * @param  string $title    The title of the table. NULL for none.
+     * @param  array  $headers  The column headers. Empty array for none.
+     * @param  array  $contents The table contents. Empty array for none.
      * @return string The markup for the table.
      */
-    public function show(array $data, array $headers=array())
+    public function show($title, array $headers, array $contents)
     {
         $document = new DOMDocument();
         $table = $document->createElement('table');
+
+        if ($title !== NULL) {
+            $caption = $document->createElement('caption');
+            $table->appendChild($caption);
+
+            $text = $document->createTextNode($title);
+            $caption->appendChild($text);
+        }
 
         if (count($headers) > 0) {
             $thead = $document->createElement('thead');
@@ -31,20 +40,24 @@ class html5table extends object
             }
         }
 
-        $tbody = $document->createElement('tbody');
-        $table->appendChild($tbody);
+        if (count($contents) > 0) {
+            $tbody = $document->createElement('tbody');
+            $table->appendChild($tbody);
 
-        foreach ($data as $row) {
-            $tr = $document->createElement('tr');
-            $tbody->appendChild($tr);
+            foreach ($contents as $row) {
+                $tr = $document->createElement('tr');
+                $tbody->appendChild($tr);
 
-            foreach ($row as $value) {
-                $td = $document->createElement('td');
-                $tr->appendChild($td);
+                foreach ($row as $value) {
+                    $td = $document->createElement('td');
+                    $tr->appendChild($td);
 
-                $text = $document->createTextNode($value);
-                $td->appendChild($text);
+                    $text = $document->createTextNode($value);
+                    $td->appendChild($text);
+                }
             }
         }
+
+        return $table->saveHTML();
     }
 }
