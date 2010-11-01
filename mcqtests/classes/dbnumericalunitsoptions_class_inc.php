bfc0377b182234e4725ae291753d5cb269f309b8
nguni52 <unknown@unknown.com>
2010-11-01 13:41:15 +0000

    added a new table for numerical questions options

---
+++ mcqtests/classes/dbnumericalunitsoptions_class_inc.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @package mcqtests
+ * @filesource
+ */
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+/**
+ * Class for providing access to the table tbl_test_question_numericaloptions in the database
+ * @author Nguni Phakela
+ *
+ * @copyright (c) 2010 University of the Witwatersrand
+ * @package mcqtests
+ * @version 1.202
+ */
+class dbnumericaloptions extends dbtable {
+    /**
+     * Method to construct the class and initialise the table.
+     *
+     * @access public
+     * @return
+     */
+    public $table;
+
+    public function init() {
+        parent::init('tbl_test_question_numericaloptions');
+        $this->table = 'tbl_test_question_numericaloptions';
+    }
+
+    public function addNumericalOptions($id, $data) {
+        //insert into this table
+        $answers = $data['answer'];
+        $answerData = array();
+        $count = 1;
+        foreach($answers as $row) {
+            $mark = 'mark'.$count;
+            $answerData['answer'] = $row;
+            $answerData['questionid'] = $id;
+            $answerData['mark'] = $data['mark'][$mark];
+            $count++;
+            $this->insert($answerData);
+        }
+    }
+
+    public function updateNumericalQuestions($id, $data) {
+        $this->delete("questionid", $id);
+        $this->addNumericalOptions($id, $data);
+    }
+
+    public function deleteNumericalOptions($id) {
+        $this->delete('questionid', $id);
+    }
+}
+?>
