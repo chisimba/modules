e3b4c3938c69a624d1a4ab8626b109aad349628b
nguni52 <unknown@unknown.com>
2010-11-01 13:22:28 +0000

    added a new table for numerical questions options

---
+++ mcqtests/sql/tbl_test_question_numericaloptions.sql
@@ -0,0 +1,34 @@
+<?php
+//Table name
+$tablename = 'tbl_test_question_numericaloptions';
+
+//Options line for comments, encoding and character set
+$options = array('comment' => 'This table stores the numerical type question options', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
+
+$fields = array(
+    'id' => array(
+        'type' => 'text',
+        'length' => 32
+        ),
+    'questionid' => array(
+        'type' => 'text',
+        'length' => 32
+        ),
+    'instructions' => array(
+        'type'=>'text',
+        'length'=> 255
+        ),
+    'showunits' => array(
+        'type'=>'text',
+        'length'=> 5
+        ),
+    'unitgradingtype' => array(
+        'type'=>'text',
+        'length'=> 5
+        ),
+    'unitpenalty' => array(
+        'type'=>'integer',
+        'length'=>10
+        )
+);
+?>
