2dc1054e60b6bd78856571a9b5fdaa54bb354c05
charlvn <unknown@unknown.com>
2010-11-08 21:31:26 +0000

    Fix calls to jQuery.

--- tweetstreamer/templates/content/main_tpl.php
+++ tweetstreamer/templates/content/main_tpl.php
@@ -1,10 +1,10 @@
 <ul id="tweetstream"></ul>
 <script type="text/javascript">
-    $.getScript("http://<?php echo $socketio_host.':'.$socketio_port; ?>/socket.io/socket.io.js", function() {
+    jQuery.getScript("http://<?php echo $socketio_host.':'.$socketio_port; ?>/socket.io/socket.io.js", function() {
         var socket = new io.Socket("<?php echo $socketio_host; ?>", {port: <?php echo $socketio_port; ?>});
         socket.on("message", function(json) {
             data = JSON.parse(json);
-            $("<li></li>").text("@" + data.user.screen_name + " " + data.text).prependTo("#tweetstream");
+            jQuery("<li></li>").text("@" + data.user.screen_name + " " + data.text).prependTo("#tweetstream");
         });
         socket.connect();
     });
