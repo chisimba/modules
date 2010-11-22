422108c8deae8d9c7cdd4295bf48114eaec0e44d
David Wafula <davidwaf@gmail.com>
2010-11-22 09:57:06 +0000

    added missing fields

--- gift/sql/tbl_gift.sql
+++ gift/sql/tbl_gift.sql
@@ -14,5 +14,8 @@ $fields = array(
 		'value'=>array('type'=>'integer','not null'),
                 'gift_type' => array('type' => 'text','length' => 32),
                 'tran_date'=>array('type'=>'timestamp'),
-		'listed'=>array('type'=>'boolean','not null'));
+		'listed'=>array('type'=>'boolean','not null'),
+                'division' => array('type' => 'text','length' => 32),
+                'comments' => array('type' => 'text')
+);
 ?>
