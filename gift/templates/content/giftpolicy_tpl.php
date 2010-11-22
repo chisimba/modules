d73da00bad25101e149c7f642705d79424f4b567
David Wafula <davidwaf@gmail.com>
2010-11-22 10:06:54 +0000

    rename to all lower case

--- gift/templates/content/giftpolicy_tpl.php
+++ gift/templates/content/giftpolicy_tpl.php
@@ -15,6 +15,10 @@ $policyURL=$objSysConfig->getValue('GIFT_POLICY_URL', 'gift');
 $objWashout = $this->getObject('washout', 'utilities');
 $policy=$objWashout->parseText('[PDF]'.$policyURL.'[/PDF]');
 
-echo $policy;
+
+
+// Add the table to the centered layer and a message of database functionality
+echo '<h1>Gift Policy</h1>'. $policy.'<div id="grouping-grid"><br /></div>'.$objbackLink->show().'&nbsp;&nbsp;'.$acceptLink->show();
+
 
 ?>
