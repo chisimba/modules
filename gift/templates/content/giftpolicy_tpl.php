b461101f9f867b156b7571fdcacb39c38700b13b
David Wafula <davidwaf@gmail.com>
2010-11-22 10:05:43 +0000

    rename to all lower case

--- gift/templates/content/giftpolicy_tpl.php
+++ gift/templates/content/giftpolicy_tpl.php
@@ -15,23 +15,6 @@ $policyURL=$objSysConfig->getValue('GIFT_POLICY_URL', 'gift');
 $objWashout = $this->getObject('washout', 'utilities');
 $policy=$objWashout->parseText('[PDF]'.$policyURL.'[/PDF]');
 
-//CSS
-// Create an instance of the css layout class
-$cssLayout = &$this->newObject('csslayout', 'htmlelements');
-// Set columns to 2
-$cssLayout->setNumColumns(2);
-// get the links on the left
-$form = $this->objHome->getLeftContent($toSelect, $this->getParam('action'), $this->getParam('archived'));
-// links are displayed on the left
-$leftSideColumn = $form;
-$cssLayout->setLeftColumnContent($leftSideColumn);
-
-// Add the table to the centered layer and a message of database functionality
-$rightSideColumn = '<h1>Gift Policy</h1>'. $policy.'<div id="grouping-grid"><br /></div>'.$objbackLink->show().'&nbsp;&nbsp;'.$acceptLink->show();
-$cssLayout->setMiddleColumnContent($rightSideColumn);
-
-
-// Output the content to the page
-echo $cssLayout->show();
+echo $policy;
 
 ?>
