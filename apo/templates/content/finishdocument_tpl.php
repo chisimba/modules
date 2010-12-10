caf2f7d10d454df4e7dd7a1ef2e4797829dc9a87
jacqui <unknown@unknown.com>
2010-12-10 12:25:32 +0000

    finish form

---
+++ apo/templates/content/finishdocument_tpl.php
@@ -0,0 +1,23 @@
+<?php
+$this->loadClass('fieldset', 'htmlelements');
+$this->loadClass('label', 'htmlelements');
+$this->loadClass('button', 'htmlelements');
+
+$form = new form('finishdocument');
+
+$label = new label();
+$label->labelValue = "You have finished filling in the form. <br>To edit any infomation in the form go to the documents panel and press edit.</br> <br>To submit the form select the form in the documents panel and press submit.</br>";
+
+$legend = "<b>Finished</b>";
+$fs = new fieldset();
+$fs->setLegend($legend);
+$fs->addContent($label->show());
+$form->addToForm($fs->show());
+
+$button = new button('ok', $this->objLanguage->languageText('word_ok'));
+$uri = $this->uri(array('action' => 'home'));
+$button->setOnClick('javascript: window.location=\'' . $uri . '\'');
+$form->addToForm($button->show());
+
+echo $form->show();
+?>
