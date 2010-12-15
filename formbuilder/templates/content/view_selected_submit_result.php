1be687eba2eb004dc0310b20de4b676336b306bf
wwindvogel <unknown@unknown.com>
2010-12-15 13:29:24 +0000

    fdfsd

--- formbuilder/templates/content/view_selected_submit_result.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
- $objSubmitResultsHandler = $this->getObject('form_submit_results_handler','formbuilder');
- echo $objSubmitResultsHandler->getParticularSubmitResult($submitNumber);
-
-?>
