4a04b7bf582fccdb7082dce207b7ee5f3aaa4357
nguni52 <unknown@unknown.com>
2010-12-15 08:15:33 +0000

    helper module for elearning skin, wits

---
+++ elsiskin/templates/content/home_tpl.php
@@ -0,0 +1,12 @@
+<?php 
+
+//$cssLayout = & $this->newObject('csslayout', 'htmlelements');// Set columns to 2
+//$cssLayout->setNumColumns(2);
+//$cssLayout->setLeftColumnContent($viewer->getTweets());
+// Add Right Column
+//$cssLayout->setMiddleColumnContent($content.'<div id="stories">' . $objTable->show() . "</div>");
+
+//Output the content to the page
+//echo $cssLayout->show();
+
+?>
