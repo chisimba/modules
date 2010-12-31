1cfe6eb480c229c321aa946c7f7be9f6af895840
Derek Keats <derek@dkeats.com>
2010-12-31 16:00:29 +0000

    Committed a demodata module containing demonstration data

---
+++ demodata/patches/installscripts_class_inc.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ *
+ * Demodata installer class
+ * 
+ * Performs any post install actions necessary to build a working demo
+ * site.
+ * 
+ * PHP version 5
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @category  Chisimba
+ * @package   wurfl
+ * @author    Charl van Niekerk <charlvn@charlvn.com>
+ * @copyright 2010 Charl van Niekerk
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id: installscripts_class_inc.php 18511 2010-07-28 09:35:42Z charlvn $
+ * @link      http://chisimba.com/
+ * @see       http://wurfl.sourceforge.net/
+ */
+
+// security check - must be included in all scripts
+if (!
+/**
+ * Description for $GLOBALS
+ * @global unknown $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+/**
+ * WURFL Installer Class
+ * 
+ * Performs the necessary actions to install the WURFL mobile device detection library.
+ * 
+ * @category  Chisimba
+ * @package   wurfl
+ * @author    Derek Keats <derek@dkeats.com>
+ * @copyright 2010 Derek Keats
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id: installscripts_class_inc.php 18511 2010-12-31 16:12:33Z dkeats $
+ * @link      http://chisimba.com/
+ * @see       http://wurfl.sourceforge.net/
+ */
+class demodata_installscripts extends object
+{
+    /**
+     * Instance of the altconfig class in the config module.
+     *
+     * @access private
+     * @var    object
+     */
+    private $objAltConfig;
+
+    /**
+     * The object property initialiser.
+     *
+     * @access public
+     */
+    public function init()
+    {
+       $this->objAltConfig = $this->getObject('altconfig', 'config');
+    }
+
+    /**
+     * The actions to perform after installation of the module.
+     *
+     * @access public
+     */
+    public function postinstall()
+    {
+      // Extract the userimages
+        $userImages = $this->objAltConfig->getModulePath() . 'demodata/resources/userimages/userimages.zip';
+        $targetPath = $this->objAltConfig->getSiteRootPath() . 'user_images/';
+
+        // Unzip the WURFL XML file.
+        $zip = new ZipArchive();
+        if ($zip->open($userImages) === TRUE) {
+            $zip->extractTo($targetPath);
+            $zip->close();
+        }
+    }
+}
+?>
