be8b76b053d23c1ffd0e9d421b5b30035acf1280
nguni52 <unknown@unknown.com>
2011-01-10 11:01:33 +0000

    modifications for helper module for elsiskin

---
+++ elsiskin/classes/elsicontent_class_inc.php
@@ -0,0 +1,160 @@
+<?php
+/*
+ *
+ * A module to get the content of the elsi website. The content displayed will depends on the
+ * page being displayed. It could be content for home page, about us, staff or contact us.
+ *
+ * PHP version 5
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * @category  Chisimba
+ * @package   elsiskin
+ * @author    Nguni Phakela nonkululeko.phakela@wits.ac.za
+ * @copyright 2007 AVOIR
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   CVS: $Id: elsicontent_class_inc.php,v 1.1 2007-11-25 09:13:27 nguni52 Exp $
+ * @link      http://avoir.uwc.ac.za
+ *
+ *
+ *
+ */
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+
+// end of security
+
+class elsicontent extends object {
+	
+	// path to root folder of skin
+	private $skinpath;
+	
+	// instance of sidebar
+	private $sidebar;
+	
+	// instance of the main contents of a page
+	private $mainContent;
+
+
+    /**
+     * Constructor
+     */
+	 
+    public function init() {
+		$this->sidebar = $this->getObject('sidebar');
+		$this->mainContent = $this->getObject('maincontent');	
+	}
+
+	/**
+     * Method to show the Toolbar
+     * @return string $retstr containing all the toolbar links.
+     */
+	 
+	 public function setSkinPath($skinpath) {
+		 $this->skinpath = $skinpath;
+	 }
+	 
+	 /**
+      * Method to show the rotating images with news right below the toolbar
+      * @return string
+      */
+	 public function show($action) {
+		 // set current action
+		$this->sidebar->setCurrentAction($action);
+		$this->sidebar->setSkinPath($this->skinpath);
+		$this->mainContent->setCurrentAction($action);
+		$this->mainContent->setSkinPath($this->skinpath);
+
+		return $this->getContent($action);
+	 }
+	 
+	 private function getContent($action) {	
+		// get the content of the page
+		$retstr = '<div id="Content">';
+		$retstr .= $this->getIntroText($action);	
+		$retstr .= $this->sidebar->show();
+		$retstr .= '<div id="Main">'.$this->mainContent->show().'</div>
+					<!-- End: Main -->
+				<!-- End: Content -->';
+
+	    return $retstr;
+	 }
+	 
+	 private function getIntroText($action) {
+		 if($action == 'about') {
+			 $ret = '<div class="clear">&nbsp;</div>
+					 <div class="grid_1">
+						<h3><a href="">Welcome to eLSI</a></h3>
+					</div>
+					<div class="grid_3">
+						<h2><a href="">About ELSI</a></h2>
+					 </div>
+					 <div class="grid_1 pull_3">
+						<h3>More about eLSI</h3>
+				  	 </div><div class="clear">&nbsp;</div>';
+		 }
+		 else if($action == 'staff') {
+			$ret = '<div class="clear">&nbsp;</div>
+					<div class="grid_1 ">
+						<h3><a href="">Staff at eLSI</a></h3>
+				    </div>
+				    <div class="grid_3">
+						<h2><a href="">Who works at ELSI?</a></h2>
+			  		</div>
+					<div class="clear">&nbsp;</div>'; 
+		 }
+		 else if($action == 'contact') {
+			$ret = '<div class="clear">&nbsp;</div>
+					<div class="grid_1">
+						<h3><a href="index.html">Contact eLSI</a></h3>
+				    </div>
+					<div class="grid_3">
+						<h2><a href="index.html">Can ELSI be of assistance?</a></h2>
+					</div>
+					<div class="grid_1 pull_3">
+						<h3>More about eLSI</h3>
+				  	</div>
+					<div class="clear">&nbsp;</div>'; 
+		 }
+		 else {
+			 $ret = '<div class="clear">&nbsp;
+				</div>
+				<div class="grid_1">
+	
+					<h3><a href="">Welcome to eLSI</a></h3>
+					
+					</div>
+					<div class="grid_3">
+						<h2><a href="">How can we help?</a></h2>
+					</div>
+					<!-- end .grid_1 -->
+					<div class="clear">&nbsp;
+					</div>
+	
+					<div class="grid_1">
+					&nbsp;
+					</div>
+					<div class="grid_3">
+						<p>The eLearning, Support and Innovation (eLSI) Unit has been established to explore, contribute to and expand the online education capacities of students and staff at the University of Witwatersrand. The unit intends to promote competent and appropriate use of digital technologies and develop an academic digital literacy amongst students and staff. </p>
+					</div>
+					<!-- end .grid_1 -->
+	
+					<div class="clear">&nbsp;</div>';
+		}
+		 
+	 	return $ret;
+	 }
+}
