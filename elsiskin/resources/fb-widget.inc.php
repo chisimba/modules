2b135c5da10de45b1dedeced5b9b880991f650e6
nguni52 <unknown@unknown.com>
2011-01-10 11:07:25 +0000

    social networks resource files

---
+++ elsiskin/resources/fb-widget.inc.php
@@ -0,0 +1,7 @@
+<script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
+<fb:like-box href="http://www.facebook.com/pages/Wits-eLSI-eLearning-Support-and-Innovation/143838025636947"
+             width="220"
+             connections="10"
+             stream="false"
+             header="true">
+</fb:like-box>
