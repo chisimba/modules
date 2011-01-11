6c8ed59e3f7fba237bebf9b175787aaeb346db83
Derek Keats <derek@dkeats.com>
2011-01-11 08:08:50 +0000

    Created a module for dkeats.com

---
+++ dkeatscom2/classes/dbdkeatscom2_class_inc.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ *
+ * A user interface for dkeats.com based on a completely blank starting page
+ * with Turn editing on functionality. The module is entirely block-based.
+ *
+ * oops
+ *
+ * PHP version 5
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * @category  Chisimba
+ * @package   dkeatscom2
+ * @author    Derek Keats derek@dkeats.com
+ * @copyright 2007 AVOIR
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   CVS: $Id: dbdkeatscom2.php,v 1.1 2007-11-25 09:13:27 dkeats Exp $
+ * @link      http://avoir.uwc.ac.za
+ */
+
+// security check - must be included in all scripts
+if (!
+/**
+ * The $GLOBALS is an array used to control access to certain constants.
+ * Here it is used to check if the file is opening in engine, if not it
+ * stops the file from running.
+ *
+ * @global entry point $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ *
+ */
+$GLOBALS['kewl_entry_point_run'])
+{
+        die("You cannot view this page directly");
+}
+// end security check
+
+/**
+*
+* Database accesss class for Chisimba for the module dkeatscom2
+*
+* @author Derek Keats
+* @package dkeatscom2
+*
+*/
+class dbdkeatscom2 extends dbtable
+{
+
+    /**
+    *
+    * Intialiser for the dkeatscom2 database connector
+    * @access public
+    *
+    */
+    public function init()
+    {
+        //Set the parent table here
+    }
+
+}
+?>
