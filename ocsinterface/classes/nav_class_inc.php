8342da19f2e79e3c50bf3293e6601d65d6b3ee93
David Wafula <davidwaf@gmail.com>
2011-01-13 11:07:12 +0000

    added 3 col layout support

--- ocsinterface/classes/nav_class_inc.php
+++ ocsinterface/classes/nav_class_inc.php
@@ -45,6 +45,43 @@ class nav extends object {
 
         return $desc;
     }
+
+
+
+    public function getRightContent() {
+        $sidecatid=$this->objDbSysconfig->getValue('RIGHT_SIDE_NAV_CATEGORY','ocsinterface');
+        $sidenavs=$this->storyparser->getStoryByCategory($sidecatid);
+        $objWashout = $this->getObject('washout', 'utilities');
+        $list=array();
+
+        foreach($sidenavs as $nav) {
+            $sidelink = new link ($this->uri(array('action'=>'viewstory', 'storyid'=>$nav['id']),"ocsinterface"));
+            $sidelink->link=   $objWashout->parseText($nav['title']);
+            $list[] = $sidelink->show();
+        }
+
+        $desc=
+            '<ul id="nav-secondary">';
+        $cssClass = '';
+        foreach($list as $element) {
+            if(strtolower($element) == strtolower($toSelect)) {
+                $cssClass = ' class="active" ';
+            }
+            $desc.='<li $cssClass>'.$element.'</li>';
+        }
+        $desc.='</ul><br/>';
+
+
+        $sidecatid2=$this->objDbSysconfig->getValue('RIGHT_SIDE_NAV2_CATEGORY','ocsinterface');
+        $sidenavs2=$this->storyparser->getStoryByCategory($sidecatid2);
+
+        foreach($sidenavs2 as $nav) {
+            $desc.=$objWashout->parseText($nav['maintext']);
+        }
+
+
+        return $desc;
+    }
 }
 
 ?>
