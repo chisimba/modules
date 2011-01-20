8728012f17c1f0327a532c12fea73098050fbb8c
nguni52 <unknown@unknown.com>
2011-01-20 07:02:29 +0000

    minor changes -- wip

--- elsiskin/classes/elsilogo_class_inc.php
+++ elsiskin/classes/elsilogo_class_inc.php
@@ -8,27 +8,32 @@ if (!$GLOBALS['kewl_entry_point_run']) {
 // end of security
 
 class elsilogo extends object {
-	
-	// path to root folder of skin
-	private $skinpath;
 
+    // path to root folder of skin
+    private $skinpath;
 
     /**
      * Constructor
      */
-    public function init() {}
+    public function init() {
+        
+    }
 
-	/**
-     * Method to show the Toolbar
-     * @return string $retstr containing all the toolbar links.
+    /**
+     * Method to set the base skin path
+     * @return none
      */
-	 
-	 public function setSkinPath($skinpath) {
-		 $this->skinpath = $skinpath;
-	 }
-	 
-	 public function show() {
-		 $retstr = '<div class="clear">&nbsp;</div>
+    public function setSkinPath($skinpath) {
+        $this->skinpath = $skinpath;
+    }
+
+    /* 
+     * Method to display the logo section of the skin
+     * @access public
+     * @return string $retstr which displays the wits logo and elsi logo
+     */
+    public function show() {
+        $retstr = '<div class="clear">&nbsp;</div>
             <div class="grid_3">
                 <img src="' . $this->skinpath . 'images/logo_wits.png">
             </div>
@@ -36,8 +41,8 @@ class elsilogo extends object {
             <div class="grid_1">
                 <img src="' . $this->skinpath . 'images/logo_elsi.gif">
             </div>';
-			
-		$retstr .= '<!-- Start: Horizontal nav -->
+
+        $retstr .= '<!-- Start: Horizontal nav -->
 			 <div class="clear">&nbsp;</div> 
 				<div id="Horizontalnav"> 
 					<div class="wide">
@@ -48,8 +53,8 @@ class elsilogo extends object {
 			<!-- End: header -->
 			 </div>
 			 <!-- End: Horizontal nav -->';
-			
-		return $retstr;
-	 }
-	 
+
+        return $retstr;
+    }
+
 }
