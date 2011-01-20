3bbeafdd77e477ba6a59735c179a1d1d774a64c8
nguni52 <unknown@unknown.com>
2011-01-20 07:06:54 +0000

    minor changes -- wip

--- elsiskin/classes/elsilogo_class_inc.php
+++ elsiskin/classes/elsilogo_class_inc.php
@@ -1,5 +1,34 @@
 <?php
-
+/*
+ *
+ * A module to display the logo of the elsi skin. 
+ *
+ * PHP version 5
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * @category  Chisimba
+ * @package   elsiskin
+ * @author    Nguni Phakela nonkululeko.phakela@wits.ac.za
+ * @copyright 2007 AVOIR
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   CVS: $Id: elsilogo_class_inc.php,v 1.1 2011-01-12 09:13:27 nguni52 Exp $
+ * @link      http://avoir.uwc.ac.za
+ *
+ *
+ *
+ */
 // security check - must be included in all scripts
 if (!$GLOBALS['kewl_entry_point_run']) {
     die("You cannot view this page directly");
