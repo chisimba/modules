0ad317d069143bce33fff0636b99df6f065267de
jacqui <unknown@unknown.com>
2011-01-24 09:46:20 +0000

    forms dbs

--- apo/sql/tbl_apo_collaborationsandcontracts.sql
+++ apo/sql/tbl_apo_collaborationsandcontracts.sql
@@ -2,6 +2,12 @@
     $tablename = 'tbl_apo_collaborationsandcontracts';
     $options = array('comment' => 'Table used to save data from user input in the collaborations and contracts form', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
     $fields = array('id' => array('type' => 'text','length' => 32, 'notnull'=>TRUE),
-                    'data' => array('type' => 'text', 'notnull'=>TRUE)
+                    'f1a' => array('type' => 'text', 'notnull'=>TRUE),
+                    'f1b' => array('type' => 'text', 'notnull'=>TRUE),
+                    'f2a' => array('type' => 'text', 'notnull'=>TRUE),
+                    'f2b' => array('type' => 'text', 'notnull'=>TRUE),
+                    'f3a' => array('type' => 'text', 'notnull'=>TRUE),
+                    'f3b' => array('type' => 'text', 'notnull'=>TRUE),
+                    'f4' => array('type' => 'text', 'notnull'=>TRUE)
                     );
 ?>
--- apo/sql/tbl_apo_outcomesandassessmenttwo.sql
+++ apo/sql/tbl_apo_outcomesandassessmenttwo.sql
@@ -2,6 +2,6 @@
     $tablename = 'tbl_apo_outcomesandassessmenttwo';
     $options = array('comment' => 'Table used to save data from user input in the outcomes and assessment two form', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
     $fields = array('id' => array('type' => 'text','length' => 32, 'notnull'=>TRUE),
-                    'data' => array('type' => 'text', 'notnull'=>TRUE)
+                    'd4' => array('type' => 'text', 'notnull'=>TRUE)
                     );
 ?>
