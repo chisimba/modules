c8309102af31dd8176243db9443f1a427bd572ee
jacqui <unknown@unknown.com>
2011-01-25 11:16:22 +0000

    inserts into overview table but not the others so far!

--- apo/classes/dbformdata_class_inc.php
+++ apo/classes/dbformdata_class_inc.php
@@ -7,26 +7,30 @@
 
 class dbformdata extends dbtable {
 
-    var $tablename = "tbl_apo_formdata";
+    var $tablename = "tbl_apo_overview";
     var $userid;
 
     public function init() {
         parent::init($this->tablename);
     }
 
-    public function saveData($formname, $formdata, $docid) {
+    public function saveData($docid, $formname, $formdata) {
+        $formdata = unserialize($formdata);
         $tablename = "tbl_apo_" . $formname;
         $this->objUser = $this->getObject('user', 'security');
         $this->objSysConfig = $this->getObject('dbsysconfig', 'sysconfig');
         $this->objUploadTable = $this->getObject('dbfileuploads');
         $this->userutils = $this->getObject('userutils');
+        $data = array();
         $data["id"] = $docid;
-        $datalength = count($data);
-        for ($i = 0; $i < $datalength; $i++) {
-            $data[$formdata[$i][$i]] = $formdata[$i][$i + 1];
-        }
+        $formdatanew = array_merge($data, $formdata);
 
-        /*    'a1' => $formdata["a1"],
+        /* $datalength = count($data);
+          for ($i = 0; $i < $datalength; $i++) {
+          $data[$formdata[$i][0]] = $formdata[$i][1];
+          }
+
+          /*    'a1' => $formdata["a1"],
           'a2' => $formdata["a2"],
           'a3' => $formdata["a3"],
           'a4' => $formdata["a4"],
@@ -34,36 +38,32 @@ class dbformdata extends dbtable {
 //'userid'=>$this->userutils->getUserId()
         //);
 
-        if ($this->exists($docid)) {
+        if ($this->exists($docid, $formname)) {
             print_r("exists");
             $existingdata = $this->getAll("where id='$docid'");
 
             if (count($existingdata) > 0) {
                 print_r("count > 0");
-                $updatedata = array();
-                for ($i = 0; $i < $datalength; $i++) {
-                    $updatedata[$formdata[$i][$i]] = $formdata[$i][$i + 1];
-                }
-               /* 'a1' => $formdata["a1"],
-                'a2' => $formdata["a2"],
-                'a3' => $formdata["a3"],
-                'a4' => $formdata["a4"],
-                'a5' => $formdata["a5"]
-//'userid' => $this->userutils->getUserId()
-                );*/
-                $this->update('id', $existingdata[0]['id'], $updatedata);
+                
+                /* 'a1' => $formdata["a1"],
+                  'a2' => $formdata["a2"],
+                  'a3' => $formdata["a3"],
+                  'a4' => $formdata["a4"],
+                  'a5' => $formdata["a5"]
+                  //'userid' => $this->userutils->getUserId()
+                  ); */
+                $this->update('id', $existingdata[0]['id'], $formdata);
                 print_r("updated");
             }
         }
         else
-            $this->insert($data); //$formname, $formdata, $docid
+            $this->insert($formdatanew); //$formname, $formdata, $docid
 
             echo 'success';
     }
 
     function exists($docid, $formname) {
-        $sql = "select * from tbl_apo_'.$formname.' where docid='$docid'";
-        $xmStr = "";
+        $sql = "select * from tbl_apo_" . $formname . " where id='$docid'";
         $rows = $this->getArray($sql);
         if (count($rows) > 0) {
             return TRUE;
@@ -74,16 +74,11 @@ class dbformdata extends dbtable {
 
     public function getFormData($formname, $docid) {
 
-        $sql = "select * from tbl_apo_'.$formname.' where docid='$docid'";
-        $xmStr = "";
-        $rows = $this->getArray($sql);
+        $sql = "select * from tbl_apo_'.$formname.' where id='$docid'";
+        $formdata = $this->getArray($sql);
 
-        foreach ($rows as $row) {
-            print_r($row);
-            $xmlStr = $row['formdata'];
-        }
 
-        return $xmlStr;
+        return $formdata;
     }
 
 }
