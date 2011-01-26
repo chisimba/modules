2151ba12518756921b7f06817e9bfbe94f9c0928
wwindvogel <unknown@unknown.com>
2011-01-26 09:19:51 +0000

    Initial commit of the Alfresco intergration module. Still under construction.

---
+++ alfresco/templates/content/display_tpl.php
@@ -0,0 +1,20 @@
+<?php
+/* -------------------- template for redmine module ----------------*/
+
+// security check-must be included in all scripts
+if(!$GLOBALS['kewl_entry_point_run']){
+    die("You cannot view this page directly");
+}
+// end security check
+
+/**
+* Template for the redmine module
+* @author Warren Windvogel
+* @package redmine
+*/
+
+$this->setLayoutTemplate('layout_tpl.php');
+
+echo $templateContent;
+
+?>
