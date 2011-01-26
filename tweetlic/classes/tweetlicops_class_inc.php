6f200a4f34c97fdbba308779fce4e7882c6dff11
paulscott <unknown@unknown.com>
2011-01-26 10:39:50 +0000

    removed urlencode

--- tweetlic/classes/tweetlicops_class_inc.php
+++ tweetlic/classes/tweetlicops_class_inc.php
@@ -131,7 +131,7 @@ class tweetlicops extends object {
     public function ccTweetBoxInvite($screen_name) {
         $gourl = $this->uri(array('user' => $screen_name), 'tweetlic');
         $this->teeny = $this->getObject ( 'tiny', 'tinyurl');
-        $gourl = $this->teeny->create(urlencode($gourl));
+        $gourl = $this->teeny->create($gourl);
         $msg = $this->objLanguage->languageText("mod_tweetlic_invitemsg", "tweetlic")." ";
         
         $js = '<script type="text/javascript">
