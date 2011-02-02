a30c19e4a5ea317bd1d16fdc9ceb8490f73d837b
jacqui <unknown@unknown.com>
2011-02-02 08:01:15 +0000

    OAA3Science db

--- apo/sql/tbl_apo_outcomesandassessmentthreeScience.sql
+++ apo/sql/tbl_apo_outcomesandassessmentthreeScience.sql
@@ -1,6 +1,6 @@
 <?php
-    $tablename = 'tbl_apo_outcomesandassessmentthree';
-    $options = array('comment' => 'Table used to save data from user input in the outcomes and assessment three form', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
+    $tablename = 'tbl_apo_outcomesandassessmentthreeScience';
+    $options = array('comment' => 'Table used to save data from user input in the outcomes and assessment three form for the science faculty', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
     $fields = array('id' => array('type' => 'text','length' => 32, 'notnull'=>TRUE),
                     'a1' => array('type' => 'text', 'notnull'=>TRUE),
                     'a2' => array('type' => 'text', 'notnull'=>TRUE),
