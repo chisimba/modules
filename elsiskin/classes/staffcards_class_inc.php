613b238bc6be2ffdc3f314b3743386fb0c39a18f
nguni52 <unknown@unknown.com>
2011-02-08 09:34:19 +0000

    corrected wrong email address

--- elsiskin/classes/staffcards_class_inc.php
+++ elsiskin/classes/staffcards_class_inc.php
@@ -306,8 +306,9 @@ class staffcards extends object {
 						  <ul>        
 							<li><a href="http://www.facebook.com/nphakela"><img src="' . $this->skinpath . 'images/profiles/facebook.png"><strong>Facebook</strong>facebook.com</a></li>
 							<li><a href="http://twitter.com/nguni52/"><img src="' . $this->skinpath . 'images/profiles/twitter.png"><strong>Twitter</strong>twitter.com</a></li>
-		
-						  </ul>
+                                                        <li><a href="http://www.delicious.com/nphakela/"><img src="' . $this->skinpath . 'images/profiles/delicious.png"><strong>Delicious</strong>delicious.com</a></li>
+							<li><a href="http://www.flickr.com/photos/nphakela"><img src="' . $this->skinpath . 'images/profiles/flickr.png"><strong>Flickr</strong>flickr.com</a></li>
+                                                  </ul>
 						  <div class="clear"></div>
 						</div>
 					</div>
@@ -811,7 +812,7 @@ class staffcards extends object {
 							</span>
 		
 						</p>
-						<p class="email">Email: <a class="value" href="davidwafula.wanyonyi@wits.ac.za">davidwafula.wanyonyi@wits.ac.za</a></p>
+						<p class="email">Email: <a class="value" href="david.wafula@wits.ac.za">david.wafula@wits.ac.za</a></p>
 				  
 							<a href="">
 								<img src="' . $this->skinpath . 'images/Icon_vCard.png" alt="download vcard icon" align="right">
