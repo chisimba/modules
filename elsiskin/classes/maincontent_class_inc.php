b23db32a846587375c19fb6eba3fd8df26dae5c1
nguni52 <unknown@unknown.com>
2011-02-09 10:03:52 +0000

    modified main content display, vcard download --- wip

--- elsiskin/classes/maincontent_class_inc.php
+++ elsiskin/classes/maincontent_class_inc.php
@@ -211,7 +211,29 @@ class maincontent extends object {
         $retstr .= "</div>
                 </div>";
 
+        /*
+         *
 
+        $v = new vCard();
+
+        $v->setPhoneNumber("+49 23 456789", "PREF;HOME;VOICE");
+        $v->setName("Mustermann", "Thomas", "", "Herr");
+        $v->setBirthday("1960-07-31");
+        $v->setAddress("", "", "Musterstrasse 20", "Musterstadt", "", "98765", "Deutschland");
+        $v->setEmail("thomas.mustermann@thomas-mustermann.de");
+        $v->setNote("You can take some notes here.
+        Multiple lines are supported via \\r\\n.");
+        $v->setURL("http://www.thomas-mustermann.de", "WORK");
+
+        $output = $v->getVCard();
+        $filename = $v->getFileName();
+
+        Header("Content-Disposition: attachment; filename=$filename");
+        Header("Content-Length: ".strlen($output));
+        Header("Connection: close");
+        Header("Content-Type: text/x-vCard; name=$filename");
+
+        echo $output;*/
 
         return $retstr;
      }
