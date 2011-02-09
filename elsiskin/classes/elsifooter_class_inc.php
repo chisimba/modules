e2516a7f295522ac79f768e3788d1fe4aa91e75f
nguni52 <unknown@unknown.com>
2011-02-09 10:08:40 +0000

    modified footer.

--- elsiskin/classes/elsifooter_class_inc.php
+++ elsiskin/classes/elsifooter_class_inc.php
@@ -60,7 +60,7 @@ class elsifooter extends object {
      * @access public
      */
     public function show() {
-        $links = array('home'=>'Home', 'about'=>'About Us', 'staff'=>'eLSI Staff', 'contact'=>'Contact Us', 'projectsresearch'=>'Project & Research', 'support'=>'Support & Training');
+        $links = array('home'=>'Home', 'about'=>'About Us', 'staff'=>'eLSI Staff', 'contact'=>'Contact Us', 'projectsresearch'=>'Projects & Research', 'support'=>'Support & Training');
         $retstr ='
            <!-- Start: Footer -->
             <div id="Footer">
