6f9330cd478d3311d11e94c4c9cea8470621d0a7
David Wafula <davidwaf@gmail.com>
2011-02-11 09:26:17 +0000

    changed case

--- apo/sql/tbl_apo_outcomesandassessmentonescience.sql
+++ apo/sql/tbl_apo_outcomesandassessmentonescience.sql
@@ -1,5 +1,5 @@
 <?php
-    $tablename = 'tbl_apo_outcomesandassessmentoneScience';
+    $tablename = 'tbl_apo_outcomesandassessmentonescience';
     $options = array('comment' => 'Table used to save data from user input in the outcomes and assessment one form for the science faculty', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
     $fields = array('id' => array('type' => 'text','length' => 32, 'notnull'=>TRUE),
                     'd1' => array('type' => 'text', 'notnull'=>TRUE),
--- apo/sql/tbl_apo_outcomesandassessmentthreescience.sql
+++ apo/sql/tbl_apo_outcomesandassessmentthreescience.sql
@@ -1,5 +1,5 @@
 <?php
-    $tablename = 'tbl_apo_outcomesandassessmentthreeScience';
+    $tablename = 'tbl_apo_outcomesandassessmentthreescience';
     $options = array('comment' => 'Table used to save data from user input in the outcomes and assessment three form for the science faculty', 'collate' => 'utf8_general_ci', 'character_set' => 'utf8');
     $fields = array('id' => array('type' => 'text','length' => 32, 'notnull'=>TRUE),
                     'a1' => array('type' => 'text', 'notnull'=>TRUE),
