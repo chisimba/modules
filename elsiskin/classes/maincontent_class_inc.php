2ea60b45e47c436584756b305db4dc53d69ebf52
nguni52 <unknown@unknown.com>
2011-02-15 13:08:42 +0000

    contact us details modification.

--- elsiskin/classes/maincontent_class_inc.php
+++ elsiskin/classes/maincontent_class_inc.php
@@ -263,7 +263,7 @@ class maincontent extends object {
             $retstr = '
                  <div class="grid_3">
                        '.$message.'
-                       <p>11-17 Joriseen Street<br> Braamfontein, 2050 <br> Johannesburg<br> Gauteng</p>
+                       <p>18th Floor, University Corner<br>11-17 Joriseen Street<br> Braamfontein, 2050 <br> Johannesburg<br> Gauteng</p>
                        <p>Telephone: 011 717 7161</p>
                        <div id="clear">&nbsp;</div>
                        <h4>Fill in the form</h4>
