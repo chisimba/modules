e731fcd5068e6c70069f70947484a18b7278623a
paulscott <unknown@unknown.com>
2011-02-22 11:23:20 +0000

    featuresuggest basics module

---
+++ featuresuggest/templates/content/view_tpl.php
@@ -0,0 +1,26 @@
+<?php
+$cssLayout = $this->newObject('csslayout', 'htmlelements');
+$cssLayout->setNumColumns(2);
+
+// get the sidebar object
+$this->leftMenu = $this->newObject('usermenu', 'toolbar');
+$this->loadClass('htmlheading', 'htmlelements');
+$this->loadClass('href', 'htmlelements');
+        
+$middleColumn = NULL;
+$leftColumn = NULL;
+
+// Add in a heading
+$header = new htmlHeading();
+$header->str = $this->objLanguage->languageText('mod_featuresuggest_welcome', 'featuresuggest');
+$header->type = 1;
+
+$middleColumn .= '<div id="heading" class="rounded">'.$header->show().'</div>';
+$middleColumn .= $str;
+$middleColumn .= $this->objUI->formatUI($str);
+
+//$leftColumn .= $this->objOps->userSearchBox();
+
+$cssLayout->setMiddleColumnContent($middleColumn);
+$cssLayout->setLeftColumnContent($leftColumn);
+echo $cssLayout->show();
