8dc50c97beaa071e07e3cebdac2dd94f0f13fefe
Jeremy O'Connor <joconnor@uwc.ac.za>
2011-03-01 13:48:03 +0000

    Fixed multiple bugs related to adding and editing announcements.

--- announcements/controller.php
+++ announcements/controller.php
@@ -31,19 +31,19 @@ class announcements extends controller
      * @access private
      */
     private $userContext;
-    
+
     /**
      * @var array $lecturerContext List of Contexts a user is a lecturer in
      * @access private
      */
     private $lecturerContext;
-    
+
     /**
     * Constructor for the Module
     */
     public function init()
-    { 
-        $this->objUser = $this->getObject('user', 'security');         
+    {
+        $this->objUser = $this->getObject('user', 'security');
         $this->objContext = $this->getObject('dbcontext','context');
         $this->objDate = $this->getObject('dateandtime', 'utilities');
         $this->objLanguage = $this->getObject('language', 'language');
@@ -52,18 +52,18 @@ class announcements extends controller
         $this->objFeedCreator = $this->getObject('feeder', 'feed');
 
         $this->objAnnouncements = $this->getObject('dbannouncements');
-       
+
         $this->userId = $this->objUser->userId();
-       
+
         $objUserContext = $this->getObject('usercontext', 'context');
         if(!empty($this->userId)){
          $this->userContext = $objUserContext->getUserContext($this->userId);
          $this->lecturerContext = $objUserContext->getContextWhereLecturer($this->userId);
-        }        
+        }
         $this->isAdmin = $this->objUser->isAdmin();
-        
+
         $this->itemsPerPage = 10;
-        
+
         $this->setVar('lecturerContext', $this->lecturerContext);
         $this->setVar('isAdmin', $this->isAdmin);
 								//Load Module Catalogue Class
@@ -97,9 +97,9 @@ class announcements extends controller
         * using case selections)
         */
         $method = $this->getMethod($action);
-        
+
         $this->setLayoutTemplate('announcements_layout_tpl.php');
-        
+
         /*
         * Return the template determined by the method resulting
         * from action
@@ -148,7 +148,7 @@ class announcements extends controller
             return FALSE;
         }
     }
-    
+
     public function requiresLogin()
     {
         return False;
@@ -161,7 +161,7 @@ class announcements extends controller
      */
     public function __feed()
     {
-        
+
         //get ther username
         $username = $this->getParam("username");
         //$username = "admin";
@@ -179,7 +179,7 @@ class announcements extends controller
 
         //description
         $feedDescription = "Some description";
-        
+
         //link back to the blog
         $feedLink = $this->objConfig->getSiteRoot() . "index.php?module=announcements&action=home&userid=" . $userid;
         //sanitize the link
@@ -202,7 +202,7 @@ class announcements extends controller
             $itemTitle = $feeditems['title'];
             $itemLink = $this->uri(array(
                 'action' => 'view',
-                'id' => $feeditems['id']               
+                'id' => $feeditems['id']
             )); //todo - add this to the posts table!
             //description
             $itemDescription = $feeditems['message'];
@@ -239,28 +239,28 @@ class announcements extends controller
         if (!is_array($item)) {
             $item = $this->objAnnouncements->getMessage($item);
         }
-        
+
         if ($item === FALSE) {
-			
+
             return FALSE;
         }
-        
+
         if ($item['contextid'] == 'site' && $this->isAdmin) {
-		
+
             return TRUE;
         }
-        
-       
+
+
         if ($item['contextid'] == 'context' && count($this->lecturerContext) > 0) {
             // See if some items match
             $diff = array_intersect($this->lecturerContext, $this->objAnnouncements->getMessageContexts($item['id']));
-            
+
             // If yes, user can edit or delete
             if (count($diff) > 0) {
                 return TRUE;
             }
         }
-        
+
         // Else
         return FALSE;
     }
@@ -279,21 +279,21 @@ class announcements extends controller
         //use own template here
          $this->setLayoutTemplate(NULL);
         $numAnnouncements = $this->objAnnouncements->getNumAnnouncements($this->userContext);
-        
+
         $this->setVarByRef('numAnnouncements', $numAnnouncements);
         return 'home_tpl.php';
     }
-    
+
     /**
      * Method to add an announcement
      */
     private function __add()
     {
         $this->setVar('mode', 'add');
-        
+
         return 'addedit_tpl.php';
     }
-    
+
     /**
      * Method to save an announcement
      */
@@ -304,22 +304,28 @@ class announcements extends controller
         $email = $this->getParam('email');
         $mode = $this->getParam('mode');
         $recipienttarget = $this->getParam('recipienttarget');
-        $contexts = $this->getParam('contexts');
+        $contexts = $this->getParam('contexts', array());
         //If context checked, target set to context by default
         if(!empty($contexts))
          $recipienttarget = 'context';
         $email = ($email == 'Y') ? TRUE : FALSE;
-        
-        if ($mode == 'add' && ($title == '' || strip_tags($message) == '')) {
+
+        if (
+            ($mode == 'add'
+            || $mode == 'fixup')
+        && ($title == ''
+            || strip_tags($message) == '')) {
             $this->setVar('mode', 'fixup');
             $this->setVar('lecturerContext', $this->lecturerContext);
             $this->setVar('isAdmin', $this->isAdmin);
             $this->setVar("message", $message);
             $this->setVarByRef("title", $title);
-            $this->setVarByRef("errormessage", "Title/content required");
-
+            $this->setVar('recipienttarget', $recipienttarget);
+            $this->setVar('contexts', $contexts);
+            $this->setVar("errormessage", $this->objLanguage->languageText('mod_announcements_errortitlemessagerequired', 'announcements'));
             return 'addedit_tpl.php';
-        } else if ($mode == 'add' || $mode == 'fixup' || $mode == 'save') {
+        } else if ($mode == 'add' || $mode == 'fixup') //  || $mode == 'save'
+        {
 
             $result = $this->objAnnouncements->addAnnouncement($title, $message, $recipienttarget, $contexts, $email);
             	//add to activity streamer/log
@@ -331,62 +337,62 @@ class announcements extends controller
     																				'contextcode' => $this->objContext->getContextCode(),
     																				'author' => $this->objUser->fullname(),
     																				'description'=>$message));
-            	}            
+            	}
             return $this->nextAction('view', array('id'=>$result));
-            
+
         }
     }
-    
+
     /**
      * Method to view an announcement
      */
     private function __view()
     {
         $id = $this->getParam('id');
-        
+
         $announcement = $this->objAnnouncements->getMessage($id);
-        
+
         if ($announcement == FALSE) {
             return $this->nextAction(NULL, array('error'=>'unknownannouncement'));
         } else {
             $this->setVarByRef('announcement', $announcement);
-            
+
             return 'view_tpl.php';
         }
     }
-    
+
     /**
      * Method to respond via ajax for a listing of all announcements
      */
     private function __getajax()
     {
         $page = $this->getParam('page', 0);
-        
+
         $announcements = $this->objAnnouncements->getAllAnnouncements($this->userContext, $this->itemsPerPage, $page);
-        
+
         if (count($announcements) == 0) {
             echo '<div class="noRecordsMessage">'.$this->objLanguage->languageText('mod_announcements_noannouncements', 'announcements', 'There are no announcements').'</div>';
         } else {
             return $this->generateAjaxResponse($announcements);
         }
     }
-    
+
     /**
      * Method to respond via ajax for a listing of the current context announcements
      */
     private function __getcontextajax()
     {
         $page = $this->getParam('page', 0);
-        
+
         $announcements = $this->objAnnouncements->getContextAnnouncements($this->objContext->getContextCode(), $this->itemsPerPage, $page);
-        
+
         if (count($announcements) == 0) {
             echo '<div class="noRecordsMessage">'.$this->objLanguage->languageText('mod_announcements_noannouncements', 'announcements', 'There are no announcements').'</div>';
         } else {
             return $this->generateAjaxResponse($announcements);
         }
     }
-    
+
     /**
      * Method to convert db results into a table before responding via ajax
      */
@@ -396,18 +402,18 @@ class announcements extends controller
             $this->loadClass('htmlheading', 'htmlelements');
             $objDateTime = $this->getObject('dateandtime', 'utilities');
             $objTrimString = $this->getObject('trimstr', 'strings');
-            
+
             $table = $this->newObject('htmltable', 'htmlelements');
 			$table->width= '80%';
             $table->startHeaderRow();
             $table->addHeaderCell($this->objLanguage->languageText('word_date', 'system', 'Date'));
             $table->addHeaderCell($this->objLanguage->languageText('word_title', 'system', 'Title'));
             $table->addHeaderCell($this->objLanguage->languageText('word_by', 'system', 'By'));
-            $table->addHeaderCell($this->objLanguage->languageText('word_type', 'system', 'Type'));            
+            $table->addHeaderCell($this->objLanguage->languageText('word_type', 'system', 'Type'));
 			$table->addHeaderCell('&nbsp;');
 			$table->endHeaderRow();
-            
-            
+
+
             foreach ($announcements as $announcement)
             {
                 $link = new link ($this->uri(array('action'=>'view', 'id'=>$announcement['id'])));
@@ -422,63 +428,63 @@ class announcements extends controller
                 $objIcon = $this->getObject('geticon', 'htmlelements');
 	 		            $objIcon->setIcon('delete');
                 //Link to delete using the delete icon
-                $deleteArray = array('action'=>'delete', 'id'=>$announcement['id']);    
+                $deleteArray = array('action'=>'delete', 'id'=>$announcement['id']);
                 $deleteLink = $objIcon->getDeleteIconWithConfirm($announcement['id'], $deleteArray, 'announcements');
-				
+
                 $table->startRow();
                 $table->addCell($objDateTime->formatDate($announcement['createdon']), '15%');
                 $table->addCell($link->show(),'50%');
                 $table->addCell($this->objUser->fullName($announcement['createdby']), '20%');
-                
+
                 if ($announcement['contextid'] == 'site') {
                     $type = $this->objLanguage->languageText('mod_announcements_siteword', 'announcements', 'Site');
                 } else {
                     $type = ucwords($this->objLanguage->code2Txt('mod_context_context', 'context', NULL, '[-context-]'));
                 }
-                
+
                 $table->addCell($type, 200);
 				if ($this->checkPermission($announcement['id'])) {
 					$table->addCell($editLink->show()."&nbsp;&nbsp;".$deleteLink, '15%');
 				}
                 $table->endRow();
-                
+
             }
-            
+
             echo $table->show();
     }
-    
+
     /**
      * Method to show the form to edit an announcement
      */
     private function __edit()
     {
         $id = $this->getParam('id');
-        
+
         $announcement = $this->objAnnouncements->getMessage($id);
-        
+
         if ($announcement == FALSE) {
             return $this->nextAction(NULL, array('error'=>'unknownannouncement'));
         } else if (!$this->checkPermission($announcement['id'])) {
             return $this->nextAction(NULL, array('error'=>'nopermission'));
         } else {
             $this->setVarByRef('announcement', $announcement);
-            
+
             $contextAnnouncementList = $this->objAnnouncements->getMessageContexts($id);
             $this->setVarByRef('contextAnnouncementList', $contextAnnouncementList);
-            
+
             $this->setVar('mode', 'edit');
-            
+
             return 'addedit_tpl.php';
         }
     }
-    
+
     /**
      * Method to update an announcement
      *
      */
     private function __update()
     {
-        
+
         $id = $this->getParam('id');
         $title = $this->getParam('title');
         $message = $this->getParam('message');
@@ -486,25 +492,25 @@ class announcements extends controller
         $mode = $this->getParam('mode');
         $recipienttarget = $this->getParam('recipienttarget');
         $contexts = $this->getParam('contexts');
-        
+
         $email = ($email == 'Y') ? TRUE : FALSE;
-        
+
         if (!$this->checkPermission($id)) {
             return $this->nextAction(NULL, array('error'=>'nopermission'));
         } else {
             $this->objAnnouncements->updateAnnouncement($id, $title, $message, $recipienttarget, $contexts, $email);
-            
+
             return $this->nextAction('view', array('id'=>$id));
         }
     }
-    
+
     /**
      * Method to delete an announcement
      */
     private function __delete()
     {
         $id = $this->getParam('id');
-        
+
         if (!$this->checkPermission($id)) {
             return $this->nextAction(NULL, array('error'=>'nopermission'));
         } else {
--- announcements/templates/content/addedit_tpl.php
+++ announcements/templates/content/addedit_tpl.php
@@ -17,9 +17,6 @@
 
 </script>
 <?php
-$outerLayer = "";
-$this->appendArrayVar('headerParams', $this->getJavaScriptFile('jquery.livequery.js', 'jquery'));
-
 $this->loadClass('htmlheading', 'htmlelements');
 $this->loadClass('link', 'htmlelements');
 $this->loadClass('form', 'htmlelements');
@@ -30,9 +27,12 @@ $this->loadClass('checkbox', 'htmlelements');
 $this->loadClass('hiddeninput', 'htmlelements');
 $this->loadClass('label', 'htmlelements');
 
+$this->appendArrayVar('headerParams', $this->getJavaScriptFile('jquery.livequery.js', 'jquery'));
+
+$outerLayer = "";
+
 $header = new htmlHeading();
 $header->type = 1;
-
 if ($mode == 'edit') {
     $header->str = $this->objLanguage->languageText('mod_announcements_update', 'announcements', 'Edit Announcement');
     $formAction = 'update';
@@ -40,7 +40,6 @@ if ($mode == 'edit') {
     $header->str = $this->objLanguage->languageText('mod_announcements_addnewannouncement', 'announcements', 'Add New Announcement');
     $formAction = 'save';
 }
-
 $outerLayer = $header->show();
 
 $form = new form ('announcement', $this->uri(array('action'=>$formAction)));
@@ -52,99 +51,86 @@ if ($mode == 'edit') {
 
 $table = $this->newObject('htmltable', 'htmlelements');
 
+if ($mode == 'fixup') {
+    $table->startRow();
+    $table->addCell('<font color="#ff0000">'. $errormessage.'</font>');
+    $table->endRow();
+}
 
+$table->startRow();
 $label = new label ($this->objLanguage->languageText('word_title', 'system', 'Title'), 'input_title');
 $titlefield = new textinput('title');
 $titlefield->size = 60;
+/*
 if(!empty($enteredtitle)){
     $title->value = $enteredtitle;
-}elseif ($mode == 'edit') {
+} elseif
+*/
+if ($mode == 'edit') {
     $titlefield->value = $announcement['title'];
-}
-if ($mode == 'fixup') {
+} else if ($mode == 'fixup') {
     $titlefield->value = $title;
 }
-if ($mode == 'fixup') {
-    $table->startRow();
-    $table->addCell('<font color="#ff0000">'. $errorMessage.'</font>');
-    $table->endRow();
-
-}
-$table->startRow();
 $table->addCell($label->show(), 120);
 $table->addCell($titlefield->show());
 $table->endRow();
 
-$contextsList = '';
-
-if (count($lecturerContext) > 0) {
-    foreach ($lecturerContext as $context) {
-        $checkbox = new checkbox ('contexts[]', $context);
-        $checkbox->value = $context;
-        $checkbox->cssId = 'context_'.$context;
-        $checkbox->cssClass = 'context_option';
-
-        if ($mode == 'add' && $context == $this->objContext->getContextCode()) {
-            $checkbox->ischecked = TRUE;
-        }
-
-        if ($mode == 'edit') {
-            if (in_array($context, $contextAnnouncementList)) {
+if (
+    $mode == 'add'
+    || $mode == 'edit'
+        && $announcement['contextid'] != 'site'
+    || $mode == 'fixup'
+        && $recipienttarget != 'site') {
+    $contextsList = '';
+    if (count($lecturerContext) > 0) {
+        foreach ($lecturerContext as $context) {
+            $checkbox = new checkbox ('contexts[]', $context);
+            $checkbox->value = $context;
+            $checkbox->cssId = 'context_'.$context;
+            $checkbox->cssClass = 'context_option';
+            if ($mode == 'add' && $context == $this->objContext->getContextCode()) {
                 $checkbox->ischecked = TRUE;
+            } else if ($mode == 'edit') {
+                if (in_array($context, $contextAnnouncementList)) {
+                    $checkbox->ischecked = TRUE;
+                }
+            } else if ($mode == 'fixup') {
+                if (in_array($context, $contexts)) {
+                    $checkbox->ischecked = TRUE;
+                }
             }
+            $contextRow=$this->objContext->getContext($context);
+            $label = new label(' '.$contextRow['title'], 'context_'.$context);
+            $contextsList .= '<br />&nbsp; &nbsp; '.$checkbox->show().$label->show();
         }
-        if ($mode == 'fixup') {
-            if (in_array($context, $enteredcontexts)) {
-                $checkbox->ischecked = TRUE;
-            }
-        }
-        $contextRow=$this->objContext->getContext($context);
-        $label = new label(' '.$contextRow['title'], 'context_'.$context);
-
-        $contextsList .= '<br />&nbsp; &nbsp; '.$checkbox->show().$label->show();
     }
 }
-
 
 if ($mode == 'add') {
     if ($isAdmin && count($lecturerContext) > 0) {
         $table->startRow();
-
         $table->addCell($this->objLanguage->languageText('mod_announcements_sendto', 'announcements', 'Send to').':');
-
         $recipientTarget = new radio ('recipienttarget');
         $recipientTarget->setBreakSpace('<br />');
         $recipientTarget->addOption('site', $this->objLanguage->languageText('mod_announcements_allusers', 'announcements', 'Site - All Users'));
         $recipientTarget->addOption('context', $this->objLanguage->code2Txt('mod_announcements_onlytofollowing', 'announcements', NULL, 'Only to the following [-contexts-]'));
-
         $recipientTarget->setSelected('site');
-
-        $str = $recipientTarget->show();
-
-        $table->addCell($str.$contextsList);
-
+        $table->addCell($recipientTarget->show().$contextsList);
         $table->endRow();
     } else if ($isAdmin) {
         $recipientTarget = new hiddeninput('recipienttarget', 'site');
         $form->addToForm($recipientTarget->show());
     } else {
-        $table->startRow();
-        $table->addCell($this->objLanguage->languageText('mod_announcements_sendto', 'announcements', 'Send to'));
-
-        $str = $this->objLanguage->code2Txt('mod_announcements_followingcontexts', 'announcements', NULL, 'the following [-contexts-]').':';
-
-        $table->addCell($str.$contextsList);
-
-        $table->endRow();
-
         $recipientTarget = new hiddeninput('recipienttarget', 'context');
         $form->addToForm($recipientTarget->show());
+        $table->startRow();
+        $table->addCell($this->objLanguage->languageText('mod_announcements_sendto', 'announcements', 'Send to'));
+        $table->addCell($this->objLanguage->code2Txt('mod_announcements_followingcontexts', 'announcements', NULL, 'the following [-contexts-]').':'.$contextsList);
+        $table->endRow();
     }
-} else {
-
+} else if ($mode == 'edit') {
     $recipientTarget = new hiddeninput('recipienttarget', $announcement['contextid']);
     $form->addToForm($recipientTarget->show());
-
     if ($announcement['contextid'] == 'site') {
         $table->startRow();
         $table->addCell($this->objLanguage->languageText('word_type', 'system', 'Type').':');
@@ -152,68 +138,73 @@ if ($mode == 'add') {
         $table->endRow();
     } else {
         $table->startRow();
-
         $label = new label ($this->objLanguage->languageText('word_title', 'system', 'Title'), 'input_title');
         $title = new textinput('title');
-
         $table->addCell($this->objLanguage->languageText('mod_announcements_sendto', 'announcements', 'Send to').':');
-
-        $str = $this->objLanguage->code2Txt('mod_announcements_followingcontexts', 'announcements', NULL, 'the following [-contexts-]').':';
-
-        $table->addCell($str.$contextsList);
-
+        $table->addCell($this->objLanguage->code2Txt('mod_announcements_followingcontexts', 'announcements', NULL, 'the following [-contexts-]').':'.$contextsList);
+        $table->endRow();
+    }
+} else if ($mode == 'fixup') {
+    $recipientTarget = new hiddeninput('recipienttarget', $recipienttarget);
+    $form->addToForm($recipientTarget->show());
+    if ($recipienttarget == 'site') {
+        $table->startRow();
+        $table->addCell($this->objLanguage->languageText('word_type', 'system', 'Type').':');
+        $table->addCell($this->objLanguage->languageText('mod_announcements_siteannouncement', 'announcements', 'Site Announcement'));
+        $table->endRow();
+    } else {
+        $table->startRow();
+        $label = new label ($this->objLanguage->languageText('word_title', 'system', 'Title'), 'input_title');
+        $title = new textinput('title');
+        $table->addCell($this->objLanguage->languageText('mod_announcements_sendto', 'announcements', 'Send to').':');
+        $table->addCell($this->objLanguage->code2Txt('mod_announcements_followingcontexts', 'announcements', NULL, 'the following [-contexts-]').':'.$contextsList);
         $table->endRow();
     }
 }
-$table->startRow();
 
+// Email to users
+$table->startRow();
 $email2Users = new radio ('email');
 $email2Users->addOption('N', $this->objLanguage->languageText('word_no', 'system', 'No'));
 $email2Users->addOption('Y', $this->objLanguage->languageText('word_yes', 'system', 'Yes'));
-
-
 $email2Users->setSelected('Y');
-
 $email2Users->setBreakSpace(' &nbsp; ');
-
 $table->addCell($this->objLanguage->languageText('mod_announcements_emailtousers', 'announcements', 'Email to Users'));
 $table->addCell($email2Users->show());
 $table->endRow();
 
-
-
+// Message
 $table->startRow();
-
 $htmlArea = $this->newObject('htmlarea', 'htmlelements');
 $htmlArea->name = 'message';
+/*
 if(!empty($enteredmessage)){
     $title->value = $enteredmessage;
-}elseif ($mode == 'edit') {
+} elseif
+*/
+if ($mode == 'edit') {
     $htmlArea->value = $announcement['message'];
-}
-if ($mode == 'fixup') {
+} else if ($mode == 'fixup') {
     $htmlArea->value = $message;
 }
-
 $table->addCell($this->objLanguage->languageText('word_message', 'system', 'Message'));
 $table->addCell($htmlArea->show());
 $table->endRow();
 
+/*
 $table->startRow();
-
 $table->addCell('&nbsp;');
 $table->addCell('&nbsp;');
 $table->endRow();
+*/
 
+// Post Announcement
 $table->startRow();
-
 $button = new button ('send', $this->objLanguage->languageText('mod_announcements_postannouncement', 'announcements', 'Post Announcement'));
 $button->setToSubmit();
-
 $table->addCell('&nbsp;');
 $table->addCell($button->show());
 $table->endRow();
-
 
 $form->addToForm($table->show());
 
@@ -222,15 +213,15 @@ $form->addToForm($modeInput->show());
 
 $outerLayer .= $form->show();
 
-$outerLayer = '<div class="outerwrapper">' . $outerLayer . '</div>';
-echo $outerLayer;
+echo
+    '<div class="outerwrapper">'
+    . $outerLayer
+    . '</div>';
 
-
-
-$backLink = new link ($this->uri(NULL));
+$backLink = new link ($this->uri(array()));
 $backLink->link = $this->objLanguage->languageText('mod_announcements_back', 'announcements', 'Back to Announcements');
-
-echo "<div class='modulehome'></div><div class='modulehomelink'>"
-  . $backLink->show() . '</div>';
-
+/*
+echo "<div class='modulehome'></div><div class='modulehomelink'>" . $backLink->show() . '</div>';
+*/
+echo $backLink->show();
 ?>
