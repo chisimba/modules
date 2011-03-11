1f935031887a0e5a78a5853169d88b2868274317
Kurtcrossman <unknown@unknown.com>
2011-03-11 10:34:20 +0000

    missing table added

---
+++ unesco_oer/sql/tbl_resource_types.sql
@@ -0,0 +1,14 @@
+<?php
+
+//define table
+$tablename = 'tbl_resources_types';
+
+$options = array('comment'=>'Table to store resources types','collate'=>'utf8_general_ci','character_set'=>'utf8');
+
+//define fields
+$fields = array(
+		'id' => array('type' => 'varchar','length' => 32,'not null'),
+		'description '=>array('type'=>'varchar','length'=>128)
+		
+);
+?>
