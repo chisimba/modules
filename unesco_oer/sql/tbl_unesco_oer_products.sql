55811bec3a0088600088c6cf7d95edeabc205419
deals shaun rikhotso <unknown@unknown.com>
2011-03-15 09:54:13 +0000

    naming convention

--- unesco_oer/sql/tbl_unesco_oer_products.sql
+++ unesco_oer/sql/tbl_unesco_oer_products.sql
@@ -15,11 +15,11 @@ $fields = array(
                 'created_on' => array('type' => 'timestamp'),
                 'resource_type ' =>array('type' =>'text','length'=>32),
                 'content_type'=>array('type'=>'text','length'=>32),
-                'format ' => array('type' => 'text','length' => 32),
-                'source ' =>array('type' =>'text','length'=>32),
-                 'theme  ' =>array('type' =>'text','length'=>32),
-                 'language   ' =>array('type' =>'text','length'=>3),
-                 'content    ' =>array('type' =>'text'),
+                'format' => array('type' => 'text','length' => 32),
+                'source' =>array('type' =>'text','length'=>32),
+                 'theme ' =>array('type' =>'text','length'=>32),
+                 'language ' =>array('type' =>'text','length'=>3),
+                 'content ' =>array('type' =>'text'),
                 'thumbnail '=>array('type'=>'text','length'=>512)
  		);
 ?>
