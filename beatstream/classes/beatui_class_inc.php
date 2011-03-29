40f45462f2fcd1a6a253fc1f843bb7b27aa0586c
paulscott <unknown@unknown.com>
2011-03-29 09:24:25 +0000

    basic beatstream module

---
+++ beatstream/classes/beatui_class_inc.php
@@ -0,0 +1,294 @@
+<?php
+/**
+ *
+ * beatstream helper class
+ *
+ * PHP version 5.1.0+
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * @category  Chisimba
+ * @package   beatstream
+ * @author    Paul Scott <pscott@uwc.ac.za>
+ * @copyright 2011 AVOIR
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version
+ * @link      http://avoir.uwc.ac.za
+ */
+
+// security check - must be included in all scripts
+if (! /**
+ * The $GLOBALS is an array used to control access to certain constants.
+ * Here it is used to check if the file is opening in engine, if not it
+ * stops the file from running.
+ *
+ * @global entry point $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ *
+ */
+$GLOBALS ['kewl_entry_point_run']) {
+    die ( "You cannot view this page directly" );
+}
+// end security check
+
+
+/**
+ *
+ * beatstream helper class
+ *
+ * PHP version 5.1.0+
+ *
+ * @author Paul Scott
+ * @package beatstream
+ *
+ */
+class beatui extends object {
+
+    /**
+     * @var string $objLanguage String object property for holding the language object
+     *
+     * @access public
+     */
+    public $objLanguage;
+
+    /**
+     * @var string $objConfig String object property for holding the config object
+     *
+     * @access public
+     */
+    public $objConfig;
+
+    /**
+     * @var string $objSysConfig String object property for holding the sysconfig object
+     *
+     * @access public
+     */
+    public $objSysConfig;
+
+    /**
+     * @var string $objUser String object property for holding the user object
+     *
+     * @access public
+     */
+    public $objUser;
+    
+    /**
+     * @var array $data Object property for holding the data
+     *
+     * @access public
+     */
+    public $data = array();
+
+    /**
+     * Constructor
+     *
+     * @access public
+     */
+    public function init() {
+        $this->objLanguage   = $this->getObject('language', 'language');
+        $this->objConfig     = $this->getObject('altconfig', 'config');
+        $this->objSysConfig  = $this->getObject ( 'dbsysconfig', 'sysconfig' );
+        $this->objUser       = $this->getObject('user', 'security');
+        $this->objCC         = $this->getObject('displaylicense', 'creativecommons');
+        $this->objOps        = $this->newObject('beatops');
+        $this->objDbFeatures = $this->getObject('dbbeats');
+        $this->objWashout    = $this->getObject('washout', 'utilities');
+        $this->objPodcast    = $this->getObject('dbpodcast', 'podcast');
+        $this->objDateTime   = $this->getObject('dateandtime', 'utilities');
+        
+        // htmlelements
+        $this->loadClass('form', 'htmlelements');
+        $this->loadClass('textinput', 'htmlelements');
+        $this->loadClass('label', 'htmlelements');
+        $this->loadClass('htmlheading', 'htmlelements');
+        $this->loadClass('htmlarea', 'htmlelements');
+        $this->loadClass('radio', 'htmlelements');
+    }
+    
+    /**
+     * Method to return the required JS to display an @anywhere tweet box for CCSA
+     *
+     * @param string screen_name the username of the user to tweet for
+     * @return string some js
+     */
+    public function featureTweetBox($screen_name) {
+        $gourl = $this->uri(array('action' => 'tweetit', 'user' => $screen_name), 'featuresuggest');
+        $this->teeny = $this->getObject ( 'tiny', 'tinyurl');
+        $gourl = str_replace("&amp;", "&", $gourl);
+        $gourl = $this->teeny->create(urlencode($gourl));
+        $msg = $this->objLanguage->languageText("mod_featuresuggest_msg", "featuresuggest")." ";
+        
+        $js = '<script type="text/javascript">
+		
+		    twttr.anywhere(function (T) {
+		
+		        T("#featuretweetbox").tweetBox({
+		            height: 80,
+		            width: 550,
+		            defaultContent: "'.$msg.' '.$gourl.'",
+		            label: "Tweet this!"
+		        });
+		
+		    });
+		
+		</script>';
+		
+		return $js;
+    }
+    
+    public function formatData($sug) {
+        // convert the array to an object now
+        $this->loadClass('link', 'htmlelements');
+        $this->loadClass('windowpop', 'htmlelements');
+        $this->loadClass('htmlheading', 'htmlelements');
+        $this->loadClass('formatfilesize', 'files');
+        $objFile = $this->getObject('dbfile', 'filemanager');
+        
+        $str = NULL;
+        $str = '<ul class="suggestions">';
+        foreach($sug as $s) {
+            $content = NULL;
+            $filesize = new formatfilesize();
+            $podcast = $this->objPodcast->getPodcast($s['suggestion']);
+            // var_dump($podcast);
+            $table = $this->newObject('htmltable', 'htmlelements');
+            $table->startRow();
+            if ($podcast['artist'] == '') {
+                $artist = $this->objUser->fullname($podcast['creatorid']);
+            } else {
+                $artist = $podcast['artist'];
+            }
+            if (isset($id)) {  
+                $table->addCell('<strong>'.$this->objLanguage->languageText('word_by', 'system').':</strong> '.$artist, '50%');
+            } else {
+                $authorLink = new link ($this->uri(array('action'=>'byuser', 'id'=>$this->objUser->userName($podcast['creatorid']))));
+                $authorLink->link = $artist;
+                $table->addCell('<strong>'.$this->objLanguage->languageText('word_by', 'system').':</strong> '.$authorLink->show(), '50%');
+            }
+            $table->addCell('<strong>'.$this->objLanguage->languageText('word_date', 'system').':</strong> '.$this->objDateTime->formatDate($podcast['datecreated']), '50%');
+            $table->endRow();
+            
+            $table->startRow();
+            $table->addCell('<strong>'.$this->objLanguage->languageText('phrase_filesize', 'system').':</strong> '.$filesize->formatsize($podcast['filesize']), '50%');
+            $playtime = $this->objDateTime->secondsToTime($podcast['playtime']);
+            $playtime = ($playtime == '0:0') ? '<em>Unknown</em>' : $playtime;
+            $table->addCell('<strong>'.$this->objLanguage->languageText('word_playtime', 'system').':</strong> '.$playtime, '50%');
+            $table->endRow();
+        
+            $content .= $table->show();
+        
+            $downloadLink = new link ($this->objConfig->getcontentPath().$podcast['path']);
+            $downloadLink->link = htmlentities($podcast['filename']);
+        
+            $this->objPop=&new windowpop;
+            $this->objPop->set('location',$this->uri(array('action'=>'playpodcast', 'id'=>$podcast['id']), 'podcast'));
+            $this->objPop->set('linktext', $this->objLanguage->languageText('mod_podcast_listenonline', 'podcast'));
+            $this->objPop->set('width','280');
+            $this->objPop->set('height','120');
+            //leave the rest at default values
+            //$this->objPop->putJs(); // you only need to do this once per page
+        
+            $objSoundPlayer = $this->newObject('buildsoundplayer', 'files');
+            $soundFile = str_replace('&', '&amp;', $objFile->getFilePath($podcast['fileid']));
+            $soundFile = str_replace(' ', '%20', $soundFile);
+            $objSoundPlayer->setSoundFile($soundFile);
+        
+            $content .= '<br /><p>'.$objSoundPlayer->show().'</p><p><strong>'.$this->objLanguage->languageText('mod_podcast_downloadpodcast', 'podcast').':</strong> ('.$this->objLanguage->languageText('mod_podcast_rightclickandchoose', 'podcast', 'Right Click, and choose Save As').') '.$downloadLink->show().'</p>';
+            $s['suggestion'] = $content;
+            $this->objOps->setData($s);
+            $str .= (string)$this->objOps;
+        }
+        $str .='</ul>';
+               
+        return $this->objWashout->parseText($str);
+    }
+    
+    public function formatUI() {
+        $js = NULL;
+        $js .= '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>';
+        $js .= $this->getJavascriptFile('script.js', 'beatstream');
+        return $js;
+    }
+    
+    public function addForm() {
+        // load up a simple form 
+        return $this->addBeat();
+    }
+    
+    public function uploadBeat() {
+        $objFileUpload = $this->getObject('uploadinput', 'filemanager');
+        $objFileUpload->enableOverwriteIncrement = TRUE;
+        $results = $objFileUpload->handleUpload('fileupload');
+        // Technically, FALSE can never be returned, this is just a precaution
+        // FALSE means there is no fileinput with that name
+        if ($results == FALSE) {
+            return FALSE;
+        } elseif ($results['success']) {
+                // add to db as podcast
+                $podcastResult = $this->objPodcast->addPodcast($results['fileid']);
+                // check result of adding as podcast
+                if ($podcastResult == 'nofile') {
+                    return FALSE;
+                } else if ($podcastResult == 'fileusedalready') {
+                    return TRUE;
+                } else {
+                    return TRUE;
+                }
+        } else {
+            // If not successfully uploaded
+            return FALSE;
+        }
+    }
+    
+    public function addBeat() {
+        $ret = NULL;
+        $this->loadClass('form', 'htmlelements');
+        // $this->loadClass('textinput', 'htmlelements');
+        $header = new htmlHeading();
+        $header->str = $this->objLanguage->languageText('mod_beatstream_uploadnewfile', 'beatstream', 'Upload new beat');
+        $header->type = 4;
+        $ret .= $header->show();
+        
+        $form = new form('addbeatbyupload', $this->uri(array('action'=>'uploadbeat')));
+        $form->extra = 'enctype="multipart/form-data"';
+        $objUpload = $this->newObject('selectfile', 'filemanager');
+        $objUpload->name = 'beatfile';
+        $objUpload->restrictFileList = array('mp3');
+        $button = new button('submitform', $this->objLanguage->languageText('mod_beatstream_uploadbeat', 'beatstream', 'Upload Beat'));
+        $button->setToSubmit();;
+        $form->addToForm($objUpload->show().'<br />'.$button->show());
+        $ret .= $form->show();
+        
+        return $ret;
+    }
+    
+    /**
+     * Method to display the login box for prelogin blog operations
+     *
+     * @param  bool   $featurebox
+     * @return string
+     */
+    public function loginBox($featurebox = FALSE)
+    {
+        $objBlocks = $this->getObject('blocks', 'blocks');
+        if ($featurebox == FALSE) {
+            return $objBlocks->showBlock('login', 'security') . "<br />" . $objBlocks->showBlock('register', 'security');
+        } else {
+            $objFeatureBox = $this->getObject('featurebox', 'navigation');
+            return $objFeatureBox->show($this->objLanguage->languageText("word_login", "system") , $objBlocks->showBlock('login', 'security', 'none')
+              . "<br />" . $objBlocks->showBlock('register', 'security', 'none') );
+        }
+    }
+}
+?>
