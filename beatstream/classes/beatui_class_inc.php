6751ab3e254440a999ec57860cec2f62e9ad2319
paulscott <unknown@unknown.com>
2011-03-30 11:11:09 +0000

    width

--- beatstream/classes/beatui_class_inc.php
+++ beatstream/classes/beatui_class_inc.php
@@ -245,6 +245,7 @@ class beatui extends object {
             $fb = '<iframe src="http://www.facebook.com/plugins/like.php?href='.$fburl.'&amp;layout=standard&amp;show_faces=true&amp;width=450&amp;action=like&amp;font&amp;colorscheme=light&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:80px;" allowTransparency="true"></iframe>';
             
             $soctable = $this->newObject('htmltable', 'htmlelements');
+            $soctable->width = '80%';
             $soctable->startRow();
             $soctable->addCell($rt, '50%');
             $soctable->addCell($fb, '50%');
