503a80157b638efc97ebde41e9f0ad58102962c9
Derek Keats <derek@dkeats.com>
2011-04-15 13:16:07 +0000

    Changed the editors to use full width when creating or editing a post.

--- blog/templates/content/postedit_tpl.php
+++ blog/templates/content/postedit_tpl.php
@@ -2,43 +2,17 @@
 //post edit template
 $cssLayout = $this->newObject('csslayout', 'htmlelements');
 $middleColumn = NULL;
-$objUi = $this->getObject('blogui');
-// left hand blocks
-$leftCol = $objUi->leftBlocks($userid);
-// right side blocks
-$rightSideColumn = $objUi->rightBlocks($userid, NULL);
-if ($leftCol == NULL || $rightSideColumn == NULL) {
-    $cssLayout->setNumColumns(2);
-} else {
-    $cssLayout->setNumColumns(3);
-}
 //get the posts editor
 $middleColumn = $this->objblogPosts->postEditor($userid, $editid);
-
-
 // Added by Tohir - Standard layout for elearn
 $layoutToUse = $this->objSysConfig->getValue('blog_layout', 'blog');
-
 if ($layoutToUse == 'elearn') {
     $this->setLayoutTemplate('blogelearn_layout_tpl.php');
     echo $middleColumn;
 } else {
-    if ($leftCol == NULL) {
-        $leftCol = $rightSideColumn;
-        $cssLayout->setMiddleColumnContent($middleColumn);
-        $cssLayout->setLeftColumnContent($leftCol);
-        //$cssLayout->setRightColumnContent($rightSideColumn);
-        echo $cssLayout->show();
-    } elseif ($rightSideColumn == NULL) {
-        $cssLayout->setMiddleColumnContent($middleColumn);
-        $cssLayout->setLeftColumnContent($leftCol);
-        echo $cssLayout->show();
-    } else {
-        $cssLayout->setNumColumns(2);
-        $cssLayout->setMiddleColumnContent($middleColumn);
-        $cssLayout->setLeftColumnContent($leftCol);
-    //    $cssLayout->setRightColumnContent($rightSideColumn);
-        echo $cssLayout->show();
-    }
+    // DWK changed editor to single column 2011 04 15
+    $cssLayout->setNumColumns(1);
+    $cssLayout->setMiddleColumnContent($middleColumn);
+    echo $cssLayout->show();
 }
 ?>
--- blog/templates/content/writepost_tpl.php
+++ blog/templates/content/writepost_tpl.php
@@ -19,33 +19,8 @@ if ($layoutToUse == 'elearn') {
     $this->setLayoutTemplate('blogelearn_layout_tpl.php');
     echo $middleColumn;
 } else {
-    $objUi = $this->getObject('blogui');
-    // left hand blocks
-    $leftCol = $objUi->leftBlocks($userid);
-    // right side blocks
-    $rightSideColumn = $objUi->rightBlocks($userid, NULL);
-
-    if ($leftCol == NULL || $rightSideColumn == NULL) {
-        $cssLayout->setNumColumns(2);
-    } else {
-        $cssLayout->setNumColumns(3);
-    }
-    if ($leftCol == NULL) {
-        $leftCol = $rightSideColumn;
-        $cssLayout->setMiddleColumnContent($middleColumn);
-        $cssLayout->setLeftColumnContent($leftCol);
-        //$cssLayout->setRightColumnContent($rightSideColumn);
-        echo $cssLayout->show();
-    } elseif ($rightSideColumn == NULL) {
-        $cssLayout->setMiddleColumnContent($middleColumn);
-        $cssLayout->setLeftColumnContent($leftCol);
-        echo $cssLayout->show();
-    } else {
-        $cssLayout->setNumColumns(2);
-        $cssLayout->setMiddleColumnContent($middleColumn);
-        $cssLayout->setLeftColumnContent($leftCol);
-        //$cssLayout->setRightColumnContent($rightSideColumn);
-        echo $cssLayout->show();
-    }
+    $cssLayout->setNumColumns(1);
+    $cssLayout->setMiddleColumnContent($middleColumn);
+    echo $cssLayout->show();
 }
 ?>
