ab9254b15fba47a035d02eed9517c25747a37d7b
boniface <unknown@unknown.com>
2011-04-17 12:15:37 +0000

    i have changed the dispatch mechanism for the module and defined a method for error notification which i still work on

--- tzschoolacedemics/controller.php
+++ tzschoolacedemics/controller.php
@@ -1,41 +1,79 @@
-<?PHP
+<?php
 // security check - must be included in all scripts
-if (!$GLOBALS['kewl_entry_point_run']) {
+if (!
+/**
+ * Description for $GLOBALS
+ * @global unknown $GLOBALS['kewl_entry_point_run']
+ * @name   $kewl_entry_point_run
+ */
+$GLOBALS['kewl_entry_point_run']) {
     die("You cannot view this page directly");
 }
+// end security check
 
 /**
-*
-* Controller class for Chisimba for the module tzschoolacedemics
-* which uses the vlc library to wrap and
-* create an online vlc player
-*
-* @author UCC-MIP2011
-* @package  tzschoolacedemics
-*
-*/
+ * Controller class for academic module
+ *
+ * @category  Chisimba
+ * @package   SMIS Academics
+ * @author    Boniface Chacha <bonifacechacha@gmail.com>
+ */
+class tzacedemics extends controller {
 
+  public $lang;
 
-class  tzschoolacedemics extends controller {
+  private $user;
 
-    var $tzacademicdb;
-	 /**
-     * Intialiser for the stories controller
-     *
-     * @param byref $ string $engine the engine object
-     */
-    public function init()
-    {        //Instantiate the language object
-       $this->tzacademicdb = $this->newObject('tzschoolacedemicsdb');
+  public function  init() {
+
+        $this->loadClass('user','security');
+        $this->lang=$this->getObject('language','language');
+        $this->user=$this->getObject('user','security');
     }
 
+    public function dispatch(){
+        //return 'AddEdit_tpl.php';
+        $action=$this->getParam('action', 'main');
+        $method=$this->__getMethod($action);
+        return $this->$method();
 
-    public function dispatch()
+    }
+
+    function __getMethod(& $action)
     {
-    	
+        if ($this->__validAction($action)) {
+            return "__" . $action;
+        } else {
+            return "__actionError";
+        }
     }
 
+    function __validAction(& $action)
+    {
+        if (method_exists($this, "__".$action)) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
 
+    private function __actionError()
+    {
+       /* echo 'error';
+        $this->setVar('str', "<h3>"
+          . $this->objLanguage->languageText("phrase_unrecognizedaction")
+          .": " . $action . "</h3>");
+        return 'dump_tpl.php';*/
+        echo 'error';
+    }
+
+    private function __main(){
+        return 'mainacademics_tpl.php';
+    }
+
+    public function  requiresLogin($action) {
+        return false;
+    }
 
 }
 ?>
