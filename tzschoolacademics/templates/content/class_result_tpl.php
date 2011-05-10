c4cf0809cba06d5a0e1e0e82a339aea7d9be4049
charles <unknown@unknown.com>
2011-05-10 15:37:11 +0000

    class results and subject result modified

--- tzschoolacademics/templates/content/class_result_tpl.php
+++ tzschoolacademics/templates/content/class_result_tpl.php
@@ -5,7 +5,7 @@
  *   @email charlesmdack@gmail.com
  */
 $displayObj=$this->newObject('reportdisplay', 'tzschoolacademics');
-if(strcmp($option,'sub_result')==0){
+if(strcmp($option,'class_result')==0){
 
 echo $displayObj->generate_class_result($exam_type, $term_id, $year_id, $class_id);
 }
