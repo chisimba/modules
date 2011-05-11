52d90a4b31ead138e5ad284d85c388d77fec4b44
mushyvenance <unknown@unknown.com>
2011-05-11 14:46:33 +0000

    contents added to modify the module

--- tzschoolfees/classes/paymentdetails_class_inc.php
+++ tzschoolfees/classes/paymentdetails_class_inc.php
@@ -4,112 +4,145 @@
  * @module name:fee module
  * @date 2011 05 06
  * and open the template in the editor.
- */
+*/
 //class which shows student payment details
-class paymentdetails extends Object{
+class paymentdetails extends Object {
     public $lang;
 
-function  init() {
+    function  init() {
 
-    $this->lang=$this->getObject('language','language');
-    
+        $this->lang=$this->getObject('language','language');
+
     }
     //function to load clases
-function loadElements(){
+    function loadElements() {
 
-    //load all clases needed
-    $this->loadClass('form','htmlelements');
-    $this->loadClass('textinput','htmlelements');
-    $this->loadClass('label','htmlelements');
-    $this->loadClass('button','htmlelements');
-    //$this->loadClass('datepicker','htmlelements');
-    //$this->loadClass('tetxarea','htmlelements');
-    $this->loadClass('dropdown', 'htmlelements');
-    $this->loadClass('fieldset','htmlelements');
+        //load all clases needed
+        $this->loadClass('htmlTable','htmlelements');
+        $this->loadClass('form','htmlelements');
+        $this->loadClass('textinput','htmlelements');
+        $this->loadClass('label','htmlelements');
+        $this->loadClass('button','htmlelements');
+        //$this->loadClass('datepicker','htmlelements');
+        //$this->loadClass('tetxarea','htmlelements');
+        $this->loadClass('dropdown', 'htmlelements');
+        $this->loadClass('fieldset','htmlelements');
 
-}
+    }
 //function for building a form
-function buildForm(){
+    function buildForm() {
 //call function loadElemnts
         $this->loadElements();
 
-        //creating new objects
-$objform = new form('payment',  $this->getAction());
-$formlabel = new label('<h2>student payment details</h2><br>');
-$objform->addToForm($formlabel->show());
+//creating new objects
+
+        $objform = new form('payment',  $this->getAction());
+        $formlabel = new label('<h2>student payment details</h2><br>');
+
+        $objform->addToForm($formlabel->show());
+
 
 //student full name
 
- $namelabel=new label('FULL NAME');
-$objform->addToForm($namelabel->show());
-$namefield = new textinput('fullname');
-$objform->addToForm($namefield->show().'<br/>');
+        $namelabel1=new label('FULL NAME');
+
+        $namefield = new textinput('fullname');
 
 //student class
+        $namelabel=new label('CLASS NAME');
 
-//$namelabel = new label($this->lang->languageText('mod_tzschoolfees_classname_label','tzschoolfees'),'class');
- $namelabel=new label('CLASS NAME');
-$objform->addToForm($namelabel->show());
-$classfield = new dropdown('class');
-$classfield->addOption('f1','FORM ONE');
-$classfield->addOption('f2','FORM TWO');
-$classfield->addOption('f3','FORM THREE');
-$classfield->addOption('f4','FORM FOUR');
-$classfield->addOption('f5','FORM FIVE');
-$classfield->addOption('f6','FORM SIX');
-$objform->addToForm($classfield->show().'<br/>');
+
+        $classfield = new dropdown('class');
+        $classfield->addOption('f1','FORM ONE');
+        $classfield->addOption('f2','FORM TWO');
+        $classfield->addOption('f3','FORM THREE');
+        $classfield->addOption('f4','FORM FOUR');
+        $classfield->addOption('f5','FORM FIVE');
+        $classfield->addOption('f6','FORM SIX');
+
 
 //amount paid by student
-//$amountlabel = new label($this->lang->languageText('mod_tzschoolfees_amount_label','tzschoolfees'),'amount');
- $amountlabel=new label('AMOUNT PAID');
-$objform->addToForm($amountlabel->show());
-$amountfield = new textinput('amount');
-$objform->addToForm($amountfield->show().'<br/>');
+        $amountlabel=new label('AMOUNT PAID');
+
+
+        $amountfield = new textinput('amount');
+//$objform->addToForm($amountfield->show().'<br/>');
 
 //installments by student
-//$installmntlabel = new label($this->lang->languageText('mod_tzschoolfees_installments_label','tzschoolfees'),'installments');
- $installmntlabel=new label('INSTALLMENTS');
-$objform->addToForm($installmntlabel->show());
-$installmentfield = new dropdown('installments');
-$installmentfield->addOption('full','FULL PAYMENT');
-$installmentfield->addOption('install','INSTALLMENT');
+        $installmntlabel=new label('INSTALLMENTS');
 
-$objform->addToForm($installmentfield->show().'<br/>');
+
+        $installmentfield = new dropdown('installments');
+        $installmentfield->addOption('full','FULL PAYMENT');
+        $installmentfield->addOption('install','INSTALLMENT');
 
 
 //complete payment
-//$paymentlabel = new label($this->lang->languageText('mod_tzschoolfees_payment_label','tzschoolfees'),'payment');
-$paymentlabel=new label('COMPLETE PAYMENT');
-$objform->addToForm($paymentlabel->show());
-$paymentfield = new textinput('amount');
-$objform->addToForm($paymentfield->show().'<br/>');
+        $paymentlabel=new label('COMPLETE PAYMENT');
+        $paymentfield = new textinput('amount');
+
 
 
 //amount payable by student
-//$payablelabel = new label($this->lang->languageText('mod_tzschoolfees_payable_label','tzschoolfees'),'payable');
- $payablelabel=new label('AMOUNT PAYABLE');
-$objform->addToForm($payablelabel->show());
-$payablefield = new textinput('payable');
-$objform->addToForm($payablefield->show().'<br/>');
+        $payablelabel=new label('AMOUNT PAYABLE');
+        $payablefield = new textinput('payable');
+      
 
- $saveButton=new button('register');
-       // $saveButton=new button('save','Submit');
-       $saveButton->setToSubmit();
-       $saveButton->value='save details';
+        $saveButton=new button('register');
+        $saveButton->setToSubmit();
+        $saveButton->value='save details';
 
-        $objform->addToForm($saveButton->showDefault());
+
+        $objTable = new htmlTable();
         
-$objfield = new fieldset();
-$objfield->width=600;
-$objfield->align='center';
+        $objTable->startRow();
+        $objTable->addCell($namelabel1->show().':');
+        $objTable->addCell($namefield->show());
+        $objTable->endRow();
 
-$objfield->addContent($objform->show());
+        $objTable->startRow();
+        $objTable->addCell($namelabel->show().':');
+        $objTable->addCell($classfield->show());
+        $objTable->endRow();
+
+        $objTable->startRow();
+        $objTable->addCell($amountlabel->show().':');
+        $objTable->addCell($amountfield->show());
+        $objTable->endRow();
+
+        $objTable->startRow();
+        $objTable->addCell($installmntlabel->show().':');
+        $objTable->addCell($installmentfield->show());
+        $objTable->endRow();
+
+        $objTable->startRow();
+        $objTable->addCell($paymentlabel->show().':');
+        $objTable->addCell($paymentfield->show());
+        $objTable->endRow();
+
+        $objTable->startRow();
+        $objTable->addCell($payablelabel->show().':');
+        $objTable->addCell($payablefield->show());
+        $objTable->endRow();
+
+        $objTable->startRow();
+        $objTable->addCell('');
+        $objTable->addCell($saveButton->show());
+        $objTable->endRow();
+
+        $objform->addToForm($objTable->show());
+        
+        $objfield = new fieldset();
+        $objfield->width=500;
+        $objfield->align='center';
+
+        $objfield->addContent($objform->show());
 
 
-echo $objfield->show();
+        echo $objfield->show();
 
-}
-        function getAction(){
+    }
+    function getAction() {
         $action=$this->getParam('action','edit');
         if($action=='edit')
             $formAction=  $this->uri(array('action'=>'edit'),'tzschoolfees');
@@ -119,14 +152,14 @@ echo $objfield->show();
         return $formAction;
     }
 
-        public function setValues($valuesArray=NULL){
+    public function setValues($valuesArray=NULL) {
 
-        }
-        function show(){
+    }
+    function show() {
 
-            return $this->buildForm();
+        return $this->buildForm();
 
-        }
+    }
 
 
 }
