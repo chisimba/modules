927ba94a56bc2c5e2971a2eee6e3bbb1edde32b9
charles <unknown@unknown.com>
2011-05-12 17:02:56 +0000

    structure changed

--- tzschoolacademics/templates/content/subject_result_tpl.php
+++ tzschoolacademics/templates/content/subject_result_tpl.php
@@ -1,26 +1,25 @@
 <?php
 
 // security check-must be included in all scripts
-if(!$GLOBALS['kewl_entry_point_run']){
+if (!$GLOBALS['kewl_entry_point_run']) {
     die("You cannot view this page directly");
 }
 // end security check
 
 
 /*
- * template for student result menu
+ * template for displaying data regarding various subject result
  *
  *  @author charles mhoja
  *  @email charlesmhoja@gmail.com
  */
 
 //loading thereport display class
-$displayObj=$this->newObject('reportdisplay', 'tzschoolacademics');
-if(strcmp($option,'sub_result')==0){
-echo $displayObj->generate_subject_result($subjectId, $exam, $term, $year_id, $class);
-
+$displayObj = $this->newObject('reportdisplay', 'tzschoolacademics');
+if (strcmp($option, 'sub_result') == 0) {
+    echo $displayObj->generate_subject_result($subject_id, $exam_id, $term_id, $year_id, $class_id);
 }
-else{
-  echo $displayObj->create_subject_result_form();
+else {
+    echo $displayObj->create_subject_result_form();
 }
 ?>
