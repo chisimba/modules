c649c6e71dce9920a75e4e02c0582deb82fbd0c5
charlvn <unknown@unknown.com>
2011-05-15 19:22:17 +0000

    Added search.

--- triplestore/templates/content/triplestore_tpl.php
+++ triplestore/templates/content/triplestore_tpl.php
@@ -1,37 +1,18 @@
 <?php
-$objExtJS = $this->getObject('extjs','ext');
-$objExtJS->show();
 
-$fullUri = $this->uri(NULL);
-$fullUri = explode("?" ,$fullUri);
-$siteUri = $fullUri[0];
+$document = new DOMDocument('1.0');
+$html5form = $this->getObject('html5form', 'html5elements');
 
-$halfUri = explode("index.php" ,$siteUri);
-$halfUri = $halfUri[0];
+$form = $html5form->form($document, 'POST', array('action'=>'search'), 'triplestore');
+$document->appendChild($form);
 
-$this->appendArrayVar('headerParams', '
-    <script type="text/javascript">
-	var baseuri = "'.$siteUri.'";
-    var uri = "'.$halfUri.'";
-    </script>');
+$p = $document->createElement('p');
+$form->appendChild($p);
 
-    
-	$ext = '<link rel="stylesheet" href="'.$this->getResourceUri('iconcss.css', 'triplestore').'" type="text/css" />';
-    $ext .= $this->getJavaScriptFile('ext-3.0-rc2/ux/fileuploadfield/FileUploadField.js', 'ext');
-    $ext .= '<link rel="stylesheet" href="'.$this->getResourceUri('ext-3.0-rc2/ux/fileuploadfield/css/fileuploadfield.css', 'ext').'" type="text/css" />';
-	$ext .= $this->getJavaScriptFile('triplestore.js', 'triplestore');
+$p->appendChild($html5form->text($document, 'id', NULL, 'Identifier'));
+$p->appendChild($html5form->text($document, 'subject', NULL, 'Subject'));
+$p->appendChild($html5form->text($document, 'predicate', NULL, 'Predicate'));
+$p->appendChild($html5form->text($document, 'object', NULL, 'Object'));
+$p->appendChild($html5form->submit($document, 'Search'));
 
-    $ext .= "
-	<style>
-	#mainpanel{
-	margin-left: auto ;
-    margin-right: auto ;
-	border:1px solid #c3daf9;
-	overflow:auto;
-	}
-	</style>";
-
-$this->appendArrayVar('headerParams', $ext);
-
-echo '<br/><div id="mainpanel"></div>';
-?>
+echo $document->saveHTML();
