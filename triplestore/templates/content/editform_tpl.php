6eabcae333823f36b3e6c1bf83b175e12d4f5257
charlvn <unknown@unknown.com>
2011-05-15 21:59:59 +0000

    Added language text.

--- triplestore/templates/content/editform_tpl.php
+++ triplestore/templates/content/editform_tpl.php
@@ -5,12 +5,11 @@ $html5form = $this->getObject('html5form', 'html5elements');
 $form = $html5form->form($document, 'POST');
 $document->appendChild($form);
 
-$fields = array();
-
-foreach (array('subject'=>'Subject', 'predicate'=>'Predicate', 'object'=>'Ohject') as $id => $caption) {
+foreach (array('subject', 'predicate', 'object') as $field) {
+    $caption = $this->objLanguage->languageText('mod_triplestore_'.$field, 'triplestore');
     $p = $document->createElement('p');
-    $p->appendChild($html5form->label($document, $id, $caption.': '));
-    $p->appendChild($html5form->text($document, $id));
+    $p->appendChild($html5form->label($document, $field, $caption.': '));
+    $p->appendChild($html5form->text($document, $field));
     $form->appendChild($p);
 }
 
