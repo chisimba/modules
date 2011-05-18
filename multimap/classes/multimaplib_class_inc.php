c48d31c493f5ab79bca6539c8969f2727039780b
charlvn <unknown@unknown.com>
2011-05-18 00:39:12 +0000

    Added multimaplib skeleton class.

---
+++ multimap/classes/multimaplib_class_inc.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Multimap library class.
+ *
+ * PHP version 5
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * @category  chisimba
+ * @package   multimap
+ * @author    Charl van Niekerk <charlvn@charlvn.com>
+ * @copyright 2011 Charl van Niekerk
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id: dbtriplestore_class_inc.php 21486 2011-05-15 19:32:27Z charlvn $
+ * @link      http://avoir.uwc.ac.za/
+ */
+// security check - must be included in all scripts
+if (!
+        /**
+         * Description for $GLOBALS
+         * @global unknown $GLOBALS['kewl_entry_point_run']
+         * @name   $kewl_entry_point_run
+         */
+        $GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+// end security check
+
+/**
+ * Multimap library class.
+ *
+ * @category  chisimba
+ * @package   multimap
+ * @author    Charl van Niekerk <charlvn@charlvn.com>
+ * @copyright 2011 Charl van Niekerk
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
+ * @version   $Id: dbtriplestore_class_inc.php 21486 2011-05-15 19:32:27Z charlvn $
+ * @link      http://avoir.uwc.ac.za/
+ */
+class multimaplib extends object
+{
+    public function init()
+    {
+    }
+}
+
+?>
