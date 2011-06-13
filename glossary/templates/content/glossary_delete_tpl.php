0f4e7c7ec47d0d42e74e6d93f4dd99075963eeab
Jeremy O'Connor <joconnor@uwc.ac.za>
2011-06-13 12:25:59 +0000

    Changed language item.

--- glossary/templates/content/glossary_delete_tpl.php
+++ glossary/templates/content/glossary_delete_tpl.php
@@ -13,18 +13,17 @@ echo $header;
 // Create Header Tag ' Add a word
 $this->objH =& $this->getObject('htmlheading', 'htmlelements');
 $this->objH->type=1;
-$this->objH->str=$objLanguage->languageText('mod_glossary_delete', 'glossary').' '.$record['term'].'??';
+$this->objH->str=$objLanguage->languageText('mod_glossary_delete', 'glossary').' '.$record['term'].'?';
 echo $this->objH->show();
 
 // Start of Form
 $addForm = new form('deleteWord', $this->uri(array(
-		'module'=>'glossary', 
-		'action'=>'deleteconfirm', 
+		'module'=>'glossary',
+		'action'=>'deleteconfirm',
 		'id'=>$id
 	)));
 
-$addForm->addToForm($objLanguage->languageText('mod_glossary_confirmDelete', 'glossary').' "'.$record['term'].'"');
-
+$addForm->addToForm($objLanguage->code2Txt('mod_glossary_confirmDelete', 'glossary', array('TERM'=>$record['term']))); //$objLanguage->languageText('mod_glossary_confirmDelete', 'glossary').' "'.$record['term'].'"'
 
 $yesButton = new button('delete', $objLanguage->languageText('word_yes'));
 $yesButton->setToSubmit();
