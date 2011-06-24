896a612d97ac0953bab3f01f06c6ad46347ddd31
manie <unknown@unknown.com>
2011-06-24 08:37:20 +0000

    changed how product languages are displayed in populateListView

--- unesco_oer/classes/productutil_class_inc.php
+++ unesco_oer/classes/productutil_class_inc.php
@@ -310,19 +310,19 @@ class productutil extends object {
                 ";
 
             $index = 0;
-            $prodLanguages = $this->objDbproductlanguages->getProductLanguages();
-            
-            foreach ($product as $languages) {
-//Check if languages is empty
-                foreach ($languages as $language) {
-//print_r($language);
-                    $content .= '<option value="">' . $language . '</option>';
-                    $index++;
-                }
-            }
-            if ($index == 0) {
-                $content .= '<option value="">' . $prodLanguages[0]['name']. '</option>';
-            }
+            $prodLanguages = $this->objDbproductlanguages->getLanguageNameByID($product['language']);
+         $content .= '<option value="">' . $prodLanguages . '</option>';
+//            foreach ($product as $languages) {
+////Check if languages is empty
+//                foreach ($languages as $language) {
+////print_r($language);
+//                    $content .= '<option value="">' . $language . '</option>';
+//                    $index++;
+//                }
+//            }
+//            if ($index == 0) {
+//                $content .= '<option value="">' . $prodLanguages[0]['name']. '</option>';
+//            }
 
             $content .= "</select>
                         </div>
