0d65f446308fcd8a263581ffb88a95eb741006df
nguni52 <unknown@unknown.com>
2011-07-04 08:16:02 +0000

    zawww popup

--- elsiskin/classes/elsicontent_class_inc.php
+++ elsiskin/classes/elsicontent_class_inc.php
@@ -204,7 +204,7 @@ class elsicontent extends object {
         $retstr .= '<script type="text/javascript">
                         function popOnLoad()
                     {
-                        mywindow = window.open("http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URL'].$this->getResourceURI('flash').'/zawww.html",
+                        mywindow = window.open("http://'.$_SERVER['SERVER_NAME'].'wip/elearning'.$this->getResourceURI('flash').'/zawww.html",
                                                 "mywindow","location=1, status=1,scrollbars=1, width=850,height=450");
                         mywindow.moveTo(0, 0);
                     }
