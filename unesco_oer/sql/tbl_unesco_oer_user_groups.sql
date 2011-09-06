3b1c67b05f21db4b649051d4a1952c9f28d11e15
deals shaun rikhotso <unknown@unknown.com>
2011-09-06 12:01:09 +0000

    admin

--- unesco_oer/sql/tbl_unesco_oer_user_groups.sql
+++ unesco_oer/sql/tbl_unesco_oer_user_groups.sql
@@ -2,7 +2,7 @@
 
 //define table
 $tablename = 'tbl_unesco_oer_user_groups';
-$options = array('comment'=>'Table to store a user and his/her belonging groups','collate'=>'utf8_general_ci','character_set'=>'utf8');
+$options = array('comment'=>'Table to store a user groups','collate'=>'utf8_general_ci','character_set'=>'utf8');
 
 //define fields
 $fields = array(
