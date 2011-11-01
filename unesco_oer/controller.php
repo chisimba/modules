1ccb5f5eb57316e7ff2730256a1ef89a56a127e7
David Wafula <davidwaf@gmail.com>
2011-11-01 10:39:52 +0000

    added register link

--- unesco_oer/controller.php
+++ unesco_oer/controller.php
@@ -592,7 +592,7 @@ class unesco_oer extends controller {
 
             return FALSE;
         }
-        $notrequired = array('aboutus', 'contactus', 'openidloginresult', 'openidauth', 'backopenid', 'restricted', 'oidauth', 'oidreturn', 'showopenidlogin', 'filterproducts', 'viewproduct', 'login', 'changelang', 'home', 'viewproductsection', 'testpdf', 'javafilternum', 'javafilter', 'filteradaptations');
+        $notrequired = array('userregistrationform', 'aboutus', 'contactus', 'openidloginresult', 'openidauth', 'backopenid', 'restricted', 'oidauth', 'oidreturn', 'showopenidlogin', 'filterproducts', 'viewproduct', 'login', 'changelang', 'home', 'viewproductsection', 'testpdf', 'javafilternum', 'javafilter', 'filteradaptations');
 
         if (in_array($action, $notrequired)) {
             return FALSE;
