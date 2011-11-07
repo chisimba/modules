189700a7df789f82f1d53d3719bbdfbfd4877902
David Wafula <davidwaf@gmail.com>
2011-11-07 10:33:39 +0000

    fixed the format

--- unesco_oer/templates/content/viewInstitutions_tpl.php
+++ unesco_oer/templates/content/viewInstitutions_tpl.php
@@ -24,25 +24,60 @@
     $this->loadClass('htmlheading', 'htmlelements');
     $this->loadClass('link', 'htmlelements');
     $objIcon = $this->newObject('geticon', 'htmlelements');
-   
-
-
- ?>
+    ?>
     <div style="clear:both;"></div> 
     <div class="breadCrumb module"> 
         <div id='breadcrumb'>
             <ul><li class="first">Home</li>
                 <?php
-                $adminTools=$this->objLanguage->languageText('mod_unesco_oer_administrative_tools', 'unesco_oer');
-                $insLabel=$this->objLanguage->languageText('mod_unesco_oer_institution', 'unesco_oer');
-              echo "<li><a href='?module=unesco_oer&action=controlpanel' alt='$adminTools' title='$adminTools'>$adminTools</a></li>";
-               echo "<li class='last'>$insLabel</li>";
-               ?>
+                $adminTools = $this->objLanguage->languageText('mod_unesco_oer_administrative_tools', 'unesco_oer');
+                $insLabel = $this->objLanguage->languageText('mod_unesco_oer_institution', 'unesco_oer');
+                echo "<li><a href='?module=unesco_oer&action=controlpanel' alt='$adminTools' title='$adminTools'>$adminTools</a></li>";
+                echo "<li class='lastc'>$insLabel</li>";
+                ?>
             </ul>
         </div>
 
     </div>
+  <div class="leftColumnDiv">
 
+        <?php
+        $filtering = $this->getobject('filterdisplay', 'unesco_oer');
+        echo $filtering->SideFilter('1a_tpl.php', $SortFilter, $TotalPages, $adaptationstring, $browsemapstring, $NumFilter, $PageNum);
+        ?>
+
+        <br/><br/>
+        <div class="blueBackground rightAlign">
+            <img src="skins/unesco_oer/images/button-reset.png" alt="Reset" width="17" height="17" class="imgFloatLeft">
+            <a href="#" class="resetLink"> 
+                <?php
+                $button = new button('Search', $this->objLanguage->languageText('mod_unesco_oer_filter_search', 'unesco_oer'));
+
+                $button->onclick = "javascript:ajaxFunction23('$adaptationstring');ajaxFunction($i)";
+                echo $button->show();
+
+                $abLink = new link($this->uri(array("action" => 'FilterProducts', "adaptationstring" => $adaptationstring, "page" => '1a_tpl.php')));
+                $abLink->link = $this->objLanguage->languageText('mod_unesco_oer_reset', 'unesco_oer');
+                echo $abLink->show();
+                ?>
+
+            </a>
+        </div>
+        <div class="filterheader">
+
+        </div>
+        <div class="rssFeed">
+            <img src="skins/unesco_oer/images/small-icon-rss-feed.png" alt="RSS Feed" width="18" height="18" class="imgFloatRight">
+            <div class="feedLinkDiv"><a href="#" class="rssFeedLink">RSS Feed</a></div>
+        </div>
+
+
+
+
+    </div>
+        <!-- Center column DIv -->
+    <div class="instCenterColumnDiv">
+    
     <?php
 // setup and show heading
     $header = new htmlheading();
@@ -50,7 +85,7 @@
     $header->str = $this->objLanguage->languageText('mod_unesco_oer_institution', 'unesco_oer');
     echo '<div id="institutionheading">';
     echo $header->show() . '<br><br />';
-   
+
     $institutionGUI = $this->getObject('institutiongui', 'unesco_oer');
 
     $buttonInstCaption = $this->objLanguage->languageText('mod_unesco_oer_add_data_newInstitution', 'unesco_oer');
@@ -67,20 +102,29 @@
 
     echo $addInstitutionLink->show() . '&nbsp;' . $BackToControlPanelLink->show();
     echo '</div>';
-//$institutionGUI = $this->getObject('institutiongui', 'unesco_oer');
-//$institutionGUI->showAllInstitutions();
-// retrieve data from tbl_unesco_oer_feturedproducts
+
     $content = '';
+    $objTable = $this->getObject('htmltable', 'htmlelements');
+   // $objTable->cssClass = "gridListingTable";
+    $objTable->width = "100%";
+
+    $newRow = true;
+    $count = 0;
+
     $Institution = $institutionGUI->showAllInstitutions();
+
+
     if (count($Institution) > 0) {
+
         foreach ($Institution as $Institutions) {
+
             $institutionGUI->getInstitution($Institutions['id']);
             $name = $institutionGUI->showInstitutionName();
             $creator = $adaptedProduct['creator'];
 
             $institutionLink = new link($this->uri(array("action" => '4', 'institutionId' => $Institutions['id'])));
             $institutionLink->cssClass = 'darkGreyColour';
-            $institutionLink->link = '<img align="top"  width="79" height="79" src="' .
+            $institutionLink->link = '<img align="left"  width="79" height="79" src="' .
                     $institutionGUI->showInstitutionThumbnail() . '" />';
 
             //Trim the text of the description
@@ -91,28 +135,47 @@
             }
 
 
-            $content.='
+            $cellcontent = '
             <div id="institutions"> ' . $institutionLink->show() . '&nbsp;&nbsp;' . $description . '<br/>
           ' . $institutionGUI->showEditInstitutionLinkThumbnail($Institutions['id']) . ' |
-          ' . $institutionGUI->showDeleteInstitutionLinkThumbnail($Institutions['id']) . '&nbsp;&nbsp;
+          ' . $institutionGUI->showDeleteInstitutionLinkThumbnail($Institutions['id']) . '&nbsp;&nbsp;<br/>
 
           <a class="greyListingHeading">' . $institutionGUI->showInstitutionWebsiteLink() . '</a> |
           <a class="greyListingHeading">' . $institutionGUI->showInstitutionCountry() . '</a> |
           <a class="greyListingHeading">' . $institutionGUI->showInstitutionCity() . '</a>
 
-<br/>
-           </div> ';
+          <br/>
+          </div> ';
+/// public function addCell($str, $width=null, $valign="top", $align=null, $class=null, $attrib=Null,$border = '0')
+   
+
+            if ($newRow) {
+                $objTable->startRow();
+                $objTable->addCell($cellcontent);
+                $newRow = false;
+            } else {
+                $objTable->addCell($cellcontent);
+            }
+            if ($count == 2) {
+                $newRow = true;
+                $objTable->endRow();
+                $count = 0;
+            }
+             $count++;
         }
     }
+    $content.=$objTable->show();
     $fieldset1 = $this->newObject('fieldset', 'htmlelements');
     $institutionsFsLegend = $this->objLanguage->languageText('mod_unesco_oer_institution', 'unesco_oer');
     $fieldset1->setLegend($institutionsFsLegend);
-    $fieldset1->addContent($content);
+    $fieldset1->addContent('<div id ="allinstitutions">'. $content.'</div>');
     echo $fieldset1->show();
 
 
 //echo $myTable->show();
     ?>
+</div>
+ 
 </div>
 <script type="text/javascript">
 
