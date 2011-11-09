2e8e2777f2d747c6d7c8ddd3de90d8e947bdb509
David Wafula <davidwaf@gmail.com>
2011-11-09 14:06:17 +0000

    fixed 2b misalign

--- unesco_oer/classes/productutil_class_inc.php
+++ unesco_oer/classes/productutil_class_inc.php
@@ -533,9 +533,9 @@ class productutil extends object {
 
         $content.='
                     <div class="adaptationListView">
-                    <table width="100%">
+                    <table width="99%">
                     <tr>
-                    <td width="40%">
+                    <td width="33%">
                         <div class="productAdaptationListViewLeftColumn">
                             <h2>' . $abLink->show() . ' </h2><br>
                             <img src="skins/unesco_oer/images/small-icon-adaptations.png" alt="Adaptation" width="18" height="18"class="smallLisitngIcons">
@@ -549,14 +549,14 @@ class productutil extends object {
             $groupLink->cssClass = "greenText";
             $groupLink->link = $adaptedProduct->getGroupName();
             $content .= '
-                        <td width="20%" align="left" valign="top">
+                        <td width="33%" align="left" valign="top">
                        <div class="productAdaptationListViewMiddleColumn">
                                 <img src="skins/unesco_oer/images/icon-managed-by.png" alt="Managed by" width="24" height="24"><br>
                                 <span class="greenText">Managed by</span>
                                 
                             </div>
                             </td>
-                            <td width="40%">
+                            <td width="33%">
                             <div class="productAdaptationListViewRightColumn">
                                 <h2 class="greenText">' . $groupLink->show() . '</h2>
                                 <br>
@@ -588,13 +588,13 @@ class productutil extends object {
             $institutionLink = new link($this->uri(array("action" => '4', 'institutionId' => $instutionID)));
             $institutionLink->link = $name;
 
-            $content .='<td width="20%"  align="left" valign="top">
+            $content .='<td width="33%"  align="left" valign="top">
                   <div class="productAdaptationListViewMiddleColumn">
                             <img src="skins/unesco_oer/images/icon-adapted-by.png" alt="Adapted by" width="24" height="24"><br>
                             <span class="pinkText centerAlign">Adapted by</span>
                         </div>
                         </td>
-                        <td width="40%">
+                        <td width="33%">
                         <div class="productAdaptationListViewRightColumn">
                             <h2 class="darkGreyColour">' . $institutionLink->show() . '</h2>
                             <br>
