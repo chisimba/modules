3ee992c761eb5e650af20403d770143a7cfcc73b
manie <unknown@unknown.com>
2011-11-21 07:04:50 +0000

    attempt to remove popups for originals on compare page

--- unesco_oer/classes/content_class_inc.php
+++ unesco_oer/classes/content_class_inc.php
@@ -330,7 +330,7 @@ class content extends object {
         } else {
             $node = new treenode(array(
 //                                                        'text' => $title . $hiddenInput->show()."<div class='treetrunc'></div>",
-                        'text' => ($highlighted ? $title . $hiddenInput->show() . $productUtil->getToolTip(trim(strip_tags($existingContent->showRemark())), "<img src='skins/unesco_oer/images/InfoBox.png' alt='help' width='12' height='12'>") : $title . $hiddenInput->show()),
+                        'text' => (($highlighted && $product->isAdaptation())  ? $title . $hiddenInput->show() . $productUtil->getToolTip(trim(strip_tags($existingContent->showRemark())), "<img src='skins/unesco_oer/images/InfoBox.png' alt='help' width='12' height='12'>") : $title . $hiddenInput->show()),
                         //     'cssClass' => ($highlighted ? 'HL' : ''),                                                       
                         'link' => $link,
                         'icon' => $icon,
