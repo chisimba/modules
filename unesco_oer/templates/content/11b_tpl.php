96c5f1825647f4ba7fad4961c2287402a96d8199
Kurtcrossman <unknown@unknown.com>
2011-11-29 10:20:59 +0000

    comments fix

--- unesco_oer/templates/content/11b_tpl.php
+++ unesco_oer/templates/content/11b_tpl.php
@@ -338,7 +338,7 @@ echo $this->objFeaturedProducUtil->displayFeaturedAdaptedProduct($objProduct);
 
                                             </script>
                                         </head>
-                             <div id="map_canvas" style="width:190; height:110"></div>
+                             <div id="map_canvas" style="width:178; height:110"></div>
 <?php
                                                 $form = new form('maps', $this->uri(array("action" => 'BrowseAdaptation', "page" => '2a_tpl.php', "page" => '2a_tpl.php',  'MapEntries' => $MapEntries)));
 
