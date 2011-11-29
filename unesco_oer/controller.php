74bae043461f985e54bad802b5b9cc76ce089b31
manie <unknown@unknown.com>
2011-11-29 10:30:55 +0000

    fixed login and register problem

--- unesco_oer/controller.php
+++ unesco_oer/controller.php
@@ -601,7 +601,7 @@ class unesco_oer extends controller {
 
             return FALSE;
         }
-        $notrequired = array('commentmanager', 'compareproducts', 'FilterAdaptations', 'CompareProducts','4', 'userregistrationform', 'aboutus', 'contactus', 'openidloginresult', 'openidauth', 'backopenid', 'restricted', 'oidauth', 'oidreturn', 'showopenidlogin', 'filterproducts', 'viewproduct', 'login', 'changelang', 'home', 'viewproductsection', 'testpdf', 'javafilternum', 'javafilter', 'filteradaptations');
+        $notrequired = array('commentmanager', 'compareproducts', 'FilterAdaptations', 'CompareProducts','4', 'userregistrationform', 'aboutus', 'contactus', 'openidloginresult', 'openidauth', 'backopenid', 'restricted', 'oidauth', 'oidreturn', 'showopenidlogin', 'filterproducts', 'viewproduct', 'login', 'changelang', 'home', 'viewproductsection', 'testpdf', 'javafilternum', 'javafilter', 'filteradaptations','savenewuser');
 
         if (in_array($action, $notrequired)) {
             return FALSE;
