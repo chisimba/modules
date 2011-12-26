2e7b0d8930a8fffdce2933e43c31787303c96325
David Wafula <davidwaf@gmail.com>
2011-12-26 20:40:52 +0000

    new refactored code base for oer platform

---
+++ oer/controller.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+ * Entry point of oer module
+ */
+
+class oer extends controller {
+
+    /**
+     * Constructor for the Module
+     */
+    public function init() {
+        
+    }
+
+    /**
+     * this determines if the action received in the controller should need login
+     * or not
+     * @param type $action
+     * @return type 
+     */
+    function requiresLogin($action='home') {
+        $allowedActions = array(NULL, 'home');
+        if (in_array($action, $allowedActions)) {
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+
+    /**
+     * Standard Dispatch Function for Controller
+     *
+     * @access public
+     * @param string $action Action being run
+     * @return string Filename of template to be displayed
+     */
+    public function dispatch($action) {
+     
+        /*
+         * Convert the action into a method (alternative to
+         * using case selections)
+         */
+        $method = $this->getMethod($action);
+        /*
+         * Return the template determined by the method resulting
+         * from action
+         */
+        return $this->$method();
+    }
+
+    /**
+     *
+     * Method to convert the action parameter into the name of
+     * a method of this class.
+     *
+     * @access private
+     * @param string $action The action parameter passed byref
+     * @return string the name of the method
+     *
+     */
+    function getMethod(& $action) {
+        if ($this->validAction($action)) {
+            return '__' . $action;
+        } else {
+            return '__home';
+        }
+    }
+
+    /**
+     *
+     * Method to check if a given action is a valid method
+     * of this class preceded by double underscore (__). If it __action
+     * is not a valid method it returns FALSE, if it is a valid method
+     * of this class it returns TRUE.
+     *
+     * @access private
+     * @param string $action The action parameter passed byref
+     * @return boolean TRUE|FALSE
+     *
+     */
+    function validAction(& $action) {
+        if (method_exists($this, '__' . $action)) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    // Beginning of Functions Relating to Actions in the Controller //
+
+    /**
+     *
+     * This is the default function
+     */
+    private function __home() {
+       //  $this->setLayoutTemplate('threecol_layout_tpl.php');
+        return "1a_tpl.php";
+    }
+
+}
+
+?>
