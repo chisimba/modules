ac9a335584adabccab71eff74071ac8c9ccac0d6
Derek Keats <derek@dkeats.com>
2011-12-31 09:20:17 +0000

    Added external javascript files

---
+++ oer/resources/institutionedit.js
@@ -0,0 +1,22 @@
+/* 
+ * Javascript to support institution edit
+ *
+ * Written by Derek Keats derek@dkeats.com
+ * Started on: December 18, 2011, 8:48 am
+ *
+ *
+ */
+
+/**
+ *
+ * jQuery code belongs inside this function.
+ *
+ */
+jQuery(function() {
+
+    // Things to do on loading the page.
+    jQuery(document).ready(function() {
+
+    });
+
+});
