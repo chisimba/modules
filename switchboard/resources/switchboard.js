595316c9079681eaa84ada0b26a7d9fd9aead8dd
Derek Keats <derek@dkeats.com>
2012-01-05 18:28:59 +0000

    Added a new module to act as a switchboard for a site. It uses dynamic blocks.

---
+++ switchboard/resources/switchboard.js
@@ -0,0 +1,26 @@
+/* 
+ * Javascript to support switchboard
+ *
+ * Written by Derek Keats derek@dkeats.com
+ * STarted on: January 4, 2012, 3:53 pm
+ *
+ * The following parameters need to be set in the
+ * PHP code for this to work:
+ *
+ *
+ */
+
+/**
+ *
+ * Put your jQuery code inside this function.
+ *
+ */
+jQuery(function() {
+
+    
+    // Things to do on loading the page.
+    jQuery(document).ready(function() {
+
+    });
+ 
+});
