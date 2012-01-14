f668672141bbe65f971d6a330f0cf88984cada56
pwando <unknown@unknown.com>
2012-01-14 08:10:14 +0000

    WIP - adding functionality to manage adaptations

---
+++ oer/classes/block_adaptationslisting_class_inc.php
@@ -0,0 +1,24 @@
+<?php
+
+$this->loadClass('link', 'htmlelements');
+
+/**
+ * This class lists the adaptations
+ *
+ * @author pwando
+ */
+class block_adaptationslisting extends object {
+
+    public function init() {
+        $this->loadClass("link", "htmlelements");
+    }
+
+    public function show() {
+
+        $objAdaptationManager = $this->getObject("adaptationmanager", "oer");
+        return $objAdaptationManager->getAdaptationsListingAsGrid();
+    }
+
+}
+
+?>
