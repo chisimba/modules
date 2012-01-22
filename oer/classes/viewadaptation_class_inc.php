449031a2ebaef631c51fda2dd6a92c8f31c700ae
pwando <unknown@unknown.com>
2012-01-22 19:27:06 +0000

    [pwando] Reformatted viewadaptation table per specs

--- oer/classes/viewadaptation_class_inc.php
+++ oer/classes/viewadaptation_class_inc.php
@@ -8,16 +8,18 @@
 class viewadaptation extends object {
 
     function init() {
-       
+
     }
 
     function buildAdaptationView($productId) {
-        $this->loadClass("link","htmlelements");
+        $this->loadClass("link", "htmlelements");
         $objLanguage = $this->getObject("language", "language");
         $objDbProducts = $this->getObject("dbproducts", "oer");
         $product = $objDbProducts->getProduct($productId);
         $table = $this->getObject("htmltable", "htmlelements");
         $table->attributes = "style='table-layout:fixed;'";
+        $table->border = 0;
+        $tablecellspacing = 20;
 
         $leftContent = "";
         $leftContent.='<h1 class="viewproduct_title">' . $product['title'] . '</h1>';
@@ -28,15 +30,15 @@ class viewadaptation extends object {
         $leftContent.='<div id="viewproduct_coverpage">' . $thumbnail . '</div>' . $product['description'];
 
         $sections = "";
-        $sections.='<h3 class="original_product_section_title">' . $objLanguage->languageText('mod_oer_sections', 'oer') . '</h3>';
-        $addSectionIcon = '<img src="skins/oer/images/add-node.png" align="left"/>';
+        $sectionTitle = '<h3 class="original_product_section_title">' . $objLanguage->languageText('mod_oer_sections', 'oer') . '</h3>';
+        $addSectionIcon = '<img src="skins/oer/images/add-node.png"/>';
         $addNodeLink = new link($this->uri(array("action" => "addsectionnode", "productid" => $productId)));
-        $addNodeLink->link = $addSectionIcon . $objLanguage->languageText('mod_oer_addnode', 'oer');
+        $addNodeLink->link = $addSectionIcon ."&nbsp;&nbsp;". $objLanguage->languageText('mod_oer_addnode', 'oer');
         $sections.=$addNodeLink->show();
         $sectionManager = $this->getObject("sectionmanager", "oer");
 
-        $navigator= $sectionManager->buildSectionsTree($product["id"],'');
-        $leftContent.='<br/>' .$sections.'<br/>'. $navigator;
+        $navigator = $sectionManager->buildSectionsTree($product["id"], '');
+        //$leftContent.='<br/>' .$sections.'<br/>'. $navigator;
 
         $rightContent = "";
         $rightContent.='<div id="viewadaptation_author_label"></div>
@@ -52,30 +54,40 @@ class viewadaptation extends object {
         $rightContent.='<div id="viewadaptation_keywords_label">' . $objLanguage->languageText('mod_oer_keywords', 'oer') . ':</div>
             <div id="viewadaptation_keywords_text"> ' . $product['keywords'] . '</div><br/><br/>';
         /*
-        $language = "Not specified";
-        if($product['language'] == 'en'){
-            $language = "English";
-        }
-        $rightContent.='<div id="viewproduct_selectlanguages_label">' . $objLanguage->languageText('mod_oer_language', 'oer') . ':<br/>' . $language . '</div><br/><br/>';
-        $rightContent.='<div id="viewproduct_relatednews_label">' . $objLanguage->languageText('mod_oer_relatednews', 'oer') . ': </div><br/><br/>';
-        $rightContent.='<div id="viewproduct_relatedevents_label">' . $objLanguage->languageText('mod_oer_relatedevents', 'oer') . ':</div><br/><br/>';
-        $rightContent.='<div id="viewproduct_usercomments_label">' . $objLanguage->languageText('mod_oer_usercomments', 'oer') . ': </div>';*/
+          $language = "Not specified";
+          if($product['language'] == 'en'){
+          $language = "English";
+          }
+          $rightContent.='<div id="viewproduct_selectlanguages_label">' . $objLanguage->languageText('mod_oer_language', 'oer') . ':<br/>' . $language . '</div><br/><br/>';
+          $rightContent.='<div id="viewproduct_relatednews_label">' . $objLanguage->languageText('mod_oer_relatednews', 'oer') . ': </div><br/><br/>';
+          $rightContent.='<div id="viewproduct_relatedevents_label">' . $objLanguage->languageText('mod_oer_relatedevents', 'oer') . ':</div><br/><br/>';
+          $rightContent.='<div id="viewproduct_usercomments_label">' . $objLanguage->languageText('mod_oer_usercomments', 'oer') . ': </div>'; */
 
         $table->startRow();
-        $table->addCell('<div id="viewproduct_leftcontent">' . $leftContent . '</div>', "60%", "top", "left");
-        $table->addCell('<div id="viewproduct_rightcontent>' . $rightContent . '</div>', "40%", "top", "left");
-
+        $table->addCell('<div id="viewproduct_leftcontent">' . $leftContent . '</div>', "60%", "top", "left", "", 'colspan="2"');
+        $table->addCell('<div id="viewproduct_rightcontent>' . $rightContent . '</div>', "40%", "top", "left", "", 'rowspan="3"');
         $table->endRow();
 
-        $homeLink=new link($this->uri(array("action"=>"home")));
-        $homeLink->link=$objLanguage->languageText('mod_oer_home', 'system');
-        
-        
+        $table->startRow();
+        $table->addCell('<div id="viewproduct_leftcontent">' . $sectionTitle . '</div>', "30%", "top", "left");
+        $table->addCell('<div id="viewproduct_leftcontent">' . $sections . '</div>', "30%", "top", "right");
+        $table->endRow();
+
+        $table->startRow();
+        $table->addCell('<div id="viewproduct_leftcontent">' . $navigator . '</div>', "30%", "top", "left", "", 'colspan="2"');
+        $table->endRow();
+
+        $homeLink = new link($this->uri(array("action" => "home")));
+        $homeLink->link = $objLanguage->languageText('mod_oer_home', 'system');
+
+
         $objTools = $this->newObject('tools', 'toolbar');
         $crumbs = array($homeLink->show());
         $objTools->addToBreadCrumbs($crumbs);
 
-        return  '<br/><div id="viewproduct">' . $table->show() . '</div>';
+        return '<br/><div id="viewproduct">' . $table->show() . '</div>';
     }
+
 }
+
 ?>
