7179e41dc30521f1c0da52da805cab96a2d8b30a
pwando <unknown@unknown.com>
2012-02-24 20:39:50 +0000

    [#pwando] - Resized width of the download dialog window

--- oer/resources/downloader.js
+++ oer/resources/downloader.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function() {
     jQuery('#downloader').hide();
 });
-function showDownload(){    
-   jQuery('#downloader').dialog();
+function showDownload(){
+
+   jQuery('#downloader').dialog({width:650});
 };
