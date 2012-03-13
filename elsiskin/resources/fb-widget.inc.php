17915f2b3c76c8ea016c0a3195c75520ac3bbacf
Derek Keats <derek@dkeats.com>
2012-03-13 12:15:21 +0000

    removed deprecated

--- elsiskin/resources/fb-widget.inc.php
+++
@@ -1,8 +0,0 @@
-<script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
-<fb:like-box href="http://www.facebook.com/pages/Wits-eLSI-eLearning-Support-and-Innovation/143838025636947"
-             width="220"
-             show_faces="false"
-             connections="10"
-             stream="false"
-             header="true">
-</fb:like-box>
