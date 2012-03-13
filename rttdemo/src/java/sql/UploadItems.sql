2c8e7d8e44e59b33ff8a3df563a5771223bd428a
Derek Keats <derek@dkeats.com>
2012-03-13 12:22:05 +0000

    removed deprecated

--- rttdemo/src/java/sql/UploadItems.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS rttdemo_uploaditems (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  filename char(50) NOT NULL,
-  date_uploaded timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  filepath varchar(100) NOT NULL,
-  usertype varchar(10) NOT NULL,
-  PRIMARY KEY (id)
-);
--- rttdemo/web/WEB-INF/jsp/timeout.jsp
+++
@@ -1,5 +0,0 @@
-<%@ include file="/WEB-INF/jsp/includes.jsp" %>
-<%@ include file="/WEB-INF/jsp/header.jsp" %>
-<center><h1>Session timed out.</h1><a href="<%= request.getContextPath()%>/login">Click here to login in</a></center>
-
-<%@ include file="/WEB-INF/jsp/footer.jsp" %>
