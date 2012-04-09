0b2d05535057e04435d2fea50d20035748941283
pwando <unknown@unknown.com>
2012-04-09 08:07:58 +0000

    [pwando] - committed textblock module

---
+++ textblock/controller.php
@@ -0,0 +1,99 @@
+<?php
+/* ----------- controller class extends controller for tbl_quotes------------*/
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
+
+
+/**
+*
+* Controller class for the table tbl_quotes
+*
+* @author Administrative User
+*
+*
+* @version $Id: controller.php 14552 2009-08-27 11:52:17Z jsc $
+* @version $Id: controller.php,v 1.3 2006/09/14 Abdurahim Ported to PHP5
+* @copyright 2005 GNU GPL
+*
+*/
+class textblock extends controller
+{
+
+    /**
+    * @var string $action The action parameter from the querystring 
+    */
+    public $action;
+
+    /**
+    * Standard constructor method 
+    */
+    public function init()
+    {
+        //Retrieve the action parameter from the querystring
+        $this->action = $this->getParam('action', Null);
+        //Create an instance of the database class for this module
+        $this->objDb = $this->getObject("dbtextblock");
+
+        //Create an instance of the language object
+        $this->objLanguage = $this->getObject("language", "language");
+        // Load the module helper Javascript
+        $this->appendArrayVar('headerParams',
+        $this->getJavaScriptFile('textblock.js',
+          'textblock'));
+        //Get the activity logger class
+        $this->objLog=$this->newObject('logactivity', 'logger');
+        //Log this module call
+        $this->objLog->log();
+    }
+
+    /**
+    * Standard dispatch method 
+    */
+    public function dispatch()
+    {
+        switch ($this->action) {
+            case null:
+            case 'text':
+            case 'view':
+                // Set the layout template to compatible one
+                $this->setLayoutTemplate('layout_tpl.php');
+                return 'narrowblockview_tpl.php';
+                break;
+            case 'widetext':
+                // Set the layout template to compatible one
+                $this->setLayoutTemplate('layout_tpl.php');
+                return 'wideblockview_tpl.php';
+                break;
+            case 'deleteajax':
+                $this->objDb->delete("id", $this->getParam('id', Null));
+                die("RECORD_DELETED");
+                break;
+            case 'ajaxedit':
+                // Set the layout template to compatible one
+                $this->setLayoutTemplate('layout_tpl.php');
+                return "editajax_tpl.php";
+                break;
+            case 'save':
+            	$objUser = $this->getObject("user", "security");
+                $this->objDb->saveRecord($this->getParam('mode', Null), $objUser->userId());
+                $blockType = $this->getParam('blocktype', 'narrowblock');
+                return $this->nextAction($blockType);
+                break;
+            default:
+                $this->setVar('str', $this->objLanguage->languageText("phrase_actionunknown",NULL,"Unknown action").": ".$this->action);
+                return 'dump_tpl.php';
+
+        }
+    }
+    
+    /**
+    * Override the default requirement for login
+    */
+    public function requiresLogin()
+    {
+        return TRUE;
+    }
+}
+?>
