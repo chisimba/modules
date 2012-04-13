5374904f8afb607b3dd9815f5100d34c8b592f5d
David Wafula <davidwaf@gmail.com>
2012-04-13 13:31:50 +0000

    updated gift policy render page

--- gift/templates/content/giftpolicy_tpl.php
+++ gift/templates/content/giftpolicy_tpl.php
@@ -3,11 +3,17 @@
 //load class
 $this->loadclass('link','htmlelements');
 
-$objbackLink = new link($this->uri(array('action'=>'back')));
-$objbackLink-> link = 'Back';
 
-$acceptLink = new link($this->uri(array('action'=>'acceptpolicy')));
-$acceptLink-> link = 'Accept';
+$backbutton = new button('back', "Back");
+$uri = $this->uri(array('action' => 'back'));
+$backbutton->setOnClick('javascript: window.location=\'' . $uri . '\'');
+
+
+$acceptbutton = new button('accept', "I understand the Gift Policy");
+$uri = $this->uri(array('action' => 'acceptpolicy'));
+$acceptbutton->setOnClick('javascript: window.location=\'' . $uri . '\'');
+
+
 
 $objSysConfig = $this->getObject('dbsysconfig', 'sysconfig');
 $policyURL=$objSysConfig->getValue('GIFT_POLICY_URL', 'gift');
@@ -15,12 +21,15 @@ $policyURL=$objSysConfig->getValue('GIFT_POLICY_URL', 'gift');
 
 
 
-$policy='<a href="'.$policyURL.'">Click here to view the policy</a>';
+$policy='
+    <strong>This is the first time you are adding a gift. Please review the gift policy before proceeding.</strong><br/>
+<a href="'.$policyURL.'">Click here to view the policy</a>';
 
 
 
 // Add the table to the centered layer and a message of database functionality
-echo '<h1>Gift Policy</h1>'. $policy.'<div id="grouping-grid"><br /></div>'.$objbackLink->show().'&nbsp;&nbsp;'.$acceptLink->show();
+echo '
+    <fieldset><legend>Review Gift Policy</legend>'. $policy.'<div id="grouping-grid"><br /></div>'.$backbutton->show().'&nbsp;&nbsp;'.$acceptbutton->show().'</fieldset>';
 
 
 ?>
