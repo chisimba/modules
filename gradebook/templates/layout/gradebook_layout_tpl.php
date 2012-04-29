c445bb9ce203cb21842f27e1604758e0269a1606
Derek Keats <derek@dkeats.com>
2012-04-29 16:05:12 +0000

    Added divs to facilitate skin building

--- gradebook/templates/layout/gradebook_layout_tpl.php
+++ gradebook/templates/layout/gradebook_layout_tpl.php
@@ -1,14 +1,12 @@
 <?php
-/* -------------------- gradebook layout template ---------------- */
-// security check - must be included in all scripts
-if (!$GLOBALS['kewl_entry_point_run']) {
-    die("You cannot view this page directly");
-}
-
 /**
 * Template layout for gradebook module
 */
 
+// security check - must be included in all scripts
+if (!$GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
+}
 $this->_objDBContext = $this->getObject('dbcontext','context');
 if($this->_objDBContext->isInContext())
 {
@@ -18,13 +16,13 @@ if($this->_objDBContext->isInContext())
     $cm ='';
 }
 $toolbar = $this->getObject('contextsidebar', 'context');
-
 $cssLayout =& $this->newObject('csslayout', 'htmlelements');
 $leftMenu =& $this->newObject('sidemenu','toolbar');
-
-//$cssLayout->setLeftColumnContent($leftMenu->menuContext().$cm);
-$cssLayout->setLeftColumnContent($toolbar->show());
-$cssLayout->setMiddleColumnContent($this->getContent());
-
+$left = $toolbar->show();
+$left = "<div class='gradebook_left'>$left</div>";
+$middle = $this->getContent();
+$middle = "<div class='gradebook_main'>$middle</div>";
+$cssLayout->setLeftColumnContent($left);
+$cssLayout->setMiddleColumnContent($middle);
 echo $cssLayout->show();
 ?>
