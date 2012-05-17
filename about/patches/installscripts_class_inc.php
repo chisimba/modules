41c9edb99aa10917252bff72fc4ea589d3d187ae
Derek Keats <derek@dkeats.com>
2012-05-17 08:46:00 +0000

    Added copyright and disclaimer notices

--- about/patches/installscripts_class_inc.php
+++ about/patches/installscripts_class_inc.php
@@ -2,34 +2,33 @@
 /**
  *
  * Installer class for the module
- * 
- * The installer class for the module creates a 
+ *
+ * The installer class for the module creates a
  * group called AboutContact and sets up files
  * etc.
- * 
+ *
  * PHP version 5
- * 
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 2 of the License, or 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the 
- * Free Software Foundation, Inc., 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ *
  * @category  Chisimba
- * @package   wurfl
- * @author    Charl van Niekerk <charlvn@charlvn.com>
- * @copyright 2010 Charl van Niekerk
+ * @package   about
+ * @author    Derek Keats <derek@dkeats.com>
+ * @copyright Kenga Solutions
  * @license   http://www.gnu.org/licenses/gpl-2.0.txt The GNU General Public License
  * @version   $Id: installscripts_class_inc.php 18511 2010-07-28 09:35:42Z charlvn $
- * @link      http://chisimba.com/
- * @see       http://wurfl.sourceforge.net/
+ * @link      http://kengasolutions.com/
  */
 
 // security check - must be included in all scripts
@@ -45,13 +44,13 @@ $GLOBALS['kewl_entry_point_run']) {
 // end security check
 
 /**
- * 
+ *
  * Installer class for the module
- * 
- * The installer class for the module creates a 
+ *
+ * The installer class for the module creates a
  * group called AboutContact and sets up files
  * etc.
- * 
+ *
  * @category  Chisimba
  * @package   oeruserdata
  * @author    Derek Keats <derek@dkeats.com>
@@ -86,44 +85,51 @@ class about_installscripts extends dbtable
      *
      * @access public
      * @return void
-     * 
+     *
      */
     public function postinstall()
     {
         $this->createAuthorGroup();
         $this->makeDefaultFile();
     }
-    
+
     /**
      * Create a group for people who can edit the about text
-     * 
+     *
      * @access public
      * @return VOID
-     * 
+     *
      */
     public function createAuthorGroup()
     {
         $objGa = $this->getObject('gamodel','groupadmin');
         $objGa->addGroup("AboutContact", "Can edit the about and contact information for the site.");
     }
-    
+
     /**
-     * 
-     * Copy the default about text file to the about directory
-     * in usrfiles
-     * 
+     *
+     * Copy the default about, contact, disclaimer and copyright
+     * text file to the about directory in usrfiles
+     *
      * @access public
      * @return VOID
-     * 
+     *
      */
     public function makeDefaultFile()
     {
         $defaultDoc = $this->objAltConfig->getModulePath() . 'about/resources/default.html';
         $contactDoc = $this->objAltConfig->getModulePath() . 'about/resources/contact.html';
+        $disclaimerDoc = $this->objAltConfig->getModulePath() . 'about/resources/disclaimer.html';
+        $copyrightDoc = $this->objAltConfig->getModulePath() . 'about/resources/copyright.html';
         $targetDefault = $this->objAltConfig->getSiteRootPath() . 'usrfiles/about/default.html';
         $targetContact = $this->objAltConfig->getSiteRootPath() . 'usrfiles/about/contact.html';
+        $targetDisclaimer = $this->objAltConfig->getSiteRootPath() . 'usrfiles/about/disclaimer.html';
+        $targetCopyright = $this->objAltConfig->getSiteRootPath() . 'usrfiles/about/copyright.html';
+
         copy($defaultDoc, $targetDefault);
         copy($contactDoc, $targetContact);
+        copy($disclaimerDoc, $targetDisclaimer);
+        copy($copyrightDoc, $targetCopyright);
     }
 
 }
