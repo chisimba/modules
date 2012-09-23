c0dc6bc37dde10ecf00472810c56e3a040ea3fac
Derek Keats <derek@dkeats.com>
2012-09-23 12:23:18 +0000

    Removing module to deprecated

--- codewriter/templates/content/editcode_tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Create an instance of the css layout class
-$cssLayout = $this->newObject('csslayout', 'htmlelements');
-$cssLayout->setNumColumns(3);
-$cssLayout->setLeftColumnContent($leftPanel);
-$cssLayout->setMiddleColumnContent($codeEditor);
-$cssLayout->setRightColumnContent($rightPanel);
-echo $cssLayout->show();
-?>
