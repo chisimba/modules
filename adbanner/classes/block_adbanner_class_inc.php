4d26e44be7edc1031fb764a1e3b30bdd659ae69d
Derek Keats <derek@dkeats.com>
2012-09-23 12:21:42 +0000

    Removing module to deprecated

--- adbanner/classes/block_adbanner_class_inc.php
+++
@@ -1,21 +0,0 @@
-<?php
-
-// security check - must be included in all scripts
-if (!
-/**
- * Description for $GLOBALS
- * @global unknown $GLOBALS['kewl_entry_point_run']
- * @name   $kewl_entry_point_run
- */
-$GLOBALS['kewl_entry_point_run']) {
-    die("You cannot view this page directly");
-}
-// end security check
-class block_adbanner extends object {
-	function init() {
-		$this->objBanner=$this->getObject('adban');
-	}
-
-	public function show() {
-		return $this->objBanner->displayBannerInBlock();	}}
-?>
--- adbanner/templates/content/view_banner_tpl.php
+++
@@ -1,5 +0,0 @@
-<?php
-//--from the url
-$id = $this->getParam('id');
-echo $this->objBanner->displayBanner($id);
-?>
