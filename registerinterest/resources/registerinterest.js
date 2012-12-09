14cada9f19c5e962e45d19b6f5171ef6a3b78b73
monwabisi <wsifumba@gmail.com>
2012-12-09 21:18:39 +0000

    update request is sent when a textbox looses focus

--- registerinterest/resources/registerinterest.js
+++ registerinterest/resources/registerinterest.js
@@ -33,12 +33,43 @@ jQuery(function() {
             type: "POST",
             data: data_string,
             success: function(msg) {
+                var msg;
+                var fullName =  jQuery("#input_fullname").val();
+                var email =  jQuery("#input_email").val();
+                if(jQuery.trim(fullName).length != 0){
+                        if(fullName.length > 1){
+                            if(jQuery.trim(email).length != 0){
+                                if(email.indexOf('@') != -1 && email.length > 2){
+                                    msg = "<span class='success' >Thank you for registering your interest.</span>";
+                                }else{
+                                    msg = "<span class='error' >Enter valid email address</span>";
+                                }
+                            }else{
+                                msg = "<span class='error' >Enter valid email address</span>";
+                            }
+                        }else{
+                            msg = "<span class='error' >Enter valid full names</span>";
+                        }
+                }else{
+                    msg = "<span class='error' >Enter valid full names</span>";
+                }
                 jQuery("#ri_save_button").attr("disabled", "");
-                jQuery("#before_riform").html('<span class="success">Thank you for registering your interest.</span>');
+                jQuery("#before_riform").html(msg);
                 jQuery("#input_fullname").val('');
                 jQuery("#input_email").val('');
             }
         });
+    });
+    //send update request when the textbox looses focus
+    jQuery('.interestEmail').focusout(function(){
+        var id = jQuery(this).attr('id');
+        var newValue = jQuery(this).val();
+        jQuery.ajax({
+            url: 'index.php?module=registerinterest&action=update',
+            type: 'POST',
+            data: {'newValue': newValue,'id':id},
+            success:  jQuery("#before_riform").html("<span class='success' >Update request sent successfully</span>")
+        })
     });
     
     // Send the message
