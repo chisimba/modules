ce5bb1b174d952ba6252cc876f28d36e51c1efed
monwabisi <wsifumba@gmail.com>
2012-12-13 22:49:37 +0000

    Prevent a person from registering morethan once

--- registerinterest/classes/riops_class_inc.php
+++ registerinterest/classes/riops_class_inc.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * Ops Class for Register interest
@@ -29,48 +30,48 @@
  * @link      http://www.chisimba.com
  *
  */
-
 // security check - must be included in all scripts
 if (!
-/**
- * The $GLOBALS is an array used to control access to certain constants.
- * Here it is used to check if the file is opening in engine, if not it
- * stops the file from running.
- *
- * @global entry point $GLOBALS['kewl_entry_point_run']
- * @name   $kewl_entry_point_run
- *
- */
-$GLOBALS['kewl_entry_point_run'])
-{
-        die("You cannot view this page directly");
+        /**
+         * The $GLOBALS is an array used to control access to certain constants.
+         * Here it is used to check if the file is opening in engine, if not it
+         * stops the file from running.
+         *
+         * @global entry point $GLOBALS['kewl_entry_point_run']
+         * @name   $kewl_entry_point_run
+         *
+         */
+        $GLOBALS['kewl_entry_point_run']) {
+    die("You cannot view this page directly");
 }
 // end security check
 
 /**
-*
-* Ops Class for Register interest
-*
-* Ops Class for Register interest - builds various user interface elements
-*
-* @package   registerinterest
-* @author    Derek Keats derek@dkeats.com
-*
-*/
-class riops extends object
-{
+ *
+ * Ops Class for Register interest
+ *
+ * Ops Class for Register interest - builds various user interface elements
+ *
+ * @package   registerinterest
+ * @author    Derek Keats derek@dkeats.com
+ *
+ */
+class riops extends object {
 
     /**
-    *
-    * Intialiser for the registerinterest ops class
-    * @access public
-    * @return VOID
-    *
-    */
-    public function init()
-    {
+     *
+     * Intialiser for the registerinterest ops class
+     * @access public
+     * @return VOID
+     *
+     */
+    public function init() {
         $this->objLanguage = $this->getObject('language', 'language');
-        $this->objUser = $this->getObject('user','security');
+        $this->objUser = $this->getObject('user', 'security');
+        $this->objDB = $this->getObject('dbregisterinterest', 'registerinterest');
+        $this->objAltConfig = $this->getObject('altconfig', 'config');
+        //the DOM document
+        $this->doc = new DOMDocument('UTF-8');
     }
 
     /**
@@ -81,43 +82,40 @@ class riops extends object
      * @access public
      *
      */
-    public function buildForm()
-    {
+    public function buildForm() {
         // Load all the required HTML classes from HTMLElements module
         $this->loadClass('form', 'htmlelements');
-        $this->loadClass('textinput','htmlelements');
-        $this->loadClass('button','htmlelements');
-        
+        $this->loadClass('textinput', 'htmlelements');
+        $this->loadClass('button', 'htmlelements');
+
         // Load the javascript.
-        $this->appendArrayVar('headerParams',
-          $this->getJavaScriptFile('registerinterest.js',
-          'registerinterest'));
-        
+        $this->appendArrayVar('headerParams', $this->getJavaScriptFile('registerinterest.js', 'registerinterest'));
+
         // Create the form and set the action to save
         $formAction = $this->uri(array('action' => 'save'), 'registerinterest');
         $myForm = new form('edituser', $formAction);
-        
+
         $fnLabel = $this->objLanguage->languageText('mod_registerinterest_fullname', 'registerinterest');
         $fullName = new textinput('fullname');
         $fn = $fnLabel . "<br />" . $fullName->show();
-        
+
         $emLabel = $this->objLanguage->languageText('mod_registerinterest_email', 'registerinterest');
         $emCheck = $this->objLanguage->languageText('mod_registerinterest_confirm', 'registerinterest');
         $eMailAddr = new textinput('email');
         $em = $emLabel . "<br />" . $eMailAddr->show() . "<br />" . $emCheck;
-        
+
         $buttonTitle = $this->objLanguage->languageText('word_save');
         $button = new button('saveDetails', $buttonTitle);
         $button->cssId = 'ri_save_button';
         $button->setToSubmit();
-        
+
         $myForm->addToForm($fn . "<br />");
         $myForm->addToForm($em . "<br />");
         $myForm->addToForm($button->show());
-        
-        return "<div class='registerinterest_form' id = 'ri_form'><div id='before_riform'></div>" . $myForm->show() . "</div>";
 
+        return "<div class='registerinterest_form' id = 'ri_form'><div id='before_riform'></div>" . $myForm->show() . "</div>";
     }
+
     /**
      * 
      * Build form for sending message to registered people.
@@ -126,175 +124,212 @@ class riops extends object
      * @access public
      * 
      */
-    public function buildMsgForm()
-    {
+    public function buildMsgForm() {
         // Load all the required HTML classes from HTMLElements module
         $this->loadClass('form', 'htmlelements');
-        $this->loadClass('textinput','htmlelements');
-        $this->loadClass('textarea','htmlelements');
-        $this->loadClass('button','htmlelements');
-        
+        $this->loadClass('textinput', 'htmlelements');
+        $this->loadClass('textarea', 'htmlelements');
+        $this->loadClass('button', 'htmlelements');
+        $this->loadClass('checkbox', 'htmlelements');
+        $this->loadClass('dropdown', 'htmlelements');
+        $this->loadClass('htmlarea', 'htmlelements');
+
         // Load the javascript.
-        $this->appendArrayVar('headerParams',
-          $this->getJavaScriptFile('registerinterest.js',
-          'registerinterest'));
-        
+        $this->appendArrayVar('headerParams', $this->getJavaScriptFile('registerinterest.js', 'registerinterest'));
+
         // Create the form and set the action to save
-        $formAction = $this->uri(array('action' => 'sendmsg'), 'registerinterest');
-        
-        
+        $formAction = $this->uri(array('action' => 'sendmessage'), 'registerinterest');
+
+
         $myForm = new form('editmsg', $formAction);
-               
+
+        $dropDown = new dropdown();
+        /* foreach($this->objDB->getAll() as $data){
+          //the table
+          $chekbox = new checkbox('chk'.$data['email'],$data['fullname'],TRUE);
+          $dropDown->addOption('To'.'<br />');
+          $dropDown->addOption('Cc'.'<br />');
+          $dropDown->addOption('Bcc'.'<br />');
+          $myForm->addToForm($dropDown->show());
+          } */
         $subjectLabel = $this->objLanguage->languageText('mod_registerinterest_msgsubject', 'registerinterest');
         $subject = new textinput('subject');
         $sj = $subjectLabel . "<br />" . $subject->show();
-        
+
         $msgLabel = $this->objLanguage->languageText('mod_registerinterest_msglabel', 'registerinterest');
-        $msg = new textarea('message');
+        //$msg = new textarea('message');
+        $msg = $this->getObject('htmlarea', 'htmlelements');
+        $msg->setName('emailmessage');
+        $msg->setCssClass("emailmessage");
         $ms = $msgLabel . "<br />" . $msg->show();
-        
+
         $buttonTitle = $this->objLanguage->languageText('word_send');
         $button = new button('saveDetails', $buttonTitle);
         $button->cssId = 'ri_savemsg_button';
         $button->setToSubmit();
-        
+
         $myForm->addToForm($sj . "<br />");
         $myForm->addToForm($ms . "<br />");
         $myForm->addToForm($button->show());
-        
-        return "<div class='registerinterest_form' id = 'ri_form'><div id='before_riform'></div>" . $myForm->show() . "</div>";
 
+        return "<div class='registerinterest_form' id = 'ri_form'><div id='before_riform'></div>" . $myForm->show() . "</div>";
     }
-    
-    
+
     /**
      * 
      * Render an HTML table of all registered people
      * 
      * @return string The rendered HTML table
      */
-    public function listAll()
-    {
-        $objDb = $this->getObject('dbregisterinterest', 'registerinterest');
-        $dataArray = $objDb->getAll();
-        $doc = new DOMDocument('UTF-8');
-        $domElements['table'] = $doc->createElement('table');
-        $domElements['table'] = $doc->createElement('table');
+    public function listAll() {
+        $dataArray = $this->objDB->getAll();
+        $domElements['table'] = $this->doc->createElement('table');
         $domElements['table']->setAttribute('class', 'ri_viewall');
+
+
+
+        // The link for the message writer.
+        $sendlink = $this->uri(array('action' => 'writemessage'), 'registerinterest');
+        $sendlink = str_replace("&amp;", "&", $sendlink);
+        $div = $this->doc->createElement('div');
+        $div->setAttribute('class', 'ri_msgpopper');
+        $h3 = $this->doc->createElement('h3');
+        $a = $this->doc->createElement('a');
+        $a->setAttribute('class', 'ri_msglink');
+        $a->setAttribute('href', $sendlink);
+        $a->appendChild($this->doc->createTextNode($this->objLanguage->languageText('phrase_sendmessage', 'system')));
+        $h3->appendChild($a);
+        $domElements['tr'] = $this->doc->createElement('tr');
+        $domElements['td'] = $this->doc->createElement('td');
+        $domElements['td']->appendChild($h3);
+        $domElements['tr']->appendChild($domElements['td']);
+        $domElements['table']->appendChild($domElements['tr']);
+
+
+
         // Create the header row.
-        $domElements['tr'] = $doc->createElement('tr');
-        $domElements['th'] = $doc->createElement('td');
-        $domElements['th']->appendChild($doc->createTextNode("Name"));
+        $domElements['tr'] = $this->doc->createElement('tr');
+        $domElements['th'] = $this->doc->createElement('td');
+        $domElements['th']->appendChild($this->doc->createTextNode("Name"));
         $domElements['tr']->appendChild($domElements['th']);
-        $domElements['th'] = $doc->createElement('td');
-        $domElements['th']->appendChild($doc->createTextNode("Email"));
+        $domElements['th'] = $this->doc->createElement('td');
+        $domElements['th']->appendChild($this->doc->createTextNode("Email"));
         $domElements['tr']->appendChild($domElements['th']);
-        $domElements['th'] = $doc->createElement('td');
-        $domElements['th']->appendChild($doc->createTextNode("Registered"));
+        $domElements['th'] = $this->doc->createElement('td');
+        $domElements['th']->appendChild($this->doc->createTextNode("Registered"));
         $domElements['tr']->appendChild($domElements['th']);
         // Add the row to the table.
         $domElements['table']->appendChild($domElements['tr']);
-        
+
         $class = "odd";
         foreach ($dataArray as $usr) {
+            //the icon object
+            $objIcon = $this->getObject('geticon', 'htmlelements');
             // Reatrieve the data.
             $id = $usr['id'];
             $fullName = $usr['fullname'];
             $emailAddress = $usr['email'];
             $dateCreated = $usr['datecreated'];
+            $objIcon->getDeleteIconWithConfirm('delete', array('action' => 'remove', 'id' => $id), NULL, NULL);
             // Create the table row.
-            $domElements['tr'] = $doc->createElement('tr');
+            $domElements['tr'] = $this->doc->createElement('tr');
+            //label
+            $domElements['label'] = $this->doc->createElement('label');
             // Fullname to table.
-            $domElements['td'] = $doc->createElement('td');
-            $domElements['td']->setAttribute('class', $class);
-            $domElements['td']->appendChild($doc->createTextNode($fullName));
+            $domElements['td'] = $this->doc->createElement('td');
+            $domElements['tr']->setAttribute('class', $class);
+            $domElements['td']->appendChild($this->doc->createTextNode($fullName));
             $domElements['tr']->appendChild($domElements['td']);
             // Email address to table.
-            $domElements['td'] = $doc->createElement('td');
-            $domElements['td']->setAttribute('class', $class);
+            $domElements['td'] = $this->doc->createElement('td');
             //check if user is admin so to display the correct control
-            if(!$this->objUser->isAdmin()){
-                $domElements['td']->appendChild($doc->createTextNode($emailAddress));
-            }  else {
-                $domElements['txtEmail'] = $doc->createElement('input');
-                $domElements['txtEmail']->setAttribute('value', $emailAddress);
-                $domElements['txtEmail']->setAttribute('type', 'text');
-                $domElements['txtEmail']->setAttribute('id', $id);
-                $domElements['txtEmail']->setAttribute('class', 'interestEmail');
-                $domElements['td']->appendChild($domElements['txtEmail']);
-            }
+            $domElements['td']->setAttribute('id', $id);
+            $domElements['label']->setAttribute('value', $emailAddress);
+            $domElements['td']->setAttribute('class', 'interestEmail');
+            $domElements['label']->appendChild($this->doc->createTextNode($emailAddress));
+            $domElements['label']->setAttribute('for', 'interestEmail');
+            $domElements['label']->setAttribute('id', $id);
+            $domElements['td']->appendChild($domElements['label']);
             $domElements['tr']->appendChild($domElements['td']);
             // Date registered to table.
-            $domElements['td'] = $doc->createElement('td');
-            $domElements['td']->setAttribute('class', $class);
-            $domElements['td']->appendChild($doc->createTextNode($dateCreated));
+            $domElements['td'] = $this->doc->createElement('td');
+            $domElements['td']->appendChild($this->doc->createTextNode($dateCreated));
             $domElements['tr']->appendChild($domElements['td']);
-            
+
             // Add the row to the table.
             $domElements['table']->appendChild($domElements['tr']);
-            
+
             //if user is admin, add the delete link
-            if($this->objUser->isAdmin()){
+            if ($this->objUser->isAdmin()) {
                 //create the delete link
-                $domElements['rmLink'] = $doc->createElement('a');
+                $domElements['rmLink'] = $this->doc->createElement('a');
                 //create the confirmation object which to retrieve the javascript confirmation message from
-                $confirmLink = $this->getObject('confirm','utilities');
-                $confirmLink->setConfirm(NULL,  str_replace('amp;','',$this->uri(array('action'=>'remove','id'=>$id))),  $this->objLanguage->languageText('mod_registerinterest_removealert','registerinterest'));
+                $confirmLink = $this->getObject('confirm', 'utilities');
+                $confirmLink->setConfirm(NULL, str_replace('amp;', '', $this->uri(array('action' => 'remove', 'id' => $id))), $this->objLanguage->languageText('mod_registerinterest_removealert', 'registerinterest'));
                 //td element for the remove link
-                $domElements['td'] = $doc->createElement('td');
-                //assing the ID value
-                $domElements['td']->setAttribute('id', $usr['id']);
+                $domElements['td'] = $this->doc->createElement('td');
+                //create the delete icon
+                $domElements['delIcon'] = $this->doc->createElement('image');
+                $domElements['delIcon']->setAttribute('id', 'deleteIcon');
+                $domElements['delIcon']->setAttribute('src', $objIcon->getSrc());
                 //add link text
                 //set the href attribute of the link
                 $domElements['rmLink']->setAttribute('href', $confirmLink->href);
-                $domElements['rmLink']->appendChild($doc->createTextNode($this->objLanguage->languageText('word_remove','system')));
-                $domElements['td']->appendChild($doc->createTextNode('|'));
+                //$domElements['rmLink']->appendChild($domElements['delIcon']);
+                $domElements['rmLink']->appendChild($domElements['delIcon']);
+                $domElements['td']->appendChild($this->doc->createTextNode('|'));
                 $domElements['td']->appendChild($domElements['rmLink']);
                 $domElements['tr']->appendChild($domElements['td']);
+                $domElements['td']->appendChild($this->doc->createElement('br'));
+                $domElements['td']->appendChild($this->doc->createElement('br'));
                 $domElements['table']->appendChild($domElements['tr']);
             }
             // Convoluted odd/even.
-            if ($class == "odd") { 
+            if ($class == "odd") {
                 $class = "even";
             } else {
                 $class = "odd";
             }
         }
         //update form
-        $domElements['updtForm'] = $doc->createElement('form');
-        $domElements['updtForm']->setAttribute('method', 'POST');
-        $domElements['updtForm']->setAttribute('id', 'frmUpdate');
-        $domElements['updtForm']->setAttribute('action', str_replace('amp;', '', $this->uri(array('action'=>'update'))));
-        $domElements['updtForm']->setAttribute('name', 'frmUpdate');
-        $domElements['updtForm']->appendChild($domElements['table']);
-        $doc->appendChild($domElements['updtForm']);
-        return $doc->saveHTML();
+        $this->doc->appendChild($domElements['table']);
+        return $this->doc->saveHTML();
     }
-    
-    /**
-     * 
-     * Render a form for an admin to post a message.
-     * 
-     * @return type
-     * 
-     */
-    public function renderPostMessage()
-    {
-        // The link for the message writer.
-        $sendlink = $this->uri(array('action' => 'writemessage'), 'registerinterest');
-        $sendlink = str_replace("&amp;", "&", $sendlink);
-        
-        $doc = new DOMDocument('UTF-8');
-        $div = $doc->createElement('div');
-        $div->setAttribute('class', 'ri_msgpopper');
-        $a = $doc->createElement('a');
-        $a->setAttribute('class', 'ri_msglink');
-        $a->setAttribute('href', $sendlink);
-        $a->appendChild($doc->createTextNode("Send message"));
-        $div->appendChild($a);
-        $doc->appendChild($div);
-        return $doc->saveHTML();
+
+    public function sendMessage($subject, $message, $userId) {
+        $objMail = & $this->getObject('mailer', 'mail');
+        $recipents = array();
+        //setting fromName
+        $fromName = $this->objUser->fullname($userId);
+        //loop through the available email addresses
+        foreach ($this->objDB->getAll() as $data) {
+            array_push($recipents, $data['email']);
+        }
+        //setting the email address using the servername and the username   
+        $from = $this->objUser->userName() . '@' . $_SERVER['SERVER_NAME'];
+        //setting the message
+        $recipents = array_unique($recipents);
+        //setting the subject
+        $subject = 'Register interest[' . $subject . ']';
+        $message = "
+        {$this->objLanguage->languageText('word_from', 'system')} : {$fromName} \n\r
+        {$this->objLanguage->languageText('word_subject', 'system')} : {$subject} \r
+        ________________________________________________\n\r
+                {$message} \n\r
+                ";
+        // Add alternative message - same version minus html tags
+        $plainMessage = strip_tags($message);
+        $objMail->setValue('to', $from);
+        $objMail->setValue('bcc', $recipents);
+        $objMail->setValue('from', $from);
+        $objMail->setValue('fromName', $fromName);
+        $objMail->setValue('subject', $subject);
+        $objMail->setValue('useHTMLMail', FALSE);
+        $objMail->setValue('body', $plainMessage);
+        $objMail->setValue('htmlbody', $message);
+        return $objMail->send();
     }
 
 }
+
 ?>
