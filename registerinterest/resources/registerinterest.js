f96f2aa58433f41a74c89d8236ad37d1d927ae45
monwabisi <wsifumba@gmail.com>
2012-12-13 22:51:29 +0000

    Prevent a person from registering morethan once

--- registerinterest/resources/registerinterest.js
+++ registerinterest/resources/registerinterest.js
@@ -22,6 +22,7 @@ jQuery(function() {
     
     // Things to do on loading the page.
     jQuery(document).ready(function() {
+        //hide the textboxes
     });
     
     // Save the intrest
@@ -60,23 +61,44 @@ jQuery(function() {
             }
         });
     });
+    
+    //edit values on the list
+    jQuery("label[for='interestEmail']").click(function(){
+        var value = jQuery(this).attr('value');
+        var textBox = jQuery("<input type='text' class='newvalue' />");
+        jQuery(textBox).val(value);
+        jQuery(this).html(textBox);
+        jQuery(textBox).focus();
+    });
+    
     //send update request when the textbox looses focus
-    jQuery('.interestEmail').focusout(function(){
+    jQuery("label[for='interestEmail']").focusout(function(){
         var id = jQuery(this).attr('id');
-        var newValue = jQuery(this).val();
+        var newValue = jQuery("input[class='newvalue'] ").val();
+        //send the value
         jQuery.ajax({
             url: 'index.php?module=registerinterest&action=update',
             type: 'POST',
             data: {'newValue': newValue,'id':id},
             success:  jQuery("#before_riform").html("<span class='success' >Update request sent successfully</span>")
         })
+        jQuery(this).html(newValue);
+        jQuery(this).attr('value',newValue);
     });
     
     // Send the message
     jQuery("#ri_savemsg_button").live("click", function(e){
-        e.preventDefault();
+        if(jQuery("#input_subject").val().length == 0){
+            if(confirm("Are you sure you want to send the email without a subject?")){
+                return TRUE;
+            }else{
+                e.preventDefault();
+                jQuery("#input_subject").ficus();
+            }
+        }
+        //e.preventDefault();
         data_string = jQuery("#form_editmsg").serialize();
-        alert("Not ready yet");
+        //alert("Not ready yet");
         /*jQuery.ajax({
             url: 'index.php?module=registerinterest&action=savemsg',
             type: "POST",
